1776403ae329e274b566b1172ec3c325d05a1dc3
SamJamCul <samuel.culley@digital.education.gov.uk>
2020-08-25 15:09:14 +0100

    [21] Add personal details page
    - Adds form to create new trainee
    - Adds Trainee model with db migrations
    - Also adds FactoryBot

--- spec/controllers/trainees_controller_spec.rb
+++ spec/controllers/trainees_controller_spec.rb
@@ -7,4 +7,36 @@ RSpec.describe TraineesController do
       expect(response).to be_successful
     end
   end
+
+  describe "#new" do
+    it "returns 200" do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
+  describe "#create" do
+    let(:trainee_attributes) { attributes_for(:trainee_for_form) }
+    let(:latest_trainee) { Trainee.last }
+
+    before do
+      post :create, params: { trainee: trainee_attributes }
+    end
+
+    it "creates record with correct data" do
+      trainee_attributes.reject { |k, _| k.to_s.include?("date_of_birth") }.each do |k, _v|
+        expect(latest_trainee.public_send(k)).to eql(trainee_attributes[k])
+      end
+
+      expect(latest_trainee.date_of_birth).to eql(
+        Date.new(trainee_attributes[:"date_of_birth(1i)"].to_i,
+                 trainee_attributes[:"date_of_birth(2i)"].to_i,
+                 trainee_attributes[:"date_of_birth(3i)"].to_i),
+      )
+    end
+
+    it "create record and redirects to show page" do
+      expect(response).to redirect_to(trainee_path(latest_trainee))
+    end
+  end
 end
