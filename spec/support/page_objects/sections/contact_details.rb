5c8a4cfec6058781df2b7c39db10ba0745edc20a
David Kewal <davidrk@hey.com>
2020-09-07 12:57:35 +0100

    Update spec/support/page_objects/sections/contact_details.rb
    Co-authored-by: Alistair Laing <aliuk2012@users.noreply.github.com>

--- spec/support/page_objects/sections/contact_details.rb
+++ spec/support/page_objects/sections/contact_details.rb
@@ -5,10 +5,10 @@ require_relative "base"
 module PageObjects
   module Sections
     class ContactDetails < PageObjects::Sections::Base
-      element :address, ".address > .govuk-summary-list__value"
-      element :postcode, ".postcode > .govuk-summary-list__value"
-      element :phone, ".phone-number > .govuk-summary-list__value"
-      element :email, ".email > .govuk-summary-list__value"
+      element :address, ".address .govuk-summary-list__value"
+      element :postcode, ".postcode .govuk-summary-list__value"
+      element :phone, ".phone-number .govuk-summary-list__value"
+      element :email, ".email .govuk-summary-list__value"
     end
   end
 end
