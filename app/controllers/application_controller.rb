901bfcc03a237333017bbd65c252e8ee636466a5
David Kewal <davidrk@hey.com>
2020-09-08 13:21:26 +0100

    Implement basic authenticable module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :enforce_basic_auth, if: -> { BasicAuthenticable.required? }
+
   default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
+
+private
+
+  def enforce_basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      BasicAuthenticable.authenticate(username, password)
+    end
+  end
 end
---
+++ app/controllers/concerns/basic_authenticable.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Enforces HTTP Basic Auth
+module BasicAuthenticable
+  class << self
+    def required?
+      Settings.basic_auth_required
+    end
+
+    def authenticate(username, password)
+      validate(username, password)
+    end
+
+    def validate(username, password)
+      utils.secure_compare(::Digest::SHA256.hexdigest(auth_username), ::Digest::SHA256.hexdigest(username)) &
+        utils.secure_compare(::Digest::SHA256.hexdigest(auth_password), ::Digest::SHA256.hexdigest(password))
+    end
+
+    def auth_username
+      @auth_username ||= Settings.basic_auth.username
+    end
+
+    def auth_password
+      @auth_password ||= Settings.basic_auth.password
+    end
+
+    def utils
+      ActiveSupport::SecurityUtils
+    end
+  end
+end
