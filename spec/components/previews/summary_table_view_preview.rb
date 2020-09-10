aee32707d6ec19f0ad04518de208747c1e7026ee
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-09-10 14:00:03 +0100

    [105] Rename Summary Table component files
    Renamed to match sidecar pattern
    
    - script.js
    - style.scss
    - view.html.erb
    - view.rb

--- spec/components/previews/summary_table_view_preview.rb
+++ spec/components/previews/summary_table_view_preview.rb
@@ -1,6 +1,6 @@
 class SummaryTable::ComponentPreview < ViewComponent::Preview
   def default_state
-    render(SummaryTable::Component.new(content_hash: {
+    render(SummaryTable::View.new(content_hash: {
       "First names": "Mike",
       "Middle names": "Larson",
       "Last name": "Doyle",
