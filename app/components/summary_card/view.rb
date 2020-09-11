70d4e18001b0eb8afae7ceb13ac16043a624d162
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-09-11 11:30:04 +0100

    [105] Add Summary Card

---
+++ app/components/summary_card/view.rb
@@ -0,0 +1,40 @@
+class SummaryCard::View < ViewComponent::Base
+  # validates :rows, presence: true
+
+  def initialize(title:, heading_level: 2, rows:, border: true, editable: true)
+    # rows = transform_hash(rows) if rows.is_a?(Hash)
+    @title = title
+    @heading_level = heading_level
+    @rows = rows_including_actions_if_editable(rows, editable)
+    @border = border
+  end
+
+  def border_css_class
+    @border ? "" : "no-border"
+  end
+
+private
+
+  attr_reader :rows
+
+  def rows_including_actions_if_editable(rows, editable)
+    rows.map do |row|
+      row.tap do |r|
+        unless editable
+          r.delete(:change_path)
+          r.delete(:action_path)
+          r.delete(:action)
+        end
+      end
+    end
+  end
+
+  def transform_hash(row_hash)
+    row_hash.map do |key, value|
+      {
+        key: key,
+        value: value,
+      }
+    end
+  end
+end
