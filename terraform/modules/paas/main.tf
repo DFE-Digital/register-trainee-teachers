359649579715e5c39b5fdd013948036064d47f94
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2020-09-11 17:55:10 +0100

    Add bg job configurations to terraform

--- terraform/modules/paas/main.tf
+++ terraform/modules/paas/main.tf
@@ -22,7 +22,7 @@ resource cloudfoundry_app web_app {
   stopped                    = var.app_stopped
   strategy                   = var.web_app_deployment_strategy
   timeout                    = var.app_start_timeout
-  environment                = local.app_environment_variables
+  environment                = local.app_environment
   routes {
     route = cloudfoundry_route.web_app_route.id
   }
@@ -31,6 +31,28 @@ resource cloudfoundry_app web_app {
   }
   service_binding {
     service_instance = cloudfoundry_service_instance.redis_instance.id
+  }
+}
+
+
+resource cloudfoundry_app worker_app {
+  name              = local.worker_app_name
+  command           = local.worker_app_start_command
+  docker_image      = var.app_docker_image
+  health_check_type = "process"
+  instances         = var.worker_app_instances
+  memory            = var.worker_app_memory
+  space             = data.cloudfoundry_space.space.id
+  stopped           = var.app_stopped
+  timeout           = var.app_start_timeout
+  environment       = local.app_environment
+
+  service_binding {
+    service_instance = cloudfoundry_service_instance.redis_instance.id
+  }
+
+  service_binding {
+    service_instance = cloudfoundry_service_instance.postgres_instance.id
   }
 }
 
