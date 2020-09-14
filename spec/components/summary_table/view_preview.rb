c4b4f6971bbed1ce268d5c913e34e19c26edfaa2
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-09-14 16:47:56 +0100

    [133] Remove our version of summary list
    govuk-components has its own implementation of the summary list component
    so there is no real need for us to have our own custom one.

--- spec/components/summary_table/view_preview.rb
+++
@@ -1,9 +0,0 @@
-class SummaryTable::ViewPreview < ViewComponent::Preview
-  def default_state
-    render(SummaryTable::View.new(content_hash: {
-      "First names": "Mike",
-      "Middle names": "Larson",
-      "Last name": "Doyle",
-    }))
-  end
-end
