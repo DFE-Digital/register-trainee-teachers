27884e8d593af83332a57892ba37bf61290dccab
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2020-09-22 15:31:25 +0100

    Add DDTP environment variables to terraform

--- terraform/modules/paas/variables.tf
+++ terraform/modules/paas/variables.tf
@@ -21,6 +21,15 @@ variable worker_app_memory { default = 512 }
 
 variable log_url {}
 
+variable settings__basic_auth__username {}
+variable settings__basic_auth__password {} #secrets
+
+variable settings__dttp__client_id {}
+variable settings__dttp__tenant_id {}
+variable settings__dttp__client_secret {} #secrets
+variable settings__dttp__api_base_url {}
+
+
 
 locals {
   postgres_service_name = "register-postgres-${var.app_environment}"
@@ -28,11 +37,19 @@ locals {
   web_app_name          = "register-${var.app_environment}"
   web_app_start_command = "bundle exec rails db:migrate && bundle exec rails server -b 0.0.0.0"
   app_environment = {
-    AUTHORISED_HOSTS         = "${local.web_app_name}.london.cloudapps.digital"
-    RAILS_ENV                = "production"
-    RACK_ENV                 = "production"
-    RAILS_SERVE_STATIC_FILES = true
-    SECRET_KEY_BASE          = "TestKey"
+    AUTHORISED_HOSTS               = "${local.web_app_name}.london.cloudapps.digital"
+    RAILS_ENV                      = "production"
+    RACK_ENV                       = "production"
+    RAILS_SERVE_STATIC_FILES       = true
+    SECRET_KEY_BASE                = "TestKey"
+    SETTINGS__BASIC_AUTH__USERNAME = var.settings__basic_auth__username
+    SETTINGS__BASIC_AUTH__PASSWORD = var.settings__basic_auth__password
+
+    SETTINGS__DTTP__CLIENT_ID     = var.settings__dttp__client_id
+    SETTINGS__DTTP__TENANT_ID     = var.settings__dttp__tenant_id
+    SETTINGS__DTTP__CLIENT_SECRET = var.settings__dttp__client_secret
+    SETTINGS__DTTP__API_BASE_URL  = var.settings__dttp__api_base_url
+
   }
   worker_app_start_command = "bundle exec sidekiq -C config/sidekiq.yml"
   worker_app_name          = "register-worker-${var.app_environment}"
