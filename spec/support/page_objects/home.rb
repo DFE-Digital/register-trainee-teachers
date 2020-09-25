6a7d9380462eb8d1e9633bdefe0238870df32ed9
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-09-25 15:23:56 +0100

    [157] Remove old home page

--- spec/support/page_objects/home.rb
+++
@@ -1,9 +0,0 @@
-require_relative "sections/home_hero"
-
-module PageObjects
-  class Home < PageObjects::Base
-    set_url "/pages/home"
-
-    section :hero, PageObjects::Sections::HomeHero, ".hero"
-  end
-end
