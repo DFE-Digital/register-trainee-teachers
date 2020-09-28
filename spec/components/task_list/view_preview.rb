dd91df3df254d98504232390d41cd8ebd487317a
David Kewal <davidrk@hey.com>
2020-09-28 15:06:27 +0100

    [162] Add TaskList component

---
+++ spec/components/task_list/view_preview.rb
@@ -0,0 +1,37 @@
+class TaskList::ViewPreview < ViewComponent::Preview
+  def default_state
+    render_component TaskList::View.new do |component|
+      component.slot(
+        :row,
+        task_name: "Personal details",
+        path: "#aardvark",
+        status: "completed",
+      )
+    end
+  end
+
+  def with_multiple_status
+    render_component TaskList::View.new do |component|
+      component.slot(
+        :row,
+        task_name: "Personal details",
+        path: "#aardvark",
+        status: "completed",
+      )
+
+      component.slot(
+        :row,
+        task_name: "Contact details",
+        path: "#details",
+        status: "in progress",
+      )
+
+      component.slot(
+        :row,
+        task_name: "Diversity information",
+        path: "#diversity",
+        status: "not started",
+      )
+    end
+  end
+end
