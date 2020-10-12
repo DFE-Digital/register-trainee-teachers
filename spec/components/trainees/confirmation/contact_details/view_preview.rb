96e4186089400254cf6789c92c44519b23d008b2
Alistair Laing <aliuk2012@users.noreply.github.com>
2020-10-12 10:17:47 +0100

    refactor preview

--- spec/components/trainees/confirmation/contact_details/view_preview.rb
+++ spec/components/trainees/confirmation/contact_details/view_preview.rb
@@ -1,24 +1,22 @@
 require "govuk/components"
 class Trainees::Confirmation::ContactDetails::ViewPreview < ViewComponent::Preview
   def default_with_uk_address
-    mocked_trainee = Trainee.new(id: 1,
-                                 locale_code: 'uk',
-                                 address_line_one: "32 Windsor Gardens",
-                                 address_line_two: "Westminster",
-                                 town_city: "London",
-                                 postcode: 'EC1 9CP',
-                                 phone_number: '0207 123 4567',
-                                 email: "Paddington@bear.com"
-    )
+    mock_trainee.locale_code = 'uk'
 
-
-    render_component(Trainees::Confirmation::ContactDetails::View.new(trainee: mocked_trainee))
-
+    render_component(Trainees::Confirmation::ContactDetails::View.new(trainee: mock_trainee))
   end
 
   def default_with_non_uk_address
-    mocked_trainee = Trainee.new(id: 1,
-                                 locale_code: 'uk',
+    mock_trainee.locale_code = 'non_uk'
+
+    render_component(Trainees::Confirmation::ContactDetails::View.new(trainee: mock_trainee))
+  end
+
+
+  private
+
+  def mock_trainee
+    @mock_trainee ||= Trainee.new(id: 1,
                                  address_line_one: "32 Windsor Gardens",
                                  address_line_two: "Westminster",
                                  town_city: "London",
@@ -26,9 +24,5 @@ class Trainees::Confirmation::ContactDetails::ViewPreview < ViewComponent::Previ
                                  phone_number: '0207 123 4567',
                                  email: "Paddington@bear.com"
     )
-
-
-    render_component(Trainees::Confirmation::ContactDetails::View.new(trainee: mocked_trainee))
-
   end
 end
