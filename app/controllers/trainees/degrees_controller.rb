676b92b9322908f66fabd27554087aef64a0f84d
Jonathan Filar <58793682+JonF1@users.noreply.github.com>
2020-10-14 21:10:37 +0100

    [222] Added edit functionality for degree
    Jon 2 hours of degree of pain

--- app/controllers/trainees/degrees_controller.rb
+++ app/controllers/trainees/degrees_controller.rb
@@ -4,12 +4,29 @@ module Trainees
       @degree = trainee.degrees.build
     end
 
+    def edit
+      @degree = trainee.degrees.find(params[:id])
+    end
+
     def create
       @degree = trainee.degrees.build(degree_params)
       if @degree.save
-        redirect_to trainee
+        if @degree.uk?
+          redirect_to edit_trainee_degree_path(@trainee, @degree)
+        else
+          redirect_to trainee
+        end
       else
         render :new
+      end
+    end
+
+    def update
+      @degree = trainee.degrees.find(params[:id])
+      if @degree.update(uk_degree_details_params)
+        redirect_to trainee
+      else
+        render :edit
       end
     end
 
@@ -17,6 +34,10 @@ module Trainees
 
     def degree_params
       params.require(:degree).permit(:locale_code, :uk_degree, :non_uk_degree)
+    end
+
+    def uk_degree_details_params
+      params.require(:degree).permit(:degree_subject, :institution, :graduation, :degree_grade)
     end
 
     def trainee
