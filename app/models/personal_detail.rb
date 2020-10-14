a555ca19c392a714941d62ed719342eeef62e0f3
David Kewal <davidrk@hey.com>
2020-10-14 11:06:46 +0100

    Update app/models/personal_detail.rb
    Co-authored-by: De Fong <de_fong2k@yahoo.com>

--- app/models/personal_detail.rb
+++ app/models/personal_detail.rb
@@ -24,7 +24,7 @@ class PersonalDetail
   validates :first_names, presence: true
   validates :last_name, presence: true
   validates :date_of_birth, presence: true
-  validates :gender, presence: true, inclusion: GENDERS
+  validates :gender, presence: true, inclusion: { in: Trainee.genders.keys }
   validate :nationalities_cannot_be_empty
 
   def initialize(trainee:)
