7fa6bd7d4b5d72aafe4e716049a4ac4df0b0b231
David Kewal <davidrk@hey.com>
2020-10-16 12:56:59 +0100

    [243] Fix linting issues

--- app/models/confirm_detail.rb
+++ app/models/confirm_detail.rb
@@ -2,10 +2,8 @@ class ConfirmDetail
   include ActiveModel::Model
   attr_accessor :trainee, :mark_as_completed
 
-
   def initialize(trainee:)
     @trainee = trainee
     super(mark_as_completed: trainee.progress[:personal_detail])
   end
-
 end
