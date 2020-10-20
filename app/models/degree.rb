28e6eed0c9b9e3ff4d12e41ebd39935c64349017
Jonathan Filar <58793682+JonF1@users.noreply.github.com>
2020-10-20 21:11:24 +0100

    [224] Add validations for non-uk degree fields

--- app/models/degree.rb
+++ app/models/degree.rb
@@ -9,6 +9,13 @@ class Degree < ApplicationRecord
                               inclusion: { in: 1900..Time.zone.today.year }
   validates :degree_grade, presence: true, on: :uk
 
+  validates :degree_subject, presence: true, on: %i[uk non_uk]
+  validates :institution, presence: true, on: :uk
+  validates :country, presence: true, on: :non_uk
+  validates :graduation_year, presence: true, on: %i[uk non_uk],
+                              inclusion: { in: 1900..Time.zone.today.year }
+  validates :degree_grade, presence: true, on: :uk
+
   belongs_to :trainee
 
   enum locale_code: { uk: 0, non_uk: 1 }
