0fd14328ff6c2c9e8a50a9c3dee183f57413e215
Jonathan Filar <58793682+JonF1@users.noreply.github.com>
2020-10-26 15:34:05 +0000

    [334] Add degree controller and views
    This alters user journey by now putting all fields that
    user needs to select from on one page

--- spec/controllers/degrees_controller_spec.rb
+++ spec/controllers/degrees_controller_spec.rb
@@ -3,7 +3,16 @@ require "rails_helper"
 RSpec.describe Trainees::DegreesController, type: :controller do
   describe "#create" do
     let(:trainee) { create(:trainee) }
-    let(:post_method) { post :create, params: { trainee_id: trainee.id, degree: { locale_code: :uk, uk_degree: "Bachelor of Arts" } } }
+    let(:post_method) do
+      post(:create, params: { trainee_id: trainee.id,
+                              degree:
+      { locale_code: :uk,
+        uk_degree: "Bachelor of Arts",
+        degree_subject: "History",
+        institution: "The University of Warwick",
+        graduation_year: 2014,
+        degree_grade: "Upper second-class honours (2:1)" } })
+    end
 
     it "saves the degree of the trainee to db" do
       expect { post_method }.to change { trainee.reload.degrees.size }.from(0).to(1)
