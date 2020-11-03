e1588419539c8cb96395095a710605b4d5b8ca7f
Felix Agbebiyi <felix.agbebiyi@digital.education.gov.uk>
2020-11-03 16:48:11 +0000

    Update terrafom files to align with v0.13

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,12 +1,17 @@
 terraform {
-  required_version = "= 0.12.29"
-
-  backend "azurerm" {
+  required_version = "~> 0.13.3"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.29.0"
+    }
+    cloudfoundry = {
+      source  = "cloudfoundry-community/cloudfoundry"
+      version = "0.12.6"
+    }
   }
-}
-
-provider azurerm {
-  version = "=2.20.0"
+  backend azurerm {
+  }
 }
 
 provider cloudfoundry {
