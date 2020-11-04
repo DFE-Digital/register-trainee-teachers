1ef5c9e892d72522dbbdf85f9ffb682b978e9be2
David Kewal <davidrk@hey.com>
2020-11-04 13:46:59 +0000

    [299] Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Load Nationalities.
+# Load Nationalities
 Settings.nationalities.each do |nationality|
   Nationality.find_or_create_by(name: nationality)
 end
