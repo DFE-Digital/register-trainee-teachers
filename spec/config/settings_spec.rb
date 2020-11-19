397f678f57b7dc31d84491213c489e59cd73b1d9
De Fong <de_fong2k@yahoo.com>
2020-11-19 15:07:21 +0000

    [482] Added settings

--- spec/config/settings_spec.rb
+++ spec/config/settings_spec.rb
@@ -3,14 +3,18 @@
 require "rails_helper"
 
 describe "Settings" do
-  let(:settings) { YAML.load_file(Rails.root.join("config/settings.yml")).with_indifferent_access }
+  settings = YAML.load_file(Rails.root.join("config/settings.yml")).with_indifferent_access
   describe ".features" do
     subject do
       settings[:features]
     end
 
-    it "default for ssl returns true" do
+    it "default for use ssl returns true" do
       expect(subject[:use_ssl]).to eq(true)
+    end
+
+    it "default for use dfe sign in returns true" do
+      expect(subject[:use_dfe_sign_in]).to eq(true)
     end
 
     it "default for home text returns false" do
@@ -26,5 +30,27 @@ describe "Settings" do
     it "default for port is 5000" do
       expect(subject).to eq(5000)
     end
+  end
+
+  shared_examples "required value" do |key, value|
+    subject do
+      value
+    end
+
+    describe ".#{key}" do
+      it "#{key} is a required value" do
+        expect(subject).to eq("#{key} required value")
+      end
+    end
+  end
+
+  describe ".dfe_sign_in" do
+    settings[:dfe_sign_in].keys.each do |key|
+      include_examples "required value", key, settings[:dfe_sign_in][key]
+    end
+  end
+
+  describe ".base_url" do
+    include_examples "required value", :base_url, settings[:base_url]
   end
 end
