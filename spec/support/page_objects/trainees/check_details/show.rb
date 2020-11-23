4885434fa4c27dfce192646a71ea3c2e54b46fb0
David Kewal <davidrk@hey.com>
2020-11-23 12:14:58 +0000

    [460] Clean up flow from check details to trn submission view

--- spec/support/page_objects/trainees/check_details/show.rb
+++ spec/support/page_objects/trainees/check_details/show.rb
@@ -5,6 +5,9 @@ module PageObjects
     module CheckDetails
       class Show < PageObjects::Base
         set_url "/trainees/{id}/check-details"
+
+        element :back_to_draft_record, "#back-to-draft-record"
+        element :return_to_draft_later, "#return-to-draft-later"
         element :submit_button, "input[name='commit']"
       end
     end
