36427601b7847191fabbfe29214f71874c8ebd86
Jonathan Filar <58793682+JonF1@users.noreply.github.com>
2020-12-04 17:54:30 +0000

    [618] Add view helper method
    View helper methods make button html element in sign in
    new.html.erb more semantically accurate.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,25 @@ module ApplicationHelper
     result.unshift(OpenStruct.new(name: nil))
     result
   end
+
+  def govuk_button_link_to(body, url, html_options = {})
+    original_html_option_class = html_options[:class]
+    html_options[:class] = css_classes("govuk-button", original_html_option_class)
+    html_options[:role] = "button" unless html_options.key?(:role)
+    html_options[:draggable] = false unless html_options.key?(:draggable)
+
+    return link_to(url, html_options) { yield } if block_given?
+
+    link_to(body, url, html_options)
+  end
+
+private
+
+  def css_classes(css_class, current_class)
+    if current_class
+      "#{css_class} #{current_class}"
+    else
+      css_class
+    end
+  end
 end
