f760d70572339d3d5d2940104bcad39ce42010d7
Abigail McPhillips <abigail.mcphillips@gmail.com>
2020-12-15 14:57:35 +0000

    [644] Add error styling to autocomplete

--- app/components/form_components/autocomplete/script.js
+++ app/components/form_components/autocomplete/script.js
@@ -7,10 +7,20 @@ const showAllValuesOption = component => Boolean(component.getAttribute('data-sh
 const defaultValueOption = component => component.getAttribute('data-default-value') || ''
 
 const setupAutoComplete = (component) => {
+  const selectEl = component.querySelector('select')
+
   accessibleAutocomplete.enhanceSelectElement({
     defaultValue: defaultValueOption(component),
-    selectElement: component.querySelector('select'),
+    selectElement: selectEl,
     showAllValues: showAllValuesOption(component)
+  })
+
+  // Fixes a bug whereby if the user enters a search term with no results
+  // accesible-autocomplete defaults to the last valid input and there is no
+  // error to handle.
+  component.querySelector('input').addEventListener('keyup', () => {
+    const noResults = component.querySelector('.autocomplete__option--no-results')
+    if (noResults) selectEl.value = ''
   })
 }
 
