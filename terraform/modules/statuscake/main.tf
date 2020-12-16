66de1190e6c412058ea6d348e8880bab4c76d38b
Misha Gorodnitzky <misaka@pobox.com>
2020-12-16 11:47:47 +0000

    add for_each loop to create alerts
    This may have been missing from an earlier commit which changes the
    structure of the alerts var to make it work with review apps, hence why
    this bug was only detected now.

--- terraform/modules/statuscake/main.tf
+++ terraform/modules/statuscake/main.tf
@@ -1,13 +1,13 @@
 resource statuscake_test alert {
-
-  website_name   = var.alerts["alert"]["website_name"]
-  website_url    = var.alerts["alert"]["website_url"]
-  test_type      = var.alerts["alert"]["test_type"]
-  check_rate     = var.alerts["alert"]["check_rate"]
-  contact_group  = var.alerts["alert"]["contact_group"]
-  trigger_rate   = var.alerts["alert"]["trigger_rate"]
-  custom_header  = var.alerts["alert"]["custom_header"]
-  status_codes   = var.alerts["alert"]["status_codes"]
+  for_each = var.alerts
+  website_name   = each.value.website_name
+  website_url    = each.value.website_url
+  test_type      = each.value.test_type
+  check_rate     = each.value.check_rate
+  contact_group  = each.value.contact_group
+  trigger_rate   = each.value.trigger_rate
+  custom_header  = each.value.custom_header
+  status_codes   = each.value.status_codes
   confirmations  = 1
-  node_locations = var.alerts["alert"]["node_locations"]
+  node_locations = each.value.node_locations
 }
