7dd83e8eb8dbc1b32b6867166213fba676cbccf5
David Kewal <davidrk@hey.com>
2020-12-21 14:02:32 +0000

    [759] Refactor ethnic background form and remove redundant service

--- app/forms/diversities/ethnic_background_form.rb
+++ app/forms/diversities/ethnic_background_form.rb
@@ -3,22 +3,45 @@
 module Diversities
   class EthnicBackgroundForm
     include ActiveModel::Model
-    attr_accessor :trainee
+    include ActiveModel::AttributeAssignment
+    include ActiveModel::Validations::Callbacks
 
     FIELDS = %w[
       ethnic_background
       additional_ethnic_background
     ].freeze
 
-    attr_accessor(*FIELDS)
+    attr_accessor(*FIELDS, :trainee)
 
     delegate :id, :persisted?, to: :trainee
 
     validates :ethnic_background, presence: true
 
+    after_validation :update_trainee, if: -> { errors.empty? }
+
     def initialize(trainee:)
       @trainee = trainee
-      super(trainee.attributes.slice(*FIELDS))
+      super(fields)
+    end
+
+    def fields
+      {
+        ethnic_background: trainee.ethnic_background,
+        additional_ethnic_background: trainee.additional_ethnic_background,
+      }
+    end
+
+    def save
+      valid? && trainee.save
+    end
+
+  private
+
+    def update_trainee
+      trainee.assign_attributes({
+        ethnic_background: ethnic_background,
+        additional_ethnic_background: additional_ethnic_background.presence,
+      })
     end
   end
 end
