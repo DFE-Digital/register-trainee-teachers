32e8cfc62cd7c05a14a013772de243b07e251498
John Henderson <john@logixworks.co.uk>
2020-12-24 12:19:35 +0000

    [782] Hide change links based on trainee state
    Change links are no displated when the trainee is in the following states:
    
    - Recommended for QTS
    - Award for QTS
    - Withdrawn

--- app/components/summary_card/view.rb
+++ app/components/summary_card/view.rb
@@ -2,13 +2,29 @@
 
 class SummaryCard::View < ViewComponent::Base
   include ViewComponent::Slotable
+
   with_content_areas :header_actions
 
-  attr_accessor :title, :heading_level, :rows
+  attr_accessor :trainee, :title, :heading_level
 
-  def initialize(title:, heading_level: 2, rows:)
+  def initialize(trainee:, title:, heading_level: 2, rows:)
+    @trainee = trainee
     @title = title
     @heading_level = heading_level
     @rows = rows
+  end
+
+  def rows
+    @rows.map do |row|
+      row.tap do |r|
+        r.delete(:action) if prevent_action?
+      end
+    end
+  end
+
+private
+
+  def prevent_action?
+    trainee.recommended_for_qts? || trainee.qts_awarded? || trainee.withdrawn?
   end
 end
