257f61328d27e7e2d798eca2c2e273692642e4ab
Abigail McPhillips <abigail.mcphillips@gmail.com>
2020-12-29 14:49:17 +0000

    [790] Hide/show the filters on smaller screens with JS

--- app/components/trainees/filters/script.js
+++ app/components/trainees/filters/script.js
@@ -1,1 +1,30 @@
 import './style.scss'
+import $ from 'jquery'
+import { FilterToggleButton } from 'moj/all.js'
+
+const prepareFilterToggle = () => {
+  const filterContainer = $('.moj-filter-layout__filter')
+
+  if (filterContainer.length) {
+    /* eslint-disable no-new */
+    new FilterToggleButton({
+      bigModeMediaQuery: '(min-width: 48.063em)',
+      startHidden: false,
+      toggleButton: {
+        container: $('.moj-action-bar__filter'),
+        showText: 'Show filters',
+        hideText: 'Hide filters',
+        classes: 'govuk-button--secondary'
+      },
+      closeButton: {
+        container: $('.moj-filter__header-action'),
+        text: 'Close'
+      },
+      filter: {
+        container: filterContainer
+      }
+    })
+  }
+}
+
+prepareFilterToggle()
