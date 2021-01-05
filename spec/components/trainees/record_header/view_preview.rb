603f77431fb3e807b5f4abe50ec68422fab8c2e8
Avin Hurry <avin.hurry@digital.education.gov.uk>
2021-01-05 17:38:04 +0000

    [784] Add record header component
    Component to render the trainee name and trn

---
+++ spec/components/trainees/record_header/view_preview.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "govuk/components"
+
+module Trainees
+  module RecordHeader
+    class ViewPreview < ViewComponent::Preview
+      def with_trn
+        render(Trainees::RecordHeader::View.new(trainee: mock_trainee("0123456789")))
+      end
+
+      def with_no_trn
+        render(Trainees::RecordHeader::View.new(trainee: mock_trainee(nil)))
+      end
+
+    private
+
+      def mock_trainee(trn)
+        @mock_trainee ||= Trainee.new(
+          first_names: "Dave",
+          middle_names: "Hendricks",
+          last_name: "Smith",
+          trn: trn,
+        )
+      end
+    end
+  end
+end
