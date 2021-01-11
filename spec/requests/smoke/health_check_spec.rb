0a8b3103e00f1ffc3693a8f8980ad7b7bb685338
David Kewal <davidrk@hey.com>
2021-01-11 13:16:15 +0000

    [710] Make health check paths more explicit

--- spec/requests/smoke/health_check_spec.rb
+++ spec/requests/smoke/health_check_spec.rb
@@ -2,21 +2,23 @@
 
 require "rails_helper"
 
-describe "system health check spec", :smoke_test do
+describe "system health check spec", :smoke do
   describe "GET /healthcheck" do
+    let(:endpoint) { "#{Settings.base_url}/healthcheck" }
+
     it "returns HTTP success" do
-      get "/healthcheck"
+      get(endpoint)
 
       expect(response.status).to eq(200)
     end
 
     it "returns JSON" do
-      get "/healthcheck"
+      get(endpoint)
       expect(response.media_type).to eq("application/json")
     end
 
     it "returns the expected response report" do
-      get "/healthcheck"
+      get(endpoint)
 
       expect(response.body).to eq(
         {
