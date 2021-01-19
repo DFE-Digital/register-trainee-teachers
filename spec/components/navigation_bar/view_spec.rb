4b4dde7da10c75d161b5c81fcf605af1f3ac74e8
Graham Pengelly <graham@three-tier.com>
2021-01-19 14:42:11 +0000

    WIP

--- spec/components/navigation_bar/view_spec.rb
+++ spec/components/navigation_bar/view_spec.rb
@@ -3,58 +3,83 @@
 require "rails_helper"
 
 module NavigationBar
+  include ApplicationHelper
+
   describe View do
     alias_method :component, :page
 
     let(:mock_link) { "https://www.gov.uk" }
-    let(:items) do
-      [
-        { name: "Home", url: mock_link },
-        { name: "Trainee records", url: mock_link },
-      ]
-    end
 
     before do
-      render_inline(described_class.new(user, items: items))
+      render_inline(described_class.new(items: items))
     end
 
-    context "when user is not signed in" do
-      let(:user) { nil }
-
-      it "does not render the navigation bar" do
-        expect(component).to_not have_css(".moj-primary-navigation")
-      end
-    end
-
-    context "when user is signed in" do
-      let(:user) { build(:user) }
-
-      it "renders the navigation bar" do
-        expect(component).to have_css(".moj-primary-navigation")
+    context "multiple links" do
+      let(:items) do
+        [
+          { name: "Home", url: mock_link },
+          { name: "Trainee records", url: mock_link },
+        ]
       end
 
       it "has two change links" do
         expect(component).to have_link("Home")
         expect(component).to have_link("Trainee records")
       end
+    end
 
-      context "with current item" do
-        let(:active_item) { { name: "Bulk actions", url: mock_link, current: true } }
 
-        before do
-          render_inline(described_class.new(:user, items: items.prepend(active_item)))
-        end
 
-        it "renders the current item with the correct class" do
-          rendered_link = component.find(".moj-primary-navigation__link", text: active_item[:name])
-          expect(rendered_link["aria-current"]).to eq("page")
+
+
+
+
+
+
+
+    context "where item current is true" do
+      let(:current_item) { { name: "Bulk actions", url: mock_link, current: true } }
+      let(:items) { [current_item] }
+
+      it "renders the link with aria-current" do
+        rendered_link = component.find(".moj-primary-navigation__link", text: current_item[:name])
+        expect(rendered_link["aria-current"]).to eq("page")
+      end
+    end
+
+    context "where item current is false" do
+      let(:non_current_item) { { name: "Trainee records", url: mock_link, current: false} }
+      let(:items) { [non_current_item] }
+
+      context "when not on the current url" do
+        it "renders the link without aria-current" do
+          rendered_link = component.find(".moj-primary-navigation__link", text: non_current_item[:name])
+          expect(rendered_link["aria-current"]).to eq(nil)
         end
       end
 
-      context "where item current is true"
+      context "when on the current url" do
+        before do
+          allow(ApplicationHelper.helpers).to receive(:current_page?).with(mock_link).and_return(true)
+        end
 
-      context "where item current is false"
-      context "where item current is not set"
+        it "renders the link with aria-current" do
+          rendered_link = component.find(".moj-primary-navigation__link", text: non_current_item[:name])
+          expect(rendered_link["aria-current"]).to eq("page")
+        end
+      end
+    end
+
+    context "where item current is not set" do
+      let(:item) { { name: "Trainee records", url: mock_link } }
+
+      context "when not on the current url" do
+
+      end
+
+      context "when on the current url" do
+
+      end
     end
   end
 end
