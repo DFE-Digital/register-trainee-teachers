95592f95998fe857d06bc131b7cba95a838f749d
John Henderson <john@logixworks.co.uk>
2021-01-29 09:09:36 +0000

    [950] Replace Dttp::Defer and Dttp::Withdraw with Dttp::UpdateTraineeStatus

--- app/jobs/defer_job.rb
+++ app/jobs/defer_job.rb
@@ -2,9 +2,12 @@
 
 class DeferJob < ApplicationJob
   queue_as :default
-  retry_on Dttp::Defer::Error
+  retry_on Dttp::UpdateTraineeStatus::Error
 
   def perform(trainee_id)
-    Dttp::Defer.call(trainee: Trainee.find(trainee_id))
+    trainee = Trainee.find(trainee_id)
+    Dttp::UpdateTraineeStatus.call(status: DttpStatuses::DEFERRED,
+                                   entity_id: trainee.placement_assignment_dttp_id,
+                                   entity_type: :placement_assignment)
   end
 end
