ade92b364d6c688afddcc8900ce3e34332c14364
De Fong <de_fong2k@yahoo.com>
2021-02-02 09:47:57 +0000

    [963] Removed unused method

--- spec/components/header/view_preview.rb
+++ spec/components/header/view_preview.rb
@@ -13,21 +13,5 @@ module Header
     def with_a_user_signed_in
       render(Header::View.new(I18n.t("service_name"), { first_name: "Ted" }))
     end
-
-  private
-
-    def setup_form
-      '<div class="govuk-form-group">
-        <label class="govuk-label" for="select-1">
-          Select a country
-        </label>
-        <select class="govuk-select" id="select-1" name="select-1">
-          <option value="">Select a country</option>
-          <option value="fr">France</option>
-          <option value="de">Germany</option>
-          <option value="gb">United Kingdom</option>
-        </select>
-      </div>'
-    end
   end
 end
