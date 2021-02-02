ae693d4261832d94b8b6aa5908098b7a6a7ec978
De Fong <de_fong2k@yahoo.com>
2021-02-02 09:50:52 +0000

    [963] Fixed code rot

--- spec/components/application_record_card/view_preview.rb
+++ spec/components/application_record_card/view_preview.rb
@@ -11,21 +11,21 @@ module ApplicationRecordCard
     end
 
     def single_card_with_incomplete_data
-      render(ApplicationRecordCard::View.new(record: Trainee.new(id: 1)))
+      render(ApplicationRecordCard::View.new(record: Trainee.new(id: 1, created_at: Time.zone.now)))
     end
 
   private
 
     def mock_trainee
-      Trainee.new(id: 1, first_names: "Tom", last_name: "Jones", record_type: "assessment_only", subject: "Primary")
+      Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Tom", last_name: "Jones", record_type: "assessment_only", subject: "Primary")
     end
 
     def mock_multiple_trainees
-      [Trainee.new(id: 1, first_names: "Tom", last_name: "Jones", record_type: "assessment_only", subject: "Primary"),
-       Trainee.new(id: 1, first_names: "Paddington", last_name: "Bear", record_type: "assessment_only", subject: "Science"),
-       Trainee.new(id: 1),
-       Trainee.new(id: 1, first_names: "Tim", last_name: "Knight", record_type: "assessment_only", subject: "Maths"),
-       Trainee.new(id: 1, first_names: "Toby", last_name: "Rocker")]
+      [Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Tom", last_name: "Jones", record_type: "assessment_only", subject: "Primary"),
+       Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Paddington", last_name: "Bear", record_type: "assessment_only", subject: "Science"),
+       Trainee.new(id: 1, created_at: Time.zone.now),
+       Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Tim", last_name: "Knight", record_type: "assessment_only", subject: "Maths"),
+       Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Toby", last_name: "Rocker")]
     end
   end
 end
