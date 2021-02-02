636aa81507f19df9b64db4a51064ae1d77a438f1
De Fong <de_fong2k@yahoo.com>
2021-02-02 09:38:55 +0000

    [963] Added tests to flush it out

---
+++ spec/features/view_components_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature "view components" do
+  all_links = (ViewComponent::Preview.all.map do |component|
+    component.examples.map do |example|
+      "#{Rails.application.config.view_component.preview_route}/#{component.preview_name}/#{example}"
+    end
+  end).flatten
+
+  shared_examples "navigate to" do |link, skip|
+    scenario "navigate to #{link}", skip: skip do
+      visit link
+
+      expect(page.status_code).to eq(200)
+      expect(page).to have_css("#main-content")
+    end
+  end
+
+  all_links.each do |link|
+    include_examples "navigate to", link, false
+  end
+end
