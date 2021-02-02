fd02ebbec97dc33251dfe440dff44a3d0dfef750
De Fong <de_fong2k@yahoo.com>
2021-02-02 09:41:42 +0000

    [963] Amended navigation bar

--- spec/components/navigation_bar/view_preview.rb
+++ spec/components/navigation_bar/view_preview.rb
@@ -3,11 +3,24 @@
 module NavigationBar
   class ViewPreview < ViewComponent::Preview
     def default
-      render NavigationBar::View.new(nil)
+      render NavigationBar::View.new(items: items, current_path: current_path)
     end
 
     def with_a_user_signed_in
-      render NavigationBar::View.new({ first_name: "Ted" })
+      render NavigationBar::View.new(items: items, current_path: current_path, current_user: { first_name: "Ted" })
+    end
+
+  private
+
+    def items
+      [
+        { name: "Home", url: "root_path" },
+        { name: "Trainee records", url: "trainees_path", current: false },
+      ]
+    end
+
+    def current_path
+      "root_path"
     end
   end
 end
