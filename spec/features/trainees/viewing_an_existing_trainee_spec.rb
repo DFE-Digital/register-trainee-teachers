8dc2c73c68f85ea52cf260bf7516d6b3783dbca0
Avin Hurry <avin.hurry@digital.education.gov.uk>
2021-02-05 10:39:39 +0000

    [973] Add forbidden error page
    - Error page renders when a user attempts to access a trainee that does not belong to them

--- spec/features/trainees/viewing_an_existing_trainee_spec.rb
+++ spec/features/trainees/viewing_an_existing_trainee_spec.rb
@@ -5,12 +5,22 @@ require "rails_helper"
 feature "View trainees" do
   background { given_i_am_authenticated }
 
-  scenario "viewing the personal details of trainee" do
-    given_a_trainee_exists
-    when_i_view_the_trainee_index_page
-    then_i_see_the_draft_trainee_data
-    and_i_click_on_trainee_name
-    then_i_should_see_the_trainee_details
+  context "when trainee belongs to me" do
+    scenario "viewing the personal details of trainee" do
+      given_a_trainee_exists
+      when_i_view_the_trainee_index_page
+      then_i_see_the_draft_trainee_data
+      and_i_click_on_trainee_name
+      then_i_should_see_the_trainee_details
+    end
+  end
+
+  context "when trainee does not belong to me" do
+    let(:trainee) { create(:trainee) }
+    scenario "viewing the personal details of trainee" do
+      and_i_visit_the_trainee
+      then_i_should_see_no_access_text
+    end
   end
 
   def when_i_view_the_trainee_index_page
@@ -27,5 +37,13 @@ feature "View trainees" do
 
   def then_i_should_see_the_trainee_details
     expect(review_draft_page).to be_displayed(id: trainee.slug)
+  end
+
+  def then_i_should_see_no_access_text
+    expect(page).to have_text(t("components.page_titles.pages.forbidden"))
+  end
+
+  def and_i_visit_the_trainee
+    visit(trainee_path(trainee))
   end
 end
