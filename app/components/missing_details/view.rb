ea9b6e053355ee9e454c5f5ca494a38f544e4ed9
De Fong <de_fong2k@yahoo.com>
2021-02-13 22:36:05 +0000

    [1020] Added dirty code

--- app/components/missing_details/view.rb
+++ app/components/missing_details/view.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class MissingDetails::View < ViewComponent::Base
+  attr_reader :section, :progress, :trainee
+
   def initialize(section:, progress:, trainee:)
     @section = section
     @progress = progress
@@ -8,34 +10,40 @@ class MissingDetails::View < ViewComponent::Base
   end
 
   def title
-    @progress.nil? ? I18n.t("components.missing_details.not_started_title.#{@section}") : I18n.t("components.missing_details.not_complete_title.#{@section}")
+    I18n.t("components.missing_details.#{progress}_title.#{section}")
   end
 
   def link_text
-    @progress.nil? ? "Start section" : "Continue section"
+    not_started? ? "Start section" : "Continue section"
   end
 
   def path
-    @progress.nil? ? not_started_path_hash[@section] : not_complete_path_hash[@section]
+    not_started? ? not_started_path_hash[@section] : not_complete_path_hash[@section]
   end
 
   def not_started_path_hash
     {
-      personal_details: edit_trainee_personal_details_path(@trainee),
-      contact_details: edit_trainee_contact_details_path(@trainee),
-      diversity: edit_trainee_diversity_disability_disclosure_path(@trainee),
-      degrees: trainee_degrees_new_type_path(@trainee),
-      programme_details: edit_trainee_programme_details_path(@trainee),
+      personal_details: edit_trainee_personal_details_path(trainee),
+      contact_details: edit_trainee_contact_details_path(trainee),
+      diversity: edit_trainee_diversity_disability_disclosure_path(trainee),
+      degrees: trainee_degrees_new_type_path(trainee),
+      programme_details: edit_trainee_programme_details_path(trainee),
     }
   end
 
   def not_complete_path_hash
     {
-      personal_details: trainee_personal_details_confirm_path(@trainee),
-      contact_details: trainee_contact_details_confirm_path(@trainee),
-      diversity: trainee_diversity_confirm_path(@trainee),
-      degrees: trainee_degrees_confirm_path(@trainee),
-      programme_details: trainee_programme_details_confirm_path(@trainee),
+      personal_details: trainee_personal_details_confirm_path(trainee),
+      contact_details: trainee_contact_details_confirm_path(trainee),
+      diversity: trainee_diversity_confirm_path(trainee),
+      degrees: trainee_degrees_confirm_path(trainee),
+      programme_details: trainee_programme_details_confirm_path(trainee),
     }
+  end
+
+private
+
+  def not_started?
+    progress == :not_started
   end
 end
---
+++ spec/components/pages/trainees/check_details/view_preview.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require "govuk/components"
+
+module Pages
+  module Trainees
+    module CheckDetails
+      class ViewPreview < ViewComponent::Preview
+        ActionView::Base.default_form_builder = GOVUKDesignSystemFormBuilder::FormBuilder
+
+        def completed_sections
+          @trainee = Trainee.new(id: 1000, trainee_id: "trainee_id",
+                                 first_names: "first_names",
+                                 last_name: "last_name",
+                                 address_line_one: "address_line_one",
+                                 address_line_two: "address_line_two",
+                                 town_city: "town_city",
+                                 middle_names: "middle_names",
+                                 international_address: "international_address",
+                                 ethnic_background: "ethnic_background",
+                                 additional_ethnic_background: "additional_ethnic_background",
+                                 subject: "subject",
+                                 gender: 1,
+                                 date_of_birth: Time.zone.now,
+                                 locale_code: 1,
+                                 postcode: "BN1 1AA",
+                                 email: "email@example.com",
+                                 programme_start_date: 6.months.ago,
+                                 programme_end_date: Time.zone.now,
+                                 nationality_ids: [1],
+                                 progress: Progress.new(
+                                   personal_details: true,
+                                   contact_details: true,
+                                   degrees: true,
+                                   diversity: true,
+                                   programme_details: true,
+                                 ),
+                                 age_range: 1,
+                                 diversity_disclosure: 1)
+
+          @trainee.degrees.new(id: 1, locale_code: 1, subject: "subject")
+
+          render template: "trainees/check_details/show.html", locals: { "@trainee": @trainee }
+        end
+
+        def continue_sections
+          @trainee = Trainee.new(id: 1000, trainee_id: "trainee_id",
+                                 first_names: "first_names",
+                                 last_name: "last_name",
+                                 address_line_one: "address_line_one",
+                                 address_line_two: "address_line_two",
+                                 town_city: "town_city",
+                                 postcode: "postcode",
+                                 email: "email",
+                                 middle_names: "middle_names",
+                                 international_address: "international_address",
+                                 ethnic_background: "ethnic_background",
+                                 additional_ethnic_background: "additional_ethnic_background",
+                                 subject: "subject")
+          @trainee.degrees.new(id: 1)
+          render template: "trainees/check_details/show.html", locals: { "@trainee": @trainee }
+        end
+
+        def start_sections
+          @trainee = Trainee.new(id: 1000)
+
+          render template: "trainees/check_details/show.html", locals: { "@trainee": @trainee }
+        end
+      end
+    end
+  end
+end
