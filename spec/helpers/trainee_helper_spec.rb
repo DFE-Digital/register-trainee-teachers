1985889f45217b9bd6c0a54b02f554977eeea635
Abigail McPhillips <abigail.mcphillips@gmail.com>
2021-02-17 15:52:20 +0000

    Add test for helper method

--- spec/helpers/trainee_helper_spec.rb
+++ spec/helpers/trainee_helper_spec.rb
@@ -45,4 +45,46 @@ describe TraineeHelper do
       end
     end
   end
+
+  describe "#trainees_page_title" do
+    let(:page_size) { 25 }
+    # Using an `object_double` here to stub ActiveRecord::Relation passed into
+    # the component. We cannot use `instance_double(ActiveRecord::Relation...)`
+    # as it lacks the extra methods that Kaminari mixes in (e.g. `total_count`).
+    let(:trainees) do
+      object_double(
+        Trainee.all.page(1),
+        total_count: total_count,
+        limit_value: page_size,
+        current_page: 1,
+        total_pages: (total_count.to_f / page_size).ceil,
+      )
+    end
+
+    subject { trainees_page_title(trainees, total_count) }
+
+    context "when there is one page of trainees" do
+      let(:total_count) { page_size }
+
+      it "returns the title containing the total count" do
+        expect(subject).to eq "Trainee records (25 records)"
+      end
+    end
+
+    context "when there is more than one page of trainees" do
+      let(:total_count) { page_size + 1 }
+
+      it "returns the title containing the total count and which page you're on" do
+        expect(subject).to eq "Trainee records (26 records) - page 1 of 2"
+      end
+    end
+
+    context "when there are no trainees" do
+      let(:total_count) { 0 }
+
+      it "returns the title containing the 0 count" do
+        expect(subject).to eq "Trainee records (0 records)"
+      end
+    end
+  end
 end
