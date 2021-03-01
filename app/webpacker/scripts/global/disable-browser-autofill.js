a45633615a8910630039e764d0a8cb30c62f8836
John Henderson <john@logixworks.co.uk>
2021-03-01 12:55:16 +0000

    [1048] IE 11 shim to add forEach() onto NodeList

--- app/webpacker/scripts/global/disable-browser-autofill.js
+++ app/webpacker/scripts/global/disable-browser-autofill.js
@@ -2,6 +2,11 @@ function disableBrowserAutofill () {
   const dataAttributeName = 'data-nameoriginal'
   const inputTypes = ['text', 'tel', 'email', 'number']
 
+  // Missing forEach on NodeList for IE11
+  if (window.NodeList && !window.NodeList.prototype.forEach) {
+    window.NodeList.prototype.forEach = Array.prototype.forEach
+  }
+
   document.querySelectorAll('form').forEach(form => {
     if (form.getAttribute('autocomplete') === 'off') {
       const inputs = [...form.querySelectorAll('input[autocomplete]')].filter(input => inputTypes.includes(input.type))
