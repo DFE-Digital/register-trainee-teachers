2239ee397bfa7f03c0eead7399eacdbc5e5ae242
Stephan Ellenberger <s.ellenberger95@gmail.com>
2021-03-03 17:30:53 +0000

    [1127] Created Inset text for route indicator

---
+++ spec/components/trainees/route_indicator/view_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Trainees::RouteIndicator::View do
+
+  alias_method :component, :page
+
+  let(:trainee) { create(:trainee) }
+
+  before do
+    render_inline(described_class.new(trainee: trainee))
+  end
+
+  describe "rendered component" do
+
+    it "renders the correct training route link" do
+      expect(component).to have_link(href: "/trainees/#{trainee.slug}/training-routes/edit")
+    end
+  end
+
+  describe "no rendered component" do
+    let(:trainee) { create(:trainee, :submitted_for_trn) }
+
+    it "wont render if the trainee is not a draft trainee" do
+      expect(component.has_content?).to eq false
+    end
+  end
+end
