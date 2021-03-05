b01c047d6a6b9b53ae31a3f95ffc9736590161ad
Ed Horsford <mail@edwardhorsford.com>
2021-03-05 14:23:16 +0000

    Make captialisation consistent

--- app/lib/dttp/code_sets/grades.rb
+++ app/lib/dttp/code_sets/grades.rb
@@ -4,7 +4,7 @@ module Dttp
   module CodeSets
     module Grades
       MAPPING = {
-        "First-class Honours" => { entity_id: "fe2fca5f-766d-e711-80d2-005056ac45bb" },
+        "First-class honours" => { entity_id: "fe2fca5f-766d-e711-80d2-005056ac45bb" },
         "Upper second-class honours (2:1)" => { entity_id: "0030ca5f-766d-e711-80d2-005056ac45bb" },
         "Lower second-class honours (2:2)" => { entity_id: "0230ca5f-766d-e711-80d2-005056ac45bb" },
         "Third-class honours" => { entity_id: "0630ca5f-766d-e711-80d2-005056ac45bb" },
