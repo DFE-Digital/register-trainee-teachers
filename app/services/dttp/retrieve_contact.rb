0db444a87f8a20b0d40e31cfcaf4d1fc1a9ccf44
Stephan Ellenberger <s.ellenberger95@gmail.com>
2021-03-11 17:10:16 +0000

    [1238] Retrieve trainee/contact entities through DTTP with custom attribute filters

---
+++ app/services/dttp/retrieve_contact.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Dttp
+  class RetrieveContact
+    include ServicePattern 
+
+    class HttpError < StandardError; end 
+
+    attr_reader :trainee
+
+    CONTACT_ATTRIBUTE_FILTERS = [
+      "emailaddress1", 
+      "firstname", 
+      "lastname", 
+      "contactid"
+    ]
+
+    def initialize(trainee:)
+      @trainee = trainee
+    end
+
+    def call
+      response = Client.get("/contacts(#{trainee.dttp_id})?$select=#{filters}")
+      if response.code != 200
+        raise HttpError, "status: #{response.code}, body: #{response.body}, headers: #{response.headers}"
+      end
+
+      JSON(response.body)
+    end
+
+    def filters
+      CONTACT_ATTRIBUTE_FILTERS.join(",")
+    end
+  end
+end
---
+++ spec/services/dttp/retrieve_contact_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Dttp
+  describe RetrieveContact do
+    describe "#call" do
+      let(:contact_entity_id) { SecureRandom.uuid }
+      let(:trainee) { create(:trainee, dttp_id: contact_entity_id) }
+      let(:filters) { "emailaddress1,firstname,lastname,contactid" }
+      let(:path) { "/contacts(#{trainee.dttp_id})?$select=#{filters}" }
+
+      before do
+        allow(AccessToken).to receive(:fetch).and_return("token")
+        allow(Client).to receive(:get).with(path).and_return(dttp_response)
+      end
+
+      context "filtered params" do
+        let(:parsed_response) { { 
+            "contactid"=>"XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+            "firstname"=>"John",
+            "lastname"=>"Smith"
+          }
+        }
+        let(:dttp_response) { double(code: 200, body: "{\"contactid\":\"XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX\",\"firstname\":\"John\",\"lastname\":\"Smith\"}") }
+
+        it "returns " do
+          expect(described_class.call(trainee: trainee)).to eq(parsed_response)
+        end
+      end
+
+      context "HTTP error" do
+        let(:status) { 400 }
+        let(:body) { "error" }
+        let(:headers) { { foo: "bar" } }
+        let(:dttp_response) { double(code: status, body: body, headers: headers) }
+
+        it "raises a HttpError error with the response body as the message" do
+          expect(Client).to receive(:get).with(path).and_return(dttp_response)
+          expect {
+            described_class.call(trainee: trainee)
+          }.to raise_error(Dttp::RetrieveContact::HttpError, "status: #{status}, body: #{body}, headers: #{headers}")
+        end
+      end
+    end
+  end
+end
