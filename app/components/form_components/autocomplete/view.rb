c2465a5a1b5a4e1e7a95b6c6708bcc975240c86b
Jack Bracewell <jack@reasonable.engineering>
2021-03-24 14:20:37 +0000

    [1306] PoC for giving specific error messages for partial autocompletes
    The goal here is handling the case where a user types something into an
    autocomplete field but doesn’t select anything from the resulting
    dropdown. Existing behaviour will either ignore the user’s input and
    keep the field filled with the previous value, or just blank it out,
    which can be confusing.
    
    We want to retain what the user entered into the text field when
    re-rendering the page with errors, and provide a specific error message
    for that field being partially submitted.
    
    This approach validates that the text input is in a list of the options
    that were presented to the user. This should be more resilient to any
    type of autocomplete we want, but the implementation seems very messy
    as it requires reassigning all the errors after the form object has
    been validated, see code comment for more details.

--- app/components/form_components/autocomplete/view.rb
+++ app/components/form_components/autocomplete/view.rb
@@ -5,8 +5,23 @@ module FormComponents
     class View < GovukComponent::Base
       attr_accessor :form_field
 
-      def initialize(form_field:, classes: [], html_attributes: {})
+      def initialize(form, attribute_name, collection, value_method, text_method, classes: [], html_attributes: {}, **kwargs)
         super(classes: classes, html_attributes: default_html_attributes.merge(html_attributes))
+
+        raw_attribute_value = form.object.send("#{attribute_name}_raw")
+        attribute_value = form.object.send(attribute_name)
+
+        # We now need to build the form field inside the component, because the form builder library doesn't allow
+        # access to the field object itself before generating html for it. This component now needs to be able to
+        # get the value of form attributes and add extra html options. This doesn't seem so bad as the autocomplete
+        # component *needs* to have a select field for it to work.
+        form_field = form.govuk_collection_select(
+          attribute_name, collection, :name, :name, **kwargs.merge(
+            html_options: {
+              data: { :'text-value' => (raw_attribute_value || attribute_value).to_s }
+            }
+          ),
+        )
         @form_field = form_field
       end
 
