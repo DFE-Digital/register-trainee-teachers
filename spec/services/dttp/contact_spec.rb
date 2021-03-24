f168eda436db3a5d270902d9488460bf4cc8016b
Stephan Ellenberger <s.ellenberger95@gmail.com>
2021-03-24 11:07:43 +0000

    [1238] Changed tests for consistency accross pull requests

--- spec/services/dttp/contact_spec.rb
+++ spec/services/dttp/contact_spec.rb
@@ -4,37 +4,23 @@ require "rails_helper"
 
 module Dttp
   describe Contact do
-    context "instantiation" do
-      let(:contact_data) do
-        {
-          "contactid" => "XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
-          "firstname" => "John",
-          "lastname" => "Smith",
-          "emailaddress1" => "example@example.com",
-        }
-      end
-
-      it "can be instantiated" do
-        expect(described_class.new(contact_data: contact_data)).to be_a Contact
-      end
+    let(:contact_data) do
+      {
+        "contactid" => "XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+        "firstname" => "John",
+        "lastname" => "Smith",
+        "emailaddress1" => "example@example.com",
+      }
     end
 
-    context "accessors" do
-      let(:contact_data) do
-        {
-          "contactid" => "XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
-          "firstname" => "John",
-          "lastname" => "Smith",
-          "emailaddress1" => "example@example.com",
-        }
-      end
+    subject { described_class.new(contact_data: contact_data) }
 
+    describe "methods" do
       it "has accessors to abstract over dttp terminology" do
-        contact = described_class.new(contact_data: contact_data)
-        expect(contact.first_name).to eq "John"
-        expect(contact.last_name).to eq "Smith"
-        expect(contact.dttp_id).to eq "XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
-        expect(contact.email_address).to eq "example@example.com"
+        expect(subject.first_name).to eq "John"
+        expect(subject.last_name).to eq "Smith"
+        expect(subject.dttp_id).to eq "XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+        expect(subject.email_address).to eq "example@example.com"
       end
     end
   end
