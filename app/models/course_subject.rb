9b73832ee6b7cf618fc9a3d80336448217f8f6f2
Stephan Ellenberger <s.ellenberger95@gmail.com>
2021-03-26 12:15:22 +0000

    [1328] Frozenstringliteral added in

--- app/models/course_subject.rb
+++ app/models/course_subject.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class CourseSubject < ApplicationRecord
   belongs_to :course
   belongs_to :subject
 
-  validates :course, presence: true, uniqueness: { scope: :subject }
+  validates :course, presence: true, uniqueness: { scope: :subject_id }
 end
--- db/migrate/20210324164000_create_course_subjects.rb
+++ db/migrate/20210324164000_create_course_subjects.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateCourseSubjects < ActiveRecord::Migration[6.1]
   def change
     create_table :course_subjects do |t|
