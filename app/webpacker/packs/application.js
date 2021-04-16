b9ea4bb23aef07efd01a34c4d49f331a0ca0f1ae
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2021-04-16 15:11:36 +0100

    [1416] moved shared js & css into webpacker

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -4,6 +4,7 @@ import '../styles/application.scss'
 import '../scripts/components'
 import '../scripts/global/nationality_select'
 import '../scripts/global/disable-browser-autofill'
+import '../scripts/global/filter_toggle_button'
 import CookieBanner from '../scripts/cookie_banner'
 import { initAll } from 'govuk-frontend'
 
