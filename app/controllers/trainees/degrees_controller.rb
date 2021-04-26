c60d73caabca22f68e3b46eb080dce2eb2cf4c2c
Jack Bracewell <jack@reasonable.engineering>
2021-04-26 16:47:42 +0100

    Revert "Merge pull request #796 from DFE-Digital/revert-autocomplete-validations"
    This reverts commit bbd9a3319666b322870e17d6f70f95566a62abd1, reversing
    changes made to f9a4ce92e063d92ebaea4a62aad09d4e34372c5f.

--- app/controllers/trainees/degrees_controller.rb
+++ app/controllers/trainees/degrees_controller.rb
@@ -10,7 +10,7 @@ module Trainees
     end
 
     def create
-      @degree_form = @degrees_form.build_degree(degree_params)
+      @degree_form = @degrees_form.build_degree(degree_params, autocomplete_params)
 
       if @degree_form.save_or_stash
         redirect_to trainee_degrees_confirm_path(trainee)
@@ -47,7 +47,11 @@ module Trainees
   private
 
     def degree_params
-      params.require(:degree).permit(DegreeForm::FIELDS.excluding(:slug))
+      params.require(:degree).permit(DegreeForm::FIELDS.excluding(:slug, *DegreeForm::AUTOCOMPLETE_FIELDS))
+    end
+
+    def autocomplete_params
+      params.require(:degree).permit(DegreeForm::AUTOCOMPLETE_FIELDS)
     end
 
     def authorize_trainee
