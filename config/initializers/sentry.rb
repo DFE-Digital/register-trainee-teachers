843015dfb9d5f97d645b40298cbb76a78652c9b1
Arlston Fernandes <gpg@arlston.com>
2021-04-27 09:16:39 +0100

    Add Sentry initializer

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+  config.before_send = lambda do |event, _hint|
+    filter.filter(event.to_hash)
+  end
+
+  config.release = ENV["COMMIT_SHA"]
+end
