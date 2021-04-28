af124f897da5ede4401e01b0e1db1ac36b25ba66
John Henderson <john@logixworks.co.uk>
2021-04-28 12:08:03 +0100

    [1615] Bug fix - updating non-draft trainee degrees broken

--- app/controllers/trainees/degrees/confirm_details_controller.rb
+++ app/controllers/trainees/degrees/confirm_details_controller.rb
@@ -12,7 +12,7 @@ module Trainees
           @confirm_detail_form = ConfirmDetailForm.new(mark_as_completed: trainee.progress.degrees)
         end
 
-        data_model = trainee.draft? ? trainee : degree_form
+        data_model = trainee.draft? ? trainee : degrees_form
 
         @confirmation_component = if trainee.degrees.empty?
                                     IncompleteSection::View.new(title: t("components.incomplete_section.degree_details_not_provided"), link_text: t("components.incomplete_section.add_degree_details"), url: trainee_degrees_new_type_path(@trainee), error: false)
@@ -23,8 +23,8 @@ module Trainees
 
     private
 
-      def degree_form
-        @degree_form ||= DegreeForm.new(trainee)
+      def degrees_form
+        @degrees_form ||= DegreesForm.new(trainee)
       end
     end
   end
