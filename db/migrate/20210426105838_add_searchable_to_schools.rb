9f72204d28d233166530f7549ec151e9610023a2
Jack Bracewell <jack@reasonable.engineering>
2021-04-28 15:31:41 +0100

    Add custom trigger to handle postcodes
    One of the problems with the previous dictionary is that postcodes
    would be indexed as two separate words. If you were to type in a
    postcode without spaces it wouldn’t return the result you wanted. To
    Fix this we need to add the postcode without the space into our tsvector.
    
    The search performance seems unaffected (this difference between 1-2ms/
    0.09ms execution time seems to fluctuate, possibly depends if the query
    gets cached or not. But now we can see the full postcode has also been
    Indexed
    
    irb(main):017:0> School.first.searchable
    => "'100000':1 '5de':6 'aldgate':3 'ec3a':5 'ec3a5de':7 'london':8 'school':4"

--- db/migrate/20210426105838_add_searchable_to_schools.rb
+++ db/migrate/20210426105838_add_searchable_to_schools.rb
@@ -14,16 +14,26 @@ class AddSearchableToSchools < ActiveRecord::Migration[6.1]
       ALTER TEXT SEARCH CONFIGURATION public.schools_search_config
       ALTER MAPPING FOR asciiword WITH public.school_dict;
 
+      CREATE FUNCTION school_trigger() RETURNS trigger AS $$
+      begin
+        new.searchable := to_tsvector(
+          'public.schools_search_config',
+          CONCAT(new.urn, ' ', new.name, ' ', new.postcode, ' ', REPLACE(new.postcode, ' ', ''), ' ', new.town)
+        );
+        return new;
+      end
+      $$ LANGUAGE plpgsql;
+
       CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
-      ON schools FOR EACH ROW EXECUTE PROCEDURE
-      tsvector_update_trigger(searchable, 'public.schools_search_config', urn, name, postcode, town);
+      ON schools FOR EACH ROW EXECUTE PROCEDURE school_trigger();
     SQL
   end
 
   def down
     execute <<-SQL
-      DROP TEXT SEARCH CONFIGURATION public.schools_search_config
-      DROP TEXT SEARCH DICTIONARY public.schools_search_config
+      DROP TEXT SEARCH CONFIGURATION public.schools_search_config;
+      DROP TEXT SEARCH DICTIONARY public.schools_search_config;
+      DROP FUNCTION school_trigger;
       DROP TRIGGER tsvectorupdate ON schools;
     SQL
 
@@ -33,7 +43,7 @@ class AddSearchableToSchools < ActiveRecord::Migration[6.1]
 end
 
 __END__
-irb(main):013:0> SchoolSearch.call(query: "a").analyze
+irb(main):015:0> SchoolSearch.call(query: "a").analyze
 =>
 EXPLAIN ANALYZE for: SELECT "schools".* FROM "schools" INNER JOIN (SELECT "schools"."id" AS pg_search_id, (ts_rank(("schools"."searchable"), (to_
 tsquery('simple', ''' ' || 'a' || ' ''' || ':*')), 0)) AS rank FROM "schools" WHERE (("schools"."searchable") @@ (to_tsquery('simple', ''' ' || '
@@ -42,27 +52,27 @@ ose_date" IS NULL ORDER BY pg_search_f79e2ecc220dc52eaea688.rank DESC, "schools"
                                                                          QUERY PLAN
 -------------------------------------------------------------------------------------------------------------------------------------------------
 ------------
- Sort  (cost=3962.35..3971.37 rows=3606 width=207) (actual time=42.427..42.885 rows=6318 loops=1)
+ Sort  (cost=4152.66..4161.57 rows=3562 width=222) (actual time=41.338..41.775 rows=6318 loops=1)
    Sort Key: (ts_rank(schools_1.searchable, '''a'':*'::tsquery, 0)) DESC, schools.id
-   Sort Method: quicksort  Memory: 2320kB
-   ->  Hash Join  (cost=1711.45..3749.30 rows=3606 width=207) (actual time=10.447..31.089 rows=6318 loops=1)
+   Sort Method: quicksort  Memory: 2531kB
+   ->  Hash Join  (cost=1806.76..3942.53 rows=3562 width=222) (actual time=10.235..30.962 rows=6318 loops=1)
          Hash Cond: (schools.id = schools_1.id)
-         ->  Seq Scan on schools  (cost=0.00..1957.29 rows=27252 width=203) (actual time=0.006..11.824 rows=27283 loops=1)
+         ->  Seq Scan on schools  (cost=0.00..2055.29 rows=27263 width=218) (actual time=0.006..12.135 rows=27283 loops=1)
                Filter: (close_date IS NULL)
                Rows Removed by Filter: 21746
-         ->  Hash  (cost=1630.36..1630.36 rows=6487 width=125) (actual time=10.422..10.423 rows=8955 loops=1)
-               Buckets: 16384 (originally 8192)  Batches: 1 (originally 1)  Memory Usage: 1586kB
-               ->  Bitmap Heap Scan on schools schools_1  (cost=82.28..1630.36 rows=6487 width=125) (actual time=3.023..7.221 rows=8955 loops=1)
+         ->  Hash  (cost=1726.70..1726.70 rows=6405 width=140) (actual time=10.215..10.216 rows=8955 loops=1)
+               Buckets: 16384 (originally 8192)  Batches: 1 (originally 1)  Memory Usage: 1718kB
+               ->  Bitmap Heap Scan on schools schools_1  (cost=81.64..1726.70 rows=6405 width=140) (actual time=2.999..7.133 rows=8955 loops=1)
                      Recheck Cond: (searchable @@ '''a'':*'::tsquery)
-                     Heap Blocks: exact=1379
-                     ->  Bitmap Index Scan on index_schools_on_searchable  (cost=0.00..80.66 rows=6487 width=0) (actual time=2.849..2.849 rows=89
+                     Heap Blocks: exact=1469
+                     ->  Bitmap Index Scan on index_schools_on_searchable  (cost=0.00..80.03 rows=6405 width=0) (actual time=2.817..2.817 rows=89
 55 loops=1)
                            Index Cond: (searchable @@ '''a'':*'::tsquery)
- Planning Time: 0.365 ms
- Execution Time: 44.405 ms
+ Planning Time: 0.368 ms
+ Execution Time: 43.233 ms
 (17 rows)
 
-irb(main):019:0> SchoolSearch.call(query: "alper").analyze
+irb(main):016:0> SchoolSearch.call(query: "alper").analyze
 =>
 EXPLAIN ANALYZE for: SELECT "schools".* FROM "schools" INNER JOIN (SELECT "schools"."id" AS pg_search_id, (ts_rank(("schools"."searchable"), (to_
 tsquery('simple', ''' ' || 'alper' || ' ''' || ':*')), 0)) AS rank FROM "schools" WHERE (("schools"."searchable") @@ (to_tsquery('simple', ''' '
@@ -71,18 +81,19 @@ ols"."close_date" IS NULL ORDER BY pg_search_f79e2ecc220dc52eaea688.rank DESC, "
                                                                     QUERY PLAN
 -------------------------------------------------------------------------------------------------------------------------------------------------
 -
- Sort  (cost=247.96..247.98 rows=11 width=207) (actual time=0.048..0.049 rows=1 loops=1)
+ Sort  (cost=248.16..248.19 rows=11 width=222) (actual time=0.048..0.049 rows=1 loops=1)
    Sort Key: (ts_rank(schools_1.searchable, '''alper'':*'::tsquery, 0)) DESC, schools.id
    Sort Method: quicksort  Memory: 25kB
-   ->  Nested Loop  (cost=24.44..247.77 rows=11 width=207) (actual time=0.041..0.043 rows=1 loops=1)
-         ->  Bitmap Heap Scan on schools schools_1  (cost=24.15..93.90 rows=19 width=125) (actual time=0.020..0.022 rows=2 loops=1)
+   ->  Nested Loop  (cost=24.44..247.97 rows=11 width=222) (actual time=0.041..0.042 rows=1 loops=1)
+         ->  Bitmap Heap Scan on schools schools_1  (cost=24.15..94.10 rows=19 width=140) (actual time=0.021..0.023 rows=2 loops=1)
                Recheck Cond: (searchable @@ '''alper'':*'::tsquery)
                Heap Blocks: exact=2
                ->  Bitmap Index Scan on index_schools_on_searchable  (cost=0.00..24.14 rows=19 width=0) (actual time=0.017..0.017 rows=2 loops=1)
                      Index Cond: (searchable @@ '''alper'':*'::tsquery)
-         ->  Index Scan using schools_pkey on schools  (cost=0.29..8.10 rows=1 width=203) (actual time=0.007..0.007 rows=0 loops=2)
+         ->  Index Scan using schools_pkey on schools  (cost=0.29..8.10 rows=1 width=218) (actual time=0.006..0.006 rows=0 loops=2)
                Index Cond: (id = schools_1.id)
                Filter: (close_date IS NULL)
                Rows Removed by Filter: 0
- Planning Time: 0.481 ms
- Execution Time: 1.250 ms
+ Planning Time: 0.487 ms
+ Execution Time: 0.095 ms
+(15 rows)
