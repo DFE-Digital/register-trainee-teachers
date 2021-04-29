a400e7036c2a93fb826bc23657a298c5bd37827b
John Henderson <john@logixworks.co.uk>
2021-04-29 15:47:57 +0100

    [1566] Refactor schools forms to remove duplication

--- app/forms/employing_school_form.rb
+++ app/forms/employing_school_form.rb
@@ -1,36 +1,15 @@
 # frozen_string_literal: true
 
-class EmployingSchoolForm < TraineeForm
-  NON_TRAINEE_FIELDS = %i[
-    results_search_again_query
-    no_results_search_again_query
-  ].freeze
-
-  attr_accessor(*NON_TRAINEE_FIELDS, :employing_school_id)
+class EmployingSchoolForm < SchoolForm
+  attr_accessor :employing_school_id
 
   validates :employing_school_id, presence: true, unless: -> { searching_again? }
-  validates :results_search_again_query, presence: true, if: -> { results_search_again_query? }
-  validates :no_results_search_again_query, presence: true, if: -> { no_results_searching_again? }
 
-  def searching_again?
-    results_search_again_query? || no_results_searching_again?
-  end
-
-  def results_search_again_query?
-    employing_school_id == "results_search_again"
-  end
-
-  def no_results_searching_again?
-    employing_school_id == "no_results_search_again"
-  end
+  alias_method :school_id, :employing_school_id
 
 private
 
   def compute_fields
     trainee.attributes.symbolize_keys.slice(:employing_school_id).merge(new_attributes)
-  end
-
-  def fields_to_ignore_before_stash_or_save
-    NON_TRAINEE_FIELDS
   end
 end
--- app/forms/lead_school_form.rb
+++ app/forms/lead_school_form.rb
@@ -1,36 +1,15 @@
 # frozen_string_literal: true
 
-class LeadSchoolForm < TraineeForm
-  NON_TRAINEE_FIELDS = %i[
-    results_search_again_query
-    no_results_search_again_query
-  ].freeze
-
-  attr_accessor(*NON_TRAINEE_FIELDS, :lead_school_id)
+class LeadSchoolForm < SchoolForm
+  attr_accessor :lead_school_id
 
   validates :lead_school_id, presence: true, unless: -> { searching_again? }
-  validates :results_search_again_query, presence: true, if: -> { results_search_again_query? }
-  validates :no_results_search_again_query, presence: true, if: -> { no_results_searching_again? }
 
-  def searching_again?
-    results_search_again_query? || no_results_searching_again?
-  end
-
-  def results_search_again_query?
-    lead_school_id == "results_search_again"
-  end
-
-  def no_results_searching_again?
-    lead_school_id == "no_results_search_again"
-  end
+  alias_method :school_id, :lead_school_id
 
 private
 
   def compute_fields
     trainee.attributes.symbolize_keys.slice(:lead_school_id).merge(new_attributes)
-  end
-
-  def fields_to_ignore_before_stash_or_save
-    NON_TRAINEE_FIELDS
   end
 end
