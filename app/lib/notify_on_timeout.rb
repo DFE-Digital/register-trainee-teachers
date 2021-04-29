e4523a716c02a2c5c04377dd6a4489a4cf776bd9
Jonathan Filar <58793682+JonF1@users.noreply.github.com>
2021-04-29 15:43:46 +0100

    [1548] Notifies slack if polling longer than 4 days

---
+++ app/lib/notify_on_timeout.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module NotifyOnTimeout
+  def send_message_to_slack(trainee, class_name)
+    SlackNotifierService.call(channel: Settings.slack.publish_register_alerts_channel, message: "#{class_name} for Trainee id: #{trainee.id} has timed out after #{Settings.jobs.max_poll_duration_days} days.",
+                              icon_emoji: ":this-is-fine:", username: "Register Trainee Teachers: Job Failure")
+  end
+end
