b237931a34ef1c7acdc39215ceec1e92bfb1a31e
Jack Bracewell <jack@reasonable.engineering>
2021-04-30 11:39:59 +0100

    Fix i18n deprecation warning
    I presume the test mistakenly references English instead of British. 
    The new i18n configuration will raise inside a test as well, previous
    I18n raising would only happen inside jobs and the request cycle.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,6 +46,6 @@ Rails.application.configure do
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations.
-  config.action_view.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
   config.active_job.queue_adapter = :test
 end
