66a8c78e0f543b5bcc48e89447d0d1c4ec282e7b
Avin Hurry <avin.hurry@digital.education.gov.uk>
2021-05-07 14:04:45 +0100

    [1646] Fix progress in Publish course details
    - Add validate_publish_course_form
    - Remove redundant test

--- spec/support/page_objects/trainees/confirm_publish_course.rb
+++ spec/support/page_objects/trainees/confirm_publish_course.rb
@@ -7,6 +7,7 @@ module PageObjects
       set_url "/trainees/{trainee_id}/confirm-publish-course/{id}/edit"
 
       element :confirm_course_button, "input[name='commit'][value='Confirm course']"
+      element :submit_button, "input[name='commit']"
     end
   end
 end
