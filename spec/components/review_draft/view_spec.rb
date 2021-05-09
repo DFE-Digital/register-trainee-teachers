e3ca7decba241c5945d847a97a6ac292f4763654
Jonathan Filar <58793682+JonF1@users.noreply.github.com>
2021-05-09 22:35:51 +0100

    [1651] Add view tests for components

---
+++ spec/components/review_draft/view_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ReviewDraft::View do
+  alias_method :component, :page
+
+  context "renders apply draft component" do
+    let(:trainee) { create(:trainee, :with_apply_application) }
+
+    it "uses apply draft trainee" do
+      expect(described_class.new(trainee: trainee).component).to be_a(ReviewDraft::ApplyDraft::View)
+      expect(described_class.new(trainee: trainee).component).to_not be_a(ReviewDraft::Draft::View)
+    end
+  end
+
+  context "renders draft component" do
+    let(:trainee) { create(:trainee, :draft) }
+
+    it "uses draft trainee" do
+      expect(described_class.new(trainee: trainee).component).to_not be_a(ReviewDraft::ApplyDraft::View)
+      expect(described_class.new(trainee: trainee).component).to be_a(ReviewDraft::Draft::View)
+    end
+  end
+end
