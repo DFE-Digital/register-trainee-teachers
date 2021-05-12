e618cd3de2058046e10e5ffea93340b648ec60ae
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2021-05-12 18:18:47 +0100

    [1654] apply routes added to component preview

--- spec/components/trainees/route_indicator/view_preview.rb
+++ spec/components/trainees/route_indicator/view_preview.rb
@@ -6,6 +6,15 @@ module RouteIndicator
       define_method training_route.to_s do
         render(Trainees::RouteIndicator::View.new(trainee: Trainee.new(training_route: training_route)))
       end
+
+      define_method "apply_#{training_route}" do
+        render(Trainees::RouteIndicator::View.new(trainee: Trainee.new(
+          training_route: training_route,
+          apply_application: ApplyApplication.new,
+          subject: Dttp::CodeSets::CourseSubjects::MAPPING.keys.sample,
+          course_code: Faker::Alphanumeric.alphanumeric(number: 4).upcase,
+        )))
+      end
     end
   end
 end
