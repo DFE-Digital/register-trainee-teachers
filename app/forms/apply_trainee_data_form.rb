36191668602a1d9335b1c7319fcd0e75239dcc1b
Jack Bracewell <jack@reasonable.engineering>
2021-05-13 11:50:16 +0100

    [1652] Collapse education section when not started
    When this section has no information, we still want to have it
    accessible on the page. A user could get into this state by deleting
    a degree when the section is expanded.
    
    Change terminology of complete vs incomplete sections to be 
    expanded/collapsed as we will now be showing expanded incomplete
    sections on the apply trainee data page.

--- app/forms/apply_trainee_data_form.rb
+++ app/forms/apply_trainee_data_form.rb
@@ -3,6 +3,19 @@
 class ApplyTraineeDataForm
   include ActiveModel::Model
 
+  class_attribute :form_validators, instance_writer: false, default: {}
+
+  class << self
+    def validator(name, options)
+      form_validators[name] = options
+    end
+  end
+
+  validator :personal_details, form: "PersonalDetailsForm"
+  validator :contact_details, form: "ContactDetailsForm"
+  validator :diversity, form: "Diversities::FormValidator"
+  validator :degrees, form: "DegreesForm"
+
   attr_accessor :mark_as_reviewed
 
   def initialize(trainee:)
@@ -17,11 +30,29 @@ class ApplyTraineeDataForm
     trainee.save!
   end
 
-  def section_status(_)
-    :in_progress
+  def progress_status(progress_key)
+    progress_service(progress_key).status.parameterize(separator: "_").to_sym
+  end
+
+  def display_type(section_key)
+    if section_key == :degrees
+      progress_status(section_key) == :not_started ? :collapsed : :expanded
+    else
+      :expanded
+    end
   end
 
 private
+
+  def progress_service(progress_key)
+    validator = validator(progress_key).new(trainee)
+    progress_value = trainee.progress.public_send(progress_key)
+    ProgressService.call(validator: validator, progress_value: progress_value)
+  end
+
+  def validator(section)
+    form_validators[section][:form].constantize
+  end
 
   attr_reader :trainee
 end
