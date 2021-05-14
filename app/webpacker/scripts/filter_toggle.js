56f3d82f5a367228d0dad6dae37f3adb92558b36
David <davidrk@hey.com>
2021-05-14 16:42:07 +0100

    Refactor filter toggle initialisation to please SonarCloud

---
+++ app/webpacker/scripts/filter_toggle.js
@@ -0,0 +1,28 @@
+import $ from 'jquery'
+import { FilterToggleButton } from 'moj/all.js'
+
+export default class FilterToggle {
+  static init () {
+    const filterContainer = $('.moj-filter-layout__filter')
+
+    if (filterContainer.length) {
+      return new FilterToggleButton({
+        bigModeMediaQuery: '(min-width: 48.063em)',
+        startHidden: false,
+        toggleButton: {
+          container: $('.moj-action-bar__filter'),
+          showText: 'Show filters',
+          hideText: 'Hide filters',
+          classes: 'govuk-button--secondary'
+        },
+        closeButton: {
+          container: $('.moj-filter__header-action'),
+          text: 'Close'
+        },
+        filter: {
+          container: filterContainer
+        }
+      })
+    }
+  }
+}
