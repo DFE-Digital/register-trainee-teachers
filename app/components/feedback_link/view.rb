db8add0a3b8a701037745235f1da6aaeb6baa704
Stephan Ellenberger <s.ellenberger95@gmail.com>
2021-05-19 17:31:07 +0100

    [1707] FeedbackLink component changed to support new feedback button and dynamic text

--- app/components/feedback_link/view.rb
+++ app/components/feedback_link/view.rb
@@ -2,11 +2,12 @@
 
 module FeedbackLink
   class View < GovukComponent::Base
-    attr_reader :feedback_link_url, :enabled
+    attr_reader :feedback_link_url, :enabled, :feedback_type_text
 
-    def initialize(enabled: true, feedback_link_url:)
+    def initialize(enabled: true, feedback_link_url:, feedback_type_text:)
       @enabled = enabled
       @feedback_link_url = feedback_link_url
+      @feedback_type_text = feedback_type_text
     end
   end
 end
--- spec/components/feedback_link/view_preview.rb
+++ spec/components/feedback_link/view_preview.rb
@@ -5,7 +5,7 @@ require "govuk/components"
 module FeedbackLink
   class ViewPreview < ViewComponent::Preview
     def default
-      render(FeedbackLink::View.new(enabled: true, feedback_link_url: "https://www.google.com"))
+      render(FeedbackLink::View.new(enabled: true, feedback_link_url: "https://www.google.com", feedback_type_text: "recommending the trainee for QTS"))
     end
   end
 end
