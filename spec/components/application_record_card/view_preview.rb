9b9694bc1185bdd507609ae3ff6b8bd31cde448e
Arlston Fernandes <gpg@arlston.com>
2021-05-21 10:24:22 +0100

    update Trainee Listing/Confirmation cards to show single/multiple subjects

--- spec/components/application_record_card/view_preview.rb
+++ spec/components/application_record_card/view_preview.rb
@@ -14,6 +14,14 @@ module ApplicationRecordCard
       render(ApplicationRecordCard::View.new(record: Trainee.new(id: 1, created_at: Time.zone.now)))
     end
 
+    def single_card_with_two_subjects
+      render(ApplicationRecordCard::View.new(record: mock_trainee_with_two_subjects))
+    end
+
+    def single_card_with_three_subjects
+      render(ApplicationRecordCard::View.new(record: mock_trainee_with_three_subjects))
+    end
+
   private
 
     def mock_trainee
@@ -27,10 +35,35 @@ module ApplicationRecordCard
       )
     end
 
+    def mock_trainee_with_two_subjects
+      Trainee.new(
+        id: 1,
+        created_at: Time.zone.now,
+        first_names: "Tom",
+        last_name: "Jones",
+        training_route: TRAINING_ROUTE_ENUMS[:assessment_only],
+        subject: "Primary",
+        subject_two: "Science",
+      )
+    end
+
+    def mock_trainee_with_three_subjects
+      Trainee.new(
+        id: 1,
+        created_at: Time.zone.now,
+        first_names: "Tom",
+        last_name: "Jones",
+        training_route: TRAINING_ROUTE_ENUMS[:assessment_only],
+        subject: "Primary",
+        subject_two: "Science",
+        subject_three: "Mathematics",
+      )
+    end
+
     def mock_multiple_trainees
       [
-        Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Tom", last_name: "Jones", training_route: TRAINING_ROUTE_ENUMS[:assessment_only], subject: "Primary"),
-        Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Paddington", last_name: "Bear", training_route: TRAINING_ROUTE_ENUMS[:assessment_only], subject: "Science"),
+        Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Tom", last_name: "Jones", training_route: TRAINING_ROUTE_ENUMS[:assessment_only], subject: "Primary", subject_two: "Mathematics", subject_three: "Latin"),
+        Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Paddington", last_name: "Bear", training_route: TRAINING_ROUTE_ENUMS[:assessment_only], subject: "Science", subject_two: "Mathematics"),
         Trainee.new(id: 1, created_at: Time.zone.now),
         Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Tim", last_name: "Knight", training_route: TRAINING_ROUTE_ENUMS[:assessment_only], subject: "Maths"),
         Trainee.new(id: 1, created_at: Time.zone.now, first_names: "Toby", last_name: "Rocker"),
