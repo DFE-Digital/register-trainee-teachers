006c4e5543edc9d554340a09cc04b82a3de51a19
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2021-05-24 12:04:58 +0100

    [1540] RunConsistencyChecksJob scheduled for every morning

--- app/jobs/run_consistency_checks_job.rb
+++ app/jobs/run_consistency_checks_job.rb
@@ -4,6 +4,8 @@ class RunConsistencyChecksJob < ApplicationJob
   queue_as :default
 
   def perform(*_args)
+    return unless FeatureService.enabled?("run_consistency_check_job") && ConsistencyCheck.any?
+
     ConsistencyCheck.all.each do |consistency_check|
       Dttp::CheckConsistencyJob.perform_later(consistency_check.id)
     end
--- spec/jobs/run_consistency_checks_job_spec.rb
+++ spec/jobs/run_consistency_checks_job_spec.rb
@@ -8,14 +8,34 @@ describe RunConsistencyChecksJob do
   let(:trainee) { create(:trainee) }
   let(:consistency_check) { create(:consistency_check, trainee_id: trainee.id) }
 
-  before do
-    consistency_check
+  context "when a consistency check exists" do
+    before do
+      consistency_check
+
+      described_class.perform_now
+    end
+
+    context "when the feature flag is turned on", feature_run_consistency_check_job: true do
+      describe ".perform" do
+        it "runs each check" do
+          expect(Dttp::CheckConsistencyJob).to have_been_enqueued.with(consistency_check.id)
+        end
+      end
+    end
+
+    context "when the feature flag is turned off", feature_run_consistency_check_job: false do
+      describe ".perform" do
+        it "does not run" do
+          expect(Dttp::CheckConsistencyJob).to_not have_been_enqueued.with(consistency_check.id)
+        end
+      end
+    end
   end
 
-  describe ".perform" do
-    it "runs each check" do
-      ConsistencyCheck.all.each do |consistency_check|
-        expect(Dttp::CheckConsistencyJob).not_to have_been_enqueued.with(consistency_check.id)
+  context "when there are no consistency checks", feature_run_consistency_check_job: true do
+    describe ".perform" do
+      it "does not run" do
+        expect(Dttp::CheckConsistencyJob).to_not have_been_enqueued.with(consistency_check.id)
       end
     end
   end
