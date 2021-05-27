ebff8211f72d3918b7b285ea53a4fdfb8cb46f3b
David <davidrk@hey.com>
2021-05-27 12:25:32 +0100

    [1767] Bump minimum coverage to 90, we levelling up

--- spec/support/features/course_details_steps.rb
+++ spec/support/features/course_details_steps.rb
@@ -2,13 +2,26 @@
 
 module Features
   module CourseDetailsSteps
-    def and_the_course_details_is_complete
+    def and_the_publish_course_details_is_complete
       given_a_course_is_available_for_selection
       review_draft_page.refresh
       review_draft_page.course_details.link.click
       publish_course_details_page.course_options.first.choose
       publish_course_details_page.submit_button.click
       confirm_publish_course_page.confirm_course_button.click
+      and_the_course_details_is_marked_completed
+    end
+
+    def and_the_course_details_is_complete
+      course_details_page.load(id: trainee_from_url.slug)
+      course_details_page.subject.select(Dttp::CodeSets::CourseSubjects::MAPPING.keys.first)
+      course_details_page.main_age_range_3_to_11.choose
+      course_details_page.set_date_fields("course_start_date", "11/3/2021")
+      course_details_page.set_date_fields("course_end_date", "11/3/2022")
+      course_details_page.submit_button.click
+      confirm_details_page.confirm.click
+      confirm_details_page.continue_button.click
+
       and_the_course_details_is_marked_completed
     end
 
--- spec/support/features/training_route_steps.rb
+++ spec/support/features/training_route_steps.rb
@@ -3,9 +3,19 @@
 module Features
   module TrainingRouteSteps
     def given_i_have_created_a_provider_led_trainee
+      choose_training_route_for(TRAINING_ROUTE_ENUMS[:provider_led_postgrad])
+    end
+
+    def given_i_have_created_an_assessment_only_trainee
+      choose_training_route_for(TRAINING_ROUTE_ENUMS[:assessment_only])
+    end
+
+  private
+
+    def choose_training_route_for(route)
       trainee_index_page.load
       trainee_index_page.add_trainee_link.click
-      new_trainee_page.provider_led_postgrad.click
+      new_trainee_page.public_send(route).click
       new_trainee_page.continue_button.click
     end
   end
