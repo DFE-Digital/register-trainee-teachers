c7aed1f230e586dbd82c3f6dcfe8acd32962e183
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2021-06-03 11:56:42 +0100

    [1810] School direct tuition fee end to end feature spec

--- spec/features/end_to_end/school_direct_tuition_fee_journey_spec.rb
+++ spec/features/end_to_end/school_direct_tuition_fee_journey_spec.rb
@@ -2,18 +2,18 @@
 
 require "rails_helper"
 
-feature "school-direct-salaried end-to-end journey", type: :feature do
+feature "school-direct-tuition-fee end-to-end journey", type: :feature do
   background { given_i_am_authenticated }
 
-  scenario "submit for TRN", "feature_routes.school_direct_salaried": true do
-    given_i_have_created_a_school_direct_salaried_trainee
+  scenario "submit for TRN", "feature_routes.school_direct_tuition_fee": true do
+    given_i_have_created_a_school_direct_tuition_fee_trainee
     and_the_personal_details_is_complete
     and_the_contact_details_is_complete
     and_the_diversity_information_is_complete
     and_the_degree_details_is_complete
     and_the_course_details_is_complete
     and_the_trainee_start_date_and_id_is_complete
-    and_the_lead_and_employing_schools_section_is_complete
+    and_the_lead_school_section_is_complete
     and_the_draft_record_has_been_reviewed
     and_all_sections_are_complete
     when_i_submit_for_trn
--- spec/support/features/lead_and_employing_school_steps.rb
+++ spec/support/features/lead_and_employing_school_steps.rb
@@ -17,11 +17,26 @@ module Features
       and_the_lead_and_employing_schools_section_is_marked_completed
     end
 
+    def and_the_lead_school_section_is_complete
+      given_a_lead_school_exists_in_the_system
+      review_draft_page.lead_and_employing_schools_section.link.click
+      and_i_fill_in_my_lead_school
+      and_i_continue
+      lead_schools_search_page.choose_school(id: @lead_school.id)
+      and_i_continue
+      and_i_confirm_my_details
+      and_the_lead_and_employing_schools_section_is_marked_completed
+    end
+
   private
 
     def given_lead_and_employing_schools_exist_in_the_system
-      @lead_school = create(:school, :lead)
+      given_a_lead_school_exists_in_the_system
       @employing_school = create(:school)
+    end
+
+    def given_a_lead_school_exists_in_the_system
+      @lead_school = create(:school, :lead)
     end
 
     def and_i_fill_in_my_lead_school
@@ -32,12 +47,8 @@ module Features
       edit_employing_school_page.no_js_employing_school.fill_in with: @employing_school.name.split(" ").first
     end
 
-    def and_i_click_the_first_item_in_the_list
-      click(edit_lead_school_page.autocomplete_list_item)
-    end
-
     def and_i_continue
-      click(edit_lead_school_page.submit)
+      find('input.govuk-button[type="submit"]').click
     end
 
     def and_the_lead_and_employing_schools_section_is_marked_completed
