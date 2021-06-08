cbdcb09b966bf4363561d4796d16f138f9ab7fc4
David <davidrk@hey.com>
2021-06-08 18:08:47 +0100

    [1882] Add component for rendering school result text (#955)
    * [1882] Add component for rendering school result text
    
    * Update config/locales/en.yml
    
    Co-authored-by: Ed Horsford <mail@edwardhorsford.com>
    
    * Update config/locales/en.yml
    
    Co-authored-by: Ed Horsford <mail@edwardhorsford.com>
    
    Co-authored-by: Ed Horsford <mail@edwardhorsford.com>

---
+++ app/components/school_result_notice/view.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module SchoolResultNotice
+  class View < GovukComponent::Base
+    attr_reader :search_query
+
+    def initialize(search_query:, search_limit:, search_count:)
+      @search_query = search_query
+      @search_limit = search_limit
+      @search_count = search_count
+    end
+
+    def render?
+      total.positive?
+    end
+
+    def remaining_search_count
+      total
+    end
+
+    def school_result_text
+      return pluralised_school_result_text if remaining_search_count > 1
+
+      t("components.school_result_notice.result_text", search_query: search_query)
+    end
+
+  private
+
+    attr_reader :search_limit, :search_count
+
+    def total
+      @total ||= search_count - search_limit
+    end
+
+    def pluralised_school_result_text
+      t("components.school_result_notice.multiple_result_text", search_query: search_query, remaining_search_count: remaining_search_count)
+    end
+  end
+end
