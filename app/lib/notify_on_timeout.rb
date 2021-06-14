5eef30b99cc2145eb094602f4442c31f99f6a2eb
Graham Pengelly <graham@three-tier.com>
2021-06-14 12:51:57 +0100

    [xxx] Update `NotifyOnTimeout` to use the new method
    The method signature of slack notifier has changed. Update this calling
    code that was missed.

--- app/lib/notify_on_timeout.rb
+++ app/lib/notify_on_timeout.rb
@@ -2,7 +2,7 @@
 
 module NotifyOnTimeout
   def send_message_to_slack(trainee, class_name)
-    SlackNotifierService.call(channel: Settings.slack.publish_register_alerts_channel, message: "#{class_name} for Trainee id: #{trainee.id} has timed out after #{Settings.jobs.max_poll_duration_days} days.",
+    SlackNotifierService.call(message: "#{class_name} for Trainee id: #{trainee.id} has timed out after #{Settings.jobs.max_poll_duration_days} days.",
                               icon_emoji: ":this-is-fine:", username: "Register Trainee Teachers: Job Failure")
   end
 end
