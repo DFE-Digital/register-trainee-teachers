329383fd8dd839476497232d2a02f4c87621b550
Graham Pengelly <graham@three-tier.com>
2021-06-18 16:11:37 +0100

    [2019] Lint/SymbolConversion
    Remove exclusions and fix

--- spec/features/auth/sign_in_page_spec.rb
+++ spec/features/auth/sign_in_page_spec.rb
@@ -7,13 +7,13 @@ feature "sign in page" do
     sign_in_page.load
   end
 
-  scenario "navigate to sign in", "feature_use_dfe_sign_in": false do
+  scenario "navigate to sign in", feature_use_dfe_sign_in: false do
     expect(sign_in_page.page_heading).to have_text("Sign in")
     expect(sign_in_page).to have_title("Sign in - Register trainee teachers - GOV.UK")
     expect(sign_in_page.sign_in_button.text).to eq("Sign in using Persona")
   end
 
-  scenario "navigate to sign in", "feature_use_dfe_sign_in": true do
+  scenario "navigate to sign in", feature_use_dfe_sign_in: true do
     expect(sign_in_page.page_heading).to have_text("Sign in")
     expect(sign_in_page).to have_title("Sign in - Register trainee teachers - GOV.UK")
     expect(sign_in_page.sign_in_button.value).to eq("Sign in using DfE Sign-in")
