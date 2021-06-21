3a0cfd571bc3c3672ed49e8c1c262e3fda5ff359
Graham Pengelly <graham@three-tier.com>
2021-06-21 15:01:42 +0100

    [2019] Style/SingleArgumentDig
    Remove exclusions and fix.

--- db/migrate/20201007151313_revert_previous_education.rb
+++ db/migrate/20201007151313_revert_previous_education.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "20200901113117_add_previous_education.rb"
+require_relative "20200901113117_add_previous_education"
 
 class RevertPreviousEducation < ActiveRecord::Migration[6.0]
   def change
