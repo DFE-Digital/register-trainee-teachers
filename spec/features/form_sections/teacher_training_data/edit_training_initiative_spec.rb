acf0f208b86a853462498b9e54437c6390542c27
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2021-06-29 14:54:32 +0100

    [1992] Funding - UI - joining everything together

--- spec/features/form_sections/teacher_training_data/edit_training_initiative_spec.rb
+++ spec/features/form_sections/teacher_training_data/edit_training_initiative_spec.rb
@@ -5,18 +5,28 @@ require "rails_helper"
 feature "edit training initiative", type: :feature do
   background { given_i_am_authenticated }
 
-  before do
-    given_a_trainee_exists
-    when_i_visit_the_training_initiative_page
-  end
+  let(:course_subject) { Dttp::CodeSets::CourseSubjects::LAW }
 
-  scenario "edit with valid parameters" do
+  scenario "edit with valid parameters with bursary" do
+    given_a_trainee_exists(:provider_led_postgrad, course_subject_one: course_subject)
+    and_a_bursary_exists_for_their_subject
+    when_i_visit_the_training_initiative_page
     and_i_update_the_training_initiative
     and_i_submit_the_form
-    then_i_am_redirected_to_the_trainee_page
+    then_i_am_redirected_to_the_bursary_page
+  end
+
+  scenario "edit with valid parameters without bursary" do
+    given_a_trainee_exists(:assessment_only)
+    when_i_visit_the_training_initiative_page
+    and_i_update_the_training_initiative
+    and_i_submit_the_form
+    then_i_am_redirected_to_the_funding_confirmation_page
   end
 
   scenario "submitting with invalid parameters" do
+    given_a_trainee_exists
+    when_i_visit_the_training_initiative_page
     and_i_submit_the_form
     then_i_see_error_messages
   end
@@ -35,13 +45,23 @@ private
     training_initiative_page.submit_button.click
   end
 
-  def then_i_am_redirected_to_the_trainee_page
-    expect(review_draft_page).to be_displayed
-  end
-
   def then_i_see_error_messages
     expect(new_provider_page).to have_text(
       I18n.t("activemodel.errors.models.funding/training_initiatives_form.attributes.training_initiative.blank"),
     )
+  end
+
+  def then_i_am_redirected_to_the_bursary_page
+    expect(bursary_page).to be_displayed(id: trainee.slug)
+  end
+
+  def and_a_bursary_exists_for_their_subject
+    bursary = create(:bursary, training_route: :provider_led_postgrad, amount: 100_000)
+    allocation_subject = create(:allocation_subject, name: "magic", bursaries: [bursary])
+    create(:subject_specialism, allocation_subject: allocation_subject, name: course_subject)
+  end
+
+  def then_i_am_redirected_to_the_funding_confirmation_page
+    expect(confirm_funding_page).to be_displayed(id: trainee.slug)
   end
 end
