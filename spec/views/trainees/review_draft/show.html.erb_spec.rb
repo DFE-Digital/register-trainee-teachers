664dbe269607a097819a30a957118930124e4a8e
Jonathan Filar <jonathanfilar@hotmail.com>
2021-07-06 15:52:23 +0100

    [2153] Add method to components & update view spec

--- spec/views/trainees/review_draft/show.html.erb_spec.rb
+++ spec/views/trainees/review_draft/show.html.erb_spec.rb
@@ -33,22 +33,38 @@ describe "trainees/review_draft/show.html.erb" do
 
   describe "trainees/review_draft/show.html.erb", "feature_routes.provider_led_postgrad": true
 
-  context "with an Apply draft trainee" do
+  context "with a named Apply draft trainee" do
     let(:trainee) { create(:trainee, :with_apply_application) }
     let(:trainee_name) { "#{trainee.first_names} #{trainee.middle_names} #{trainee.last_name}" }
 
     it "renders Apply draft text" do
-      expect(rendered).to have_text("Apply draft for #{trainee_name}")
+      expect(rendered).to have_text("Apply draft")
+      expect(rendered).to have_text(trainee_name)
+    end
+  end
+
+  context "with an unnamed Apply draft trainee" do
+    let(:trainee) { create(:trainee, :with_apply_application, first_names: nil, middle_names: nil, last_name: nil) }
+
+    it "renders Apply draft text" do
       expect(rendered).to have_text("Register a trainee")
     end
   end
 
-  context "with an normal draft trainee" do
+  context "with a named normal draft trainee" do
     let(:trainee) { create(:trainee, :draft) }
     let(:trainee_name) { "#{trainee.first_names} #{trainee.middle_names} #{trainee.last_name}" }
 
     it "renders draft record text" do
-      expect(rendered).to have_text("Draft record for #{trainee_name}")
+      expect(rendered).to have_text("Draft record")
+      expect(rendered).to have_text(trainee_name)
+    end
+  end
+
+  context "with an unnamed normal draft trainee" do
+    let(:trainee) { create(:trainee, first_names: nil, middle_names: nil, last_name: nil) }
+
+    it "renders draft record text" do
       expect(rendered).to have_text("Add a trainee")
     end
   end
