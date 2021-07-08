0d612648163a308d4f09dd4b11f6f25d2657ce66
Arlston Fernandes <gpg@arlston.com>
2021-07-08 12:36:15 +0100

    [2182] Ensure consitent failures

--- spec/factories/subjects.rb
+++ spec/factories/subjects.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :subject do
-    sequence(:name) { PUBLISH_SUBJECT_SPECIALISM_MAPPING.keys.sample }
+    name { ["Primary with geography and history", "Primary with modern languages", "Primary with physical education", "Primary with science"].sample }
     code { Faker::Alphanumeric.unique.alphanumeric(number: 2).upcase }
   end
 
