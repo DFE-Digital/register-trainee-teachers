ad6317c5fe75d1eb1c711d6a49dd0d6917e96d6a
Jack Bracewell <jack@reasonable.engineering>
2021-07-12 16:24:19 +0100

    [2104] Show specialisms on publish course confirm page

--- spec/support/page_objects/trainees/confirm_publish_course.rb
+++ spec/support/page_objects/trainees/confirm_publish_course.rb
@@ -2,12 +2,24 @@
 
 module PageObjects
   module Trainees
+    class SummaryListRows < SitePrism::Section
+      element :key, ".govuk-summary-list__key"
+      element :value, ".govuk-summary-list__value"
+    end
+
     class ConfirmPublishCourse < PageObjects::Base
       include PageObjects::Helpers
       set_url "/trainees/{trainee_id}/confirm-publish-course/edit"
 
+      sections :summary_list_rows, SummaryListRows, ".govuk-summary-list__row"
+
       element :confirm_course_button, "button[type='submit']", text: "Confirm course"
       element :submit_button, "button[type='submit']"
+
+      def subject_description
+        subject_row = summary_list_rows.find { |row| row.key.text =~ /Subject/ }
+        subject_row.value.text
+      end
     end
   end
 end
