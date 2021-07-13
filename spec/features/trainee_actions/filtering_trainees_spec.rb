1adbe97123714635db00fe388f46b0d74bb0e3ad
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2021-07-13 11:39:32 +0100

    [2212] Added conditional to show apply_draft filter

--- spec/features/trainee_actions/filtering_trainees_spec.rb
+++ spec/features/trainee_actions/filtering_trainees_spec.rb
@@ -22,6 +22,11 @@ RSpec.feature "Filtering trainees" do
     then_all_trainees_are_visible
   end
 
+  scenario "can filter by apply_drafts" do
+    when_i_filter_by_apply_draft_status
+    then_only_the_apply_draft_trainee_is_visible
+  end
+
   scenario "can filter by status" do
     when_i_filter_by_draft_status
     then_only_the_draft_trainee_is_visible
@@ -104,6 +109,7 @@ private
     @withdrawn_trainee ||= create(:trainee, :withdrawn)
     @early_years_trainee ||= create(:trainee, :early_years_undergrad)
     @primary_trainee ||= create(:trainee, course_age_range: AgeRange::THREE_TO_EIGHT)
+    @apply_draft_trainee ||= create(:trainee, :with_apply_application)
     Trainee.update_all(provider_id: @current_user.provider.id)
   end
 
@@ -123,6 +129,11 @@ private
 
   def when_i_filter_by_draft_status
     trainee_index_page.draft_checkbox.click
+    trainee_index_page.apply_filters.click
+  end
+
+  def when_i_filter_by_apply_draft_status
+    trainee_index_page.apply_draft_checkbox.click
     trainee_index_page.apply_filters.click
   end
 
@@ -192,6 +203,11 @@ private
     ].each do |trainee|
       expect(trainee_index_page).not_to have_text(full_name(trainee))
     end
+  end
+
+  def then_only_the_apply_draft_trainee_is_visible
+    expect(trainee_index_page).to have_text(full_name(@apply_draft_trainee))
+    expect(trainee_index_page).not_to have_text(full_name(@draft_trainee))
   end
 
   def then_only_the_draft_trainee_is_visible
