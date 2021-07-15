cf3f23f33c0395dd862f5841bf02b674b9378859
De Fong <de_fong2k@yahoo.com>
2021-07-15 12:30:23 +0100

    [2265] Added tests

--- spec/components/collapsed_section/view_spec.rb
+++ spec/components/collapsed_section/view_spec.rb
@@ -7,12 +7,13 @@ module CollapsedSection
     alias_method :component, :page
     let(:title) { "title" }
     let(:link_text) { "link_text" }
+    let(:hint_text) { "hint_text" }
     let(:url) { "url" }
 
     context "default" do
       before do
         render_inline(
-          described_class.new(title: title, link_text: link_text, url: url),
+          described_class.new(title: title, link_text: link_text, hint_text: hint_text, url: url),
         )
       end
 
@@ -22,6 +23,10 @@ module CollapsedSection
 
       it "renders the link" do
         expect(component).to have_link(link_text, href: url)
+      end
+
+      it "renders the hint text" do
+        expect(component).to have_css(".govuk-hint", text: hint_text)
       end
 
       it "renders the correct css classes" do
