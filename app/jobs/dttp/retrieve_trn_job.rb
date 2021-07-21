01d16bd60f7fbc11f3c3706f4fc598d6377df376
Graham Pengelly <graham@three-tier.com>
2021-07-21 14:12:08 +0100

    [xxx] Allow AO before clockover
    We are requeuing register for trn and retrieve trn jobs that are
    submitted now for after clockover.
    
    We need to allow AO and Early years AO to go through now but that was
    missed from the original work.

--- app/jobs/dttp/retrieve_trn_job.rb
+++ app/jobs/dttp/retrieve_trn_job.rb
@@ -13,7 +13,7 @@ module Dttp
       @timeout_after = timeout_after
       @trainee = trainee
 
-      if before_clockover?
+      if before_clockover? && trainee_is_not_assessment_only?
         requeue_after_clockover
         return
       end
@@ -59,6 +59,10 @@ module Dttp
 
     def requeue_after_clockover
       self.class.set(wait_until: clockover_date).perform_later(trainee, clockover_date + Settings.jobs.max_poll_duration_days.days)
+    end
+
+    def trainee_is_not_assessment_only?
+      !(trainee.assessment_only? || trainee.early_years_assessment_only?)
     end
   end
 end
