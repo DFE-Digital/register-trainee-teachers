9c90bde2a73c3d2e22667017d60c9e2439c32db1
De Fong <de_fong2k@yahoo.com>
2021-07-29 09:13:44 +0100

    [2364] Added validation for ITT start date

--- app/forms/itt_start_date_form.rb
+++ app/forms/itt_start_date_form.rb
@@ -6,4 +6,22 @@ private
   def date_field
     @date_field ||= :course_start_date
   end
+
+  def date_valid
+    if date_string.nil?
+      errors.add(:date_string, :blank)
+    elsif date_string == "other" && [day, month, year].all?(&:blank?)
+      errors.add(:date, :blank)
+    elsif year.to_i > next_year
+      errors.add(:date, :future)
+    elsif !date.is_a?(Date)
+      errors.add(:date, :invalid)
+    elsif date < 10.years.ago
+      errors.add(:date, :too_old)
+    end
+  end
+
+  def next_year
+    Time.zone.now.year.next
+  end
 end
