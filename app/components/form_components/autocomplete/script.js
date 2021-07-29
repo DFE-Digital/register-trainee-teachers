8fc8918071910be5c0ed12fa6d21151ccb1dba7f
Abigail McPhillips <abigail.mcphillips@gmail.com>
2021-07-29 12:51:13 +0100

    [2838] Send failed searches and final selection to GTM

--- app/components/form_components/autocomplete/script.js
+++ app/components/form_components/autocomplete/script.js
@@ -22,6 +22,26 @@ const enhanceOption = (option) => {
   }
 }
 
+const trackFailedSearch = (query, queries, failedSearches) => {
+  const lastQuery = queries[queries.length - 2]
+  const lastFailedSeach = failedSearches[failedSearches.length - 1]
+  const pressedDelete = lastQuery && query.length < lastQuery.length
+  const isSubstring = lastFailedSeach && lastFailedSeach.indexOf(query) !== -1
+  // We don't store substrings of previous failed searches - this is probably
+  // the user deleting a word.
+  if (pressedDelete && !isSubstring) failedSearches.push(lastQuery)
+}
+
+const sendTrackingEvent = (val, failedSearches) => {
+  if (val && failedSearches.length > 0) {
+    window.dataLayer.push({
+      event: 'autocomplete-search',
+      failedSearches: failedSearches,
+      match: val
+    })
+  }
+}
+
 const setupAutoComplete = (component) => {
   const selectEl = component.querySelector('select')
   const selectOptions = Array.from(selectEl.options)
@@ -35,16 +55,28 @@ const setupAutoComplete = (component) => {
   const matches = /(\w+)\[(\w+)\]/.exec(selectEl.name)
   const rawFieldName = `${matches[1]}[${matches[2]}_raw]`
 
+  const queries = []
+  const failedSearches = []
+
   accessibleAutocomplete.enhanceSelectElement({
     defaultValue: inError ? '' : inputValue,
     selectElement: selectEl,
     minLength: 2,
     source: (query, populateResults) => {
+      queries.push(query)
+      trackFailedSearch(query, queries, failedSearches)
       populateResults(sort(query, options))
     },
     autoselect: true,
     templates: { suggestion: (value) => suggestion(value, options) },
-    name: rawFieldName
+    name: rawFieldName,
+    onConfirm: (val) => {
+      sendTrackingEvent(val, failedSearches)
+      // The below is copied directly from autocomplete source code. Providing
+      // your own `onConfirm` function seems to override the default functionality... https://github.com/alphagov/accessible-autocomplete/blob/935f0d43aea1c606e6b38985e3fe7049ddbe98be/src/wrapper.js
+      const requestedOption = [].filter.call(selectOptions, option => (option.textContent || option.innerText) === val)[0]
+      if (requestedOption) requestedOption.selected = true
+    }
   })
 
   if (inError) {
