f273446dbe7316440ac593bf2269b7ddb578ba83
Abigail McPhillips <abigail.mcphillips@gmail.com>
2021-08-02 12:38:55 +0100

    [2383] Pull tracking code out into separate file

--- app/components/form_components/autocomplete/script.js
+++ app/components/form_components/autocomplete/script.js
@@ -2,6 +2,7 @@ import './style.scss'
 import accessibleAutocomplete from 'accessible-autocomplete'
 import { nodeListForEach } from 'govuk-frontend/govuk/common'
 import sort from './sort.js'
+import { trackFailedSearch, sendTrackingEvent } from './tracker.js'
 
 const $allAutocompleteElements = document.querySelectorAll('[data-module="app-autocomplete"]')
 const defaultValueOption = component => component.getAttribute('data-default-value') || ''
@@ -22,26 +23,6 @@ const enhanceOption = (option) => {
   }
 }
 
-const trackFailedSearch = (query, queries, failedSearches) => {
-  const lastQuery = queries[queries.length - 2]
-  const lastFailedSeach = failedSearches[failedSearches.length - 1]
-  const pressedDelete = lastQuery && query.length < lastQuery.length
-  const isSubstring = lastFailedSeach && lastFailedSeach.indexOf(query) !== -1
-  // We don't store substrings of previous failed searches - this is probably
-  // the user deleting a word.
-  if (pressedDelete && !isSubstring) failedSearches.push(lastQuery)
-}
-
-const sendTrackingEvent = (val, failedSearches) => {
-  if (val && failedSearches.length > 0) {
-    window.dataLayer.push({
-      event: 'autocomplete-search',
-      failedSearches: failedSearches,
-      match: val
-    })
-  }
-}
-
 const setupAutoComplete = (component) => {
   const selectEl = component.querySelector('select')
   const selectOptions = Array.from(selectEl.options)
@@ -55,6 +36,7 @@ const setupAutoComplete = (component) => {
   const matches = /(\w+)\[(\w+)\]/.exec(selectEl.name)
   const rawFieldName = `${matches[1]}[${matches[2]}_raw]`
 
+  // For tracking failed search attempts
   const queries = []
   const failedSearches = []
 
@@ -64,7 +46,7 @@ const setupAutoComplete = (component) => {
     minLength: 2,
     source: (query, populateResults) => {
       queries.push(query)
-      trackFailedSearch(query, queries, failedSearches)
+      trackFailedSearch(queries, failedSearches)
       populateResults(sort(query, options))
     },
     autoselect: true,
