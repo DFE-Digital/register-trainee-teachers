35fd93f0bf605431ea5f352be1e5c608b7ae4d21
Abigail McPhillips <abigail.mcphillips@gmail.com>
2021-08-02 17:27:46 +0100

    [2419] Add back homepage squares for missing states, remove academic year heading

--- app/components/badges/view.rb
+++ app/components/badges/view.rb
@@ -4,8 +4,21 @@ module Badges
   class View < GovukComponent::Base
     attr_reader :state_counts
 
+    INCLUDED_STATES = %w[
+      submitted_for_trn
+      trn_received
+      recommended_for_award
+      awarded
+      eyts_recommended
+      eyts_awarded
+      qts_recommended
+      qts_awarded
+      deferred
+      withdrawn
+    ].freeze
+
     def initialize(state_counts)
-      @state_counts = state_counts
+      @state_counts = state_counts.slice(*INCLUDED_STATES)
     end
 
     def map_state_to_filter_params(state)
