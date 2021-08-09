a0e7051e7a6caefbaee51f5edd9e639189ba3f1a
Arlston Fernandes <gpg@arlston.com>
2021-08-09 09:09:40 +0100

    [2414] Enable multiple missing information items with the same labels

--- app/view_objects/missing_data_view.rb
+++ app/view_objects/missing_data_view.rb
@@ -15,15 +15,17 @@ class MissingDataView
 
   def missing_items_content
     @missing_items_content ||= safe_join(
-      missing_fields.map do |field|
-        tag.li(
-          link_to(
-            I18n.t("views.missing_data_view.missing_field_text", missing_field: get_display_name(field)),
-            "##{get_display_name(field).parameterize}",
-            class: "govuk-notification-banner__link",
-          ),
-        )
-      end.uniq,
+      missing_fields.map.with_index(1) do |fieldset, index|
+        fieldset.map do |field|
+          tag.li(
+            link_to(
+              I18n.t("views.missing_data_view.missing_field_text", missing_field: get_display_name(field)),
+              get_link_anchor(field, index),
+              class: "govuk-notification-banner__link",
+            ),
+          )
+        end.uniq
+      end,
     )
   end
 
@@ -34,6 +36,12 @@ class MissingDataView
 private
 
   attr_reader :form_instance, :missing_fields
+
+  def get_link_anchor(field, index)
+    return "##{get_display_name(field).parameterize}" if missing_fields.size == 1
+
+    "##{get_display_name(field).parameterize}-#{index}"
+  end
 
   def get_display_name(field)
     I18n.t("views.missing_data_view.missing_fields_mapping.#{field}")
