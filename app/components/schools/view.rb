0aa75b1e48261c1b2e2f6728ea3aa4eaa22a8239
David <davidrk@hey.com>
2021-08-11 17:42:11 +0100

    [2418] Handle missing data for schools

--- app/components/schools/view.rb
+++ app/components/schools/view.rb
@@ -8,8 +8,8 @@ module Schools
     def initialize(data_model:, has_errors: false)
       @data_model = data_model
       @has_errors = has_errors
-      @lead_school = trainee.lead_school
-      @employing_school = trainee.employing_school
+      @lead_school = fetch_lead_school
+      @employing_school = fetch_employing_school
     end
 
     def trainee
@@ -18,13 +18,41 @@ module Schools
 
   private
 
-    attr_accessor :data_model, :lead_school, :employing_school
+    attr_accessor :data_model, :lead_school, :employing_school, :has_errors
 
     def change_paths(school_type)
       {
         lead: edit_trainee_lead_schools_path(trainee),
         employing: edit_trainee_employing_schools_path(trainee),
       }[school_type.to_sym]
+    end
+
+    def mappable_field(field_value, field_label, section_url)
+      MappableFieldRow.new(
+        field_value: field_value,
+        field_label: field_label,
+        text: t("components.confirmation.missing"),
+        action_url: section_url,
+        has_errors: has_errors,
+      ).to_h
+    end
+
+    def fetch_lead_school
+      return data_model.lead_school if data_model.respond_to?(:lead_school)
+
+      fetch_school(data_model.lead_school_id)
+    end
+
+    def fetch_employing_school
+      return data_model.employing_school if data_model.respond_to?(:employing_school)
+
+      fetch_school(data_model.employing_school_id)
+    end
+
+    def fetch_school(id)
+      return if id.blank?
+
+      School.find(id)
     end
   end
 end
