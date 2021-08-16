6132090976eb02e023207ea9a24a04ebbdd7b050
Abigail McPhillips <abigail.mcphillips@gmail.com>
2021-08-16 11:17:19 +0100

    [2411] Future Teaching Scholars DTTP mapping

--- app/lib/dttp/code_sets/routes.rb
+++ app/lib/dttp/code_sets/routes.rb
@@ -12,6 +12,8 @@ module Dttp
         TRAINING_ROUTE_ENUMS[:early_years_postgrad] => { entity_id: "6789922e-acc2-e611-80be-00155d010316" },
         TRAINING_ROUTE_ENUMS[:early_years_assessment_only] => { entity_id: "6589922e-acc2-e611-80be-00155d010316" },
         TRAINING_ROUTE_ENUMS[:pg_teaching_apprenticeship] => { entity_id: "cf9154ee-5678-e811-80f6-005056ac45bb" },
+        # DTTP recognise future_teaching_scholars as a route not an initiative, hence this 'odd' mapping.
+        ROUTE_INITIATIVES_ENUMS[:future_teaching_scholars] => { entity_id: "e7b6efbc-8d96-ea11-a811-000d3ab55801" },
       }.freeze
     end
   end
--- app/lib/dttp/code_sets/training_initiatives.rb
+++ app/lib/dttp/code_sets/training_initiatives.rb
@@ -3,11 +3,12 @@
 module Dttp
   module CodeSets
     module TrainingInitiatives
+      # DTTP recognise future_teaching_scholars as a route not an initiative,
+      # hence there is no mapping. See Dttp::CodeSets::Routes.
       MAPPING = {
         ROUTE_INITIATIVES_ENUMS[:transition_to_teach] => { entity_id: "544a6435-e2af-ea11-a812-000d3ab55801" },
         ROUTE_INITIATIVES_ENUMS[:now_teach] => { entity_id: "04516b48-e2af-ea11-a812-000d3ab55801" },
         ROUTE_INITIATIVES_ENUMS[:maths_physics_chairs_programme_researchers_in_schools] => { entity_id: "6a08fd3f-276e-e711-80d2-005056ac45bb" },
-        ROUTE_INITIATIVES_ENUMS[:future_teaching_scholars] => { entity_id: "" },
       }.freeze
     end
   end
