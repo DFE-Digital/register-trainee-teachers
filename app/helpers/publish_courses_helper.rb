f6a63710afd7e2d54648edfa9fd4e22d5ff5c630
David <davidrk@hey.com>
2021-08-25 16:46:27 +0100

    [2633] Fix publish courses fieldset text

--- app/helpers/publish_courses_helper.rb
+++ app/helpers/publish_courses_helper.rb
@@ -5,4 +5,10 @@ module PublishCoursesHelper
     summary_with_route = t(".summary_with_route", summary: course.summary, route: t("activerecord.attributes.trainee.training_routes.#{course.route}"))
     trainee.apply_application? ? summary_with_route : course.summary
   end
+
+  def courses_fieldset_text_for(trainee)
+    return t("views.forms.publish_course_details.all_courses_message") if trainee.apply_application?
+
+    t("views.forms.publish_course_details.route_message", route: route_title(trainee.training_route))
+  end
 end
