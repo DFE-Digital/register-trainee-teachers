ae967b6fd5c033be1b6147e44a8127c0291ec131
Arlston Fernandes <gpg@arlston.com>
2021-09-13 15:18:23 +0100

    Exclude primary subjects from course subject list

--- app/models/subject_specialism.rb
+++ app/models/subject_specialism.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class SubjectSpecialism < ApplicationRecord
+  PRIMARY_SUBJECT_NAMES = [
+    CourseSubjects::EARLY_YEARS_TEACHING,
+    CourseSubjects::PRIMARY_TEACHING,
+    CourseSubjects::SPECIALIST_TEACHING_PRIMARY_WITH_MATHEMETICS,
+  ].freeze
+
   belongs_to :allocation_subject, inverse_of: :subject_specialisms
 
   scope :order_by_name, -> { order("LOWER(name)") }
+  scope :secondary, -> { where.not(name: PRIMARY_SUBJECT_NAMES) }
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/subject_specialism_spec.rb
+++ spec/models/subject_specialism_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe SubjectSpecialism, type: :model do
 
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+
+  describe ".secondary" do
+    subject { described_class.secondary }
+
+    let!(:secondary_specialism) { create(:subject_specialism, name: CourseSubjects::PRODUCT_DESIGN) }
+
+    before do
+      create(:subject_specialism, name: CourseSubjects::EARLY_YEARS_TEACHING)
+      create(:subject_specialism, name: CourseSubjects::PRIMARY_TEACHING)
+      create(:subject_specialism, name: CourseSubjects::SPECIALIST_TEACHING_PRIMARY_WITH_MATHEMETICS)
+    end
+
+    it { is_expected.to match_array([secondary_specialism]) }
+  end
 end
