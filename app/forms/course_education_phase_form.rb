0da4b4dede42a402c47b32fcd0291f7c7330afd7
Arlston Fernandes <gpg@arlston.com>
2021-09-13 16:18:53 +0100

    [2700] Clear out the course subjects when changing education phase

--- app/forms/course_education_phase_form.rb
+++ app/forms/course_education_phase_form.rb
@@ -9,9 +9,27 @@ class CourseEducationPhaseForm < TraineeForm
 
   validates :course_education_phase, presence: true
 
+  def save!
+    if valid?
+      trainee.assign_attributes(fields)
+      clear_course_subjects if trainee.course_education_phase_changed?
+      trainee.save!
+      clear_stash
+    else
+      false
+    end
+  end
+
 private
 
   def compute_fields
     trainee.attributes.symbolize_keys.slice(*FIELDS).merge(new_attributes)
+  end
+
+  def clear_course_subjects
+    trainee.assign_attributes(course_subject_one: nil,
+                              course_subject_two: nil,
+                              course_subject_three: nil,
+                              course_age_range: nil)
   end
 end
--- spec/forms/course_education_phase_form_spec.rb
+++ spec/forms/course_education_phase_form_spec.rb
@@ -3,9 +3,10 @@
 require "rails_helper"
 
 describe CourseEducationPhaseForm, type: :model do
+  let(:params) { {} }
   let(:trainee) { build(:trainee) }
 
-  subject { described_class.new(trainee) }
+  subject { described_class.new(trainee, params: params) }
 
   describe "validations" do
     context "course_education_phase is nil" do
@@ -17,6 +18,34 @@ describe CourseEducationPhaseForm, type: :model do
             "activemodel.errors.models.course_education_phase_form.attributes.course_education_phase.blank",
           ),
         )
+      end
+    end
+  end
+
+  describe "#save!" do
+    let(:trainee) { create(:trainee, :with_course_details_and_study_mode) }
+
+    let(:params) do
+      {
+        course_education_phase: COURSE_EDUCATION_PHASE_ENUMS[:secondary],
+      }
+    end
+
+    it "clears out the trainee course subject" do
+      expect {
+        subject.save!
+      }.to change { subject.trainee.course_subject_one }
+       .from(subject.trainee.course_subject_one).to(nil)
+       .and change { subject.trainee.course_age_range }
+       .from(subject.trainee.course_age_range).to([])
+    end
+
+    context "when the education phase is not changed" do
+      let(:trainee) { create(:trainee, :with_course_details_and_study_mode, :with_secondary_education) }
+
+      it "does not clear out the course subject" do
+        subject.save!
+        expect(trainee.reload.course_subject_one).not_to be_nil
       end
     end
   end
