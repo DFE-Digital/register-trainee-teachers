8b541f87507b3e8789cbd4abfd78fd3bc9597b6a
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2021-09-22 15:36:04 +0100

    [2674] Backfill part time and full time study mode for trainees based on the course

---
+++ db/data/20210922135515_backfill_study_mode.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class BackfillStudyMode < ActiveRecord::Migration[6.1]
+  def up
+    Trainee.includes(:published_course).where(study_mode: nil).where.not(course_code: nil).find_each do |trainee|
+      if (course = trainee.published_course)
+        study_mode = course_study_mode_if_valid(course)
+        trainee.update!(study_mode: study_mode) if study_mode
+      end
+    end
+  end
+
+  def course_study_mode_if_valid(course)
+    course.study_mode if TRAINEE_STUDY_MODE_ENUMS.keys.include?(course.study_mode)
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
