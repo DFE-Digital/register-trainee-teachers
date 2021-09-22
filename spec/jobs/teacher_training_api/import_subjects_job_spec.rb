5c6320e66bddc459f26a4cc341f1c0d8129116af
Arlston Fernandes <gpg@arlston.com>
2021-09-22 15:49:27 +0100

    Rename ambiguous variable name subject to course/degree subjects

--- spec/jobs/teacher_training_api/import_subjects_job_spec.rb
+++ spec/jobs/teacher_training_api/import_subjects_job_spec.rb
@@ -6,12 +6,12 @@ module TeacherTrainingApi
   describe ImportSubjectsJob do
     include ActiveJob::TestHelper
 
-    let(:subject) { double }
+    let(:course_subject) { double }
     let(:course_attributes) { double }
 
     before do
-      allow(ImportSubject).to receive(:call).with(subject: subject).and_return(true)
-      allow(RetrieveSubjects).to receive(:call).and_return([subject])
+      allow(ImportSubject).to receive(:call).with(subject: course_subject).and_return(true)
+      allow(RetrieveSubjects).to receive(:call).and_return([course_subject])
 
       described_class.perform_now
     end
@@ -19,7 +19,7 @@ module TeacherTrainingApi
     context "when the feature flag is turned on", feature_import_courses_from_ttapi: true do
       it "retrieves all the subjects" do
         expect(RetrieveSubjects).to have_received(:call)
-        expect(ImportSubject).to have_received(:call).with(subject: subject)
+        expect(ImportSubject).to have_received(:call).with(subject: course_subject)
       end
     end
   end
