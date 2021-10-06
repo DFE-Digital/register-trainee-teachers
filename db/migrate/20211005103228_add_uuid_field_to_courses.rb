8cdb8d57c0e90d1c42c579949a907642f9e0e3d9
David <davidrk@hey.com>
2021-10-06 10:27:40 +0100

    [2860] Enable pgcrypto in schema if it's not already setup

--- db/migrate/20211005103228_add_uuid_field_to_courses.rb
+++ db/migrate/20211005103228_add_uuid_field_to_courses.rb
@@ -2,6 +2,8 @@
 
 class AddUuidFieldToCourses < ActiveRecord::Migration[6.1]
   def change
+    enable_extension "pgcrypto" unless extension_enabled?("pgcrypto")
+
     add_column :courses, :uuid, :uuid
     add_index :courses, :uuid, unique: true
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_10_05_103228) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "allocation_subjects", force: :cascade do |t|
