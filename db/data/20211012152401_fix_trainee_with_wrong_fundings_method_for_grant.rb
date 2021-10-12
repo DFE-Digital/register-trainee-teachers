b89764ee55129e8b465745caf2f1c450a039d636
De Fong <de_fong2k@yahoo.com>
2021-10-12 16:32:18 +0100

    [2821] Fixed early year salaried trainee grant issue

---
+++ db/data/20211012152401_fix_trainee_with_wrong_fundings_method_for_grant.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class FixTraineeWithWrongFundingsMethodForGrant < ActiveRecord::Migration[6.1]
+  def up
+    all_early_years_subject_specialisms = AllocationSubject.find_by(name: "Early years ITT").subject_specialisms.pluck(:name)
+
+    early_years_salaried = TRAINING_ROUTE_ENUMS[:early_years_salaried]
+
+    Trainee.where(applying_for_bursary: true, training_route: early_years_salaried, course_subject_one: all_early_years_subject_specialisms)
+      .update_all(applying_for_bursary: nil, applying_for_grant: true)
+
+    Trainee.where(applying_for_bursary: false, training_route: early_years_salaried, course_subject_one: all_early_years_subject_specialisms)
+      .update_all(applying_for_bursary: nil, applying_for_grant: false)
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
