034265644d2009c825d50d27a72ea075ffdbef78
David <davidrk@hey.com>
2021-10-18 11:00:14 +0100

    [2954] Fix degrees flow post trn submission

--- app/controllers/trainees/degrees/confirm_details_controller.rb
+++ app/controllers/trainees/degrees/confirm_details_controller.rb
@@ -10,7 +10,7 @@ module Trainees
           @confirm_detail_form = ConfirmDetailForm.new(mark_as_completed: trainee.progress.degrees)
         end
 
-        @confirmation_component = if trainee.degrees.empty?
+        @confirmation_component = if trainee.degrees.empty? && trainee.draft?
                                     CollapsedSection::View.new(
                                       title: t("components.incomplete_section.degree_details_not_provided"),
                                       link_text: t("components.incomplete_section.add_degree_details"),
@@ -22,6 +22,14 @@ module Trainees
                                   end
       end
 
+      def update
+        if @form.trainee_reset_degrees?
+          update_degrees_for_non_draft_trainee
+        else
+          update_degrees_for_trainee
+        end
+      end
+
     private
 
       def load_missing_data_view
@@ -30,6 +38,28 @@ module Trainees
 
       def build_form
         DegreesForm.new(trainee)
+      end
+
+      def update_degrees_for_trainee
+        if @form.valid?
+          trainee.draft? ? toggle_trainee_progress_field : @form.save!
+
+          flash[:success] = "Trainee #{flash_message_title} updated"
+
+          redirect_to(page_tracker.last_origin_page_path || trainee_path(trainee))
+        else
+          @confirmation_component = view_component.new(data_model: data_model, has_errors: true)
+
+          render(:show)
+        end
+      end
+
+      def update_degrees_for_non_draft_trainee
+        @form.save!
+
+        flash[:success] = "Trainee #{flash_message_title} updated"
+
+        redirect_to(page_tracker.last_origin_page_path || trainee_path(trainee))
       end
     end
   end
