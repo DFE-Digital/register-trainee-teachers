21c5147b8daeeedec5ebde68a31d2823612d89ea
De Fong <de_fong2k@yahoo.com>
2021-10-21 09:34:28 +0100

    [2552] Added parameters to be filter from log

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,4 +3,21 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[email password]
+SANITIZED_REQUEST_PARAMS = %i[
+  address_line_one
+  address_line_two
+  date_of_birth
+  email
+  first_name
+  first_names
+  international_address
+  last_name
+  middle_names
+  password
+  postcode
+  region
+  token
+  town_city
+].freeze
+
+Rails.application.config.filter_parameters += SANITIZED_REQUEST_PARAMS
