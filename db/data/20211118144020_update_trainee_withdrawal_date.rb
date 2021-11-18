4d8e929ab6fff6cdeca98f6b3a91dd7e8a5ea05c
John Henderson <john@logixworks.co.uk>
2021-11-18 14:45:29 +0000

    [3230] Change trainee withdraw date

---
+++ db/data/20211118144020_update_trainee_withdrawal_date.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class UpdateTraineeWithdrawalDate < ActiveRecord::Migration[6.1]
+  def up
+    Trainee.find_by(trn: 2075439).update(withdraw_date: Date.parse("01/09/2021"))
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
