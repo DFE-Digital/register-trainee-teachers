313d151eb21b0b8033214368e39c1044c9539515
Arlston Fernandes <gpg@arlston.com>
2021-12-01 17:12:50 +0000

    [3317] Bare bones create trainee
    Create a trainee with first name, last name and DOB.
    Expose provider ID on the trainee model.

--- spec/factories/dttp/trainees.rb
+++ spec/factories/dttp/trainees.rb
@@ -2,12 +2,23 @@
 
 FactoryBot.define do
   factory :dttp_trainee, class: "Dttp::Trainee" do
+    transient do
+      provider_dttp_id { SecureRandom.uuid }
+    end
+
     dttp_id { SecureRandom.uuid }
     response {
-      {
-        contactid: dttp_id,
+      ApiStubs::Dttp::Contact.attributes.merge {
+        {
+          "contactid" => dttp_id,
+          "_parentcustomerid_value" => provider_dttp_id,
+        }
       }
     }
     state { "unprocessed" }
+
+    trait :with_placement_assignment do
+      placement_assignments { [build(:dttp_placement_assignment, contact_dttp_id: dttp_id)] }
+    end
   end
 end
