2099fd30c46ba221d7b936f94f9f0b7dabcdcd2c
Abigail McPhillips <abigail.mcphillips@gmail.com>
2021-12-02 15:26:08 +0000

    [3317] Map TRN from dttp_trainee to trainee
    Additionally, swap to using a factory to mock the api response rather than the API stub

--- spec/factories/dttp/api_trainee.rb
+++ spec/factories/dttp/api_trainee.rb
@@ -3,6 +3,19 @@
 FactoryBot.define do
   factory :api_trainee, class: Hash do
     contactid { SecureRandom.uuid }
+    _parentcustomerid_value { SecureRandom.uuid }
+    firstname { Faker::Name.first_name }
+    lastname { Faker::Name.last_name }
+    emailaddress1 { "#{firstname}.#{lastname}@example.com" }
+    birthdate { Faker::Date.birthday(min_age: 18, max_age: 65).strftime("%Y-%m-%d") }
+    gendercode { 1 }
+    address1_line1 { Faker::Address.street_address }
+    address1_line2 { Faker::Address.street_name }
+    address1_line3 { Faker::Address.city }
+    address1_postalcode { Faker::Address.postcode }
+    dfe_traineeid { "#{firstname}.#{lastname}.#{birthdate}" }
+    _dfe_nationality_value { "d17d640e-5c62-e711-80d1-005056ac45bb" }
+    dfe_trn { Faker::Number.number(digits: 7) }
 
     initialize_with { attributes.stringify_keys }
     to_create { |instance| instance }
