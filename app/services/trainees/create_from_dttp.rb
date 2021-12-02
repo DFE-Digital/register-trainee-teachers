14a04082fb1b08517cf9158e5b3a040d02414342
Abigail McPhillips <abigail.mcphillips@gmail.com>
2021-12-02 14:17:25 +0000

    [3317] Map diversity_disclosed enum
    We assume that if a disability disclosure is made, then the trainee disclosed diversity information

--- app/services/trainees/create_from_dttp.rb
+++ app/services/trainees/create_from_dttp.rb
@@ -103,7 +103,7 @@ module Trainees
 
       if disability == Diversities::NO_KNOWN_DISABILITY
         return {
-          diversity_disclosure: true,
+          diversity_disclosure: Diversities::DIVERSITY_DISCLOSURE_ENUMS[:diversity_disclosed],
           disability_disclosure: Diversities::DISABILITY_DISCLOSURE_ENUMS[:no_disability],
         }
       end
@@ -111,14 +111,14 @@ module Trainees
       # TODO: This needs a decision, since DTTP may have 'multiple disabilities'
       if disability == Diversities::MULTIPLE_DISABILITIES
         return {
-          diversity_disclosure: true,
+          diversity_disclosure: Diversities::DIVERSITY_DISCLOSURE_ENUMS[:diversity_disclosed],
           disability_disclosure: Diversities::DISABILITY_DISCLOSURE_ENUMS[:disabled],
           disabilities: Disability.where(name: ::Diversities::OTHER),
         }
       end
 
       {
-        diversity_disclosure: true,
+        diversity_disclosure: Diversities::DIVERSITY_DISCLOSURE_ENUMS[:diversity_disclosed],
         disability_disclosure: Diversities::DISABILITY_DISCLOSURE_ENUMS[:disabled],
         disabilities: Disability.where(name: disability),
       }
