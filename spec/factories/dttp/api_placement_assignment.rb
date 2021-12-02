8a1951739c9c31e12b57686f7ed2e314201022b3
Abigail McPhillips <abigail.mcphillips@gmail.com>
2021-12-02 17:06:07 +0000

    [3317] Map course-related attributes from dttp_trainee to trainee

--- spec/factories/dttp/api_placement_assignment.rb
+++ spec/factories/dttp/api_placement_assignment.rb
@@ -4,6 +4,14 @@ FactoryBot.define do
   factory :api_placement_assignment, class: Hash do
     dfe_placementassignmentid { SecureRandom.uuid }
     _dfe_contactid_value { SecureRandom.uuid }
+    _dfe_ittsubject1id_value { Dttp::CodeSets::CourseSubjects::MAPPING.to_a.sample[1][:entity_id] }
+    _dfe_ittsubject2id_value { Dttp::CodeSets::CourseSubjects::MAPPING.to_a.sample[1][:entity_id] }
+    _dfe_ittsubject3id_value { Dttp::CodeSets::CourseSubjects::MAPPING.to_a.sample[1][:entity_id] }
+    dfe_programmestartdate { Faker::Date.in_date_period(month: 9) }
+    dfe_programmeeenddate { Faker::Date.in_date_period(month: 8, year: Faker::Date.in_date_period.year + 1) }
+    dfe_commencementdate { Faker::Date.between(from: dfe_programmestartdate, to: dfe_programmeeenddate) }
+    _dfe_coursephaseid_value { Dttp::CodeSets::AgeRanges::MAPPING.to_a.sample[1][:entity_id] }
+    _dfe_studymodeid_value { Dttp::CodeSets::CourseStudyModes::MAPPING.to_a.sample[1][:entity_id] }
 
     initialize_with { attributes.stringify_keys }
     to_create { |instance| instance }
