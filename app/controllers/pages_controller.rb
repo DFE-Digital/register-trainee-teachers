7d81dd4ccf56955b39e6e2f31be8af592c4787b4
Graham Pengelly <graham@three-tier.com>
2022-01-05 15:16:14 +0000

    [xxx] 'Fix' the start page
    Redirect from the start page if organisation context isn't set.
    
    The start page bypasses the authenticate before action and annoyingly
    has it's own 'implementation' of it so we need to include the
    organisation check in here too. We should try and refactor this.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,9 @@ class PagesController < ApplicationController
     session[:requested_path] = root_path
     if authenticated?
       @trainees = policy_scope(Trainee.all)
+      # TODO this is annoying
+      redirect_to(organisation_contexts_path) && return if current_user.organisation.nil? && !current_user.system_admin?
+
       @home_view = HomeView.new(@trainees)
       @registered_states_for_filter = HomeView::REGISTERED_STATES_FOR_FILTER
       render(:home)
