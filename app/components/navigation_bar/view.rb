93ffd4581156984ddb591c4590716bd5fcb55d43
Graham Pengelly <graham@three-tier.com>
2022-01-05 15:15:03 +0000

    [xxx] Update navigation bar
    Add link to the organisation context switcher to the nav bar which
    renders the current organisation name.

--- app/components/navigation_bar/view.rb
+++ app/components/navigation_bar/view.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class NavigationBar::View < GovukComponent::Base
-  attr_reader :items, :current_path
+  attr_reader :items, :current_path, :current_user
 
   def initialize(items:, current_path:, current_user: {})
     @items = items
@@ -10,13 +10,31 @@ class NavigationBar::View < GovukComponent::Base
   end
 
   def item_link(item)
-    link_params = { class: "moj-primary-navigation__link" }
+    link_params = { class: link_class }
     link_params.merge!(aria: { current: "page" }) if show_current_link?(item)
     govuk_link_to(item[:name], item[:url], link_params)
   end
 
+  def organisation_link
+    govuk_link_to(organisation.name, organisation_contexts_path, class: link_class )
+  end
+
   def user_signed_in?
-    @current_user.present?
+    current_user.present?
+  end
+
+  def show_organisation_link?
+    user_signed_in? &&
+      current_user.has_multiple_organisations? &&
+      current_user.organisation.present?
+  end
+
+  def organisation
+    current_user.organisation
+  end
+
+  def link_class
+    "moj-primary-navigation__link"
   end
 
 private
