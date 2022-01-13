a70b502be4f03f64289acb9bb1d2194076ace4de
Graham Pengelly <graham@three-tier.com>
2022-01-13 17:00:48 +0000

    [3508] OrganisationContexts#show
    Basic routing and redirect to root.

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -5,4 +5,11 @@ class OrganisationsController < ApplicationController
     @providers = current_user.providers
     @lead_schools = current_user.lead_schools
   end
+
+  def show
+    # session[:current_organisation] = { type: params[:type], id: params[:id] }
+
+    # redirect_to session.delete(:requested_path) || root_path
+    redirect_to(root_path)
+  end
 end
