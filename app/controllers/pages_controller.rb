bbe5e1483dd6a86c1f0c2f64e4910256fa0bf8e0
Graham Pengelly <graham@three-tier.com>
2022-01-21 14:33:45 +0000

    [3508] Handle organisation context in pages#start
    * Check for nil organisation and redirect
    
    This controller annoyingly bypasses the normal `authenticate` because
    the action has an authenticated? branch. So we need to duplicate the
    organisation check here.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,10 @@ class PagesController < ApplicationController
   def start
     session[:requested_path] = root_path
     if authenticated?
+      if FeatureService.enabled?(:user_can_have_multiple_organisations) && organisation_not_set?
+        redirect_to(organisation_contexts_path) && return
+      end
+
       @trainees = policy_scope(Trainee.all)
       @home_view = HomeView.new(@trainees)
       render(:home)
@@ -24,5 +28,11 @@ class PagesController < ApplicationController
 
   def guidance
     render(:guidance)
+  end
+
+private
+
+  def organisation_not_set?
+    current_user.organisation.nil? && !current_user.system_admin?
   end
 end
