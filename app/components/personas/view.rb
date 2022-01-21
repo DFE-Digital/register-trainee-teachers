80a1d5b95d4cfc2b45e5e3903f7ee1224c6198b6
Graham Pengelly <graham@three-tier.com>
2022-01-21 16:53:07 +0000

    [3508] Add multi org persona
    * Add a persona with a provider and a lead school
    * Update the personas component to render this

--- app/components/personas/view.rb
+++ app/components/personas/view.rb
@@ -9,5 +9,16 @@ module Personas
     def initialize(persona:)
       @persona = persona
     end
+
+    def association_strings
+      providers = persona.providers.map do |provider|
+        t("components.personas.view.provider_user", provider_name: provider.name)
+      end
+      lead_schools = persona.lead_schools.map do |lead_school|
+        t("components.personas.view.lead_school_user", lead_school_name: lead_school.name)
+      end
+
+      providers + lead_schools
+    end
   end
 end
