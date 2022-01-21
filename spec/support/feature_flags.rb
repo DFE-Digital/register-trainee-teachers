891d1191704fa5c5b4dc844e5703303e49ed93d1
Graham Pengelly <graham@three-tier.com>
2022-01-21 18:14:28 +0000

    [3508] Update feature flag spec helper
    * Stub configured feature flags before enabling/disabling
    
    Once a flag is stubbed then the existing code errors if an unexpected
    feature flag is checked within a spec. The existing 'enable' method
    disabled all flags before enabling the requested ones. Disable did not.
    
    Loop through the configured flags and stub those before
    enabling/disabling.

--- spec/support/feature_flags.rb
+++ spec/support/feature_flags.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
 
 def enable_features(*feature_keys)
-  allow(FeatureService).to receive(:enabled?).and_return(false)
+  stub_configured_features
   feature_keys.each do |feature_key|
     allow(FeatureService).to receive(:enabled?).with(feature_key).and_return(true)
   end
 end
 
 def disable_features(*feature_keys)
+  stub_configured_features
   feature_keys.each do |feature_key|
     allow(FeatureService).to receive(:enabled?).with(feature_key).and_return(false)
+  end
+end
+
+def stub_configured_features
+  flatten(Settings.features.to_h).each do |k, v|
+    allow(FeatureService).to receive(:enabled?).with(k).and_return(v)
+    allow(FeatureService).to receive(:enabled?).with(k.to_sym).and_return(v)
+  end
+end
+
+def flatten(hash_to_flatten)
+  hash_to_flatten.reduce({}) do |result, (k, v)|
+    flattened = v.is_a?(Hash) ? flatten(v).transform_keys { |k2| "#{k}.#{k2}" } : { k.to_s => v }
+    result.merge(flattened)
   end
 end
