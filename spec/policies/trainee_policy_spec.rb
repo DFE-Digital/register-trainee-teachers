ccaa413b4edbc3bee2eb136dc4287bcafaf9df8f
Graham Pengelly <graham@three-tier.com>
2022-01-27 12:07:18 +0000

    [3508] Update TraineePolicy
    * Add support for lead school user context
    * Permissions are now either read? or write?
    
    Lead school users only have read access, provider users (and system
    admins) have read and write.
    
    The spec is a bit tricky for this as we've mixed the concerns a bit by
    adding the trainee state transition logic into the policy. Add an
    exclusion to rubocop-todo as it didn't like the specs. Not sure if it
    understands the `permission` blocks as describe/context.

--- spec/policies/trainee_policy_spec.rb
+++ spec/policies/trainee_policy_spec.rb
@@ -5,25 +5,206 @@ require "rails_helper"
 describe TraineePolicy do
   let(:system_admin_user) { create(:user, :system_admin) }
   let(:provider) { create(:provider) }
-  let(:provider_user) { create(:user, providers: [provider]) }
-  let(:other_provider_user) { create(:user) }
-  let(:trainee) { create(:trainee, provider: provider) }
-  let(:deferred_trainee) { create(:trainee, :deferred, provider: provider) }
+  let(:other_provider) { create(:provider) }
+  let(:provider_user) { user_with_organisation(create(:user, providers: [provider]), provider) }
+  let(:other_provider_user) { user_with_organisation(create(:user, providers: [other_provider]), other_provider) }
+  let(:lead_school) { create(:school, :lead) }
+  let(:lead_school_user) { user_with_organisation(create(:user, providers: []), lead_school) }
+  let(:other_lead_school_user) { user_with_organisation(create(:user, providers: []), create(:school, :lead)) }
+
+  let(:provider_trainee) { create(:trainee, provider: provider) }
+  let(:lead_school_trainee) { create(:trainee, lead_school: lead_school) }
 
   subject { described_class }
 
-  permissions :show?, :create?, :update? do
-    it { is_expected.to permit(provider_user, trainee) }
-    it { is_expected.to permit(system_admin_user, trainee) }
-    it { is_expected.not_to permit(other_provider_user, trainee) }
+  def user_with_organisation(user, organisation)
+    UserWithOrganisationContext.new(user: user, session: {}).tap do |user_with_org|
+      allow(user_with_org).to receive(:organisation).and_return(organisation)
+    end
+  end
+
+  permissions :show?, :index? do
+    it { is_expected.to permit(provider_user, provider_trainee) }
+    it { is_expected.to permit(lead_school_user, lead_school_trainee) }
+
+    it { is_expected.to permit(system_admin_user, provider_trainee) }
+    it { is_expected.to permit(system_admin_user, lead_school_trainee) }
+
+    it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+  end
+
+  permissions :create?, :update?, :edit?, :new?, :destroy?, :confirm?, :recommended? do
+    it { is_expected.to permit(provider_user, provider_trainee) }
+    it { is_expected.not_to permit(lead_school_user, lead_school_trainee) }
+
+    it { is_expected.to permit(system_admin_user, provider_trainee) }
+    it { is_expected.to permit(system_admin_user, lead_school_trainee) }
+
+    it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+  end
+
+  permissions :withdraw? do
+    context "when trainee is deferred?" do
+      before do
+        allow(provider_trainee).to receive(:deferred?).and_return(true)
+      end
+
+      it { is_expected.to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.to permit(system_admin_user, provider_trainee) }
+    end
+
+    context "when trainee is submitted_for_trn?" do
+      before do
+        allow(provider_trainee).to receive(:submitted_for_trn?).and_return(true)
+      end
+
+      it { is_expected.to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.to permit(system_admin_user, provider_trainee) }
+    end
+
+    context "when trainee is trn_received?" do
+      before do
+        allow(provider_trainee).to receive(:trn_received?).and_return(true)
+      end
+
+      it { is_expected.to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.to permit(system_admin_user, provider_trainee) }
+    end
+
+    context "when trainee is recommended" do
+      before do
+        allow(provider_trainee).to receive(:recommended_for_award?).and_return(true)
+      end
+
+      it { is_expected.not_to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.not_to permit(system_admin_user, provider_trainee) }
+    end
+  end
+
+  permissions :defer? do
+    context "when trainee is deferred?" do
+      before do
+        allow(provider_trainee).to receive(:deferred?).and_return(true)
+      end
+
+      it { is_expected.not_to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.not_to permit(system_admin_user, provider_trainee) }
+    end
+
+    context "when trainee is submitted_for_trn?" do
+      before do
+        allow(provider_trainee).to receive(:submitted_for_trn?).and_return(true)
+      end
+
+      it { is_expected.to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.to permit(system_admin_user, provider_trainee) }
+    end
+
+    context "when trainee is trn_received?" do
+      before do
+        allow(provider_trainee).to receive(:trn_received?).and_return(true)
+      end
+
+      it { is_expected.to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.to permit(system_admin_user, provider_trainee) }
+    end
+
+    context "when trainee is recommended" do
+      before do
+        allow(provider_trainee).to receive(:recommended_for_award?).and_return(true)
+      end
+
+      it { is_expected.not_to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.not_to permit(system_admin_user, provider_trainee) }
+    end
   end
 
   permissions :reinstate? do
-    it { is_expected.to permit(provider_user, deferred_trainee) }
-    it { is_expected.to permit(system_admin_user, deferred_trainee) }
-    it { is_expected.not_to permit(other_provider_user, deferred_trainee) }
-    it { is_expected.not_to permit(provider_user, trainee) }
-    it { is_expected.not_to permit(system_admin_user, trainee) }
+    context "when trainee is deferred?" do
+      before do
+        allow(provider_trainee).to receive(:deferred?).and_return(true)
+      end
+
+      it { is_expected.to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.to permit(system_admin_user, provider_trainee) }
+    end
+
+    context "when trainee is recommended" do
+      before do
+        allow(provider_trainee).to receive(:recommended_for_award?).and_return(true)
+      end
+
+      it { is_expected.not_to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.not_to permit(system_admin_user, provider_trainee) }
+    end
+  end
+
+  permissions :show_recommended? do
+    context "when trainee is recommended" do
+      before do
+        allow(provider_trainee).to receive(:recommended_for_award?).and_return(true)
+      end
+
+      it { is_expected.to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.to permit(system_admin_user, provider_trainee) }
+    end
+
+    context "when trainee is deferred?" do
+      before do
+        allow(provider_trainee).to receive(:deferred?).and_return(true)
+      end
+
+      it { is_expected.not_to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.not_to permit(system_admin_user, provider_trainee) }
+    end
+  end
+
+  permissions :recommend_for_award? do
+    context "when trainee is trn_received?" do
+      before do
+        allow(provider_trainee).to receive(:trn_received?).and_return(true)
+      end
+
+      it { is_expected.to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.to permit(system_admin_user, provider_trainee) }
+    end
+
+    context "when trainee is recommended_for_award?" do
+      before do
+        allow(provider_trainee).to receive(:recommended_for_award?).and_return(true)
+      end
+
+      it { is_expected.not_to permit(provider_user, provider_trainee) }
+      it { is_expected.not_to permit(other_provider_user, provider_trainee) }
+      it { is_expected.not_to permit(lead_school_user, provider_trainee) }
+      it { is_expected.not_to permit(system_admin_user, provider_trainee) }
+    end
   end
 
   describe TraineePolicy::Scope do
