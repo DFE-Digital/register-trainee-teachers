5d91c06958182fe5508f73d2ee342e95f5f1a0e9
John Henderson <john@logixworks.co.uk>
2022-03-01 16:53:37 +0000

    [3744] Allow HESA import job to be run in the console using custom reference and date

--- app/jobs/hesa/retrieve_collection_job.rb
+++ app/jobs/hesa/retrieve_collection_job.rb
@@ -2,8 +2,13 @@
 
 module Hesa
   class RetrieveCollectionJob < ApplicationJob
-    def perform
-      return unless FeatureService.enabled?(:sync_from_hesa)
+    def perform(updates_since: HesaCollectionRequest.next_from_date,
+                collection_reference: Settings.hesa.current_collection_reference,
+                sync_from_hesa: FeatureService.enabled?(:sync_from_hesa))
+      @updates_since = updates_since
+      @collection_reference = collection_reference
+
+      return unless sync_from_hesa
 
       request_time = Time.zone.now
       xml_response = Hesa::Client.get(url: url)
@@ -22,22 +27,14 @@ module Hesa
     def save_hesa_request(xml_response, request_time)
       HesaCollectionRequest.create(
         requested_at: request_time,
-        collection_reference: collection_reference,
-        updates_since: updates_since,
+        collection_reference: @collection_reference,
+        updates_since: @updates_since,
         response_body: xml_response,
       )
     end
 
     def url
-      "#{Settings.hesa.collection_base_url}/#{collection_reference}/#{updates_since}"
-    end
-
-    def collection_reference
-      Settings.hesa.current_collection_reference
-    end
-
-    def updates_since
-      @updates_since ||= HesaCollectionRequest.next_from_date
+      "#{Settings.hesa.collection_base_url}/#{@collection_reference}/#{@updates_since}"
     end
   end
 end
