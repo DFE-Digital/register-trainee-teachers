d08f9948e6f07bf03168d7a47b486a88dc474d7c
Misha Gorodnitzky <misaka@pobox.com>
2022-03-18 00:13:21 +0000

    [3833] да товариш rubocop, слушаю

--- spec/requests/traineeteacherportal_requests_spec.rb
+++ spec/requests/traineeteacherportal_requests_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe "requests to traineeteacherportal-test.education.gov.uk" do
   it "redirects / to register" do
-    get "/", headers: { host: "traineeteacherportal-test.education.gov.uk"}
+    get "/", headers: { host: "traineeteacherportal-test.education.gov.uk" }
 
     expect(response).to redirect_to("https://www.register-trainee-teachers.education.gov.uk/start_traineeteacherportal")
   end
@@ -13,7 +13,7 @@ RSpec.describe "requests to traineeteacherportal-test.education.gov.uk" do
     # Example URL:
     #
     # https://traineeteacherportal.education.gov.uk/default.aspx#!/app/partials/dfeAppHome&id=0C1A66E4-0B9A-4C83-A0CB-4F902E76496E
-    get "/default.aspx#!/app/partials/dfeAppHome&id=0C1A66E4-0B9A-4C83-A0CB-4F902E76496E", headers: { host: "traineeteacherportal-test.education.gov.uk"}
+    get "/default.aspx#!/app/partials/dfeAppHome&id=0C1A66E4-0B9A-4C83-A0CB-4F902E76496E", headers: { host: "traineeteacherportal-test.education.gov.uk" }
 
     expect(response).to redirect_to("https://www.register-trainee-teachers.education.gov.uk/start_traineeteacherportal")
   end
