5744fd26407f95308aef6a97b996809633688ba8
Abigail McPhillips <abigail.mcphillips@gmail.com>
2022-03-23 14:05:03 +0000

    [3851] Add UKPRNs

--- db/data/20220323125945_add_missing_dttp_import_providers.rb
+++ db/data/20220323125945_add_missing_dttp_import_providers.rb
@@ -3,9 +3,9 @@
 class AddMissingDttpImportProviders < ActiveRecord::Migration[6.1]
   def up
     [
-      { name: "Consilium SCITT", dttp_id: "d470f34a-2887-e711-80d8-005056ac45bb", code: "1MS" },
-      { name: "Newcastle University", dttp_id: "ea70f34a-2887-e711-80d8-005056ac45bb", code: "N21" },
-      { name: "St Mary's University", dttp_id: "f670f34a-2887-e711-80d8-005056ac45bb", code: "S64" },
+      { ukprn: 10061209, name: "Consilium SCITT", dttp_id: "d470f34a-2887-e711-80d8-005056ac45bb", code: "1MS" },
+      { ukprn: 10007799, name: "Newcastle University", dttp_id: "ea70f34a-2887-e711-80d8-005056ac45bb", code: "N21" },
+      { ukprn: 10007843, name: "St Mary's University", dttp_id: "f670f34a-2887-e711-80d8-005056ac45bb", code: "S64" },
     ].each do |attributes|
       Provider.find_or_create_by(attributes)
     end
