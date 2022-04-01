fd4a2d5a4fba452385a00cb6919ceabc5044db88
Graham Pengelly <graham@three-tier.com>
2022-04-01 13:15:15 +0100

    [3905] Trainees::SetCohort
    Add `Trainees::SetCohort`
    
    Service to set the `Trainee#cohort` field based on the trainee state and
    start and end (or withdraw etc) dates.

---
+++ app/services/trainees/set_cohort.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module Trainees
+  class SetCohort
+    include ServicePattern
+
+    def initialize(trainee:)
+      @trainee = trainee
+    end
+
+    def call
+      return trainee if trainee_is_current? && trainee.current!
+      return trainee if trainee_is_future? && trainee.future!
+      return trainee if trainee_is_past? && trainee.past!
+
+      trainee
+    end
+
+  private
+
+    attr_reader :trainee
+
+    def trainee_is_current?
+      starts_in_the_current_academic_cycle? ||
+        withdrawn_in_the_current_cycle? ||
+        trainee.deferred?
+    end
+
+    def trainee_is_future?
+      trainee.itt_start_date > current_academic_cycle.end_date
+    end
+
+    def trainee_is_past?
+      trainee.itt_end_date < current_academic_cycle.start_date &&
+        (
+          awarded_before_the_current_cycle? ||
+          withdrawn_before_the_current_cycle?
+        )
+    end
+
+    def current_academic_cycle
+      @current_academic_cycle ||= AcademicCycle.current
+    end
+
+    def awarded_before_the_current_cycle?
+      trainee.awarded? && trainee.awarded_at < current_academic_cycle.start_date
+    end
+
+    def withdrawn_before_the_current_cycle?
+      trainee.withdrawn? && trainee.withdraw_date < current_academic_cycle.start_date
+    end
+
+    def starts_in_the_current_academic_cycle?
+      trainee.itt_start_date >= current_academic_cycle.start_date &&
+        trainee.itt_start_date <= current_academic_cycle.end_date
+    end
+
+    def withdrawn_in_the_current_cycle?
+      trainee.withdrawn? &&
+        trainee.withdraw_date >= current_academic_cycle.start_date
+    end
+  end
+end
