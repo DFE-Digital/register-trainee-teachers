0374b77de8f7a0d33b8af387c875f20fb4305f5c
Graham Pengelly <graham@three-tier.com>
2022-04-08 16:16:45 +0100

    [3976] Add Trainees::Update
    A service that:
    
    * updates the trainee
    * queues a Dqt::UpdateTaineeJob
    * queues a Trainees::SetCohortJob
    
    We have used after_commit callbacks for this but the cohort job causes a
    sidekiq loop as it requeues itself and this approach is more explicit
    and means we can update the trainee without queueing all of the jobs
    too.
    
    In some places in the app we set attributes on the trainee prior to
    update rather than passing params to update so support that too.

---
+++ app/services/trainees/update.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Trainees
+  class Update
+    include ServicePattern
+
+    def initialize(trainee:, params: {})
+      @trainee = trainee
+      @params = params
+    end
+
+    def call
+      trainee.update!(params)
+      Dqt::UpdateTraineeJob.perform_later(trainee)
+      Trainees::SetCohortJob.perform_later(trainee)
+      true
+    end
+
+  private
+
+    attr_reader :trainee, :params
+  end
+end
