fd3184ab66c422f2701d1fa3c2c0bd6906ccae2f
Hettie Street <hettie.street@digital.education.gov.uk>
2022-04-20 12:29:00 +0100

    [4001] Backfill provider accreditation_id field
    * Backfill provider accreditation_id from the dttp_accounts table
    * Link on dttp_id

---
+++ db/data/20220422144717_backfill_provider_accreditation_id.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class BackfillProviderAccreditationId < ActiveRecord::Migration[6.1]
+  def up
+    Provider.where(accreditation_id: nil).each do |provider|
+      dttp_account = Dttp::Account.find_by(dttp_id: provider.dttp_id)
+      provider.update(accreditation_id: dttp_account.accreditation_id) if dttp_account.present?
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
