1fa17da178a9d1374082311d8d25fbfb2413e361
Richard Pattinson <richie.pattinson@googlemail.com>
2022-04-29 12:32:01 +0100

    [3947] Backfill hesa metadata
    Ensure that dttp imports from hesa have attached metadata.

---
+++ app/lib/dttp/code_sets/itt_aims.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Dttp
+  module CodeSets
+    module IttAims
+      MAPPING = {
+        "27a0ae31-141e-e711-80c8-0050568902d3" => "Both professional status and academic award",
+        "25a0ae31-141e-e711-80c8-0050568902d3" => "Professional status only",
+      }.freeze
+    end
+  end
+end
