f26d2fd17f42b6f5f83b427f1efa1a48877b9ebe
Hettie Street <hettie.street@digital.education.gov.uk>
2022-05-10 20:10:27 +0100

    [4005] Add payment type booleans to model
    * Add custom booleans for tiered_bursary? and untiered_bursary? to TraineeSummaryRowAmount
    * untiered_bursary? is a regular bursary and has been named to distinguish from tiered_bursary
    * Booleans make it easier to retrieve the right data

--- spec/models/funding/trainee_summary_row_amount_spec.rb
+++ spec/models/funding/trainee_summary_row_amount_spec.rb
@@ -4,8 +4,35 @@ require "rails_helper"
 
 module Funding
   describe TraineeSummaryRowAmount do
+    let(:summary) { create(:trainee_summary, :for_provider) }
+    let(:summary_row) { create(:trainee_summary_row, trainee_summary: summary) }
+
     describe "associations" do
       it { is_expected.to belong_to(:row) }
+    end
+
+    describe "untiered_bursary?" do
+      let(:summary_row_amount) { create(:trainee_summary_row_amount, :with_bursary, row: summary_row) }
+
+      subject { summary_row_amount.untiered_bursary? }
+
+      context "a bursary payment type exists" do
+        it "returns true" do
+          expect(subject).to be true
+        end
+      end
+    end
+
+    describe "tiered_bursary?" do
+      let(:summary_row_amount) { create(:trainee_summary_row_amount, :with_tiered_bursary, row: summary_row) }
+
+      subject { summary_row_amount.tiered_bursary? }
+
+      context "a tiered bursary payment type exists" do
+        it "returns true" do
+          expect(subject).to be true
+        end
+      end
     end
   end
 end
