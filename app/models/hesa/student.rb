cb4f997933473239684d8131857a19711509d90f
Graham Pengelly <graham@three-tier.com>
2022-05-18 17:05:50 +0100

    [xxx] Fix Trainee#hesa_student
    The association specified an inverse but the inverse doesn't exist so
    it's erroring.
    
    Add the inverse on Hesa::Student.

--- app/models/hesa/student.rb
+++ app/models/hesa/student.rb
@@ -3,5 +3,11 @@
 module Hesa
   class Student < ApplicationRecord
     self.table_name = "hesa_students"
+
+    belongs_to :trainee,
+               foreign_key: :hesa_id,
+               primary_key: :hesa_id,
+               inverse_of: :hesa_student,
+               optional: true
   end
 end
