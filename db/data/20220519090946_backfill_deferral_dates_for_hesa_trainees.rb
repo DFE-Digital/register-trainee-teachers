1f0091c3fd3066d76d8a09b4bb4bde8cac39de87
Abigail McPhillips <abigail.mcphillips@gmail.com>
2022-05-19 10:16:39 +0100

    [4117] Add backfill for adding deferral dates to deferred trainees

---
+++ db/data/20220519090946_backfill_deferral_dates_for_hesa_trainees.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class BackfillDeferralDatesForHesaTrainees < ActiveRecord::Migration[6.1]
+  def up
+    Trainee.joins(:hesa_student).where(state: "deferred", defer_date: nil).each do |trainee|
+      trainee.without_auditing do
+        trainee.update(defer_date: trainee.hesa_student.end_date)
+      end
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
