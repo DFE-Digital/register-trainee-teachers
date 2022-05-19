b1ad7976b2f3422aadc15fcb82702e4a3dfd83cc
Steve Hook <steve.hook@gmail.com>
2022-05-19 12:28:50 +0100

    Workaround for undefined `current_user`
    `current_user` is undefined (not just nil) in the component previews so
    we need to handle that just as the original code did.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,8 @@ module UsersHelper
   def lead_school_user?
     defined?(current_user) && current_user&.lead_school?
   end
+
+  def can_view_drafts?
+    defined?(current_user).present? && current_user.present? && UserPolicy.new(current_user, nil).drafts?
+  end
 end
