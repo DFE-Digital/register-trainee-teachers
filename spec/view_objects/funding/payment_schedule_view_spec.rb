1965aa32b9617d6d87c45b91f82db8c12579fb79
Graham Pengelly <graham@three-tier.com>
2022-05-30 18:11:19 +0100

    [4187] Fix payment schedule ordering
    Order rows by year, month
    
    November 2021
    December 2021
    January 2022
    February 2022
    
    This should happen in actual and predicted payments and the summaries.

--- spec/view_objects/funding/payment_schedule_view_spec.rb
+++ spec/view_objects/funding/payment_schedule_view_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 module Funding
   describe PaymentScheduleView do
-    let(:payment_schedule) { build(:payment_schedule, :for_provider, rows: payment_schedule_rows) }
+    let(:payment_schedule) { create(:payment_schedule, :for_provider, rows: payment_schedule_rows) }
 
     subject { described_class.new(payment_schedule: payment_schedule) }
 
@@ -12,23 +12,29 @@ module Funding
       let(:payment_schedule_rows) do
         [
           build(:payment_schedule_row, amounts: [
-            build(:payment_schedule_row_amount, month: 1, amount_in_pence: 100),
-            build(:payment_schedule_row_amount, month: 2, amount_in_pence: 200),
+            build(:payment_schedule_row_amount, month: 1, year: 2022, amount_in_pence: 100),
+            build(:payment_schedule_row_amount, month: 2, year: 2022, amount_in_pence: 200),
+            build(:payment_schedule_row_amount, month: 8, year: 2021, amount_in_pence: 50),
           ]),
         ]
       end
 
-      it "returns actual payments" do
+      it "returns actual payments ordered year, month" do
         expect(subject.actual_payments).to eq([
+          {
+            month: "August 2021",
+            total: "£0.50",
+            running_total: "£0.50",
+          },
           {
             month: "January 2022",
             total: "£1.00",
-            running_total: "£1.00",
+            running_total: "£1.50",
           },
           {
             month: "February 2022",
             total: "£2.00",
-            running_total: "£3.00",
+            running_total: "£3.50",
           },
         ])
       end
@@ -38,9 +44,9 @@ module Funding
       let(:payment_schedule_rows) do
         [
           build(:payment_schedule_row, amounts: [
-            build(:payment_schedule_row_amount, month: 1, amount_in_pence: 100),
-            build(:payment_schedule_row_amount, month: 2, amount_in_pence: 200, predicted: true),
-            build(:payment_schedule_row_amount, month: 3, amount_in_pence: 600, predicted: true),
+            build(:payment_schedule_row_amount, month: 1, year: 2022, amount_in_pence: 600, predicted: true),
+            build(:payment_schedule_row_amount, month: 11, year: 2021, amount_in_pence: 100),
+            build(:payment_schedule_row_amount, month: 12, year: 2021, amount_in_pence: 200, predicted: true),
           ]),
         ]
       end
@@ -48,12 +54,12 @@ module Funding
       it "only returns predicted payments" do
         expect(subject.predicted_payments).to eq([
           {
-            month: "February 2022",
+            month: "December 2021",
             total: "£2.00",
             running_total: "£2.00",
           },
           {
-            month: "March 2022",
+            month: "January 2022",
             total: "£6.00",
             running_total: "£8.00",
           },
@@ -62,17 +68,16 @@ module Funding
     end
 
     describe "#payment_breakdown" do
-      let(:current_year) { Time.zone.now.year }
       let(:payment_schedule_rows) do
         [
           build(:payment_schedule_row, description: "Payment Schedule 1", amounts: [
-            build(:payment_schedule_row_amount, month: 1, amount_in_pence: 200),
-            build(:payment_schedule_row_amount, month: 2, amount_in_pence: 500),
+            build(:payment_schedule_row_amount, month: 1, year: 2022, amount_in_pence: 200),
+            build(:payment_schedule_row_amount, month: 12, year: 2021, amount_in_pence: 500),
 
           ]),
           build(:payment_schedule_row, description: "Payment Schedule 2", amounts: [
-            build(:payment_schedule_row_amount, month: 1, amount_in_pence: 300),
-            build(:payment_schedule_row_amount, month: 2, amount_in_pence: 700),
+            build(:payment_schedule_row_amount, month: 1, year: 2022, amount_in_pence: 300),
+            build(:payment_schedule_row_amount, month: 12, year: 2021, amount_in_pence: 700),
           ]),
         ]
       end
@@ -80,21 +85,21 @@ module Funding
       it "returns a list of monthly breakdown objects grouped by month and year" do
         expect(subject.payment_breakdown.map(&:to_h)).to eq([
           {
-            title: "January #{current_year}",
+            title: "December 2021",
             rows: [
-              { description: "Payment Schedule 1", amount: "£2.00", running_total: "£2.00" },
-              { description: "Payment Schedule 2", amount: "£3.00", running_total: "£3.00" },
-            ],
-            total_amount: "£5.00",
-            total_running_total: "£5.00",
-          },
-          {
-            title: "February #{current_year}",
-            rows: [
-              { description: "Payment Schedule 1", amount: "£5.00", running_total: "£7.00" },
-              { description: "Payment Schedule 2", amount: "£7.00", running_total: "£10.00" },
+              { description: "Payment Schedule 1", amount: "£5.00", running_total: "£5.00" },
+              { description: "Payment Schedule 2", amount: "£7.00", running_total: "£7.00" },
             ],
             total_amount: "£12.00",
+            total_running_total: "£12.00",
+          },
+          {
+            title: "January 2022",
+            rows: [
+              { description: "Payment Schedule 1", amount: "£2.00", running_total: "£7.00" },
+              { description: "Payment Schedule 2", amount: "£3.00", running_total: "£10.00" },
+            ],
+            total_amount: "£5.00",
             total_running_total: "£17.00",
           },
         ])
