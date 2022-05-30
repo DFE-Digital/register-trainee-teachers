c20f625893d601deab2d785021eb166a78e90608
Abigail McPhillips <abigail.mcphillips@gmail.com>
2022-05-30 16:34:59 +0100

    [4180] Create payment schedule amounts in correct year

--- spec/factories/funding/payment_schedule_rows.rb
+++ spec/factories/funding/payment_schedule_rows.rb
@@ -10,9 +10,15 @@ FactoryBot.define do
       amounts do
         month_order = Funding::PayablePaymentSchedulesImporter::MONTH_ORDER
         current_month_index = month_order.index(Time.zone.today.month)
+        academic_cycle = AcademicCycle.current
 
         month_order.map.with_index do |month, index|
-          build(:payment_schedule_row_amount, month: month, predicted: index > current_month_index)
+          build(
+            :payment_schedule_row_amount,
+            month: month,
+            year: (1..7).include?(month) ? academic_cycle.end_date.year : academic_cycle.start_date.year,
+            predicted: index > current_month_index,
+          )
         end
       end
     end
