2dd051b6560230d0e59a8baa1e3190dd4336c7a0
Graham Pengelly <graham@three-tier.com>
2022-06-16 15:56:48 +0100

    [4270] Skip funding rows without amounts
    * Skip rows without amounts when building the funding trainee summary
      CSV files
    
    These rows error and we don't want to display them anyway.

--- spec/services/exports/funding_trainee_summary_data_spec.rb
+++ spec/services/exports/funding_trainee_summary_data_spec.rb
@@ -8,6 +8,7 @@ module Exports
     let!(:trainee_summary) { create(:trainee_summary, :for_school) }
     let!(:trainee_summary_row) { create(:trainee_summary_row, trainee_summary: trainee_summary, lead_school_urn: lead_school.urn) }
     let!(:tiered_bursary_amount) { create(:trainee_summary_row_amount, :with_tiered_bursary, row: trainee_summary_row) }
+    let!(:empty_trainee_summary_row) { create(:trainee_summary_row, trainee_summary: trainee_summary) }
 
     before do
       create(:academic_cycle, :current)
@@ -35,6 +36,11 @@ module Exports
 
       it "sets the correct row values" do
         expect(subject.csv).to include(expected_output.values.join(","))
+      end
+
+      it "skips rows without amounts" do
+        expected_csv_with_headers_row_count = 2
+        expect(CSV.parse(subject.csv).length).to eq(expected_csv_with_headers_row_count)
       end
     end
   end
