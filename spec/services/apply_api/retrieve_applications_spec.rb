uk-gov-mirror/DFE-Digital.register-trainee-teachers

[4243] Handle pagination in ApplyApi::RetrieveApplications

spec/services/apply_api/retrieve_applications_spec.rb
@@ -6,11 +6,11 @@ module ApplyApi
   describe RetrieveApplications do
     let(:changed_since) { nil }
     let(:recruitment_cycle_year) { Settings.current_recruitment_cycle_year }
-    let(:expected_path) { "/applications?recruitment_cycle_year=#{recruitment_cycle_year}" }
+    let(:expected_path) { "/applications?recruitment_cycle_year=#{recruitment_cycle_year}&page=1" }
     let(:http_response) { { status: status, body: ApiStubs::ApplyApi.applications } }
     let(:request_url) { "#{Settings.apply_api.base_url}#{expected_path}" }
 
-    subject { described_class.call(changed_since: changed_since, recruitment_cycle_year: recruitment_cycle_year) }
+    subject { described_class.call(changed_since: changed_since, recruitment_cycle_year: recruitment_cycle_year).to_a }
 
     before do
       stub_request(:get, request_url).to_return(http_response)
@@ -22,7 +22,7 @@ module ApplyApi
 
         context "when no 'changed_since' is provided" do
           it "logs the successful request" do
-            expect(subject).to change { ApplyApplicationSyncRequest.count }.by(1)
+            expect { subject }.to change { ApplyApplicationSyncRequest.count }.by(1)
           end
 
           it "returns all the Apply applications" do
@@ -33,14 +33,60 @@ module ApplyApi
             let(:http_response) { { status: status, body: { data: [] }.to_json } }
 
             it "returns empty array" do
-              expect(subject).to eq []
+              expect(subject.to_a).to eq([])
             end
+          end
+        end
+
+        context "when there are multiple pages of results" do
+          let(:http_response_page_1) {
+            {
+              status: status,
+              body: ApiStubs::ApplyApi.applications_page_1,
+              headers: {
+                "Current-Page" => "1",
+                "Page-Items" => "2",
+                "Total-Pages" => "2",
+                "Total-Count" => "3",
+              },
+            }
+          }
+          let(:http_response_page_2) {
+            {
+              status: status,
+              body: ApiStubs::ApplyApi.applications_page_2,
+              headers: {
+                "Current-Page" => "2",
+                "Page-Items" => "1",
+                "Total-Pages" => "2",
+                "Total-Count" => "3",
+              },
+            }
+          }
+          let(:expected_path_page_2) { "/applications?recruitment_cycle_year=#{recruitment_cycle_year}&page=2" }
+          let(:request_url_page_2) { "#{Settings.apply_api.base_url}#{expected_path_page_2}" }
+
+          before do
+            stub_request(:get, request_url).to_return(http_response_page_1)
+            stub_request(:get, request_url_page_2).to_return(http_response_page_2)
+          end
+
+          it "logs the successful request" do
+            expect { subject }.to change { ApplyApplicationSyncRequest.count }.by(1)
+          end
+
+          it "returns all the Apply applications" do
+            expect(subject).to contain_exactly(
+              ApiStubs::ApplyApi.application(id: "3772"),
+              ApiStubs::ApplyApi.application(id: "3773"),
+              ApiStubs::ApplyApi.application(id: "3774"),
+            )
           end
         end
 
         context "when a 'changed_at' is provided" do
           let(:changed_since) { Time.zone.now }
-          let(:expected_query) { { recruitment_cycle_year: recruitment_cycle_year, changed_since: changed_since.utc.iso8601 }.to_query }
+          let(:expected_query) { { recruitment_cycle_year: recruitment_cycle_year, changed_since: changed_since.utc.iso8601, page: "1" }.to_query }
           let(:expected_path) { "/applications?#{expected_query}" }
 
           it "includes the changed_at param in the request" do

DFE-Digital/register-trainee-teachers

spec/services/apply_api/retrieve_applications_spec.rb
@@ -6,11 +6,11 @@ module ApplyApi
   describe RetrieveApplications do
     let(:changed_since) { nil }
     let(:recruitment_cycle_year) { Settings.current_recruitment_cycle_year }
-    let(:expected_path) { "/applications?recruitment_cycle_year=#{recruitment_cycle_year}" }
+    let(:expected_path) { "/applications?recruitment_cycle_year=#{recruitment_cycle_year}&page=1" }
     let(:http_response) { { status: status, body: ApiStubs::ApplyApi.applications } }
     let(:request_url) { "#{Settings.apply_api.base_url}#{expected_path}" }
 
-    subject { described_class.call(changed_since: changed_since, recruitment_cycle_year: recruitment_cycle_year) }
+    subject { described_class.call(changed_since: changed_since, recruitment_cycle_year: recruitment_cycle_year).to_a }
 
     before do
       stub_request(:get, request_url).to_return(http_response)
@@ -33,14 +33,60 @@ module ApplyApi
             let(:http_response) { { status: status, body: { data: [] }.to_json } }
 
             it "returns empty array" do
-              expect(subject).to eq []
+              expect(subject.to_a).to eq([])
             end
+          end
+        end
+
+        context "when there are multiple pages of results" do
+          let(:http_response_page1) {
+            {
+              status: status,
+              body: ApiStubs::ApplyApi.applications_page1,
+              headers: {
+                "Current-Page" => "1",
+                "Page-Items" => "2",
+                "Total-Pages" => "2",
+                "Total-Count" => "3",
+              },
+            }
+          }
+          let(:http_response_page2) {
+            {
+              status: status,
+              body: ApiStubs::ApplyApi.applications_page2,
+              headers: {
+                "Current-Page" => "2",
+                "Page-Items" => "1",
+                "Total-Pages" => "2",
+                "Total-Count" => "3",
+              },
+            }
+          }
+          let(:expected_path_page2) { "/applications?recruitment_cycle_year=#{recruitment_cycle_year}&page=2" }
+          let(:request_url_page2) { "#{Settings.apply_api.base_url}#{expected_path_page2}" }
+
+          before do
+            stub_request(:get, request_url).to_return(http_response_page1)
+            stub_request(:get, request_url_page2).to_return(http_response_page2)
+          end
+
+          it "logs the successful request" do
+            expect { subject }.to change { ApplyApplicationSyncRequest.count }.by(1)
+          end
+
+          it "returns all the Apply applications" do
+            expect(subject).to contain_exactly(
+              ApiStubs::ApplyApi.application(id: "3772"),
+              ApiStubs::ApplyApi.application(id: "3773"),
+              ApiStubs::ApplyApi.application(id: "3774"),
+            )
           end
         end
 
         context "when a 'changed_at' is provided" do
           let(:changed_since) { Time.zone.now }
-          let(:expected_query) { { recruitment_cycle_year: recruitment_cycle_year, changed_since: changed_since.utc.iso8601 }.to_query }
+          let(:expected_query) { { recruitment_cycle_year: recruitment_cycle_year, changed_since: changed_since.utc.iso8601, page: "1" }.to_query }
           let(:expected_path) { "/applications?#{expected_query}" }
 
           it "includes the changed_at param in the request" do