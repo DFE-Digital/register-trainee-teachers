88f2d35a62ee144f9225e7629361ecc6e59f80ba
Abigail McPhillips <abigail.mcphillips@gmail.com>
2022-06-23 16:34:12 +0100

    [4281] Backfill missing institution UUIDs for remaining degrees
    - These degrees were still missing institution UUIDs after we ran
      our big backfill based on HESA codes, names and name synonyms
    - I've manually pulled the UUIDs from the list in the dfe-reference-data
      gem

---
+++ db/data/20220623151318_backfill_missing_institution_uuids.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class BackfillMissingInstitutionUuids < ActiveRecord::Migration[6.1]
+  def up
+    institutions = {
+      "The Queen's University of Belfast" => "a7db7129-7042-e811-80ff-3863bb3640b8",
+      "The University of Wales (central functions)" => "6a228041-7042-e811-80ff-3863bb3640b8",
+      "Ravensbourne" => "4ff3791d-7042-e811-80ff-3863bb3640b8",
+      "St George's Hospital Medical School" => "94407223-7042-e811-80ff-3863bb3640b8",
+      "Leeds College of Music" => "733e182c-1425-ec11-b6e6-000d3adf095a",
+    }
+
+    institutions.each do |institution, uuid|
+      Degree.where(institution: institution).update_all(institution_uuid: uuid)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
