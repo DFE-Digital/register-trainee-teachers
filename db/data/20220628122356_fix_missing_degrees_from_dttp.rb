e960c66e8ecf6b1678b9daf02094b030b3972314
Steve Hook <steve.hook@gmail.com>
2022-06-28 15:09:10 +0100

    Add data migration for unimported degrees from DTTP
    This data migration fetches a list of trainees that do not have an
    associated `Degree` record but do have importable DTTP degrees. It then
    attempts to run the `Degrees::CreateFromDttp` service on each trainee
    found.

---
+++ db/data/20220628122356_fix_missing_degrees_from_dttp.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class FixMissingDegreesFromDttp < ActiveRecord::Migration[6.1]
+  def up
+    trainees_with_missing_degrees =
+      Trainee
+      .joins(dttp_trainee: [:degree_qualifications])
+      .where("dttp_degree_qualifications.state": 0)
+      .where(
+        "NOT EXISTS (:degrees)",
+        degrees: Degree.select(1).where("degrees.trainee_id = trainees.id"),
+      )
+
+    trainees_with_missing_degrees.find_each do |trainee|
+      Degrees::CreateFromDttp.call(trainee: trainee)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
