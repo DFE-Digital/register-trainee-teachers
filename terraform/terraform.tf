ec27c55befd19e2254dde6a80743017e403d30ce
Stephen Neal <smneal@users.noreply.github.com>
2022-07-07 16:30:24 +0100

    Update azurerm provider to 3.12.0
    Update the azurerm provider so we can maintain
    compatibility and include latest bug fixes.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.45.1"
+      version = "3.12.0"
     }
     cloudfoundry = {
       source  = "cloudfoundry-community/cloudfoundry"
@@ -15,12 +15,12 @@ terraform {
       version = "1.0.1"
     }
   }
-  backend azurerm {
+  backend "azurerm" {
 
   }
 }
 
-provider cloudfoundry {
+provider "cloudfoundry" {
   api_url           = local.paas_api_url
   user              = var.paas_sso_passcode == "" ? local.infra_secrets.CF_USER : null
   password          = var.paas_sso_passcode == "" ? local.infra_secrets.CF_PASSWORD : null
@@ -29,7 +29,7 @@ provider cloudfoundry {
 }
 
 
-provider azurerm {
+provider "azurerm" {
   features {}
 
   skip_provider_registration = true
@@ -40,38 +40,38 @@ provider azurerm {
 }
 
 
-module paas {
+module "paas" {
   source = "./modules/paas"
 
-  app_environment       = var.paas_app_environment
-  app_docker_image      = var.paas_app_docker_image
-  app_start_timeout     = var.paas_app_start_timeout
-  postgres_service_plan = var.paas_postgres_service_plan
-  redis_service_plan    = var.paas_redis_service_plan
-  space_name            = var.paas_space_name
-  app_name              = var.paas_app_name
-  web_app_hostname      = var.paas_web_app_hostname
-  dttp_portal           = var.paas_dttp_portal
-  deployment_strategy   = var.paas_deployment_strategy
-  web_app_instances     = var.paas_web_app_instances
-  web_app_memory        = var.paas_web_app_memory
-  worker_app_instances  = var.paas_worker_app_instances
-  worker_app_memory     = var.paas_worker_app_memory
-  log_url               = local.infra_secrets.LOGSTASH_URL
-  app_secrets_variable  = local.app_secrets
-  app_config_variable   = local.app_config
-  worker_app_stopped    = var.paas_worker_app_stopped
-  restore_from_db_guid      = var.paas_restore_from_db_guid
+  app_environment                = var.paas_app_environment
+  app_docker_image               = var.paas_app_docker_image
+  app_start_timeout              = var.paas_app_start_timeout
+  postgres_service_plan          = var.paas_postgres_service_plan
+  redis_service_plan             = var.paas_redis_service_plan
+  space_name                     = var.paas_space_name
+  app_name                       = var.paas_app_name
+  web_app_hostname               = var.paas_web_app_hostname
+  dttp_portal                    = var.paas_dttp_portal
+  deployment_strategy            = var.paas_deployment_strategy
+  web_app_instances              = var.paas_web_app_instances
+  web_app_memory                 = var.paas_web_app_memory
+  worker_app_instances           = var.paas_worker_app_instances
+  worker_app_memory              = var.paas_worker_app_memory
+  log_url                        = local.infra_secrets.LOGSTASH_URL
+  app_secrets_variable           = local.app_secrets
+  app_config_variable            = local.app_config
+  worker_app_stopped             = var.paas_worker_app_stopped
+  restore_from_db_guid           = var.paas_restore_from_db_guid
   db_backup_before_point_in_time = var.paas_db_backup_before_point_in_time
 }
 
 #authenticate into provider
-provider statuscake {
+provider "statuscake" {
   username = local.infra_secrets.STATUSCAKE_USERNAME
   apikey   = local.infra_secrets.STATUSCAKE_PASSWORD
 }
 # interface into statusCake module
-module statuscake {
+module "statuscake" {
   source = "./modules/statuscake"
   alerts = var.statuscake_alerts
 }
