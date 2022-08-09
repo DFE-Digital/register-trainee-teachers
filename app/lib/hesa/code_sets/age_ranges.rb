0d29c97b6f87b4490e8f35ae386e852f07629d91
Abigail McPhillips <abigail.mcphillips@gmail.com>
2022-08-09 15:00:27 +0100

    [4435] Update HESA codes for age ranges

--- app/lib/hesa/code_sets/age_ranges.rb
+++ app/lib/hesa/code_sets/age_ranges.rb
@@ -3,21 +3,21 @@
 module Hesa
   module CodeSets
     module AgeRanges
-      # https://www.hesa.ac.uk/collection/c21053/xml/c21053/c21053codelists.xsd
-      # https://www.hesa.ac.uk/collection/c21053/e/ittphsc
+      # https://www.hesa.ac.uk/collection/c22053/xml/c21053/c21053codelists.xsd
+      # https://www.hesa.ac.uk/collection/c22053/e/ittphsc
       MAPPING = {
-        # "31" => "Further education",
-        # "49" => "Other",
-        "71" => AgeRange::THREE_TO_SEVEN,
-        "73" => AgeRange::THREE_TO_NINE,
-        "74" => AgeRange::THREE_TO_ELEVEN,
-        "75" => AgeRange::FIVE_TO_NINE,
-        "76" => AgeRange::FIVE_TO_ELEVEN,
-        "77" => AgeRange::SEVEN_TO_ELEVEN,
-        "78" => AgeRange::SEVEN_TO_FOURTEEN,
-        "79" => AgeRange::NINE_TO_FOURTEEN,
-        "80" => AgeRange::ELEVEN_TO_SIXTEEN,
-        "81" => AgeRange::ELEVEN_TO_NINETEEN,
+        "13909"	=> AgeRange::THREE_TO_SEVEN,
+        "13911"	=> AgeRange::THREE_TO_NINE,
+        "13912"	=> AgeRange::THREE_TO_ELEVEN,
+        "13913"	=> AgeRange::FIVE_TO_NINE,
+        "13914"	=> AgeRange::FIVE_TO_ELEVEN,
+        "13915"	=> AgeRange::SEVEN_TO_ELEVEN,
+        "13916"	=> AgeRange::SEVEN_TO_FOURTEEN,
+        "13917" => AgeRange::NINE_TO_FOURTEEN,
+        "13918"	=> AgeRange::ELEVEN_TO_SIXTEEN,
+        "13919"	=> AgeRange::ELEVEN_TO_NINETEEN,
+        # "99803"	=> "Teacher training qualification: Other"
+        # "99801"	=> "Teacher training qualification: Further education/Higher education"
       }.freeze
     end
   end
