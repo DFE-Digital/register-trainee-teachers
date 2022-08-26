3f4c01a4c528af15ed9eba0e408be763f53fb787
Abigail McPhillips <abigail.mcphillips@gmail.com>
2022-08-26 12:29:43 +0100

    [xxx] Remove the limit on the trainees we're syncing states for from DQT

--- app/jobs/dqt/sync_states_job.rb
+++ app/jobs/dqt/sync_states_job.rb
@@ -13,7 +13,6 @@ module Dqt
       trainees = Trainee.trn_received
                         .imported_from_hesa
                         .where(dqt_trn_validation_clause)
-                        .limit(Settings.dqt.trainee_sync_state_limit)
 
       trainees.find_in_batches(batch_size: batch_size).with_index do |group, batch|
         Dqt::SyncStatesBatchJob.set(wait: interval * batch).perform_later(group.pluck(:id))
