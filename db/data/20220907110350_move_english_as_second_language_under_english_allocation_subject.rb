994c23bd2577f4d55edb2ad224e1acce6a36c43f
John Henderson <john@logixworks.co.uk>
2022-09-07 12:27:42 +0100

    [4621] Remove 'English as a second or other language' from modern languages
    The specialism 'English as a second language' should also have the allocation subject of 'English', not 'modern languages' - this was a mistake.

---
+++ db/data/20220907110350_move_english_as_second_language_under_english_allocation_subject.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class MoveEnglishAsSecondLanguageUnderEnglishAllocationSubject < ActiveRecord::Migration[6.1]
+  def up
+    AllocationSubject.find_by(name: AllocationSubjects::ENGLISH).tap do |allocation_subject|
+      subject_specialism = SubjectSpecialism.find_by(name: PublishSubjects::ENGLISH_AS_SECOND_LANGUAGE)
+      subject_specialism.update(allocation_subject: allocation_subject)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
