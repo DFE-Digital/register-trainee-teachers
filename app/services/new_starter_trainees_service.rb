94b5c0919883a8db6c0d60af8a9e65a6fc20ec94
John Henderson <john@logixworks.co.uk>
2022-09-08 11:24:36 +0100

    [4634] Exclude HESA TRNData trainees from new starter report

--- app/services/new_starter_trainees_service.rb
+++ app/services/new_starter_trainees_service.rb
@@ -6,10 +6,12 @@ class NewStarterTraineesService
   end
 
   def call
-    Trainee.where("itt_start_date <= :date or commencement_date <= :date", date: census_date)
-           .or(Trainee.where(commencement_date: nil))
-           .where(start_academic_cycle_id: AcademicCycle.current)
-           .where.not(state: 0)
+    trainees = Trainee.where("itt_start_date <= :date or commencement_date <= :date", date: census_date)
+                      .or(Trainee.where(commencement_date: nil))
+                      .where(start_academic_cycle_id: AcademicCycle.current)
+                      .not_draft
+
+    Trainees::Filter.call(trainees: trainees, filters: nil)
   end
 
   attr_reader :census_date
