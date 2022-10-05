c1047968a1b35d3f50e6a463030cdddf876e87b8
John Henderson <john@logixworks.co.uk>
2022-10-05 15:17:11 +0100

    [4711] Data migration to import Apply applications no longer seen as duplicates

---
+++ db/data/20221005141340_import_apply_applications_no_longer_seen_as_duplicates.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class ImportApplyApplicationsNoLongerSeenAsDuplicates < ActiveRecord::Migration[6.1]
+  def up
+    ApplyApplication.non_importable_duplicate.each do |application|
+      next unless application.provider && application.course
+
+      Trainees::CreateFromApply.call(application: application)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
