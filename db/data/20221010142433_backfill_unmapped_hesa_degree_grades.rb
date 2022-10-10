c6bc611745a70fdfaf401fa302c3c90a02fcd347
John Henderson <john@logixworks.co.uk>
2022-10-10 16:55:24 +0100

    [4764] Data migration to backfill unmapped grades from HESA degrees

---
+++ db/data/20221010142433_backfill_unmapped_hesa_degree_grades.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class BackfillUnmappedHesaDegreeGrades < ActiveRecord::Migration[6.1]
+  def up
+    from_date = Settings.hesa.current_collection_start_date
+    collection_reference = Settings.hesa.current_collection_reference
+    url = "#{Settings.hesa.collection_base_url}/#{collection_reference}/#{from_date}"
+    xml_response = Hesa::Client.get(url: url)
+
+    Nokogiri::XML::Reader(xml_response).each do |node|
+      next unless node.name == "Student" && node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
+
+      student_node = Nokogiri::XML(node.outer_xml).at("./Student")
+      hesa_trainee = Hesa::Parsers::IttRecord.to_attributes(student_node: student_node)
+
+      Degree.without_auditing do
+        trainee = Trainee.find_by(hesa_id: hesa_trainee[:hesa_id])
+        if trainee
+          trainee.degrees.delete_all
+          Degrees::CreateFromHesa.call(trainee: trainee, hesa_degrees: hesa_trainee[:degrees])
+        end
+      end
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
