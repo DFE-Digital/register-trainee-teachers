5085f8cc1bbacdb6dbb8f3f6a5c0ac7b88636629
David Georgiou <david.georgiou@gmail.com>
2022-10-10 11:38:56 +0100

    [4758] - update analytics.yml

--- lib/tasks/data_migrations.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-namespace :data_migrations do
-  desc "backfill hesa_trn_submissions for trainees"
-  task backfill_hesa_trn_submissions: :environment do
-    DataMigrations::BackFillHesaTrnSubmissions.call
-  end
-end
