b4fe212361684ec05506373c4d4cab5a01d3e89b
John Henderson <john@logixworks.co.uk>
2022-10-12 11:44:11 +0100

    [4778] Protect Trainees::SetAcademicCyclesJob from deleted trainees

--- app/jobs/trainees/set_academic_cycles_job.rb
+++ app/jobs/trainees/set_academic_cycles_job.rb
@@ -3,6 +3,8 @@
 module Trainees
   class SetAcademicCyclesJob < ApplicationJob
     def perform(trainee)
+      return unless trainee.persisted? # Trainees can be deleted before this job runs
+
       trainee = Trainees::SetAcademicCycles.call(trainee: trainee)
       trainee.save!
     end
