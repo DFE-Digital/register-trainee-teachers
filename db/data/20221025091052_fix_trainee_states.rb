ecfce61331d4300a00594f35773cca55d6d5b850
John Henderson <john@logixworks.co.uk>
2022-10-25 10:42:18 +0100

    [4860] Data migration to correct trainee states
    The trainees should be TRN received, returning from deferrals, but they're still shown as 'deferred'.

---
+++ db/data/20221025091052_fix_trainee_states.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class FixTraineeStates < ActiveRecord::Migration[6.1]
+  def up
+    %w[0263134/2/03 1317509/2/03 1505948/2/03 2010427/1/03 2035745/1/03].each do |trainee_id|
+      Trainee.find_by(trainee_id: trainee_id)&.update_columns(state: :trn_received)
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
