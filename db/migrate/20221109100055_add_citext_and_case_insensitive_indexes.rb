1cf5ba9362c9e8da6d95295355321a3bcc80e134
David Georgiou <david.georgiou@gmail.com>
2022-11-09 10:09:03 +0000

    [4953] - use CITEXT for degree and trainee slugs

---
+++ db/migrate/20221109100055_add_citext_and_case_insensitive_indexes.rb
@@ -0,0 +1,25 @@
+class AddCitextAndCaseInsensitiveIndexes < ActiveRecord::Migration[6.1]
+  def up
+    enable_extension("citext")
+
+    remove_index :trainees, :slug, unique: true
+    change_column :trainees, :slug, :citext
+    add_index :trainees, :slug, unique: true
+
+    remove_index :degrees, :slug, unique: true
+    change_column :degrees, :slug, :citext
+    add_index :degrees, :slug, unique: true
+  end
+
+  def down
+    remove_index :trainees, :slug, unique: true
+    change_column :trainees, :slug, :string
+    add_index :trainees, :slug, unique: true
+
+    remove_index :degrees, :slug, unique: true
+    change_column :degrees, :slug, :string
+    add_index :degrees, :slug, unique: true
+
+    disable_extension("citext")
+  end
+end
