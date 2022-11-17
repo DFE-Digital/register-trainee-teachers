2eca6ebe1095203f9c3e841b037fdc30a49bee63
Hettie Street <hettie.street@digital.education.gov.uk>
2022-11-17 14:37:52 +0000

    [4927] Add rake to correct trainee state
    * Add rake and service to correctly add withdrawn attributes to relevant trainees

---
+++ lib/tasks/fix_incorrect_hesa_trainee_states.rake
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+namespace :hesa do
+  task :fix_deferred_states do
+    Trainee.includes(:hesa_student).deferred.where(record_source: %w[hesa_collection hesa_trn_data]).find_each do |trainee|
+      Hesa::BackfillTraineeStates.call(trainee: trainee)
+    end
+  end
+end
---
+++ spec/services/hesa/backfill_trainee_states_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Hesa
+  describe BackfillTraineeStates do
+    let(:trainee) { create(:trainee, :trn_received, :imported_from_hesa) }
+    let(:end_date) { Time.zone.today }
+    let(:reason_for_leaving_code) { %w[11 05 03].sample }
+    let(:reason_for_leaving) { Hesa::CodeSets::ReasonsForLeavingCourse::MAPPING[reason_for_leaving_code] }
+
+    subject { described_class.call(trainee: trainee) }
+
+    context "when HESA student is withdrawn" do
+      before do
+        trainee.hesa_student.update_columns(reason_for_leaving: reason_for_leaving_code, end_date: end_date)
+        subject
+      end
+
+      it "updates trainee state" do
+        expect(trainee.state).to eq("withdrawn")
+      end
+
+      it "updates withdraw_reason" do
+        expect(trainee.withdraw_reason).to eq(reason_for_leaving)
+      end
+
+      it "updates withdraw_data" do
+        expect(trainee.withdraw_date).to eq(end_date)
+      end
+    end
+
+    context "when HESA student is not withdrawn" do
+      before do
+        subject
+      end
+
+      it "does not change the state" do
+        expect(trainee.state).to eq("trn_received")
+      end
+
+      it "does not change withdraw_reason" do
+        expect(trainee.withdraw_reason).to be_nil
+      end
+
+      it "does not change withdraw_data" do
+        expect(trainee.withdraw_date).to be_nil
+      end
+    end
+
+    context "when there is no hesa_student for the trainee" do
+      let(:trainee) { create(:trainee, :trn_received) }
+
+      it "does not raise an error" do
+        expect { subject }.not_to raise_error
+      end
+
+      it "does not change the trainee" do
+        expect { subject }.not_to change { trainee.state }
+      end
+    end
+  end
+end
