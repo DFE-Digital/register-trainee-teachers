53e69d62a080a4123f248687f379c426dfc235c8
Mark Allen <markhallen@gmail.com>
2022-11-30 14:50:13 +0000

    Add specs to prevent divergance from scopes

--- spec/view_objects/home_view_spec.rb
+++ spec/view_objects/home_view_spec.rb
@@ -147,4 +147,68 @@ describe HomeView do
       end
     end
   end
+
+  context "when there trainees in various states" do
+    let(:not_started_trainee) { create_list(:trainee, 3, :trn_received, itt_start_date: current_academic_cycle.end_date + 2.months) }
+    let(:in_training_trainees) { create_list(:trainee, 6, :trn_received) }
+    let(:awarded_this_year_trainee) { create_list(:trainee, 5, :awarded, end_academic_cycle: current_academic_cycle) }
+    let(:awarded_last_year_trainee) { create_list(:trainee, 2, :awarded, end_academic_cycle: previous_academic_cycle) }
+    let(:deferred_trainees) { create_list(:trainee, 4, :deferred) }
+    let(:incomplete_trainee) { create(:trainee, :trn_received, :incomplete) }
+
+    let(:trainees) do
+      trainee_ids = [
+        not_started_trainee.pluck(:id),
+        awarded_this_year_trainee.pluck(:id),
+        awarded_last_year_trainee.pluck(:id),
+        incomplete_trainee.id,
+        in_training_trainees.pluck(:id),
+        deferred_trainees.pluck(:id),
+      ].flatten
+
+      Trainee.where(id: trainee_ids)
+    end
+
+    describe "#awarded_this_year_size" do
+      it "returns the same value as the #count of the ::awarded.merge(current_academic_cycle.trainees_ending) scope" do
+        expect(subject.send(:awarded_this_year_size)).to eq(Trainee.awarded.merge(current_academic_cycle.trainees_ending).count)
+      end
+    end
+
+    describe "#course_not_yet_started_size" do
+      it "returns the same value as the #count of the ::course_not_yet_started scope" do
+        expect(subject.send(:course_not_yet_started_size)).to eq(Trainee.course_not_yet_started.count)
+      end
+    end
+
+    describe "#deferred_size" do
+      it "returns the same value as the #count of the ::deferred scope" do
+        expect(subject.send(:deferred_size)).to eq(Trainee.deferred.count)
+      end
+    end
+
+    describe "#draft_apply_trainees_count" do
+      it "returns the same value as the #count of the ::draft and ::with_apply_application scopes" do
+        expect(subject.send(:draft_apply_trainees_count)).to eq(Trainee.draft.with_apply_application.count)
+      end
+    end
+
+    describe "#draft_trainees_count" do
+      it "returns the same value as the #count of the ::draft scope" do
+        expect(subject.send(:draft_trainees_count)).to eq(Trainee.draft.count)
+      end
+    end
+
+    describe "#incomplete_size" do
+      it "returns the same value as the #count of the ::not_draft and ::incomplete_for_filter scopes" do
+        expect(subject.send(:incomplete_size)).to eq(Trainee.not_draft.incomplete_for_filter.count)
+      end
+    end
+
+    describe "#trainees_in_training_size" do
+      it "returns the same value as the #count of the ::in_training scope" do
+        expect(subject.send(:trainees_in_training_size)).to eq(Trainee.in_training.count)
+      end
+    end
+  end
 end
