a4d979aa8d249c19014447bb303eba2b2df07314
Ed Horsford <mail@edwardhorsford.com>
2022-12-20 18:11:48 +0000

    Add end year to application cards (#2993)

--- spec/components/application_record_card/view_spec.rb
+++ spec/components/application_record_card/view_spec.rb
@@ -10,10 +10,10 @@ module ApplicationRecordCard
       double(UserWithOrganisationContext, system_admin?: false, lead_school?: false)
     end
     let(:trainee) { create(:trainee, first_names: nil, provider: provider, course_uuid: course.uuid, trainee_id: nil) }
+    let(:cycle) { create(:academic_cycle, cycle_year: 2019) }
 
     before do
       allow(trainee).to receive(:timeline).and_return([double(date: Time.zone.now)])
-      create(:academic_cycle, cycle_year: 2019)
       render_inline(described_class.new(record: trainee, current_user: current_user))
     end
 
@@ -125,6 +125,7 @@ module ApplicationRecordCard
           trainee_id: "132456",
           trn: "789456",
           trainee_start_date: DateTime.new(2020, 1, 2),
+          end_academic_cycle: cycle,
           provider: provider,
           state: state,
         )
@@ -168,6 +169,10 @@ module ApplicationRecordCard
 
       it "renders start_year" do
         expect(rendered_component).to have_text("Start year: 2019 to 2020")
+      end
+
+      it "renders end_year" do
+        expect(rendered_component).to have_text("End year: 2019 to 2020")
       end
 
       it "renders trainee name" do
