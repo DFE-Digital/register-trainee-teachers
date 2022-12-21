ea5fbaf9f7c0f29bb8ef1d523ec1431af220ca30
Abigail McPhillips <abigail.mcphillips@gmail.com>
2022-12-21 11:34:28 +0000

    [5063] Use the new academic cycle filter to return the correct trainees

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,7 @@
 class ReportsController < BaseTraineeController
   include DateOfTheNthWeekdayHelper
 
-  before_action :set_year_labels
+  before_action :set_cycle_variables
 
   def index
     authorize(current_user, :reports?)
@@ -15,8 +15,9 @@ class ReportsController < BaseTraineeController
     respond_to do |format|
       format.html do
         @sign_off_url = Settings.sign_off_trainee_data_url
-        @census_date = census_date(current_academic_cycle_start_year).strftime("%d %B %Y")
+        @census_date = census_date(@current_academic_cycle.start_year).strftime("%d %B %Y")
       end
+
       format.csv do
         authorize(:trainee, :export?)
         send_data(
@@ -33,8 +34,9 @@ class ReportsController < BaseTraineeController
 
     respond_to do |format|
       format.html do
-        @sign_off_date = Date.new(current_academic_cycle.end_year, 1, 31).strftime("%d %B %Y")
+        @sign_off_date = Date.new(@current_academic_cycle.end_year, 1, 31).strftime("%d %B %Y")
       end
+
       format.csv do
         authorize(:trainee, :export?)
         send_data(
@@ -49,39 +51,33 @@ class ReportsController < BaseTraineeController
 private
 
   def itt_new_starter_trainees
-    policy_scope(FindNewStarterTrainees.new(census_date(current_academic_cycle_start_year)).call)
+    policy_scope(FindNewStarterTrainees.new(census_date(@current_academic_cycle.start_year)).call)
   end
 
   def performance_profiles_trainees
-    policy_scope(FindNewStarterTrainees.new(census_date(current_academic_cycle_start_year)).call)
+    Trainees::Filter.call(trainees: base_trainee_scope, filters: { academic_year: [@previous_academic_cycle.start_year] })
   end
 
   def itt_new_starter_filename
-    "#{Time.zone.now.strftime('%F_%H_%M_%S')}_New-trainees-#{current_academic_cycle_start_year}-#{current_academic_cycle.end_year}-sign-off-Register-trainee-teachers_exported_records.csv"
+    "#{Time.zone.now.strftime('%F_%H_%M_%S')}_New-trainees-#{@current_academic_cycle.label('-')}-sign-off-Register-trainee-teachers_exported_records.csv"
   end
 
   def performance_profiles_filename
-    "#{Time.zone.now.strftime('%F_%H_%M_%S')}_Performance-profiles-sign-off-Register-trainee-teachers_exported_records.csv"
+    "#{Time.zone.now.strftime('%F_%H_%M_%S')}_#{@previous_academic_cycle.label('-')}_trainees_performance-profiles-sign-off_register-trainee-teachers.csv"
   end
 
   def census_date(year)
     date_of_nth_weekday(10, year, 3, 2)
   end
 
-  def set_year_labels
-    @current_academic_cycle_label = current_academic_cycle.label
-    @previous_academic_cycle_label = previous_academic_cycle.label
+  def set_cycle_variables
+    @current_academic_cycle = AcademicCycle.current
+    @previous_academic_cycle = AcademicCycle.previous
+    @current_academic_cycle_label = @current_academic_cycle.label
+    @previous_academic_cycle_label = @previous_academic_cycle.label
   end
 
-  def current_academic_cycle
-    @current_academic_cycle ||= AcademicCycle.current
-  end
-
-  def current_academic_cycle_start_year
-    @current_academic_cycle_start_year ||= current_academic_cycle.start_year
-  end
-
-  def previous_academic_cycle
-    @previous_academic_cycle ||= AcademicCycle.previous
+  def base_trainee_scope
+    policy_scope(Trainee.includes({ provider: [:courses] }, :start_academic_cycle, :end_academic_cycle).not_draft)
   end
 end
