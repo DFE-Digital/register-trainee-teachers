cdcc338f071547986ecca9f752a1701d77d5c808
David Georgiou <david.georgiou@gmail.com>
2022-12-23 14:54:57 +0000

    [5021] - addresses PR feedback (JH)

--- app/mailers/otp_mailer.rb
+++ app/mailers/otp_mailer.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class OtpMailer < GovukNotifyRails::Mailer
-  self.delivery_method = Rails.configuration.action_mailer.delivery_method
-
   def generate(name:, email:, code:)
     set_template(Settings.govuk_notify.otp_email_template_id)
 
