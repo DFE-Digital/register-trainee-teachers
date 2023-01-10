bffb523a4680eb2ee44db219766a09f3cf119685
David Georgiou <david.georgiou@gmail.com>
2023-01-10 10:50:18 +0000

    [5111] - DRY things up

--- app/services/dead_jobs/base.rb
+++ app/services/dead_jobs/base.rb
@@ -2,6 +2,11 @@
 
 module DeadJobs
   class Base
+    def initialize(with_errors: false, with_params: false)
+      @with_errors = with_errors
+      @with_params = with_params
+    end
+
     def csv
       CSV.generate do |csv|
         csv << headers
@@ -32,6 +37,27 @@ module DeadJobs
     delegate :count, to: :dead_jobs
 
   private
+
+    attr_reader :with_errors, :with_params
+
+    def trainees
+      Trainee.includes(:provider).find(dead_jobs.keys)
+    end
+
+    def to_a
+      @to_a ||= trainees.map do |trainee|
+        {
+          register_id: trainee.id,
+          trainee_name: trainee.full_name,
+          trainee_trn: trainee.trn,
+          trainee_dob: trainee.date_of_birth,
+          provider_name: trainee.provider.name,
+          provider_ukprn: trainee.provider.ukprn,
+          error_message: (dead_jobs[trainee.id] if with_errors),
+          params_sent: (Dqt::Params::TraineeRequest.new(trainee:) if with_params),
+        }.compact
+      end
+    end
 
     # returns: [{ record_id => error_message }, ... ]
     def dead_jobs
