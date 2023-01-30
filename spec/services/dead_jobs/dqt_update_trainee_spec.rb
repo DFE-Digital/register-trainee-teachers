27f3225cabbfad20a37be97f0b021ed37a50d401
David Georgiou <david.georgiou@gmail.com>
2023-01-30 16:02:07 +0000

    simplifies dqt_update_trainee_spec.rb

--- spec/services/dead_jobs/dqt_update_trainee_spec.rb
+++ spec/services/dead_jobs/dqt_update_trainee_spec.rb
@@ -8,27 +8,6 @@ module DeadJobs
       let(:klass) { "Dqt::UpdateTraineeJob" }
       let(:name) { "Dqt update trainee" }
 
-      let(:dead_set) do
-        [
-          OpenStruct.new(
-            item: {
-              wrapped: klass,
-              args:
-              [
-                {
-                  arguments: [
-                    { _aj_globalid: "gid://register-trainee-teachers/Trainee/#{trainee.id}" },
-                    { _aj_serialized: "ActiveJob::Serializers::TimeWithZoneSerializer", value: "2023-01-15T00:00:42.798653522Z" },
-                  ],
-                },
-              ],
-              error_message: 'status: 400, body: {"title":"Teacher has no incomplete ITT record","status":400,"errorCode":10005}, headers: ',
-              jid: "jobid1234",
-            }.with_indifferent_access,
-          ),
-        ]
-      end
-
       let(:params_sent) { Dqt::Params::TraineeRequest.new(trainee:).to_json.to_s.gsub('"', "'") }
 
       let(:csv) do
