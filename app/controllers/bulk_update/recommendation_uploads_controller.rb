df984d41e3e013530937fa7533d482b709612599
Hettie Street <hettie.street@digital.education.gov.uk>
2023-02-14 16:36:48 +0000

    [5250] Add RecommendationUploadsController
    - Add routes for bulk recommend
    - Add new BulkUpdate::RecommendationUploadsController which will be responsible
      for creating, updating, fix errors and eventually deleting (restarting) the
      upload of trainees for award recommendation
    - Set up new feature test, page helper etc
    - Add link to bulk recommend in the navigation (available for all users at
      the moment)
    - Add basic content to new.html.erb
    - New check.html.erb which will show the user their full trainee details
      in accordians
    - New show.html.erb which summarises the trainees uploaded (basic view,
      missing counts etc)

---
+++ app/controllers/bulk_update/recommendation_uploads_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module BulkUpdate
+  class RecommendationUploadsController < ApplicationController
+    def show; end
+
+    def new; end
+
+    def create
+      redirect_to(bulk_update_recommendation_upload_summary_url(1))
+    end
+
+    def check; end
+  end
+end
