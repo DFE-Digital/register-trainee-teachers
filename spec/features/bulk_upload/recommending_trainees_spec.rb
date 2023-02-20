30069ecc56f9f3993a7b3cf68ceb5f3af4cb0e12
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-02-20 13:08:28 +0000

    [5250] Remove the bulk recommend export from the reports page
    - Update controller so that it only bulk_recommend_export only responds to csv format
    - Update routes file since user-friendly URL no longer needed
    - Deleted views and test

--- spec/features/bulk_upload/recommending_trainees_spec.rb
+++ spec/features/bulk_upload/recommending_trainees_spec.rb
@@ -3,21 +3,47 @@
 require "rails_helper"
 
 feature "recommending trainees" do
-  background do
-    create(:academic_cycle, previous_cycle: true)
+  before do
     given_i_am_authenticated
   end
 
-  scenario "uploading trainees for recommendation" do
-    given_i_am_on_the_recommendation_upload_page
-    and_i_upload_a_csv
-    and_i_check_who_ill_recommend
+  context "given no trainees exist to recommend" do
+    scenario "i see 'no trainees' content" do
+      given_i_am_on_the_recommendation_upload_page
+      then_i_see_no_trainees_content
+    end
+  end
+
+  context "given multiple trainees exist to recommend" do
+    before do
+      given_multiple_trainees_exist_to_recommend
+    end
+
+    scenario "i can upload trainees for recommendation" do
+      given_i_am_on_the_recommendation_upload_page
+      then_i_see_how_many_trainees_i_can_recommend
+      and_i_upload_a_csv
+      and_i_check_who_ill_recommend
+    end
   end
 
 private
 
   def given_i_am_on_the_recommendation_upload_page
     recommendation_upload_page.load
+  end
+
+  def given_multiple_trainees_exist_to_recommend
+    create(:trainee, :trn_received, itt_end_date: Time.zone.today, provider: current_user.organisation)
+    create(:trainee, :trn_received, itt_end_date: Time.zone.today + 1.month, provider: current_user.organisation)
+  end
+
+  def then_i_see_how_many_trainees_i_can_recommend
+    expect(recommendation_upload_page).to have_text("2 trainees")
+  end
+
+  def then_i_see_no_trainees_content
+    expect(recommendation_upload_page).to have_text("You do not have any trainees")
   end
 
   def and_i_upload_a_csv
