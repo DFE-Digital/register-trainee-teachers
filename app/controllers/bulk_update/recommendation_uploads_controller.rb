658cb3ff4d02a00faba281482a47d5242f36f87b
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-02-20 13:08:28 +0000

    [5250] Remove the bulk recommend export from the reports page
    - Update controller so that it only bulk_recommend_export only responds to csv format
    - Update routes file since user-friendly URL no longer needed
    - Deleted views and test

--- app/controllers/bulk_update/recommendation_uploads_controller.rb
+++ app/controllers/bulk_update/recommendation_uploads_controller.rb
@@ -2,14 +2,19 @@
 
 module BulkUpdate
   class RecommendationUploadsController < ApplicationController
+    # TODO: Find the user's upload and pick out counts
     def show; end
 
-    def new; end
+    def new
+      @bulk_recommend_count = policy_scope(FindBulkRecommendTrainees.call).count
+    end
 
     def create
+      # TODO: Actually save a recommendation_upload here
       redirect_to(bulk_update_recommendation_upload_summary_path(1))
     end
 
+    # TODO: Find the user's upload trainees
     def check; end
   end
 end
