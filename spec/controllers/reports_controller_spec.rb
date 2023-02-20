c7946e30334708580808073b0ef9721853553aaa
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-02-20 13:08:28 +0000

    [5250] Remove the bulk recommend export from the reports page
    - Update controller so that it only bulk_recommend_export only responds to csv format
    - Update routes file since user-friendly URL no longer needed
    - Deleted views and test

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -63,11 +63,6 @@ describe ReportsController do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders the application template" do
-      get :bulk_recommend_export
-      expect(response).to render_template("application")
-    end
-
     it "renders a csv" do
       get :bulk_recommend_export, params: { format: :csv }
       expect(response.content_type).to eq("text/csv")
