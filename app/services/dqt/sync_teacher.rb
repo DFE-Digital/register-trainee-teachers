b128ab950a2e76916e5bd79dfb7d60ce11710550
John Henderson <john@logixworks.co.uk>
2023-02-28 14:17:10 +0000

    [5295] Add HESA ID to DQT tables
    This will be useful to make sure the HESA ID is correct between Register and DQT, which has been a big issue recently as we’ve had so many duplicate trainees with 2 different HESA IDs.

--- app/services/dqt/sync_teacher.rb
+++ app/services/dqt/sync_teacher.rb
@@ -10,11 +10,22 @@ module Dqt
       @trainee = trainee
     end
 
+    # Why is HESA ID captured in both models?
+    #
+    # Teacher model represents 'contact' details in DQT and TeacherTraining represents 'initialteachertraining'
+    #
+    # So for example:
+    #  - Trainee starts course 1 - contact HESA ID and training course 1 HESA ID align, in other words
+    #    Teacher#hesa_id == TeacherTraining#hesa_id
+    #  - Trainee withdraws from course 1 - contact HESA ID and training course 1 HESA ID remain aligned
+    #  - Trainee starts course 2 - contact HESA ID now matches training course 2 HESA ID, whilst course 1
+    #    HESA ID remains on the now inactivated 'older' withdrawn record
     def call
       dqt_data = RetrieveTeacher.call(trainee:)
       dqt_teacher = Teacher.find_or_initialize_by(trn: dqt_data["trn"], date_of_birth: dqt_data["dateOfBirth"])
       dqt_teacher.assign_attributes(first_name: dqt_data["firstName"],
                                     last_name: dqt_data["lastName"],
+                                    hesa_id: dqt_data["husId"], # related to current training - dynamic
                                     qts_date: dqt_data["qtsDate"],
                                     eyts_date: dqt_data["eytsDate"],
                                     early_years_status_name: dqt_data.dig("earlyYearsStatus", "name"),
@@ -27,6 +38,7 @@ module Dqt
                                                programme_end_date: training_data["programmeEndDate"],
                                                programme_type: training_data["programmeType"],
                                                result: training_data["result"],
+                                               hesa_id: training_data["husId"], # associated with that period of training
                                                provider_ukprn: training_data.dig("provider", "ukprn"))
         dqt_teacher_training.save
       end
