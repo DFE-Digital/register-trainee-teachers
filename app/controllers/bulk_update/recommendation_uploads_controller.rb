211b482a411cbf89c49b620b7633a3171eeb3bb7
Dave G <david.georgiou@digital.education.gov.uk>
2023-02-28 09:32:45 +0000

    [5264] - Bulk recommend CSV import (#3113)

--- app/controllers/bulk_update/recommendation_uploads_controller.rb
+++ app/controllers/bulk_update/recommendation_uploads_controller.rb
@@ -2,19 +2,61 @@
 
 module BulkUpdate
   class RecommendationUploadsController < ApplicationController
+    helper_method :bulk_recommend_count, :recommendations_upload_form
+    before_action :redirect
+
     # TODO: Find the user's upload and pick out counts
     def show; end
 
     def new
-      @bulk_recommend_count = policy_scope(FindBulkRecommendTrainees.call).count
+      @recommendations_upload_form = RecommendationsUploadForm.new
     end
 
     def create
-      # TODO: Actually save a recommendation_upload here
-      redirect_to(bulk_update_recommendation_upload_summary_path(1))
+      @recommendations_upload_form = RecommendationsUploadForm.new(provider:, file:)
+
+      if recommendations_upload_form.save
+        create_recommended_trainees!
+        redirect_to(root_path)
+      else
+        render(:new)
+      end
     end
 
     # TODO: Find the user's upload trainees
     def check; end
+
+  private
+
+    attr_reader :recommendations_upload_form
+
+    delegate :recommendations_upload, to: :recommendations_upload_form
+
+    def file
+      @file ||= params[:bulk_update_recommendations_upload_form][:file]
+    end
+
+    def provider
+      @provider ||= current_user.organisation
+    end
+
+    def bulk_recommend_count
+      @bulk_recommend_count ||= policy_scope(FindBulkRecommendTrainees.call).count
+    end
+
+    # for now, if anything goes wrong during creation of trainees
+    # delete the recommend_upload record (and uploaded file)
+    def create_recommended_trainees!
+      Recommend::CreateTrainees.call(
+        recommendations_upload_id: recommendations_upload.id,
+        csv: recommendations_upload_form.csv,
+      )
+    rescue StandardError
+      recommendations_upload.destroy
+    end
+
+    def redirect
+      redirect_to(root_path) unless provider.is_a?(Provider)
+    end
   end
 end
---
+++ app/services/bulk_update/recommend/create_trainees.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module BulkUpdate
+  module Recommend
+    class CreateTrainees
+      include ServicePattern
+
+      def initialize(recommendations_upload_id:, csv:)
+        @recommendations_upload_id = recommendations_upload_id
+        @csv = csv
+      end
+
+      # we skip the first non-header row as this will just contain "do not edit" warning.
+      def call
+        csv[1..].map.with_index(3) do |row, row_number|
+          ::BulkUpdate::RecommendedTrainee.create(
+            bulk_update_recommendations_upload_id: recommendations_upload_id,
+            csv_row_number: row_number,
+            standards_met_at: row["Date QTS or EYTS standards met"].to_date,
+            trn: row["TRN"],
+            hesa_id: row["HESA ID"],
+          )
+        end
+      end
+
+    private
+
+      attr_reader :recommendations_upload_id, :csv
+    end
+  end
+end
