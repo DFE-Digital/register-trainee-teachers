d4584b650ad02d3393540b5deb35a0c97cc30d64
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-02-28 10:14:44 +0000

    [5273] Bulk recommend trainees
    - Add new BulkUpdate::RecommendationsController responsible for recommending
     the trainees in bulk
    - Add new BulkUpdate::Recommend service that recommends all trainees given
     a recommendations_uplpad
    - Add a new confirmation page

---
+++ app/services/bulk_update/recommend.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module BulkUpdate
+  class Recommend
+    include ServicePattern
+
+    def initialize(recommendations_upload:)
+      @awardable_rows = recommendations_upload.awardable_rows
+    end
+
+    def call
+      return if awardable_rows.empty?
+
+      awardable_rows.find_each do |row|
+        # TODO: this will actually be an association on the row
+        trainee = Trainee.find_by(trn: row.trn)
+
+        next if trainee.nil? || !trainee.trn_received?
+
+        trainee.outcome_date = row.standards_met_at
+
+        if trainee.save!
+          trainee.recommend_for_award!
+
+          if FeatureService.enabled?(:integrate_with_dqt)
+            Dqt::RecommendForAwardJob.perform_later(trainee)
+          end
+        end
+      end
+    end
+
+  private
+
+    attr_reader :awardable_rows
+  end
+end
