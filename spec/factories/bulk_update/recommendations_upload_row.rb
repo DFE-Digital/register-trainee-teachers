535c956cfda1affd61678d19321922f64e1fbb75
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-03-03 16:56:27 +0000

    [5274] Upload summary content and counts
    - Add counts to upload summary
    - Add conditions around error content
    - Add content for all dates missing
    - Add awardable count to check page
    - Add count to confirmation page
    - Conditional logic around bullet points on summary

--- spec/factories/bulk_update/recommendations_upload_row.rb
+++ spec/factories/bulk_update/recommendations_upload_row.rb
@@ -7,5 +7,21 @@ FactoryBot.define do
     trn { "12345" }
     hesa_id { "54321" }
     standards_met_at { "2023-02-16" }
+
+    trait :missing_date do
+      standards_met_at { nil }
+    end
+
+    trait :with_error do
+      after(:create) do |row, _|
+        create(:bulk_update_row_error, errored_on: row)
+      end
+    end
+
+    trait :with_multiple_errors do
+      after(:create) do |row, _|
+        create_list(:bulk_update_row_error, 2, errored_on: row)
+      end
+    end
   end
 end
--- spec/models/bulk_update/recommendations_upload_spec.rb
+++ spec/models/bulk_update/recommendations_upload_spec.rb
@@ -8,4 +8,48 @@ RSpec.describe BulkUpdate::RecommendationsUpload do
     it { is_expected.to belong_to(:provider) }
     it { is_expected.to have_one_attached(:file) }
   end
+
+  describe "instance methods" do
+    let(:recommendations_upload) { create(:bulk_update_recommendations_upload) }
+    let(:complete_row) { create(:bulk_update_recommendations_upload_row) }
+    let(:error_row) { create(:bulk_update_recommendations_upload_row, :with_error) }
+    let(:missing_date_row) { create(:bulk_update_recommendations_upload_row, :missing_date) }
+
+    before do
+      recommendations_upload.recommendations_upload_rows = [complete_row, error_row, missing_date_row]
+    end
+
+    describe "awardable_rows" do
+      it "returns the rows with dates and no errors" do
+        expect(recommendations_upload.awardable_rows).to eq([complete_row])
+      end
+    end
+
+    describe "missing_date_rows" do
+      it "returns the rows with missing dates and no errors" do
+        expect(recommendations_upload.missing_date_rows).to eq([missing_date_row])
+      end
+    end
+
+    describe "error_rows" do
+      context "with a row with single error"
+      it "returns the rows with errors" do
+        expect(recommendations_upload.error_rows).to eq([error_row])
+      end
+    end
+
+    context "with a row with multiple errors" do
+      let(:multiple_errors_row) do
+        create(
+          :bulk_update_recommendations_upload_row,
+          :with_multiple_errors,
+          recommendations_upload:,
+        )
+      end
+
+      it "returns the rows with errors" do
+        expect(recommendations_upload.error_rows).to eq([error_row, multiple_errors_row])
+      end
+    end
+  end
 end
