053ebac9058a549a390e13dcf9a832283fb34615
Dave G <david.georgiou@digital.education.gov.uk>
2023-03-07 11:56:05 +0000

    [5249] Bulk QTS import validation (#3144)

--- spec/services/bulk_update/recommendations_uploads/validate_csv_spec.rb
+++ spec/services/bulk_update/recommendations_uploads/validate_csv_spec.rb
@@ -5,22 +5,17 @@ require "rails_helper"
 module BulkUpdate
   module RecommendationsUploads
     describe ValidateCsv do
-      subject(:service) { described_class.new(csv) }
+      subject(:service) { described_class.new(csv:, record:) }
 
       let(:csv) { CSV.new(file.read, headers: true).read }
+      let(:record) { ::BulkUpdate::RecommendationsUploadForm.new }
 
-      describe "#valid?" do
-        context "given a CSV with required headers" do
-          let(:file) { file_fixture("bulk_update/recommendations_upload/complete.csv") }
+      before { service.validate! }
 
-          it { expect(service.valid?).to be true }
-        end
+      context "given a CSV with missing headers" do
+        let(:file) { file_fixture("bulk_update/recommendations_upload/missing_required_header.csv") }
 
-        context "given a CSV with missing headers" do
-          let(:file) { file_fixture("bulk_update/recommendations_upload/missing_required_header.csv") }
-
-          it { expect(service.valid?).to be false }
-        end
+        it { expect(record.errors.first.message).to eql "CSV is missing the required headers" }
       end
     end
   end
