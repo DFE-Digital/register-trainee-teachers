e9d82c1ab27c1b5290d7383c55777f96a7c57922
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-03-15 14:27:55 +0000

    [5313] Cancel bulk recommendations
    - New cancel page with button link back to the new page
    - Feature tests for cancelling
    - Also link to cancel from the fix errors page and the check page

--- spec/support/page_objects/bulk_update/recommendations_uploads/show.rb
+++ spec/support/page_objects/bulk_update/recommendations_uploads/show.rb
@@ -5,6 +5,7 @@ module PageObjects
     class Show < PageObjects::Base
       set_url "/bulk-update/recommend/{id}/upload-summary"
 
+      element :cancel_link, ".govuk-link", text: "Cancel bulk recommending trainees"
       element :check_button, ".govuk-button", text: "Check who you’ll recommend"
       element :review_errors_button, ".govuk-button", text: "Review errors"
     end
