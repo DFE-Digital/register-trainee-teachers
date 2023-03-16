690c299ce6318e7b84fa5c213140b5357bb84952
John Henderson <john@logixworks.co.uk>
2023-03-16 11:55:46 +0000

    [5287] Bulk recommend - accordion page

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -4,11 +4,14 @@ import '../styles/application.scss'
 import '../scripts/components'
 import '../scripts/global/nationality_select'
 import '../scripts/global/disable-browser-autofill'
+import '../scripts/global/stick_to_window_when_scrolling'
 import LiveFilter from '../scripts/live_filter'
 import FilterToggle from '../scripts/filter_toggle'
 import CookieBanner from '../scripts/cookie_banner'
 
 import { initAll } from 'govuk-frontend'
+
+global.$ = require('jquery')
 
 initAll()
 LiveFilter.init()
