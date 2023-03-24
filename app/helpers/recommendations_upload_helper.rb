ffe55d6588a7447e08d55860c34f449724b65ed9
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-03-24 12:08:32 +0000

    [5331] Update all back links in recommendations upload flow

--- app/helpers/recommendations_upload_helper.rb
+++ app/helpers/recommendations_upload_helper.rb
@@ -12,4 +12,15 @@ module RecommendationsUploadHelper
       "QTS or EYTS"
     end
   end
+
+  # We can't use url_for(:back) because if the user proceeds to 'Change' and
+  # presses back, they would get stuck in a loop between 'Change' and 'Check'.
+  def back_url_for_check_page(recommendations_upload)
+    if recommendations_upload.error_rows.any?
+      # This means the user skipped fixing errors, so 'Back' goes to errors page.
+      bulk_update_recommendations_upload_recommendations_errors_url(recommendations_upload)
+    else
+      bulk_update_recommendations_upload_summary_path(recommendations_upload)
+    end
+  end
 end
