1998ebe4a4ca837e17840727a304e3ceabaf5a40
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-03-31 14:56:25 +0100

    [5359] Update URLs, h1 and captions

--- spec/support/page_objects/bulk_update/recommendations_uploads/cancel.rb
+++ spec/support/page_objects/bulk_update/recommendations_uploads/cancel.rb
@@ -3,7 +3,7 @@
 module PageObjects
   module RecommendationsUploads
     class Cancel < PageObjects::Base
-      set_url "/bulk-update/recommend/{id}/cancel-bulk-updates"
+      set_url "/bulk-update/recommend/{id}/cancel"
 
       element :confirm_button, ".govuk-button", text: "Cancel bulk recommending trainees"
     end
