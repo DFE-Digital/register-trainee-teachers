d73ba2f5c17debd04db5d28e081705a13fe98384
David Georgiou <david.georgiou@gmail.com>
2023-03-31 14:33:59 +0100

    [5347] - validate the presence of a header row for an uploaded CSV

--- app/services/bulk_update/recommendations_uploads/validate_csv.rb
+++ app/services/bulk_update/recommendations_uploads/validate_csv.rb
@@ -4,12 +4,17 @@ module BulkUpdate
   module RecommendationsUploads
     class ValidateCsv
       def initialize(csv:, record:)
+        # Remove the non-data warning row
+        csv.delete_if do |row|
+          row.fields.any? { |cell| cell&.downcase&.include?(Reports::BulkRecommendReport::DO_NOT_EDIT.downcase) }
+        end
+
         @csv = csv
         @record = record
       end
 
-      # do all required headers exist in the CSV headers
       def validate!
+        header_row!
         identifier_header!
         date_header!
         dates!
@@ -19,14 +24,16 @@ module BulkUpdate
 
       attr_reader :csv, :record
 
-      def identifier_header!
-        return if headers & identifying_headers != []
+      def header_row!
+        return if (headers & all_headers).any?
 
-        record.errors.add(:file, :no_id_header)
+        record.errors.add(:file, :no_header_detected)
       end
 
-      def identifying_headers
-        Reports::BulkRecommendReport::IDENTIFIERS.map(&:downcase)
+      def identifier_header!
+        return if (headers & identifying_headers).any?
+
+        record.errors.add(:file, :no_id_header)
       end
 
       def date_header!
@@ -35,15 +42,23 @@ module BulkUpdate
         record.errors.add(:file, :no_date_header)
       end
 
-      def date_header
-        Reports::BulkRecommendReport::DATE.downcase
-      end
-
       def dates!
         return unless headers.include?(date_header)
         return if csv[date_header].to_a.map(&:presence).any?
 
         record.errors.add(:file, :no_dates_given)
+      end
+
+      def all_headers
+        Reports::BulkRecommendReport::DEFAULT_HEADERS.map(&:downcase)
+      end
+
+      def identifying_headers
+        Reports::BulkRecommendReport::IDENTIFIERS.map(&:downcase)
+      end
+
+      def date_header
+        Reports::BulkRecommendReport::DATE.downcase
       end
 
       def headers
