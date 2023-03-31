fda68741c4ceba769cbd1d40fea6cacb4a273931
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-03-31 14:56:25 +0100

    [5359] Update URLs, h1 and captions

--- spec/support/page_objects/bulk_update/recommendations_errors/show.rb
+++ spec/support/page_objects/bulk_update/recommendations_errors/show.rb
@@ -3,7 +3,7 @@
 module PageObjects
   module RecommendationsUploads
     class Show < PageObjects::Base
-      set_url "/bulk-update/recommend/{id}/fix-errors"
+      set_url "/bulk-update/recommend/{id}/review-errors"
     end
   end
 end
--- spec/support/page_objects/bulk_update/recommendations_uploads/edit.rb
+++ spec/support/page_objects/bulk_update/recommendations_uploads/edit.rb
@@ -3,7 +3,7 @@
 module PageObjects
   module RecommendationsUploads
     class Edit < PageObjects::Base
-      set_url "/bulk-update/recommend/{id}/upload-changes"
+      set_url "/bulk-update/recommend/{id}/change-who-youll-recommend"
     end
   end
 end
