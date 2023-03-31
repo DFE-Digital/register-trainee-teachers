0827b074256bcfe8764b2091c5a70cc2102c6c6a
Abigail McPhillips <abigail.mcphillips@gmail.com>
2023-03-31 14:56:25 +0100

    [5359] Update URLs, h1 and captions

--- spec/support/page_objects/bulk_update/recommendations_checks/show.rb
+++ spec/support/page_objects/bulk_update/recommendations_checks/show.rb
@@ -3,7 +3,7 @@
 module PageObjects
   module RecommendationsChecks
     class Show < PageObjects::Base
-      set_url "/bulk-update/recommend/{id}/check-pending-updates"
+      set_url "/bulk-update/recommend/{id}/check-who-youll-recommend"
 
       element :upload_button, ".govuk-button", text: "Upload file and check who you’ll recommend"
       element :change_link, ".govuk-link", text: "Change who you’ll recommend"
