94a671cba63ed23f889cbde3c477bd0d17a7095f
Dave G <david.georgiou@digital.education.gov.uk>
2023-04-04 14:49:24 +0100

    [5347] show error when bulk qts eyts csv is uploaded without a header (#3186)
    https://trello.com/c/fS5sF0ZJ/5347

--- spec/services/bulk_update/recommendations_uploads/validate_csv_spec.rb
+++ spec/services/bulk_update/recommendations_uploads/validate_csv_spec.rb
@@ -5,17 +5,40 @@ require "rails_helper"
 module BulkUpdate
   module RecommendationsUploads
     describe ValidateCsv do
+      include RecommendationsUploadHelper
+
       subject(:service) { described_class.new(csv:, record:) }
 
-      let(:csv) { CSV.new(file.read, headers: true).read }
       let(:record) { ::BulkUpdate::RecommendationsUploadForm.new }
+      let(:csv)    { create_recommendations_upload_csv!(columns_to_delete:) }
 
-      before { service.validate! }
+      before do
+        create(:trainee, :bulk_recommend_from_hesa)
+        service.validate!
+      end
 
-      context "given a CSV with missing headers" do
-        let(:file) { file_fixture("bulk_update/recommendations_upload/missing_required_header.csv") }
+      context "given a CSV with no header row" do
+        let(:csv) { CSV.new("", **BulkUpdate::RecommendationsUploadForm::CSV_ARGS).read }
+
+        it { expect(record.errors.first.message).to eql "No header was detected" }
+      end
+
+      context "given a CSV with a missing identifying header" do
+        let(:columns_to_delete) { Reports::BulkRecommendReport::IDENTIFIERS.map(&:downcase) }
 
         it { expect(record.errors.first.message).to eql "At least one identifying column is required (TRN, HESA ID or Trainee provider ID)" }
+      end
+
+      context "given a CSV with no date header" do
+        let(:columns_to_delete) { [Reports::BulkRecommendReport::DATE.downcase] }
+
+        it { expect(record.errors.first.message).to eql "Date QTS or EYTS standards met is required" }
+      end
+
+      context "given a CSV with no dates" do
+        let(:csv) { create_recommendations_upload_csv! }
+
+        it { expect(record.errors.first.message).to eql "No dates have been provided in this CSV" }
       end
     end
   end
