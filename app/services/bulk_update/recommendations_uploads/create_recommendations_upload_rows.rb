b9744653ebd739556860e81ebd2ed6a50bd11e6b
Dave G <david.georgiou@digital.education.gov.uk>
2023-04-04 14:49:24 +0100

    [5347] show error when bulk qts eyts csv is uploaded without a header (#3186)
    https://trello.com/c/fS5sF0ZJ/5347

--- app/services/bulk_update/recommendations_uploads/create_recommendations_upload_rows.rb
+++ app/services/bulk_update/recommendations_uploads/create_recommendations_upload_rows.rb
@@ -24,7 +24,7 @@ module BulkUpdate
           csv_row_validator = ValidateCsvRow.new(csv: csv, row: row, trainee: trainee_validator.trainee)
 
           # create the recommendations_upload_row and associate it with the matched trainee (if any)
-          upload_row = create_recommendations_upload_row!(trainee_validator.trainee, row, index + FIRST_CSV_ROW_NUMBER)
+          upload_row = create_recommendations_upload_row!(trainee_validator.trainee, row, index + FIRST_CSV_ROW_NUMBER, csv_row_validator.date)
 
           # create any validation errors and associate them with the recommendations_upload_row just created
           create_validation_errors!(upload_row, csv_row_validator.messages) unless csv_row_validator.valid?
@@ -36,11 +36,11 @@ module BulkUpdate
 
       attr_reader :recommendations_upload, :csv
 
-      def create_recommendations_upload_row!(trainee, row, row_number)
+      def create_recommendations_upload_row!(trainee, row, row_number, date)
         recommendations_upload.rows.create(
           matched_trainee_id: trainee&.id,
           csv_row_number: row_number,
-          standards_met_at: row.standards_met_at&.to_date,
+          standards_met_at: date,
           trn: row.trn,
           hesa_id: row.hesa_id,
           age_range: row.age_range,
