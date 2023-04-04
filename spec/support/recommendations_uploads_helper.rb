61d09a8554bf9b6e549c9aa554c7183d3c2d04c7
Dave G <david.georgiou@digital.education.gov.uk>
2023-04-04 14:49:24 +0100

    [5347] show error when bulk qts eyts csv is uploaded without a header (#3186)
    https://trello.com/c/fS5sF0ZJ/5347

--- spec/support/recommendations_uploads_helper.rb
+++ spec/support/recommendations_uploads_helper.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 module RecommendationsUploadHelper
-  def create_recommendations_upload_csv!(trainees: Trainee.all, overwrite: [], write_to_disk: false)
+  def create_recommendations_upload_csv!(trainees: Trainee.all, overwrite: [], write_to_disk: false, columns_to_delete: [])
+    # use bulk recommend export to generate a CSV using the same CSV args that are
+    # used to read it in the upload form
     csv = CSV.parse(
       ::Exports::BulkRecommendExport.call(trainees).to_s,
       **::BulkUpdate::RecommendationsUploadForm::CSV_ARGS,
     )
 
+    # overwrite cell values
     overwrite.each_with_index do |row, i|
       row.each do |key, value|
         # plus one because the first row is "do not edit"
@@ -14,8 +17,13 @@ module RecommendationsUploadHelper
       end
     end
 
+    # delete any unwanted columns
+    columns_to_delete.each { |column| csv.delete(column) }
+
+    # return a CSV object
     return csv unless write_to_disk
 
+    # or return the path to a temp file on disk
     tempfile = Tempfile.new("csv")
     tempfile.write(csv.to_s)
     tempfile.rewind
