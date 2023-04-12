47d10a69fcf5a7a1998fde9b4f9e0fb39198606d
AbigailMcP <abigail.mcphillips@gmail.com>
2023-04-12 11:36:51 +0100

    [5380] Improve CSV validations and upload (#3206)
    - Limit upload choice to only CSVs
    - Ignore case during CSV row validations where case could apply
    - Remove hyphen for missing fields in report
    - Tidy up of FindBulkRecommendTrainees, removing ordering to the controller where its needed

--- app/services/find_bulk_recommend_trainees.rb
+++ app/services/find_bulk_recommend_trainees.rb
@@ -3,18 +3,14 @@
 class FindBulkRecommendTrainees
   include ServicePattern
 
-  # rubocop:disable Style/TrailingCommaInArguments
   def call
     itt_end_date_range = [(Time.zone.today - 6.months).iso8601, (Time.zone.today + 6.months).iso8601]
 
-    Trainee
-      .where(state: :trn_received)
-      .where(
-        <<~SQL
-          '#{itt_end_date_range}'::daterange @> trainees.itt_end_date OR
-          trainees.itt_end_date IS NULL
-        SQL
-      ).order(last_name: :asc)
+    itt_end_date_within_six_months = <<~SQL
+      '#{itt_end_date_range}'::daterange @> trainees.itt_end_date OR
+      trainees.itt_end_date IS NULL
+    SQL
+
+    Trainee.trn_received.where(itt_end_date_within_six_months)
   end
-  # rubocop:enable Style/TrailingCommaInArguments
 end
