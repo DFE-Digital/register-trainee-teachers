75707e2109bc4236ee721cabc33ca98d83a1eaa7
AbigailMcP <abigail.mcphillips@gmail.com>
2023-04-26 10:21:53 +0100

    [5415] Order by csv_row_number on check who youll recommend page (#3230)

--- app/controllers/bulk_update/recommendations_checks_controller.rb
+++ app/controllers/bulk_update/recommendations_checks_controller.rb
@@ -4,6 +4,7 @@ module BulkUpdate
   class RecommendationsChecksController < RecommendationsBaseController
     def show
       @table_row_limit = 50
+      @rows = recommendations_upload.awardable_rows.includes(:trainee).order(:csv_row_number)
     end
   end
 end
