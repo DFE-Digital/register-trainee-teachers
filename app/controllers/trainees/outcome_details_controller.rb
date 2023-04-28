b144968982cd3f37c1ac7304391cfdc690479843
John Henderson <john@logixworks.co.uk>
2023-04-28 11:55:13 +0100

    [5446] Allow for HESA trainees to be awarded individually

--- app/controllers/trainees/outcome_details_controller.rb
+++ app/controllers/trainees/outcome_details_controller.rb
@@ -2,10 +2,15 @@
 
 module Trainees
   class OutcomeDetailsController < BaseController
+    skip_before_action :authorize_trainee
+
     def confirm
+      authorize(trainee, :recommend_for_award?)
       @outcome_form = OutcomeDateForm.new(trainee)
     end
 
-    def recommended; end
+    def recommended
+      authorize(trainee, :recommended?)
+    end
   end
 end
