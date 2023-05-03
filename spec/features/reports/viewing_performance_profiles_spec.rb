eb6e72200cb1bb5acd81d84918019c13d0ccae51
David Georgiou <david.georgiou@gmail.com>
2023-05-03 09:57:32 +0100

    [5434] - new guidance for outside sign-off period

--- spec/features/reports/viewing_performance_profiles_spec.rb
+++ spec/features/reports/viewing_performance_profiles_spec.rb
@@ -7,14 +7,32 @@ feature "viewing performance profiles" do
   let!(:current_cycle) { create(:academic_cycle, :current) }
   let!(:trainee) { create(:trainee, :trn_received, start_academic_cycle: previous_cycle, end_academic_cycle: previous_cycle) }
 
-  background do
-    given_i_am_authenticated
-    given_i_am_on_the_reports_page
-    and_i_click_the_performance_profiles_link
+  context "inside sign-off period" do
+    before { allow(Settings).to receive(:in_sign_off_period?).and_return(true) }
+
+    background do
+      given_i_am_authenticated
+      given_i_am_on_the_reports_page
+      and_i_click_the_performance_profiles_link
+    end
+
+    scenario "shows the correct year" do
+      then_i_should_see_the_correct_performance_profiles_guidance
+    end
   end
 
-  scenario "shows the correct year" do
-    then_i_should_see_the_correct_performance_profiles_guidance
+  context "outside sign-off period" do
+    before { allow(Settings).to receive(:in_sign_off_period?).and_return(false) }
+
+    background do
+      given_i_am_authenticated
+      given_i_am_on_the_reports_page
+      and_i_click_the_performance_profiles_guidance_link
+    end
+
+    scenario "shows the correct content" do
+      then_i_should_see_the_correct_performance_profiles_guidance_outside_of_sign_off_period
+    end
   end
 
 private
@@ -27,7 +45,15 @@ private
     reports_page.performance_profiles_link.click
   end
 
+  def and_i_click_the_performance_profiles_guidance_link
+    reports_page.performance_profiles_guidance_link.click
+  end
+
   def then_i_should_see_the_correct_performance_profiles_guidance
     expect(performance_profiles_page).to have_text(previous_cycle.label)
+  end
+
+  def then_i_should_see_the_correct_performance_profiles_guidance_outside_of_sign_off_period
+    expect(performance_profiles_page).to have_text("Each year you need to sign off your list of trainee teachers from the previous academic year.")
   end
 end
