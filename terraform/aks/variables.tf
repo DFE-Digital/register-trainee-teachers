c3735477dded99a21e3c43f6ce31ac19ba45b786
RMcVelia <rick.mcvelia@gmail.com>
2023-05-18 16:30:21 +0100

    updates for aks env db settings

--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -94,6 +94,10 @@ variable "main_app" {
 
 variable "probe_path" { default = [] }
 
+variable "azure_maintenance_window" { default = null }
+variable "postgres_flexible_server_sku" { default = "B_Standard_B1ms" }
+variable "postgres_enable_high_availability" { default = false }
+
 locals {
   app_name_suffix  = var.app_name == null ? var.paas_app_environment : var.app_name
 
