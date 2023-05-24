d2e7d73b18afa572d6553118e11961ff4ec8caf8
John Henderson <john@logixworks.co.uk>
2023-05-24 15:11:46 +0100

    [5476] Speed up the process of bulk recommending trainees for award

---
+++ app/jobs/auditing/trainee_auditor_job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Auditing
+  class TraineeAuditorJob < ApplicationJob
+    def perform(trainee, audited_changes, user, remote_address)
+      Audited.audit_class.as_user(user) do
+        # Not ideal to be calling a private method, but it's a trade-off to allow us to create audit records
+        # asynchronously. The audit gem degrades performance massively when updating 1000's of records.
+        trainee.send(:write_audit, action: "update", audited_changes: audited_changes, remote_address: remote_address)
+      end
+    end
+  end
+end
