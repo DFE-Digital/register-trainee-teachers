8e9eb3d7c356197d45711c11f50e6a224419fe91
RMcVelia <rick.mcvelia@gmail.com>
2023-05-26 10:30:05 +0100

    Add application probes

--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -92,8 +92,6 @@ variable "main_app" {
   default = {}
 }
 
-variable "probe_path" { default = [] }
-
 variable "azure_maintenance_window" { default = null }
 variable "postgres_flexible_server_sku" { default = "B_Standard_B1ms" }
 variable "postgres_enable_high_availability" { default = false }
