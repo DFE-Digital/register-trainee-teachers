b0086af41e0fdcbd16759d94bcd5d938554f8d6c
RMcVelia <rick.mcvelia@gmail.com>
2023-06-14 15:50:08 +0100

    Add aks production env to workflows

--- terraform/aks/databases.tf
+++ terraform/aks/databases.tf
@@ -57,3 +57,29 @@ module "postgres" {
   azure_maintenance_window       = var.azure_maintenance_window
   azure_enable_backup_storage    = var.azure_enable_backup_storage
 }
+
+module "postgres_snapshot" {
+  count = var.snapshot_databases_to_deploy
+
+  source = "./vendor/modules/aks//aks/postgres"
+
+  name                  = "analysis"
+  namespace             = var.namespace
+  environment           = "analysis"
+  azure_resource_prefix = var.azure_resource_prefix
+  service_short         = var.service_short
+  config_short          = var.config_short
+  service_name          = var.service_name
+
+  cluster_configuration_map = module.cluster_data.configuration_map
+
+  use_azure               = true
+  azure_enable_monitoring = var.enable_monitoring
+  azure_extensions        = ["PGCRYPTO","BTREE_GIST","CITEXT","PG_TRGM"]
+  server_version          = var.postgres_version
+  azure_sku_name          = var.postgres_flexible_server_sku
+
+  azure_enable_high_availability      = false
+  azure_maintenance_window            = var.azure_maintenance_window
+  azure_enable_backup_storage         = false
+}
