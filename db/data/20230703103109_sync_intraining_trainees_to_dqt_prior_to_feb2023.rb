47b7e49405e1c0e845c37ba6d8cca22d0661b004
David Kewal <davidrk@hey.com>
2023-07-03 11:32:32 +0100

    [5579] Sync a test batch of 500 trainees to DQT

---
+++ db/data/20230703103109_sync_intraining_trainees_to_dqt_prior_to_feb2023.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class SyncIntrainingTraineesToDqtPriorToFeb2023 < ActiveRecord::Migration[7.0]
+  def up
+    in_training = Trainee.where.not(trn: nil).where("length(trn) = 7").in_training.where("updated_at <= ?", Date.new(2023, 2).end_of_month).limit(500)
+
+    in_training.find_in_batches(batch_size: 500).with_index do |group, batch|
+      group.each do |trainee|
+        Dqt::UpdateTraineeJob.set(wait: 30.seconds * batch).perform_later(trainee)
+      end
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
