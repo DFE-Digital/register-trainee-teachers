2ffd54e0587d149a75c8554878e571b61f7eb3ea
Steve Hook <steve.hook@gmail.com>
2023-07-06 11:38:47 +0100

    Add view preview
    Three new preview routes:
    
    - /view_components/year_change_banner/view/default_state
    - /view_components/year_change_banner/view/july
    - /view_components/year_change_banner/view/august

--- app/components/year_change_banner/view.rb
+++ app/components/year_change_banner/view.rb
@@ -3,25 +3,26 @@
 module YearChangeBanner
   class View < GovukComponent::Base
     def initialize
+      @today = Time.zone.today
       super(classes: "", html_attributes: "")
     end
 
     def render?
-      Time.zone.today.month == 7 || Time.zone.today.month == 8
+      current_month == 7 || current_month == 8
     end
 
     def title
-      if Time.zone.today.month == 7
+      if current_month == 7
         "The #{current_year} to #{current_year + 1} academic year will start on 1 August"
-      elsif Time.zone.today.month == 8
+      elsif current_month == 8
         "The #{current_year} to #{current_year + 1} academic year started on 1 August"
       end
     end
 
     def content
-      if Time.zone.today.month == 7
+      if current_month == 7
         "<a class=\"govuk-notification-banner__link\" href=\"#{dates_and_deadlines_guidance_path}\">View dates and deadlines</a> for the upcoming academic year.".html_safe
-      elsif Time.zone.today.month == 8
+      elsif current_month == 8
         "<a class=\"govuk-notification-banner__link\" href=\"#{dates_and_deadlines_guidance_path}\">View dates and deadlines</a> for this academic year.".html_safe
       end
     end
@@ -29,11 +30,11 @@ module YearChangeBanner
   private
 
     def current_month
-      Time.zone.today.month
+      @today.month
     end
 
     def current_year
-      Time.zone.today.year
+      @today.year
     end
   end
 end
