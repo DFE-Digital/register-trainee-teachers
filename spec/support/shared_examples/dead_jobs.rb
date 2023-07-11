01a12ba93f1e91a8f4ad33a320c825398d18034d
David Kewal <davidrk@hey.com>
2023-07-11 16:57:16 +0100

    [5677] Handle nil DQT records for dead job export with DQT status (#3406)

--- spec/support/shared_examples/dead_jobs.rb
+++ spec/support/shared_examples/dead_jobs.rb
@@ -28,17 +28,7 @@ RSpec.shared_examples "Dead jobs" do |dead_jobs_klass, name|
     ]
   end
 
-  let(:dqt_response_humanised) do
-    <<~TEXT
-      trn: #{dqt_teacher.trn}
-      first_name: #{dqt_teacher.first_name}
-      last_name: #{dqt_teacher.last_name}
-      date_of_birth: #{dqt_teacher.date_of_birth}
-      dqt_teacher_trainings: [{"programme_start_date"=>"#{dqt_teacher_training.programme_start_date}", "programme_end_date"=>"#{dqt_teacher_training.programme_end_date}", "programme_type"=>"#{dqt_teacher_training.programme_type}", "result"=>"#{dqt_teacher_training.result}", "provider_ukprn"=>"#{dqt_teacher_training.provider_ukprn}", "hesa_id"=>"#{dqt_teacher_training.hesa_id}", "active"=>#{dqt_teacher_training.active}}]
-    TEXT
-  end
-
-  subject { described_class.new(dead_set: dead_set, include_dqt_status: true) }
+  subject { described_class.new(dead_set:) }
 
   describe "#to_csv" do
     it "returns CSV content" do
@@ -70,7 +60,34 @@ RSpec.shared_examples "Dead jobs" do |dead_jobs_klass, name|
       expect(csv[0]["register_id"]).to eql trainee.id.to_s
       expect(csv[0]["job_id"]).to eql job_id.to_s
       expect(csv[0]["error_message"]).to eql "{'title'=>'Teacher has no incomplete ITT record', 'status'=>400, 'errorCode'=>10005}"
-      expect(csv[0]["dqt"]).to eql dqt_response_humanised
+    end
+
+    context "with dqt status" do
+      subject { described_class.new(dead_set: dead_set, include_dqt_status: true) }
+
+      context "with dqt teacher" do
+        let(:dqt_response_humanised) do
+          <<~TEXT
+            trn: #{dqt_teacher.trn}
+            first_name: #{dqt_teacher.first_name}
+            last_name: #{dqt_teacher.last_name}
+            date_of_birth: #{dqt_teacher.date_of_birth}
+            dqt_teacher_trainings: [{"programme_start_date"=>"#{dqt_teacher_training.programme_start_date}", "programme_end_date"=>"#{dqt_teacher_training.programme_end_date}", "programme_type"=>"#{dqt_teacher_training.programme_type}", "result"=>"#{dqt_teacher_training.result}", "provider_ukprn"=>"#{dqt_teacher_training.provider_ukprn}", "hesa_id"=>"#{dqt_teacher_training.hesa_id}", "active"=>#{dqt_teacher_training.active}}]
+          TEXT
+        end
+
+        it "returns the dqt status" do
+          expect(csv[0]["dqt"]).to eql dqt_response_humanised
+        end
+      end
+
+      context "without dqt teacher" do
+        let(:dqt_teacher) { nil }
+
+        it "returns nil" do
+          expect(csv[0]["dqt"]).to be_nil
+        end
+      end
     end
   end
 
