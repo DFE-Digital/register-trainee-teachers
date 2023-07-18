dc156fa76c3b052125f4267a002bddb77f615472
David Georgiou <david.georgiou@gmail.com>
2023-07-18 10:17:31 +0100

    [5679] - rubocop

--- db/migrate/20230717091901_remove_withdraw_reason_from_trainee.rb
+++ db/migrate/20230717091901_remove_withdraw_reason_from_trainee.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveWithdrawReasonFromTrainee < ActiveRecord::Migration[7.0]
   def change
     safety_assured { remove_column :trainees, :withdraw_reason, :integer }
