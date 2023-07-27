4167360b0421c40d07dd9be68700bc248f37a1ed
Steve Hook <steve.hook@gmail.com>
2023-07-27 12:38:09 +0100

    Extract I18n string from AwardDetails component

--- app/components/award_details/view.rb
+++ app/components/award_details/view.rb
@@ -14,7 +14,10 @@ module AwardDetails
       if trainee.awarded_at.present?
         date_for_summary_view(trainee.awarded_at)
       elsif trainee.recommended_for_award?
-        "Waiting for award - met standards on #{date_for_summary_view(trainee.recommended_for_award_at)}"
+        I18n.t(
+          "components.award_details.waiting_for_award",
+          recommended_for_award_at: date_for_summary_view(trainee.recommended_for_award_at),
+        )
       end
     end
   end
