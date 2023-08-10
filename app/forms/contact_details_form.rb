ac5cb7b04adad1e9e92386fcadac037e3dcee922
Steve Hook <steve.hook@gmail.com>
2023-08-10 15:59:49 +0100

    Remove address fields from `ContactDetailsForm`

--- app/forms/contact_details_form.rb
+++ app/forms/contact_details_form.rb
@@ -1,35 +1,15 @@
 # frozen_string_literal: true
 
 class ContactDetailsForm < TraineeForm
-  MANDATORY_UK_ADDRESS_FIELDS = %i[
-    address_line_one
-    postcode
-  ].freeze
-
-  FIELDS = %i[
-    locale_code
-    address_line_two
-    town_city
-    international_address
-    email
-  ].concat(MANDATORY_UK_ADDRESS_FIELDS).freeze
+  FIELDS = %i[email].freeze
 
   attr_accessor(*FIELDS)
 
   before_validation :sanitise_email
-
-  validates :locale_code, presence: true, if: :address_required?
-  validate :uk_address_valid, if: -> { address_required? && uk? }
-  validate :international_address_valid, if: -> { address_required? && non_uk? }
-  validates :postcode, postcode: true, if: -> { address_required? && postcode.present? }
   validates :email, presence: true
 
   validate do |record|
     EmailFormatValidator.new(record).validate
-  end
-
-  def uk?
-    fields[:locale_code] == "uk"
   end
 
   def save!
@@ -42,61 +22,18 @@ class ContactDetailsForm < TraineeForm
     end
   end
 
-  def address_required?
-    trainee.hesa_id.blank?
-  end
-
 private
 
   def compute_fields
     trainee.attributes.symbolize_keys.slice(*FIELDS).merge(new_attributes)
   end
 
-  def non_uk?
-    fields[:locale_code] == "non_uk"
-  end
-
   def update_trainee_attributes
-    nullify_unused_address_fields!
     # Need to save the email attributes formatted by the ContactDetailsForm.
     trainee.assign_attributes(fields.merge(email:))
   end
 
   def sanitise_email
     self.email = email.gsub(/\s+/, "") unless email.nil?
-  end
-
-  def nullify_unused_address_fields!
-    if !address_required?
-      fields.merge!(
-        locale_code: nil,
-        international_address: nil,
-        address_line_one: nil,
-        address_line_two: nil,
-        town_city: nil,
-        postcode: nil,
-      )
-    elsif uk?
-      fields.merge!(international_address: nil)
-    else
-      fields.merge!(
-        address_line_one: nil,
-        address_line_two: nil,
-        town_city: nil,
-        postcode: nil,
-      )
-    end
-  end
-
-  def international_address_valid
-    errors.add(:international_address, :blank) if fields[:international_address].blank?
-  end
-
-  def uk_address_valid
-    MANDATORY_UK_ADDRESS_FIELDS.each do |field|
-      next if fields[field].present?
-
-      errors.add(field, :blank)
-    end
   end
 end
--- spec/forms/contact_details_form_spec.rb
+++ spec/forms/contact_details_form_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 describe ContactDetailsForm, type: :model do
   let(:params) { {} }
-  let(:trainee) { build(:trainee, locale_code: nil) }
+  let(:trainee) { build(:trainee) }
   let(:form_store) { class_double(FormStore) }
 
   subject { described_class.new(trainee, params: params, store: form_store) }
@@ -14,18 +14,12 @@ describe ContactDetailsForm, type: :model do
   end
 
   describe "validations" do
-    it { is_expected.to validate_presence_of(:locale_code) }
     it { is_expected.to validate_presence_of(:email) }
 
     context "empty form data" do
       let(:params) do
         {
-          "address_line_two" => "",
-          "international_address" => "",
           "email" => "",
-          "address_line_one" => "",
-          "town_city" => "",
-          "postcode" => "",
         }
       end
 
@@ -33,88 +27,88 @@ describe ContactDetailsForm, type: :model do
         subject.valid?
       end
 
-      it "returns 2 errors about locale code and email" do
-        expect(subject.errors.attribute_names).to match(%i[locale_code email])
+      it "returns 1 error about email" do
+        expect(subject.errors.attribute_names).to match(%i[email])
       end
     end
 
-    context "international address validations" do
-      let(:params) { { "locale_code" => "non_uk", "international_address" => "" } }
+    # context "international address validations" do
+    #   let(:params) { { "locale_code" => "non_uk", "international_address" => "" } }
 
-      before do
-        subject.valid?
-      end
+    #   before do
+    #     subject.valid?
+    #   end
 
-      it "returns an error if its empty" do
-        expect(subject.errors[:international_address]).to include(
-          I18n.t(
-            "activemodel.errors.models.contact_details_form.attributes.international_address.blank",
-          ),
-        )
-      end
-    end
+    #   it "returns an error if its empty" do
+    #     expect(subject.errors[:international_address]).to include(
+    #       I18n.t(
+    #         "activemodel.errors.models.contact_details_form.attributes.international_address.blank",
+    #       ),
+    #     )
+    #   end
+    # end
 
-    context "UK address fields validations" do
-      let(:params) do
-        {
-          "locale_code" => "uk",
-          "address_line_two" => "",
-          "international_address" => "",
-          "address_line_one" => "",
-          "town_city" => "",
-          "postcode" => "",
-        }
-      end
+    # context "UK address fields validations" do
+    #   let(:params) do
+    #     {
+    #       "locale_code" => "uk",
+    #       "address_line_two" => "",
+    #       "international_address" => "",
+    #       "address_line_one" => "",
+    #       "town_city" => "",
+    #       "postcode" => "",
+    #     }
+    #   end
 
-      before do
-        subject.valid?
-      end
+    #   before do
+    #     subject.valid?
+    #   end
 
-      it "returns an error if its empty" do
-        ContactDetailsForm::MANDATORY_UK_ADDRESS_FIELDS.each do |field|
-          expect(subject.errors[field]).to include(
-            I18n.t("activemodel.errors.models.contact_details_form.attributes.#{field}.blank"),
-          )
-        end
-      end
+    #   it "returns an error if its empty" do
+    #     ContactDetailsForm::MANDATORY_UK_ADDRESS_FIELDS.each do |field|
+    #       expect(subject.errors[field]).to include(
+    #         I18n.t("activemodel.errors.models.contact_details_form.attributes.#{field}.blank"),
+    #       )
+    #     end
+    #   end
 
-      it "returns no error for address_line_two because it is optional" do
-        expect(subject.errors[:address_line_two]).to be_empty
-      end
-    end
+    #   it "returns no error for address_line_two because it is optional" do
+    #     expect(subject.errors[:address_line_two]).to be_empty
+    #   end
+    # end
 
-    context "HESA trainee record" do
-      let(:trainee) { build(:trainee, locale_code: nil, hesa_id: "XXX") }
-      let(:params) do
-        {
-          "locale_code" => "uk",
-          "address_line_one" => "test",
-          "address_line_two" => "test",
-          "international_address" => "test",
-          "town_city" => "test",
-          "postcode" => "E1 5DJ",
-        }
-      end
+    # context "HESA trainee record" do
+    #   let(:trainee) { build(:trainee, locale_code: nil, hesa_id: "XXX") }
+    #   let(:params) do
+    #     {
+    #       "locale_code" => "uk",
+    #       "address_line_one" => "test",
+    #       "address_line_two" => "test",
+    #       "international_address" => "test",
+    #       "town_city" => "test",
+    #       "postcode" => "E1 5DJ",
+    #     }
+    #   end
 
-      before do
-        subject.valid?
-      end
+    #   before do
+    #     subject.valid?
+    #   end
 
-      it "does not show address validation errors" do
-        expect(subject.errors[:international_address]).to be_empty
-        expect(subject.errors[:address_line_two]).to be_empty
-        ContactDetailsForm::MANDATORY_UK_ADDRESS_FIELDS.each do |field|
-          expect(subject.errors[field]).to be_empty
-        end
-      end
+    #   it "does not show address validation errors" do
+    #     expect(subject.errors[:international_address]).to be_empty
+    #     expect(subject.errors[:address_line_two]).to be_empty
+    #     ContactDetailsForm::MANDATORY_UK_ADDRESS_FIELDS.each do |field|
+    #       expect(subject.errors[field]).to be_empty
+    #     end
+    #   end
 
-      it "nullifies all the address fields" do
-        subject.send(:nullify_unused_address_fields!)
-        params.each do |k, _v|
-          expect(subject.fields[k.to_sym]).to be_nil
-        end
-      end
-    end
+    #   it "nullifies all the address fields" do
+    #     subject.send(:nullify_unused_address_fields!)
+    #     params.each do |k, _v|
+    #       expect(subject.fields[k.to_sym]).to be_nil
+    #     end
+    #   end
+    # end
   end
 
   describe "#stash" do
@@ -130,27 +124,27 @@ describe ContactDetailsForm, type: :model do
   describe "#save!" do
     let(:params) { {} }
     let(:trainee) { create(:trainee) }
-    let(:address_line_one) { Faker::Address.street_address }
+    # let(:address_line_one) { Faker::Address.street_address }
 
     before do
       allow(form_store).to receive(:get).and_return({
-        "address_line_one" => address_line_one,
+        # "address_line_one" => address_line_one,
         "email" => "test @example.com",
-        "postcode" => " SW1P 3BT ",
+        # "postcode" => " SW1P 3BT ",
       })
       allow(form_store).to receive(:set).with(trainee.id, :contact_details, nil)
     end
 
-    it "takes any data from the form store and saves it to the database" do
-      expect { subject.save! }.to change(trainee, :address_line_one).to(address_line_one)
-    end
+    # it "takes any data from the form store and saves it to the database" do
+    #   expect { subject.save! }.to change(trainee, :address_line_one).to(address_line_one)
+    # end
 
     it "strips all whitespace from emails" do
       expect { subject.save! }.to change(trainee, :email).to("test@example.com")
     end
 
-    it "strips leading and trailing whitespace from postcodes" do
-      expect { subject.save! }.to change(trainee, :postcode).to("SW1P 3BT")
-    end
+    # it "strips leading and trailing whitespace from postcodes" do
+    #   expect { subject.save! }.to change(trainee, :postcode).to("SW1P 3BT")
+    # end
   end
 end
