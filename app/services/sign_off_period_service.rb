00bb23d8a017c5962323e9a047b5b7bf2cb4181b
David Georgiou <david.georgiou@gmail.com>
2023-08-10 10:17:47 +0100

    [5787] - rubocop

--- app/services/sign_off_period_service.rb
+++ app/services/sign_off_period_service.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class SignOffPeriodService
-  CENSUS_MONTH = 10          # October
-  PERFORMANCE_MONTH = 1     # January
+  CENSUS_MONTH = 10 # October
+  PERFORMANCE_MONTH = 1 # January
 
   def self.call
-    today_month = Date.today.month
+    today_month = Time.zone.today.month
 
     return :census_period if today_month == CENSUS_MONTH
     return :performance_period if today_month == PERFORMANCE_MONTH
-    
+
     :outside_period
   end
 end
