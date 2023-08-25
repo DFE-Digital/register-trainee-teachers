21fde3d5ce45304d25164f32d36eae0a3f2345f0
Steve Hook <steve.hook@gmail.com>
2023-08-25 09:17:34 +0100

    Error handling in factory for test diagnostics

--- spec/support/features/trainee_steps.rb
+++ spec/support/features/trainee_steps.rb
@@ -6,6 +6,8 @@ module Features
 
     def given_a_trainee_exists(*traits, **overrides)
       @trainee ||= create(:trainee, *traits, **overrides, provider: current_user.organisation)
+      raise "Factory failed to save Trainee #{trainee.attributes}" unless @trainee&.persisted?
+      @trainee
     end
 
     def trainee_from_url
