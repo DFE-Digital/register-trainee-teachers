54a96652bede46b5d18a7633f676d928a3993699
defong <de_fong2k@yahoo.com>
2023-08-31 15:22:10 +0100

    [3292] Fixed dirty before hook setup

--- spec/controllers/trainees/training_details_controller_spec.rb
+++ spec/controllers/trainees/training_details_controller_spec.rb
@@ -13,13 +13,8 @@ describe Trainees::TrainingDetailsController do
     context "with an apply draft trainee" do
       let(:trainee) { create(:trainee, :incomplete_draft, :with_apply_application, provider: user.organisation) }
 
-      before do
-        allow(TrainingDetailsForm).to receive(:new).and_return(double(save: true))
-        allow(controller).to receive(:trainee_params).and_return(nil)
-      end
-
       it "redirects to /training-details/confirm after update" do
-        expect(put(:update, params: { trainee_id: trainee.slug })).to redirect_to("/trainees/#{trainee.slug}/training-details/confirm")
+        expect(put(:update, params: { trainee_id: trainee.slug, training_details_form: { trainee_id: "test" } })).to redirect_to("/trainees/#{trainee.slug}/training-details/confirm")
       end
     end
   end
