3a245c54c2b00bd9acc574f6d081d69a7075615b
David Georgiou <david.georgiou@gmail.com>
2023-09-05 15:23:34 +0100

    [5957] - splits funding types on the index page

--- app/controllers/funding_controller.rb
+++ app/controllers/funding_controller.rb
@@ -1,20 +1,33 @@
 # frozen_string_literal: true
 
 class FundingController < ApplicationController
-  helper_method :academic_years
+  helper_method :trainee_summary_academic_years, :payment_schedule_academic_years
 
   def show; end
 
 private
 
-  def academic_years
+  def trainee_summary_academic_years
     current_user.organisation.funding_trainee_summaries.pluck(:academic_year).map do |year|
       year = year.split("/").first.to_i
 
       {
         label: "#{year} to #{year + 1}",
-        url: funding_trainee_schedule_url(year),
+        url: funding_trainee_summary_path(year),
       }
     end
+  end
+
+  def payment_schedule_academic_years
+    current_user
+      .organisation
+      .funding_payment_schedules
+      .joins(rows: :amounts)
+      .pluck(Arel.sql("DISTINCT funding_payment_schedule_row_amounts.year")).map do |year|
+        {
+          label: "#{year} to #{year + 1}",
+          url: funding_payment_schedule_path(year),
+        }
+      end
   end
 end
