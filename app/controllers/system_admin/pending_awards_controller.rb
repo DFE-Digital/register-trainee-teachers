eb44596d7e58729d1ab8d36a397aaae827430666
Steve Hook <steve.hook@gmail.com>
2023-09-08 15:57:27 +0100

    Extract logic from controller to helper

--- app/controllers/system_admin/pending_awards_controller.rb
+++ app/controllers/system_admin/pending_awards_controller.rb
@@ -4,38 +4,8 @@ module SystemAdmin
   class PendingAwardsController < ApplicationController
     add_flash_types :dqt_error
 
-    helper_method :last_run_or_scheduled_at, :job_status
-
     def index
       @trainees = Trainee.recommended_for_award.undiscarded.order(:recommended_for_award_at)
-    end
-
-    def last_run_or_scheduled_at(trainee)
-      if dead_jobs[trainee.id]
-        dead_jobs[trainee.id][:scheduled_at]
-      elsif retry_jobs[trainee.id]
-        retry_jobs[trainee.id][:scheduled_at]
-      end
-    end
-
-    def job_status(trainee)
-      if dead_jobs[trainee.id]
-        "dead"
-      elsif retry_jobs[trainee.id]
-        "retrying"
-      else
-        "unknown"
-      end
-    end
-
-  private
-
-    def dead_jobs
-      @dead_jobs ||= Dqt::FindDeadJobs.call
-    end
-
-    def retry_jobs
-      @retry_jobs ||= Dqt::FindRetryJobs.call
     end
   end
 end
---
+++ spec/helpers/sidekiq_jobs_helper_spec.rb
@@ -0,0 +1,107 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe SidekiqJobsHelper do
+  include SidekiqJobsHelper
+
+  around { |example| Timecop.freeze { example.run } }
+
+  let(:trainee) { build(:trainee) }
+  let(:last_run_at) { 3.days.ago }
+  let(:scheduled_to_run_at) { 2.hours.from_now }
+
+  describe "#last_run_or_scheduled_at" do
+    context "when there is a dead job" do
+      before do
+        allow(Dqt::FindDeadJobs).to receive(:call).and_return(
+          trainee.id => {
+            job_id: "dead_job_id",
+            error_message: "dead_job_error_message",
+            scheduled_at: last_run_at,
+          },
+        )
+        allow(Dqt::FindRetryJobs).to receive(:call).and_return({})
+      end
+
+      it "returns the last time the dead job ran" do
+        expect(last_run_or_scheduled_at(trainee)).to eq(last_run_at)
+      end
+    end
+
+    context "when there is a retry job" do
+      before do
+        allow(Dqt::FindDeadJobs).to receive(:call).and_return({})
+        allow(Dqt::FindRetryJobs).to receive(:call).and_return(
+          trainee.id => {
+            job_id: "retry_job_id",
+            error_message: "retry_job_error_message",
+            scheduled_at: scheduled_to_run_at,
+          },
+        )
+      end
+
+      it "returns the next time the job is scheduled to re-run" do
+        expect(last_run_or_scheduled_at(trainee)).to eq(scheduled_to_run_at)
+      end
+    end
+
+    context "when there is no job in Sidekiq" do
+      before do
+        allow(Dqt::FindDeadJobs).to receive(:call).and_return({})
+        allow(Dqt::FindRetryJobs).to receive(:call).and_return({})
+      end
+
+      it "returns nil" do
+        expect(last_run_or_scheduled_at(trainee)).to be_nil
+      end
+    end
+  end
+
+  describe "#job_status" do
+    context "when there is a dead job" do
+      before do
+        allow(Dqt::FindDeadJobs).to receive(:call).and_return(
+          trainee.id => {
+            job_id: "dead_job_id",
+            error_message: "dead_job_error_message",
+            scheduled_at: last_run_at,
+          },
+        )
+        allow(Dqt::FindRetryJobs).to receive(:call).and_return({})
+      end
+
+      it "the status is `dead`" do
+        expect(job_status(trainee)).to eq("dead")
+      end
+    end
+
+    context "when there is a retry job" do
+      before do
+        allow(Dqt::FindDeadJobs).to receive(:call).and_return({})
+        allow(Dqt::FindRetryJobs).to receive(:call).and_return(
+          trainee.id => {
+            job_id: "retry_job_id",
+            error_message: "retry_job_error_message",
+            scheduled_at: scheduled_to_run_at,
+          },
+        )
+      end
+
+      it "the status is `retrying`" do
+        expect(job_status(trainee)).to eq("retrying")
+      end
+    end
+
+    context "when there is no job in Sidekiq" do
+      before do
+        allow(Dqt::FindDeadJobs).to receive(:call).and_return({})
+        allow(Dqt::FindRetryJobs).to receive(:call).and_return({})
+      end
+
+      it "the status is `unknown`" do
+        expect(job_status(trainee)).to eq("unknown")
+      end
+    end
+  end
+end
