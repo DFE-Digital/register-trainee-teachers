a5fa7ce9184e5e6d8e77c8698cd1c01a96261a97
Steve Hook <steve.hook@gmail.com>
2023-09-15 17:15:08 +0100

    Start feature spec for changing accredited provider

---
+++ spec/features/system_admin/accredited_providers/changed_accredited_provider_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+feature "Change a trainee's accredited provider" do
+  context "as a system admin" do
+    let(:user) { create(:user, system_admin: true) }
+    let(:trainee) { create(:trainee, :trn_received) }
+    let(:new_provider) { create(:provider) }
+
+    before do
+      given_i_am_authenticated(user:)
+      and_the_change_accredited_provider_feature_is_enabled
+    end
+
+    scenario "i can edit a trainee's accredited provider" do
+      when_i_visit_the_trainee_detail_page
+      and_i_click_the_change_provider_link
+      then_i_see_the_change_accredited_providers_page
+    end
+  end
+
+  def and_the_change_accredited_provider_feature_is_enabled
+    enable_features(:change_accredited_provider)
+  end
+
+  def when_i_visit_the_trainee_detail_page
+    visit "/trainees/#{trainee.slug}"
+  end
+
+  def and_i_click_the_change_provider_link
+    within ".govuk-summary-list__row.accrediting-provider" do
+      click_on "Change"
+    end
+  end
+
+  def then_i_see_the_change_accredited_providers_page
+    expect(page).to have_content("Change accredited provider")
+  end
+end
