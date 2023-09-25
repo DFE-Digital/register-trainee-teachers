b71bb1c5770ea0cdfc19715f8a22f59df8414be6
defong <de_fong2k@yahoo.com>
2023-09-25 16:21:37 +0100

    [6022] Added grant and tiered bursary form

---
+++ spec/forms/funding/grant_and_tiered_bursary_form_spec.rb
@@ -0,0 +1,112 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Funding
+  describe GrantAndTieredBursaryForm, type: :model do
+    let(:params) { {} }
+
+    context "trainee with grant and tiered bursary" do
+      let(:trainee) { create(:trainee, :with_grant_and_tiered_bursary) }
+
+      let(:form_store) { FormStore }
+
+      subject { described_class.new(trainee, params: params, store: form_store) }
+
+      describe "validations" do
+        it { is_expected.to validate_inclusion_of(:custom_bursary_tier).in_array(Trainee.bursary_tiers.keys + ["none"]) }
+        it { is_expected.to validate_inclusion_of(:custom_applying_for_grant).in_array(%w[yes no]) }
+      end
+
+      describe "#stash" do
+        before do
+          allow(form_store).to receive(:get).and_return(nil)
+        end
+
+        subject { described_class.new(trainee, params: params, store: form_store) }
+
+        let(:form_store) { class_double(FormStore) }
+
+        it "uses FormStore to temporarily save the fields under a key combination of trainee ID and grant_and_tiered_bursary" do
+          expect(form_store).to receive(:set).with(trainee.id, :grant_and_tiered_bursary, subject.fields)
+
+          subject.stash
+        end
+      end
+
+      describe "#save!" do
+        let(:custom_bursary_tier) { "none" }
+        let(:custom_applying_for_grant) { "no" }
+
+        let(:params) { { custom_bursary_tier:, custom_applying_for_grant: } }
+
+        it "sets applying_for_bursary to false" do
+          expect { subject.save! }.to change(trainee.reload, :applying_for_bursary).from(true).to(false)
+        end
+
+        it "sets applying_for_grant to false" do
+          expect { subject.save! }.to change(trainee.reload, :applying_for_grant).from(true).to(false)
+        end
+
+        it "sets bursary_tier to nil" do
+          expect { subject.save! }.to change(trainee.reload, :bursary_tier).from("tier_one").to(nil)
+        end
+
+        it "sets applying_for_scholarship to nil" do
+          expect { subject.save! }.to change(trainee.reload, :applying_for_scholarship).to(false)
+        end
+      end
+    end
+
+    context "trainee with tiered bursary" do
+      let(:trainee) { create(:trainee, :early_years_postgrad, :with_tiered_bursary) }
+
+      let(:form_store) { FormStore }
+
+      subject { described_class.new(trainee, params: params, store: form_store) }
+
+      describe "validations" do
+        it { is_expected.to validate_inclusion_of(:custom_bursary_tier).in_array(Trainee.bursary_tiers.keys + ["none"]) }
+        it { is_expected.not_to validate_inclusion_of(:custom_applying_for_grant).in_array(%w[yes no]) }
+      end
+
+      describe "#stash" do
+        before do
+          allow(form_store).to receive(:get).and_return(nil)
+        end
+
+        subject { described_class.new(trainee, params: params, store: form_store) }
+
+        let(:form_store) { class_double(FormStore) }
+
+        it "uses FormStore to temporarily save the fields under a key combination of trainee ID and grant_and_tiered_bursary" do
+          expect(form_store).to receive(:set).with(trainee.id, :grant_and_tiered_bursary, subject.fields)
+
+          subject.stash
+        end
+      end
+
+      describe "#save!" do
+        let(:custom_bursary_tier) { "none" }
+
+        let(:params) { { custom_bursary_tier: } }
+
+        it "sets applying_for_bursary to false" do
+          expect { subject.save! }.to change(trainee.reload, :applying_for_bursary).from(true).to(false)
+        end
+
+        it "sets applying_for_grant to false" do
+          expect { subject.save! }.to change(trainee.reload, :applying_for_grant).from(nil).to(false)
+        end
+
+        it "sets bursary_tier to nil" do
+          expect { subject.save! }.to change(trainee.reload, :bursary_tier).from("tier_one").to(nil)
+        end
+
+        it "sets applying_for_scholarship to nil" do
+          expect { subject.save! }.to change(trainee.reload, :applying_for_scholarship).to(false)
+        end
+      end
+    end
+  end
+end
