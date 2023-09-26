50a151d2e4bae434e79b94b4ab31b0ebc9700da8
defong <de_fong2k@yahoo.com>
2023-09-26 10:21:38 +0100

    [xxxx] Fixed click_on vs click_link or click_button

--- spec/features/system_admin/accredited_providers/change_accredited_provider_spec.rb
+++ spec/features/system_admin/accredited_providers/change_accredited_provider_spec.rb
@@ -70,7 +70,7 @@ feature "Change a trainee's accredited provider" do
 
   def and_i_click_the_change_provider_link
     within ".govuk-summary-list__row.accrediting-provider" do
-      click_on "Change"
+      click_link "Change"
     end
   end
 
@@ -83,7 +83,7 @@ feature "Change a trainee's accredited provider" do
   end
 
   def and_click_continue
-    click_on "Continue"
+    click_button "Continue"
   end
   alias_method :when_i_click_continue, :and_click_continue
 
@@ -107,7 +107,7 @@ feature "Change a trainee's accredited provider" do
   end
 
   def when_i_click_update
-    click_on "Update"
+    click_button "Update"
   end
 
   def then_i_see_a_flash_message
