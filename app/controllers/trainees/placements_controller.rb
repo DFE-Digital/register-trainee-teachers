e586f2f28193fe755a1956efb28f2c9720f48428
Steve Hook <steve.hook@gmail.com>
2023-10-19 08:42:36 +0100

    Add feature flag check

--- app/controllers/trainees/placements_controller.rb
+++ app/controllers/trainees/placements_controller.rb
@@ -2,9 +2,17 @@
 
 module Trainees
   class PlacementsController < BaseController
+    before_action :require_feature_flag
+
     def new
       @trainee = trainee
       @placement_form = PlacementForm.new(trainee:)
+    end
+
+  private
+
+    def require_feature_flag
+      redirect_to(not_found_path) unless FeatureService.enabled?(:trainee_placement)
     end
   end
 end
--- spec/features/trainee_actions/adding_trainee_placements_spec.rb
+++ spec/features/trainee_actions/adding_trainee_placements_spec.rb
@@ -2,12 +2,16 @@
 
 require "rails_helper"
 
-feature "Add a placement", feature_trainee_placement: true do
+feature "Add a placement" do
   scenario "Add a new placement to an existing trainee" do
     given_i_am_authenticated
     and_a_trainee_exists_with_trn_received
 
     when_i_navigate_to_the_new_placement_form
+    then_i_see_the_not_found_page
+
+    when_the_feature_flag_is_active
+    and_i_navigate_to_the_new_placement_form
     then_i_see_the_new_placement_form
   end
 
@@ -19,6 +23,15 @@ private
 
   def when_i_navigate_to_the_new_placement_form
     visit new_trainee_placements_path(trainee_id: @trainee.slug)
+  end
+  alias_method :and_i_navigate_to_the_new_placement_form, :when_i_navigate_to_the_new_placement_form
+
+  def then_i_see_the_not_found_page
+    expect(page).to have_current_path(not_found_path)
+  end
+
+  def when_the_feature_flag_is_active
+    enable_features(:trainee_placement)
   end
 
   def then_i_see_the_new_placement_form
