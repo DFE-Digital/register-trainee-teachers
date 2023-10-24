45f1f6c890b622ed99f31d97b86f683f62628ae4
Dave G <david.georgiou@digital.education.gov.uk>
2023-10-24 14:57:17 +0100

    [6292] - send table name not model (#3697)

--- app/jobs/bulk_update/analytics_job.rb
+++ app/jobs/bulk_update/analytics_job.rb
@@ -15,7 +15,7 @@ module BulkUpdate
       @events ||= records.map do |record|
         DfE::Analytics::Event.new
                               .with_type("update_entity")
-                              .with_entity_table_name(@model)
+                              .with_entity_table_name(@model.table_name)
                               .with_data(DfE::Analytics.extract_model_attributes(record))
       end.as_json
     end
