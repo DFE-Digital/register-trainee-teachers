e25df60ac5f4acfcd49f3ca752cc5bbf93c37d6a
David Georgiou <david.georgiou@gmail.com>
2023-10-24 11:28:54 +0100

    [6201] - card summary titles default to H3

--- app/components/summary_card/view.rb
+++ app/components/summary_card/view.rb
@@ -3,7 +3,7 @@
 class SummaryCard::View < ViewComponent::Base
   renders_one :header_actions
 
-  def initialize(title:, heading_level: 2, rows:, id_suffix: nil, editable: true)
+  def initialize(title:, heading_level: 3, rows:, id_suffix: nil, editable: true)
     @title = title
     @heading_level = heading_level
     @rows = rows
