1473fc5c4f245c1b767d20f17abf213aff725c37
defong <de_fong2k@yahoo.com>
2023-10-31 09:41:51 +0000

    [6251] Added placements validation for base

--- app/forms/submissions/base_validator.rb
+++ app/forms/submissions/base_validator.rb
@@ -22,9 +22,10 @@ module Submissions
     validator :schools, form: "Schools::FormValidator", if: :requires_schools?
     validator :funding, form: "Funding::FormValidator", if: :requires_funding?
     validator :iqts_country, form: "IqtsCountryForm", if: :requires_iqts_country?
+    validator :placements, form: "PlacementDetailForm", if: :requires_placements?
 
     delegate :requires_schools?, :requires_degree?, :apply_application?,
-             :requires_funding?, :requires_iqts_country?, to: :trainee
+             :requires_funding?, :requires_iqts_country?, :requires_placements?, to: :trainee
 
     validate :submission_ready
 
