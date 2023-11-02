868502591b745706f02655efd37cbac2459e4677
David Kewal <davidrk@hey.com>
2023-11-02 12:46:35 +0000

    [6326] Make sure back links behave correctly

--- spec/features/form_sections/teacher_training_data/placements/progress_spec.rb
+++ spec/features/form_sections/teacher_training_data/placements/progress_spec.rb
@@ -29,6 +29,14 @@ feature "completing the placements section", feature_trainee_placement: true do
     then_the_placements_section_should_be(completed)
   end
 
+  scenario "back links" do
+    given_i_am_reviewing_my_changes_on_the_confirmation_page
+    when_i_click_to_add_a_placement
+    then_i_see_the_placements_form_page
+    when_i_click_back
+    then_i_see_the_confirmation_page
+  end
+
 private
 
   def when_i_visit_the_placement_details_page
@@ -62,6 +70,30 @@ private
     within(".app-task-list__item.placement-details") do
       expect(page).to have_text(status)
     end
+  end
+
+  def given_i_am_reviewing_my_changes_on_the_confirmation_page
+    when_i_visit_the_placement_details_page
+    and_i_do_not_have_the_placement_detail
+    and_i_continue
+  end
+
+  def when_i_click_to_add_a_placement
+    within(".govuk-summary-list__row.placements") do
+      click_link("Add a placement")
+    end
+  end
+
+  def then_i_see_the_placements_form_page
+    expect(page).to have_current_path(new_trainee_placements_path(trainee))
+  end
+
+  def when_i_click_back
+    click_link "Back"
+  end
+
+  def then_i_see_the_confirmation_page
+    expect(page).to have_current_path(trainee_placements_confirm_path(trainee))
   end
 
   def valid_training_routes
