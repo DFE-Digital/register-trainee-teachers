162541de2e1d1b66a824c5fa04af8838b19f9f82
defong <de_fong2k@yahoo.com>
2023-11-02 11:11:07 +0000

    [6251] Removed pointless test

--- spec/models/trainee_spec.rb
+++ spec/models/trainee_spec.rb
@@ -743,26 +743,4 @@ describe Trainee do
       expect(trainee.duplicate?).to be(true)
     end
   end
-
-  describe "placements" do
-    subject(:trainee) {
-      create(:trainee, :imported_from_hesa)
-    }
-
-    context "when no placement data exists" do
-      it "returns nil" do
-        expect(trainee.placements).to be_empty
-      end
-    end
-
-    context "when placement data exists" do
-      let!(:placements) { create_list(:placement, 2, trainee:) }
-
-      it "returns true" do
-        trainee.placements.each do |placement|
-          expect(placement.name).to eq(placement.school.name)
-        end
-      end
-    end
-  end
 end
