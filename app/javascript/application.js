8583b6ef3548529b2701e50487a4bafc07b3b765
David Georgiou <david.georgiou@gmail.com>
2023-11-03 15:02:01 +0000

    [6172]- move component styles

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,17 @@
 import 'babel-polyfill'
+import jQuery from "jquery";
+import { initAll } from "govuk-frontend"
+
 import './scripts/govuk_assets_import'
 import '../../assets/stylesheets/application.scss'
-import './scripts/components'
 import './scripts/global/nationality_select'
 import './scripts/global/disable-browser-autofill'
 import LiveFilter from './scripts/live_filter'
 import FilterToggle from './scripts/filter_toggle'
 import CookieBanner from './scripts/cookie_banner'
 
-import { initAll } from 'govuk-frontend'
-
-global.$ = require('jquery')
+window.jQuery = jQuery;
+window.$ = jQuery;
 
 initAll()
 LiveFilter.init()
