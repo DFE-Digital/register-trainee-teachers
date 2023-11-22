11166ce1aef6e3cd8866655eec42e91bba00d26b
David Kewal <davidrk@hey.com>
2023-11-22 09:23:12 +0000

    [6397] Restrict bulk update page to only provider users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -17,6 +17,10 @@ module UsersHelper
     FeatureService.enabled?("funding") && defined?(current_user) && !current_user&.system_admin
   end
 
+  def can_bulk_update?
+    current_user&.organisation.is_a?(Provider)
+  end
+
   def can_bulk_recommend?
     defined?(current_user).present? && current_user.present? && UserPolicy.new(current_user, nil).bulk_recommend?
   end
