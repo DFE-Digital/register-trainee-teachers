aea38a6a8fb448329872d53573f6d1647d25d665
defong <de_fong2k@yahoo.com>
2023-11-23 17:24:25 +0000

    [6433] Amended the redirects for no

--- spec/features/form_sections/teacher_training_data/placements/progress_spec.rb
+++ spec/features/form_sections/teacher_training_data/placements/progress_spec.rb
@@ -13,28 +13,29 @@ feature "completing the placements section", feature_trainee_placement: true do
     then_the_placements_section_should_be(incomplete)
   end
 
-  scenario "renders an 'in progress' status when placement details partially provided" do
+  scenario "renders an 'completed' status when no placement details provided" do
     when_i_visit_the_placement_details_page
     and_i_do_not_have_the_placement_detail
     and_i_continue
-    and_i_do_not_confirm_my_details
-    then_the_placements_section_should_be(in_progress)
-  end
-
-  scenario "renders a completed status when placement details provided" do
-    when_i_visit_the_placement_details_page
-    and_i_do_not_have_the_placement_detail
-    and_i_continue
-    and_i_confirm_my_details
     then_the_placements_section_should_be(completed)
   end
 
   scenario "back links" do
-    given_i_am_reviewing_my_changes_on_the_confirmation_page
-    when_i_click_to_add_a_placement
-    then_i_see_the_placements_form_page
+    given_i_visit_the_placement_details_page
     when_i_click_back
-    then_i_see_the_confirmation_page
+    then_i_see_the_trainee_review_drafts_page
+
+    given_i_click_placements
+    and_i_see_the_trainee_placement_details_page
+    and_i_have_the_placement_detail
+    and_i_continue
+    and_i_see_the_placements_form_page
+    when_i_click_back
+    then_i_see_the_trainee_placement_details_page
+
+    given_i_see_the_trainee_placement_details_page
+    when_i_click_back
+    then_i_see_the_trainee_review_drafts_page
   end
 
 private
@@ -44,7 +45,7 @@ private
     click_link("Placements")
   end
 
-  def and_i_have_the_placement_details
+  def and_i_have_the_placement_detail
     page.choose(t("views.forms.placement_details.label_names.has_placement_detail"))
   end
 
@@ -56,16 +57,6 @@ private
     click_button(t("continue"))
   end
 
-  def and_i_do_not_confirm_my_details
-    expect(page).to have_current_path(trainee_placements_confirm_path(trainee))
-    and_i_continue
-  end
-
-  def and_i_confirm_my_details
-    page.check("I have completed this section")
-    and_i_continue
-  end
-
   def then_the_placements_section_should_be(status)
     within(".app-task-list__item.placement-details") do
       expect(page).to have_text(status)
@@ -74,29 +65,41 @@ private
 
   def given_i_am_reviewing_my_changes_on_the_confirmation_page
     when_i_visit_the_placement_details_page
-    and_i_do_not_have_the_placement_detail
+    and_i_have_the_placement_detail
     and_i_continue
-  end
-
-  def when_i_click_to_add_a_placement
-    within(".govuk-summary-list__row.placements") do
-      click_link("Add a placement")
-    end
-  end
-
-  def then_i_see_the_placements_form_page
-    expect(page).to have_current_path(new_trainee_placement_path(trainee))
   end
 
   def when_i_click_back
     click_link "Back"
   end
 
+  def then_i_see_the_placements_form_page
+    expect(page).to have_current_path(new_trainee_placement_path(trainee))
+  end
+
+  def when_i_click_placements
+    click_link("Placements")
+  end
+
   def then_i_see_the_confirmation_page
     expect(page).to have_current_path(trainee_placements_confirm_path(trainee))
+  end
+
+  def then_i_see_the_trainee_review_drafts_page
+    expect(page).to have_current_path(trainee_review_drafts_path(trainee))
+  end
+
+  def then_i_see_the_trainee_placement_details_page
+    expect(page).to have_current_path(edit_trainee_placements_details_path(trainee))
   end
 
   def valid_training_routes
     TRAINING_ROUTE_ENUMS.keys - %i[assessment_only early_years_assessment_only]
   end
+
+  alias_method :given_i_visit_the_placement_details_page, :when_i_visit_the_placement_details_page
+  alias_method :given_i_click_placements, :when_i_click_placements
+  alias_method :and_i_see_the_trainee_placement_details_page, :then_i_see_the_trainee_placement_details_page
+  alias_method :given_i_see_the_trainee_placement_details_page, :then_i_see_the_trainee_placement_details_page
+  alias_method :and_i_see_the_placements_form_page, :then_i_see_the_placements_form_page
 end
