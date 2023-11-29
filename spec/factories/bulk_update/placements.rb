c71d3794d87e60c28fd45ff9205e88ce78cbfd06
David Georgiou <david.georgiou@gmail.com>
2023-11-29 14:32:45 +0000

    [6399] - BulkUpdate::Placement factory

--- spec/factories/bulk_update/placements.rb
+++ spec/factories/bulk_update/placements.rb
@@ -2,6 +2,13 @@
 
 FactoryBot.define do
   factory :bulk_update_placement, class: "BulkUpdate::Placement" do
-    provider { nil }
+    provider
+
+    after(:build) do |upload|
+      upload.file.attach(
+        io: Rails.root.join("spec/fixtures/files/bulk_update/placements/complete.csv").open,
+        filename: "test.csv",
+      )
+    end
   end
 end
