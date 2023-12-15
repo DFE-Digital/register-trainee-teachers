c76da7eca187523d5fd943bc34cd4d4a2401f2cc
Adam Colvin <adamc@colvinapps.com>
2023-12-15 09:34:08 +0000

    Add before_validation callback to apply character limits

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
+  STRING_CHARACTER_LIMIT = 255
+
   self.abstract_class = true
+
+  before_validation :apply_character_limits
+
+  private
+
+  def apply_character_limits
+    self.class.columns.each do |column|
+      if column.type == :string && self[column.name].is_a?(String)
+        if self[column.name].length > STRING_CHARACTER_LIMIT
+          errors.add(column.name, "is too long (maximum is #{STRING_CHARACTER_LIMIT} characters)")
+        end
+      end
+    end
+  end
 end
