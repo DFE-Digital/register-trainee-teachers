02111968a446ea25f805defb04cd4317e9ee531e
Adam Colvin <adamc@colvinapps.com>
2023-12-18 13:03:52 +0000

    Use client-side validation for character lengths

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,25 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
-  STRING_CHARACTER_LIMIT = 255
-
   self.abstract_class = true
-
-  before_validation :apply_character_limits
-
-private
-
-  def apply_character_limits
-    self.class.columns.each do |column|
-      if string_too_long?(column)
-        errors.add(column.name, "is too long (maximum is #{STRING_CHARACTER_LIMIT} characters)")
-      end
-    end
-  end
-
-  def string_too_long?(column)
-    column.type == :string &&
-    self[column.name].is_a?(String) &&
-    self[column.name].length > STRING_CHARACTER_LIMIT
-  end
 end
