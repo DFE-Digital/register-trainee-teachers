2ab4c6899c01383bfd79e258c206ee0a12df2a74
Steve Hook <steve.hook@gmail.com>
2023-12-18 14:58:00 +0000

    Prevent block on OTP authenticated sysadmins
    The guard on sysadmin routes should work the same way regardless of
    authentication method.

--- app/lib/system_admin_constraint.rb
+++ app/lib/system_admin_constraint.rb
@@ -8,7 +8,17 @@ class SystemAdminConstraint
 private
 
   def system_admin?(request)
-    signin_user = DfESignInUser.load_from_session(request.session)
+    signin_user =
+      if otp_feature_enabled?
+        OtpSignInUser.load_from_session(request.session)
+      else
+        DfESignInUser.load_from_session(request.session)
+      end
+
     signin_user.present? && User.system_admins.kept.exists?(email: signin_user.email)
+  end
+
+  def otp_feature_enabled?
+    Settings.features.sign_in_method == "otp"
   end
 end
