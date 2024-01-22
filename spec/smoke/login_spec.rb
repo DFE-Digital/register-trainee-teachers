c1a1c2e302d2d6dda6c3e91d1faa5fb43d2407f0
Dave G <david.georgiou@digital.education.gov.uk>
2024-01-22 13:29:51 +0000

    [6528] - Add logins to the smoke tests (#3954)
    https://trello.com/c/Lm4cAF3n/6528

---
+++ spec/smoke/login_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require "spec_helper_smoke"
+
+describe "User login spec" do
+  before do
+    skip "DfE sign-in not enabled" unless Settings.features.sign_in_method == "dfe-sign-in"
+  end
+
+  scenario "signing in successfully" do
+    visit_sign_in_page
+    fill_in_login_credentials
+    submit_login_form
+    verify_successful_login
+    sign_out
+  end
+
+private
+
+  def visit_sign_in_page
+    visit "#{url_with_basic_auth}/sign-in"
+    click_on "Sign in using DfE Sign-in"
+  end
+
+  def fill_in_login_credentials
+    fill_in "username", with: Settings.smoke_test&.username
+    fill_in "password", with: Settings.smoke_test&.password
+  end
+
+  def submit_login_form
+    click_on "Sign in"
+  end
+
+  def verify_successful_login
+    expect(page).to have_current_path("/")
+    expect(page).to have_content("Sign out")
+  end
+
+  def sign_out
+    click_on "Sign out"
+    expect(page).to have_content("Sign in")
+  end
+
+  def url_with_basic_auth
+    uri = URI.parse(Settings.base_url)
+    uri.user = Settings.basic_auth&.username
+    uri.password = Settings.basic_auth&.password
+    uri.to_s
+  end
+end
