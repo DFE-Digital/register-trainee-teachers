831128f775df2ab335856574d82ae9c4ba9b96fc
David Georgiou <david.georgiou@gmail.com>
2024-01-22 15:22:58 +0000

    [6528] - smoke test take three

--- spec/smoke/login_spec.rb
+++ spec/smoke/login_spec.rb
@@ -2,6 +2,19 @@
 
 require "spec_helper_smoke"
 
+# To successfully test this locally you will need to add to your .env file:
+#
+# SMOKE_TEST_USERNAME
+# SMOKE_TEST_PASSWORD
+# BASIC_AUTH_USERNAME
+# BASIC_AUTH_PASSWORD
+#
+# These are stored as secrets in github https://github.com/DFE-Digital/register-trainee-teachers/settings/secrets/actions
+#
+# When running, pass in the remote environment name, for example:
+#
+# RAILS_ENV=production bundle exec rspec spec/smoke/login_spec.rb
+
 describe "User login spec" do
   before do
     skip "DfE sign-in not enabled" unless Settings.features.sign_in_method == "dfe-sign-in"
@@ -23,8 +36,8 @@ private
   end
 
   def fill_in_login_credentials
-    fill_in "username", with: Settings.smoke_test&.username
-    fill_in "password", with: Settings.smoke_test&.password
+    fill_in "username", with: ENV.fetch("SMOKE_TEST_USERNAME", nil)
+    fill_in "password", with: ENV.fetch("SMOKE_TEST_PASSWORD", nil)
   end
 
   def submit_login_form
@@ -42,8 +55,11 @@ private
 
   def url_with_basic_auth
     uri = URI.parse(Settings.base_url)
-    uri.user = Settings.basic_auth&.username
-    uri.password = Settings.basic_auth&.password
+
+    return uri.to_s unless Settings.features.basic_auth
+
+    uri.user = ENV.fetch("BASIC_AUTH_USERNAME", nil)
+    uri.password = ENV.fetch("BASIC_AUTH_PASSWORD", nil)
     uri.to_s
   end
 end
