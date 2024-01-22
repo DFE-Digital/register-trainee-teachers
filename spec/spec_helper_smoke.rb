9bef364bf499bd1e4ef4c38a3e732590eb3bd557
David Georgiou <david.georgiou@gmail.com>
2024-01-22 10:45:43 +0000

    [6528] - adds smoke test login

--- spec/spec_helper_smoke.rb
+++ spec/spec_helper_smoke.rb
@@ -1,9 +1,30 @@
 # frozen_string_literal: true
 
+require 'dotenv/load'
+
 ENV["RAILS_ENV"] ||= "test"
 
 require "rspec/core"
 require "config"
 require "httparty"
+require "capybara/rspec"
+require "selenium-webdriver"
+require "byebug"
 
 Config.load_and_set_settings(Config.setting_files("config", ENV.fetch("RAILS_ENV", nil)))
+
+Capybara.register_driver :selenium_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless")
+  options.add_argument("--disable-gpu")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.default_driver = :selenium_headless
+Capybara.javascript_driver = :selenium_headless
+Capybara.default_max_wait_time = 5
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+end
