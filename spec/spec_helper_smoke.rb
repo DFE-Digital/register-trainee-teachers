df1c54a9a7ed6bbfb2017d8e8078af0e5cb24c20
David Georgiou <david.georgiou@gmail.com>
2024-01-23 23:14:48 +0000

    [6528] - dotenv

--- spec/spec_helper_smoke.rb
+++ spec/spec_helper_smoke.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "dotenv/load"
+
 ENV["RAILS_ENV"] ||= "staging"
 
 require "rspec/core"
@@ -7,12 +9,13 @@ require "config"
 require "httparty"
 require "capybara/rspec"
 require "selenium-webdriver"
+require "byebug"
 
 Config.load_and_set_settings(Config.setting_files("config", ENV.fetch("RAILS_ENV", nil)))
 
 Capybara.register_driver :selenium_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument("--headless")
+  # options.add_argument("--headless")
   options.add_argument("--disable-gpu")
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
@@ -20,7 +23,7 @@ end
 
 Capybara.default_driver = :selenium_headless
 Capybara.javascript_driver = :selenium_headless
-Capybara.default_max_wait_time = 5
+Capybara.default_max_wait_time = 20
 
 RSpec.configure do |config|
   config.include Capybara::DSL
