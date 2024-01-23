98e38bebac7b828455dc11fe37bfecbd2de1a4fb
David Georgiou <david.georgiou@gmail.com>
2024-01-23 23:15:08 +0000

    [6528] - back to headless

--- spec/spec_helper_smoke.rb
+++ spec/spec_helper_smoke.rb
@@ -15,7 +15,7 @@ Config.load_and_set_settings(Config.setting_files("config", ENV.fetch("RAILS_ENV
 
 Capybara.register_driver :selenium_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new
-  # options.add_argument("--headless")
+  options.add_argument("--headless")
   options.add_argument("--disable-gpu")
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
