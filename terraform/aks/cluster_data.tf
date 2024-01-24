496177f1cf9f97348ec1269551cbd1182c6110b9
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2024-01-24 10:53:24 +0000

    wip

--- terraform/aks/cluster_data.tf
+++ terraform/aks/cluster_data.tf
@@ -1,4 +1,5 @@
 module "cluster_data" {
-  source = "./vendor/modules/aks//aks/cluster_data"
-  name   = var.cluster
+  source             = "./vendor/modules/aks//aks/cluster_data"
+  name               = var.cluster
+  spn_authentication = var.spn_authentication
 }
--- terraform/aks/provider.tf
+++ terraform/aks/provider.tf
@@ -31,53 +31,23 @@ provider "azurerm" {
 provider "statuscake" {
   api_token = local.infra_secrets.STATUSCAKE_PASSWORD
 }
-data "azurerm_client_config" "current" {
-}
 
 variable "spn_authentication" {
   default = false
 }
 
-variable "enable_azure_RBAC" {
-  default = false
-}
-
-locals {
-  kubelogin_args_map = {
-    spn = [
-      "get-token",
-      "--login",
-      "spn",
-      "--environment",
-      "AzurePublicCloud",
-      "--tenant-id",
-      data.azurerm_client_config.current.tenant_id,
-      "--server-id",
-      "6dae42f8-4368-4678-94ff-3960e28e3630"
-    ],
-    azurecli = [
-      "get-token",
-      "--login",
-      "azurecli",
-      "--server-id",
-      "6dae42f8-4368-4678-94ff-3960e28e3630" # See https://azure.github.io/kubelogin/concepts/aks.html
-    ]
-  }
-
-  kubelogin_args = var.spn_authentication ? local.kubelogin_args_map["spn"] : local.kubelogin_args_map["azurecli"]
-}
 provider "kubernetes" {
   host                   = module.cluster_data.kubernetes_host
   cluster_ca_certificate = module.cluster_data.kubernetes_cluster_ca_certificate
-  client_certificate     = var.enable_azure_RBAC ? null : module.cluster_data.kubernetes_client_certificate
-  client_key             = var.enable_azure_RBAC ? null : module.cluster_data.kubernetes_client_key
+  client_certificate     = module.cluster_data.azure_RBAC_enabled ? null : module.cluster_data.kubernetes_client_certificate
+  client_key             = module.cluster_data.azure_RBAC_enabled ? null : module.cluster_data.kubernetes_client_key
 
   dynamic "exec" {
-    for_each = var.enable_azure_RBAC ? [1] : []
+    for_each = module.cluster_data.azure_RBAC_enabled ? [1] : []
     content {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "kubelogin"
-      args        = local.kubelogin_args
+      args        = module.cluster_data.kubelogin_args
     }
   }
 
