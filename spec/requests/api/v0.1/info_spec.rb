d8e8ce845109a7b71f552ab85845b000f402de30
Steve Hook <steve.hook@gmail.com>
2024-01-26 15:04:28 +0000

    Make register_api ON by default except on prod

--- spec/requests/api/v0.1/info_spec.rb
+++ spec/requests/api/v0.1/info_spec.rb
@@ -3,21 +3,21 @@
 require "rails_helper"
 
 describe "info endpoint" do
-  context "when the feature flag is off" do
+  context "when the feature flag is off", feature_register_api: false do
     it "returns status 404" do
       get "/api/v0.1/info", headers: { Authorization: "Bearer bat" }
       expect(response).to have_http_status(:not_found)
     end
   end
 
-  context "without a valid authentication token", feature_register_api: true do
+  context "without a valid authentication token" do
     it "returns status 401" do
       get "/api/v0.1/info", headers: { Authorization: "Bearer bob" }
       expect(response).to have_http_status(:unauthorized)
     end
   end
 
-  context "with a valid authentication token and the feature flag on", feature_register_api: true do
+  context "with a valid authentication token and the feature flag on" do
     it "returns status 200 with a valid JSON response" do
       get "/api/v0.1/info", headers: { Authorization: "Bearer bat" }
       expect(response).to have_http_status(:ok)
