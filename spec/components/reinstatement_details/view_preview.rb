002674e39f751ae5b69429034be338e552ca419e
defong <de_fong2k@yahoo.com>
2024-01-30 16:21:16 +0000

    [6620] Amended preview

--- spec/components/reinstatement_details/view_preview.rb
+++ spec/components/reinstatement_details/view_preview.rb
@@ -3,14 +3,21 @@
 module ReinstatementDetails
   class ViewPreview < ViewComponent::Preview
     def default
-      render(View.new(data_model))
+      render(View.new(reinstatement_form:, itt_end_date_form:))
     end
 
   private
 
-    def data_model
-      trainee = OpenStruct.new(id: 1, reinstate_date: Faker::Date.in_date_period)
+    def trainee
+      @trainee ||= OpenStruct.new(id: 1, reinstate_date: Faker::Date.in_date_period)
+    end
+
+    def reinstatement_form
       OpenStruct.new(trainee: trainee, date: trainee.reinstate_date)
+    end
+
+    def itt_end_date_form
+      OpenStruct.new(trainee: trainee, itt_end_date: 1.year.since(trainee.reinstate_date))
     end
   end
 end
