935630b9aef096352e556793ff99d5770b5e1216
David Georgiou <david.georgiou@gmail.com>
2024-01-31 12:31:07 +0000

    [6668] - use feature flag

--- app/controllers/api/guide_controller.rb
+++ app/controllers/api/guide_controller.rb
@@ -3,6 +3,7 @@
 module Api
   class GuideController < ::ApplicationController
     skip_before_action :authenticate
+    before_action { require_feature_flag(:register_api) }
 
     def show; end
   end
--- spec/requests/api/v0.1/guide_spec.rb
+++ spec/requests/api/v0.1/guide_spec.rb
@@ -3,10 +3,19 @@
 require "rails_helper"
 
 describe "guidance endpoint" do
-  context "visiting" do
+  context "visiting with api feature flag", feature_register_api: true do
     it "returns status 200" do
       get "/api/v0.1/guide"
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  context "visiting without api feature flag", feature_register_api: false do
+    it "returns status 404" do
+      get "/api/v0.1/guide"
+      expect(response).to have_http_status(:found)
+      follow_redirect!
+      expect(response).to have_http_status(:not_found)
     end
   end
 end
