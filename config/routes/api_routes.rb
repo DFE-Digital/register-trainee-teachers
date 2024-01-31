6db3514d2828ae81dfc189b748a4242444c61522
Dave G <david.georgiou@digital.education.gov.uk>
2024-01-31 12:42:17 +0000

    [6668] initial api documentation set up framework to read content (#3985)

--- config/routes/api_routes.rb
+++ config/routes/api_routes.rb
@@ -5,6 +5,7 @@ module ApiRoutes
     router.instance_exec do
       namespace :api, path: "api/:api_version", api_version: /v[.0-9]+/ do
         resource :info, only: :show, controller: "info", constraints: RouteConstraints::RegisterApiConstraint
+        resource :guide, only: :show, controller: "guide", constraints: RouteConstraints::RegisterApiConstraint
         match "*url" => "base#not_found", via: :all
       end
     end
