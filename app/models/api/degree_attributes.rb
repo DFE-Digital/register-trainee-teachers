4fb040d42330fa5c09051de9f041fe7c995e5ebb
Adam Colvin <adamc@colvinapps.com>
2024-02-02 14:32:45 +0000

    Remove unnecessary validations in DegreeAttributes

--- app/models/api/degree_attributes.rb
+++ app/models/api/degree_attributes.rb
@@ -3,7 +3,6 @@
 module Api
   class DegreeAttributes
     include ActiveModel::Model
-    include DegreeValidations
 
     ATTRIBUTES = %i[
       id
@@ -22,14 +21,6 @@ module Api
 
     attr_accessor(*ATTRIBUTES)
 
-    validates :locale_code, presence: true
-    validates :institution, presence: true, on: :uk
-    validates :country, presence: true, on: :non_uk
-    validates :subject, presence: true, on: %i[uk non_uk]
-    validates :uk_degree, presence: true, on: :uk
-    validates :non_uk_degree, presence: true, on: :non_uk
-    validates :grade, presence: true, on: :uk
-    validates :graduation_year, presence: true, on: %i[uk non_uk]
     validates :institution, inclusion: { in: DfEReference::DegreesQuery::INSTITUTIONS.all.map(&:name) }, allow_nil: true
     validates :subject, inclusion: { in: DfEReference::DegreesQuery::SUBJECTS.all.map(&:name) }, allow_nil: true
     validates :uk_degree, inclusion: { in: DfEReference::DegreesQuery::TYPES.all.map(&:name) }, allow_nil: true
