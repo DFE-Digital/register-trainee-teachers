28a8e69d8d9238ec9f1a7bd81a61855aa41bc0f3
defong <de_fong2k@yahoo.com>
2024-02-02 17:22:18 +0000

    [6715] Added controller for withdrawal

---
+++ app/controllers/api/trainees/withdraws_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Api
+  module Trainees
+    class WithdrawsController < Api::BaseController
+      def create
+        if trainee.blank?
+          render(json: { error: "Trainee not found" }, status: :not_found)
+        else
+          trainee.update(withdrawal_params) if withdraw_allowed?
+          render(json: { status: "Trainee withdrawal request accpeted", trainee: trainee }, status: :accepted)
+        end
+      end
+
+    private
+
+      def trainee
+        @trainee ||= current_provider&.trainees&.find_by(slug:)
+      end
+
+      def slug
+        @slug ||= params[:trainee_id]
+      end
+
+      def withdraw_allowed?
+        !trainee.starts_course_in_the_future? && !trainee.itt_not_yet_started? && trainee.awaiting_action?
+      end
+
+      def withdrawal_params
+        params.permit(:withdraw_reasons_details, :withdraw_date)
+      end
+    end
+  end
+end
