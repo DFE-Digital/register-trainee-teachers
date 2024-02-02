0971fbe385bc9584059022ef963a9e4562590cd3
defong <de_fong2k@yahoo.com>
2024-02-02 17:22:40 +0000

    [6715] Added spec for withdrawal

---
+++ spec/requests/api/v0.1/trainees/withdraw_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "info endpoint" do
+  context "with a valid authentication token" do
+    context "non existant trainee" do
+      let(:slug) { "non-existant" }
+
+      it "returns status 404 with a valid JSON response" do
+        post "/api/v0.1/trainees/#{slug}/withdraw", headers: { Authorization: "Bearer bat" }
+        expect(response).to have_http_status(:not_found)
+        expect(response.parsed_body["error"]).to eq("Trainee not found")
+      end
+    end
+
+    context "with a withdrawable trainee" do
+      let(:trainee) { create(:trainee) }
+      let(:withdraw_params) do
+        build(:trainee, :withdrawn_for_specific_reason)
+          .attributes.symbolize_keys.slice(:withdraw_reasons_details, :withdraw_date)
+      end
+      let(:params) { withdraw_params }
+      let(:slug) { trainee.slug }
+
+      before { trainee }
+
+      it "returns status 202 with a valid JSON response" do
+        post("/api/v0.1/trainees/#{slug}/withdraw", headers: { Authorization: "Bearer bat" }, params: params)
+        expect(response).to have_http_status(:accepted)
+        expect(response.parsed_body["status"]).to eq("Trainee withdrawal request accpeted")
+      end
+
+      it "change the trainee" do
+        expect {
+          post("/api/v0.1/trainees/#{slug}/withdraw", headers: { Authorization: "Bearer bat" }, params: params)
+        } .to change { trainee.reload.withdraw_reasons_details }.from(nil).to(withdraw_params[:withdraw_reasons_details])
+        .and change { trainee.reload.withdraw_date }.from(nil).to(withdraw_params[:withdraw_date])
+      end
+
+      context "with invalid params" do
+        let(:params) { { withdraw_reasons_details: nil, withdraw_date: nil } }
+
+        it "returns status 202 with a valid JSON response" do
+          post("/api/v0.1/trainees/#{slug}/withdraw", headers: { Authorization: "Bearer bat" }, params: params)
+
+          expect(response).to have_http_status(:accepted)
+          expect(response.parsed_body["status"]).to eq("Trainee withdrawal request accpeted")
+        end
+
+        it "did not change the trainee" do
+          expect {
+            post "/api/v0.1/trainees/#{slug}/withdraw", headers: { Authorization: "Bearer bat" }
+          }.not_to change(trainee, :withdraw_date)
+        end
+      end
+    end
+
+    context "with a non-withdrawable trainee" do
+      let(:trainee) { create(:trainee, :itt_start_date_in_the_future) }
+      let(:slug) { trainee.slug }
+
+      before { trainee }
+
+      it "returns status 202 with a valid JSON response" do
+        post "/api/v0.1/trainees/#{slug}/withdraw", headers: { Authorization: "Bearer bat" }
+        expect(response).to have_http_status(:accepted)
+        expect(response.parsed_body["status"]).to eq("Trainee withdrawal request accpeted")
+      end
+
+      it "did not change the trainee" do
+        expect {
+          post "/api/v0.1/trainees/#{slug}/withdraw", headers: { Authorization: "Bearer bat" }
+        }.not_to change(trainee, :withdraw_date)
+      end
+    end
+  end
+end
