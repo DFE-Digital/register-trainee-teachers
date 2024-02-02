8b4e869b27127c09a73dd73269b91b5c496ac80a
Adam Colvin <adamc@colvinapps.com>
2024-02-02 11:44:27 +0000

    Add create action to trainees_controller

--- app/controllers/api/trainees_controller.rb
+++ app/controllers/api/trainees_controller.rb
@@ -10,5 +10,28 @@ module Api
         render(json: { error: "Trainee not found" }, status: :not_found)
       end
     end
+
+    def create
+      trainee_attributes = TraineeAttributes.new(trainee_params)
+
+      if trainee_attributes.valid?
+        trainee = Trainee.new(trainee_attributes.to_h)
+
+        if trainee.save
+          render json: Api::TraineeSerializer.new(trainee).as_json, status: :ok
+        else
+          render json: { errors: trainee.errors.full_messages }, status: :unprocessable_entity
+        end
+      else
+        render json: { errors: trainee_attributes.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def trainee_params
+      params.require(:trainee).permit(TraineeAttributes::ATTRIBUTES)
+    end
+
   end
 end
