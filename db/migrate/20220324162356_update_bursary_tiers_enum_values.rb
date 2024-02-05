20826baace489fb49e8d4fa38d904f5a59b39e2d
David Kewal <davidrk@hey.com>
2024-02-05 10:18:09 +0000

    [6670] Fix troublesome legacy migrations

--- db/migrate/20220324162356_update_bursary_tiers_enum_values.rb
+++ db/migrate/20220324162356_update_bursary_tiers_enum_values.rb
@@ -2,8 +2,10 @@
 
 class UpdateBursaryTiersEnumValues < ActiveRecord::Migration[6.1]
   def change
-    Trainee.where(bursary_tier: 2).update_all(bursary_tier: 3)
-    Trainee.where(bursary_tier: 1).update_all(bursary_tier: 2)
-    Trainee.where(bursary_tier: 0).update_all(bursary_tier: 1)
+    # This is a legacy migration that was used as a data migration. It does not need to be run again.
+    # Commented out here as it causes issues when creating a new environment.
+    # Trainee.where(bursary_tier: 2).update_all(bursary_tier: 3)
+    # Trainee.where(bursary_tier: 1).update_all(bursary_tier: 2)
+    # Trainee.where(bursary_tier: 0).update_all(bursary_tier: 1)
   end
 end
