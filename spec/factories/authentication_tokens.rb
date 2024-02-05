1c4495461d18610cc3834b4d5aa226538d30c72d
Adam Colvin <adamc@colvinapps.com>
2024-02-05 15:20:44 +0000

    Update specs and factories

--- spec/factories/authentication_tokens.rb
+++ spec/factories/authentication_tokens.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :authentication_token do
     hashed_token { "MyString" }
-    enabled { false }
-    provider_id { 1 }
+    enabled { true }
+    association :provider
     expires_at { "2024-02-05" }
   end
 end
--- spec/requests/api/v0.1/trainees_spec.rb
+++ spec/requests/api/v0.1/trainees_spec.rb
@@ -4,17 +4,14 @@ require "rails_helper"
 
 describe "Trainees API" do
   describe "GET /api/v0.1/trainees/:id" do
-    let!(:trainee) { create(:trainee, slug: "12345") }
+    let!(:auth_token) { create(:authentication_token, hashed_token: "trainee_token") }
+    let!(:trainee) { create(:trainee, slug: "12345", provider: auth_token.provider) }
 
-    before do
-      allow_any_instance_of(Api::TraineesController).to receive(:current_provider).and_return(trainee.provider) # rubocop:disable RSpec/AnyInstance
-    end
-
-    it_behaves_like "a register API endpoint", "/api/v0.1/trainees/12345"
+    it_behaves_like "a register API endpoint", "/api/v0.1/trainees/12345", "trainee_token"
 
     context "when the trainee exists", feature_register_api: true do
       before do
-        get "/api/v0.1/trainees/#{trainee.slug}", headers: { Authorization: "Bearer bat" }
+        get "/api/v0.1/trainees/#{trainee.slug}", headers: { Authorization: "trainee_token" }
       end
 
       it "returns the trainee" do
@@ -28,7 +25,7 @@ describe "Trainees API" do
 
     context "when the trainee does not exist", feature_register_api: true do
       before do
-        get "/api/v0.1/trainees/nonexistent", headers: { Authorization: "Bearer bat" }
+        get "/api/v0.1/trainees/nonexistent", headers: { Authorization: "trainee_token" }
       end
 
       it "returns status code 404" do
