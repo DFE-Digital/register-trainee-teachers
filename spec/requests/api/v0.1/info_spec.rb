e73ac9a1616fb886e27137b7447988b458a7a924
Adam Colvin <adamc@colvinapps.com>
2024-02-05 15:28:26 +0000

    Update info_spec

--- spec/requests/api/v0.1/info_spec.rb
+++ spec/requests/api/v0.1/info_spec.rb
@@ -3,11 +3,12 @@
 require "rails_helper"
 
 describe "info endpoint" do
-  it_behaves_like "a register API endpoint", "/api/v0.1/info"
+  let!(:auth_token) { create(:authentication_token, hashed_token: "valid_token") }
+  it_behaves_like "a register API endpoint", "/api/v0.1/info", "valid_token"
 
   context "with a valid authentication token and the feature flag on" do
     it "returns status 200 with a valid JSON response" do
-      get "/api/v0.1/info", headers: { Authorization: "Bearer bat" }
+      get "/api/v0.1/info", headers: { Authorization: "valid_token" }
       expect(response).to have_http_status(:ok)
       expect(response.parsed_body["status"]).to eq("ok")
     end
