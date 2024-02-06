dab4b339ffd4d7a610c2d0e37ba57936f9190db3
David Georgiou <david.georgiou@gmail.com>
2024-02-06 10:02:40 +0000

    [6674] - rubocop

--- config/routes/api_routes.rb
+++ config/routes/api_routes.rb
@@ -4,7 +4,7 @@ module ApiRoutes
   def self.extended(router)
     router.instance_exec do
       namespace :api, path: "api/:api_version", api_version: /v[.0-9]+/ do
-        resources :trainees, only: [:index, :show], controller: "trainees", constraints: RouteConstraints::RegisterApiConstraint
+        resources :trainees, only: %i[index show], controller: "trainees", constraints: RouteConstraints::RegisterApiConstraint
         resource :info, only: :show, controller: "info", constraints: RouteConstraints::RegisterApiConstraint
         resource :guide, only: :show, controller: "guide", constraints: RouteConstraints::RegisterApiConstraint
         match "*url" => "base#not_found", via: :all
