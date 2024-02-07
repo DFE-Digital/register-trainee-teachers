7d1507b05814ec1bb5e8f3e024e457eb4351af26
Adam Colvin <adamc@colvinapps.com>
2024-02-07 13:36:15 +0000

    More linting fixes

--- spec/requests/api/v0.1/info_spec.rb
+++ spec/requests/api/v0.1/info_spec.rb
@@ -10,7 +10,7 @@ describe "info endpoint" do
 
   context "with a valid authentication token and the feature flag on" do
     it "returns status 200 with a valid JSON response" do
-      api_get 0.1, :info, token: token
+      api_get(0.1, :info, token:)
       expect(response).to have_http_status(:ok)
       expect(response.parsed_body["status"]).to eq("ok")
     end
