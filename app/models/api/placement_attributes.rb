e7db020537c97273d312f182fab6acc7869fde8e
Adam Colvin <adamc@colvinapps.com>
2024-02-08 15:04:56 +0000

    Add specs for placement_attributes and degree_attributes

--- app/models/api/placement_attributes.rb
+++ app/models/api/placement_attributes.rb
@@ -20,28 +20,28 @@ module Api
     end
 
     validates :name, presence: true, if: -> { school_id.blank? }
-    validate :school_valid, on: %i[create update]
+    validate :school_valid
     validate :urn_valid
     validate :postcode_valid
-  end
 
-private
+    private
 
-  def school_valid
-    if school_id.blank? && [name, urn, postcode].all?(&:blank?)
-      errors.add(:school_id, :blank)
+    def school_valid
+      if school_id.blank? && [name, urn, postcode].all?(&:blank?)
+        errors.add(:school_id, :blank)
+      end
     end
-  end
 
-  def urn_valid
-    if urn.present? && !urn.match?(URN_REGEX)
-      errors.add(:urn, :invalid_format)
+    def urn_valid
+      if urn.present? && !urn.match?(URN_REGEX)
+        errors.add(:urn, :invalid)
+      end
     end
-  end
 
-  def postcode_valid
-    if postcode.present? && !UKPostcode.parse(postcode).valid?
-      errors.add(:postcode, I18n.t("activemodel.errors.validators.postcode.invalid"))
+    def postcode_valid
+      if postcode.present? && !UKPostcode.parse(postcode).valid?
+        errors.add(:postcode, I18n.t("activemodel.errors.validators.postcode.invalid"))
+      end
     end
   end
 end
