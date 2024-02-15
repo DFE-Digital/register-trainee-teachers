491ae1a503cf69c99429096d1bf0476e07bf3bbd
David Georgiou <david.georgiou@gmail.com>
2024-02-15 14:32:54 +0000

    [6704] - remove redundant data migration

--- db/data/20240208151204_add_ethnicity_to_best_practice_network.rb
+++ db/data/20240208151204_add_ethnicity_to_best_practice_network.rb
@@ -4,53 +4,8 @@ require "csv"
 
 class AddEthnicityToBestPracticeNetwork < ActiveRecord::Migration[7.1]
   def up
-    Service.new.call
+    # removed and moved to app/services/add_ethnicity_from_csv.rb
   end
 
   def down; end
-
-  class Service
-    def call
-      return unless upload
-
-      upload.file.download do |csv|
-        CSV.parse(csv, headers: true) do |row|
-          trainee_id = row["trainee_id"]
-          ethnicity = row["ethnicity"]
-          trainee = provider.where(trainee_id:).first
-
-          next unless trainee
-
-          update_trainee(trainee, ethnicity)
-        end
-      end
-    end
-
-  private
-
-    def upload
-      @_upload ||= Upload.find_by_name("bpn-ethnicity.csv")
-    end
-
-    # Best Practice Network
-    def provider
-      @_provider ||= Provider.find_by(code: "6B1").trainees
-    end
-
-    def update_trainee(trainee, ethnicity)
-      ethnicity_split = ethnicity.split(":").map(&:strip)
-
-      ethnic_background = ::Hesa::CodeSets::Ethnicities::NAME_MAPPING[ethnicity_split.first]
-      ethnic_group = ::Diversities::BACKGROUNDS.select { |_key, values| values.include?(ethnic_background) }.keys.first
-      additional_ethnic_background = ethnicity_split.length == 1 ? nil : ethnicity_split.last
-      diversity_disclosure = ["Prefer not to say", "Not available"].include?(ethnic_background) ? :diversity_not_disclosed : :diversity_disclosed
-
-      trainee.update(
-        ethnic_group:,
-        ethnic_background:,
-        additional_ethnic_background:,
-        diversity_disclosure:,
-      )
-    end
-  end
 end
