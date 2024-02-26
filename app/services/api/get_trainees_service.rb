76822c04353a333f6598c63ae9b66a0822ac99ec
Adam Colvin <adamc@colvinapps.com>
2024-02-26 16:20:04 +0000

    Fix linting errors

--- app/services/api/get_trainees_service.rb
+++ app/services/api/get_trainees_service.rb
@@ -16,7 +16,7 @@ module Api
                 .page(page)
                 .per(pagination_per_page)
 
-      filtered_trainees = ::Trainees::Filter.call(trainees: trainees, filters: filters)
+      filtered_trainees = ::Trainees::Filter.call(trainees:, filters:)
       filtered_trainees
     end
 
@@ -29,7 +29,7 @@ module Api
     end
 
     def filters
-      @filters ||= TraineeFilter.new(params: params).filters
+      @filters ||= TraineeFilter.new(params:).filters
     end
   end
 end
