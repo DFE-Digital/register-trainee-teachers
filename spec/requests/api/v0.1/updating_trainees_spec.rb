1f6c8c3231254714c8d578fae442307b1c8f6d22
De Fong <de_fong2k@yahoo.com>
2024-04-16 19:35:15 +0100

    [6983] Added a test to flush out funding rule issues

--- spec/requests/api/v0.1/updating_trainees_spec.rb
+++ spec/requests/api/v0.1/updating_trainees_spec.rb
@@ -7,10 +7,10 @@ describe "Updating a newly created trainee", feature_register_api: true do
   let!(:auth_token) { create(:authentication_token, hashed_token: AuthenticationToken.hash_token(token)) }
   let!(:nationality) { create(:nationality, :british) }
 
-  let!(:course_allocation_subject) do
-    create(:subject_specialism, name: CourseSubjects::BIOLOGY).allocation_subject
-  end
   let(:headers) { { Authorization: token } }
+
+  let(:start_academic_cycle) { create(:academic_cycle, :current) }
+  let(:end_academic_cycle) { create(:academic_cycle, next_cycle: true) }
 
   let(:params_for_create) do
     {
@@ -22,9 +22,9 @@ describe "Updating a newly created trainee", feature_register_api: true do
         email: "john.doe@example.com",
         nationality: "GB",
         training_route: Hesa::CodeSets::TrainingRoutes::MAPPING.invert[TRAINING_ROUTE_ENUMS[:provider_led_undergrad]],
-        itt_start_date: "2023-01-01",
-        itt_end_date: "2023-10-01",
-        course_subject_one: Hesa::CodeSets::CourseSubjects::MAPPING.invert[CourseSubjects::BIOLOGY],
+        itt_start_date: start_academic_cycle.start_date,
+        itt_end_date: end_academic_cycle.end_date,
+        course_subject_one: Hesa::CodeSets::CourseSubjects::MAPPING.invert[course_subject],
         study_mode: Hesa::CodeSets::StudyModes::MAPPING.invert[TRAINEE_STUDY_MODE_ENUMS["full_time"]],
         degrees_attributes: [
           {
@@ -52,37 +52,70 @@ describe "Updating a newly created trainee", feature_register_api: true do
     }
   end
 
-  context "when creating a new trainee with valid params" do
-    before do
-      allow(Api::MapHesaAttributes::V01).to receive(:call).and_call_original
-      allow(Trainees::MapFundingFromDttpEntityId).to receive(:call).and_call_original
+  let(:course_allocation_subject) do
+    subject_specialism = create(:subject_specialism, name: course_subject)
 
-      post "/api/v0.1/trainees", params: params_for_create, headers: headers
-    end
+    subject_specialism.allocation_subject
+  end
 
-    let(:slug) { response.parsed_body["trainee_id"] }
-    let(:trainee) { Trainee.last.reload }
+  [CourseSubjects::PHYSICS, CourseSubjects::BIOLOGY].each do |cs|
+    context "when creating a new trainee with #{cs} course with valid params" do
+      if cs == CourseSubjects::PHYSICS
+        let!(:funding_method) {
+          funding_method = create(:funding_method, :bursary, amount: 9000, training_route: TRAINING_ROUTE_ENUMS[:provider_led_undergrad])
+          create(:funding_method_subject, funding_method: funding_method, allocation_subject: course_allocation_subject)
+        }
+      end
 
-    it "creates a trainee" do
-      expect(response).to have_http_status(:created)
-      expect(Trainee.count).to eq(1)
+      let(:course_subject) { cs }
+      let(:slug) { response.parsed_body["trainee_id"] }
+      let(:trainee) { Trainee.last.reload }
 
-      expect(trainee.state).to eq("submitted_for_trn")
-      expect(trainee.slug).to eq(slug)
-    end
+      before do
+        allow(Api::MapHesaAttributes::V01).to receive(:call).and_call_original
+        allow(Trainees::MapFundingFromDttpEntityId).to receive(:call).and_call_original
 
-    context "when updating a newly created trainee with valid params" do
-      let(:params_for_update) { { data: { first_names: "Alice" } } }
+        post "/api/v0.1/trainees", params: params_for_create, headers: headers
+      end
 
-      it "updates the trainee" do
-        put(
-          "/api/v0.1/trainees/#{slug}",
-          params: params_for_update,
-          headers: headers,
-        )
-        expect(response).to have_http_status(:ok)
-        expect(trainee.first_names).to eq("Alice")
-        expect(response.parsed_body[:data]["trainee_id"]).to eq(slug)
+      it "creates a trainee" do
+        expect(response).to have_http_status(:created)
+        expect(Trainee.count).to eq(1)
+
+        expect(trainee.state).to eq("submitted_for_trn")
+        expect(trainee.slug).to eq(slug)
+      end
+
+      context "when updating a newly created trainee with valid params" do
+        let(:params_for_update) { { data: { first_names: "Alice" } } }
+
+        if cs == CourseSubjects::PHYSICS
+          it "fails to updates the trainee" do
+            put(
+              "/api/v0.1/trainees/#{slug}",
+              params: params_for_update,
+              headers: headers,
+            )
+
+            expect(response).to have_http_status(:unprocessable_entity)
+            expect(response.parsed_body).to match(
+              { "message" => "Validation failed: 2 errors prohibited this trainee from being saved",
+                "errors" => { "funding" => { "applying_for_bursary" => ["Select if you are applying for a funding for this trainee"], "applying_for_grant" => ["Select if you are applying for a funding for this trainee"] } } },
+            )
+          end
+        else
+          it "updates the trainee" do
+            put(
+              "/api/v0.1/trainees/#{slug}",
+              params: params_for_update,
+              headers: headers,
+            )
+
+            expect(response).to have_http_status(:ok)
+            expect(trainee.first_names).to eq("Alice")
+            expect(response.parsed_body[:data]["trainee_id"]).to eq(slug)
+          end
+        end
       end
     end
   end
