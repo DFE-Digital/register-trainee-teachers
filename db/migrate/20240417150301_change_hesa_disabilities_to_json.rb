16109045a97b0217d1eb3edd753a1eb4a96e9db9
Adam Colvin <adamc@colvinapps.com>
2024-04-17 16:11:19 +0100

    Change hesa_disabilities to a json column

---
+++ db/migrate/20240417150301_change_hesa_disabilities_to_json.rb
@@ -0,0 +1,11 @@
+class ChangeHesaDisabilitiesToJson < ActiveRecord::Migration[7.1]
+  def up
+    safety_assured { remove_column :hesa_trainee_details, :hesa_disabilities }
+    add_column :hesa_trainee_details, :hesa_disabilities, :jsonb, default: {}
+  end
+
+  def down
+    safety_assured { remove_column :hesa_trainee_details, :hesa_disabilities }
+    add_column :hesa_trainee_details, :hesa_disabilities, :string, default: [], array: true
+  end
+end
