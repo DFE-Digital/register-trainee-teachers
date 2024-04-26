3f23ef108c7ac588dbc0d07c40d252b28596db68
David Georgiou <david.georgiou@gmail.com>
2024-04-26 15:03:23 +0100

    [6877] - updates openapi init

--- config/initializers/openapi.rb
+++ config/initializers/openapi.rb
@@ -2,5 +2,13 @@
 
 require "rspec/openapi"
 
-RSpec::OpenAPI.path = "public/openapi.json"
-RSpec::OpenAPI.title = "Register API"
+RSpec::OpenAPI.title = "Send Trainee Data Direct API"
+
+RSpec::OpenAPI.path = -> (example) {
+  if match = example.file_path.match(%r[spec/requests/api/(v\d+\.\d+)/])
+    version = match[1]  # Capture the whole version string including 'v'
+    "public/openapi/#{version}.yaml"
+  else
+    'public/openapi.yaml'  # Default path if no version pattern is matched
+  end
+}
