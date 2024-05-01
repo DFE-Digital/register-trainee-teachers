42f9cc80540effe13a8a9ff75ac09908d7e1a8df
De Fong <de_fong2k@yahoo.com>
2024-05-01 13:12:42 +0100

    [7055] Added TraineeAttributesValidation as a stop gap as the chain for `update` and `create` should be similar

--- app/services/api/update_trainee_service/v01.rb
+++ app/services/api/update_trainee_service/v01.rb
@@ -11,11 +11,9 @@ module Api
       end
 
       def call
-        if validation_errors.any?
-          [false, OpenStruct.new(
-            error_count: validation_errors.count,
-            all_errors: validation_errors,
-          )]
+        trainee_attributes_validation = TraineeAttributesValidation.new(trainee_attributes:)
+        if trainee_attributes_validation.invalid?
+          [false, trainee_attributes_validation]
         else
           trainee.assign_attributes(attributes.deep_attributes)
 
@@ -32,21 +30,28 @@ module Api
       attr_reader :trainee, :attributes
 
       alias_method :trainee_attributes, :attributes
-      def validation_errors
-        [*trainee_errors, *hesa_trainee_detail_attributes_errors].compact.flatten
-      end
 
-      def trainee_errors
-        @trainee_errors ||= begin
-          trainee_attributes.validate
-          trainee_attributes.errors&.full_messages
+      TraineeAttributesValidation = Struct.new(:trainee_attributes) do
+        def errors_count = validation_errors.count
+        def all_errors = validation_errors
+        def invalid? = !errors_count.zero?
+
+        def validation_errors
+          [*trainee_errors, *hesa_trainee_detail_attributes_errors].compact.flatten
         end
-      end
 
-      def hesa_trainee_detail_attributes_errors
-        @hesa_trainee_detail_attributes_errors ||= begin
-          trainee_attributes.hesa_trainee_detail_attributes.validate
-          trainee_attributes.hesa_trainee_detail_attributes.errors&.full_messages
+        def trainee_errors
+          @trainee_errors ||= begin
+            trainee_attributes.validate
+            trainee_attributes.errors&.full_messages
+          end
+        end
+
+        def hesa_trainee_detail_attributes_errors
+          @hesa_trainee_detail_attributes_errors ||= begin
+            trainee_attributes.hesa_trainee_detail_attributes.validate
+            trainee_attributes.hesa_trainee_detail_attributes.errors&.full_messages
+          end
         end
       end
 
