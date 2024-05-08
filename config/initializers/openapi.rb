05ca035d5c346f32e75daedfdb5dcc96e1e73c98
David Georgiou <david.georgiou@gmail.com>
2024-05-08 16:51:51 +0100

    [6877] - openapi json => yaml

--- config/initializers/openapi.rb
+++ config/initializers/openapi.rb
@@ -6,7 +6,7 @@ RSpec::OpenAPI.title = "Send Trainee Data Direct API"
 
 RSpec::OpenAPI.path = lambda { |example|
   if (match = example.file_path.match(%r{spec/requests/api/(v\d+\.\d+)/}))
-    "public/openapi/#{match[1]}.json"
+    "public/openapi/#{match[1]}.yaml"
   end
 }
 
