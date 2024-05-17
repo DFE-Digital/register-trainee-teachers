0c42fd2f2e56d62f26500c62c396b66cf7081ac1
De Fong <470137+defong@users.noreply.github.com>
2024-05-17 17:41:27 +0100

    [7077] Added current and next version

--- spec/requests/api/v0.1/get_info_spec.rb
+++ spec/requests/api/v0.1/get_info_spec.rb
@@ -3,5 +3,20 @@
 require "rails_helper"
 
 describe "`GET /info` endpoint" do
-  it_behaves_like "a register API endpoint", "/api/v0.1/info"
+  let(:token) { "info_token" }
+  let!(:auth_token) { create(:authentication_token, hashed_token: AuthenticationToken.hash_token(token)) }
+
+  before do
+    get "/api/#{version}/info", headers: { Authorization: token }
+  end
+
+  context "using version v0.1" do
+    let(:version) { "v0.1" }
+
+    it_behaves_like "a register API endpoint", "/api/v0.1/info", "info_token"
+
+    it "shows the requested version" do
+      expect(response.parsed_body).to eq({ "status" => "ok", "version" => { "next" => "v1.0", "requested" => "v0.1" } })
+    end
+  end
 end
