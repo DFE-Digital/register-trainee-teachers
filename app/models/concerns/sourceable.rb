09a44ce24f961c0e4e8a0c2df41cae53afcf8789
Adam Colvin <adamc@colvinapps.com>
2024-05-17 10:47:33 +0100

    Minor refactor to Sourceable

--- app/models/concerns/sourceable.rb
+++ app/models/concerns/sourceable.rb
@@ -23,7 +23,7 @@ module Sourceable
   included do
     enum record_source: ALL.to_h { |r| [r, r] }, _suffix: :record
 
-    before_save :set_manual_record_source, if: :record_source_missing?
+    before_save :set_manual_record_source, if: -> { record_source.nil? }
 
     def hesa_record?
       hesa_collection_record? || hesa_trn_data_record?
@@ -33,10 +33,6 @@ module Sourceable
 
     def set_manual_record_source
       self.record_source = MANUAL_SOURCE
-    end
-
-    def record_source_missing?
-      record_source.nil? && hesa_id.nil? && apply_application.nil?
     end
   end
 end
