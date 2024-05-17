dfbe6a06a5e5e8ca3564fad8b03a03bf7ca94c2c
De Fong <470137+defong@users.noreply.github.com>
2024-05-17 17:47:44 +0100

    [7077] Added v1.0 version

--- app/lib/route_constraints/register_api_constraint.rb
+++ app/lib/route_constraints/register_api_constraint.rb
@@ -2,8 +2,10 @@
 
 module RouteConstraints
   class RegisterApiConstraint
+    AVAILABLE_VERSIONS = ["v0.1", "v1.0"].freeze
+
     def self.matches?(request)
-      request.path_parameters[:api_version] == "v0.1"
+      AVAILABLE_VERSIONS.include?(request.path_parameters[:api_version])
     end
   end
 end
---
+++ spec/requests/api/v0.1/get_all_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+paths_to_exclude = ["/api-docs/:api_version/openapi", "/api-docs/:api_version/reference", "/api/:api_version/info"]
+paths = Rails.application.routes.routes.select { |route| route.verb == "GET" && route.parts.include?(:api_version) }
+  .map { |route| route.path.spec.to_s.gsub("(.:format)", "") }
+  .select { |path| paths_to_exclude.exclude?(path) }
+  .map { |path| path.gsub(":api_version", "v0.1") }
+
+paths.each do |url|
+  describe "`GET #{url}` endpoint" do
+    let(:token) { "info_token" }
+    let!(:auth_token) { create(:authentication_token, hashed_token: AuthenticationToken.hash_token(token)) }
+
+    let(:trainee_with_trainee_slug) do
+      create(:trainee,
+             :trn_received,
+             provider: auth_token.provider,
+             slug: ":trainee_slug",
+             placements: [build(:placement, slug: ":slug")],
+             degrees: [build(:degree, slug: ":slug")])
+    end
+
+    let(:trainee_with_slug) { create(:trainee, :trn_received, provider: auth_token.provider, slug: ":slug") }
+
+    let(:academic_cycle) { create(:academic_cycle, :current) }
+
+    before do
+      academic_cycle
+      trainee_with_slug
+      trainee_with_trainee_slug
+      get url, headers: { Authorization: token }
+    end
+
+    it "has http status ok" do
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
