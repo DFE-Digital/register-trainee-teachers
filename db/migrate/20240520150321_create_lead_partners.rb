6de317c4d213d1fc25851c92b1f1da22c5b57949
Steve Hook <steve.hook@gmail.com>
2024-05-20 16:25:07 +0100

    Add migration to create `lead_partners` table

--- db/migrate/20240520150321_create_lead_partners.rb
+++ db/migrate/20240520150321_create_lead_partners.rb
@@ -3,10 +3,17 @@
 class CreateLeadPartners < ActiveRecord::Migration[7.1]
   def change
     create_table :lead_partners do |t|
+<<<<<<< HEAD
       t.citext :urn, null: false
       t.string :record_type, null: false
       t.string :name
       t.citext :ukprn
+=======
+      t.string :urn, null: false
+      t.string :record_type, null: false
+      t.string :name
+      t.string :ukprn
+>>>>>>> 2c6dc5660 (Add migration to create `lead_partners` table)
       t.references :school, null: true, foreign_key: true
       t.references :provider, null: true, foreign_key: true
       t.timestamps
