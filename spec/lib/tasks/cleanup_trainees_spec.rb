741905a3aaf388d21ee40f0bb148e6b178e0425e
Adam Colvin <adamc@colvinapps.com>
2024-05-21 11:37:47 +0100

    Fix rubocop issues

--- spec/lib/tasks/cleanup_trainees_spec.rb
+++ spec/lib/tasks/cleanup_trainees_spec.rb
@@ -1,35 +1,37 @@
-require 'rails_helper'
-require 'rake'
+# frozen_string_literal: true
 
-describe 'cleanup:discard_trainees' do
+require "rails_helper"
+require "rake"
+
+describe "cleanup:discard_trainees" do
   before :all do
     Rake.application.rake_require "tasks/cleanup"
     Rake::Task.define_task(:environment)
   end
 
   let(:run_rake_task) do
-    Rake::Task['cleanup:discard_trainees'].reenable
+    Rake::Task["cleanup:discard_trainees"].reenable
     Rake.application.invoke_task "cleanup:discard_trainees[#{file_path}]"
   end
 
-  context 'when file path is provided' do
-    let(:file_path) { Rails.root.join('spec', 'fixtures', 'files', 'cleanup_emails.txt') }
+  context "when file path is provided" do
+    let(:file_path) { Rails.root.join("spec/fixtures/files/cleanup_emails.txt") }
     let(:emails) { File.readlines(file_path).map(&:chomp) }
 
     before do
-      emails.each { |email| create(:trainee, email: email) }
+      emails.each { |email| create(:trainee, email:) }
     end
 
-    it 'destroys the trainees with emails in the file' do
+    it "destroys the trainees with emails in the file" do
       expect { run_rake_task }.to change { Trainee.discarded.count }.by(emails.count)
     end
   end
 
-  context 'when file path is not provided' do
-    let(:file_path) { '' }
+  context "when file path is not provided" do
+    let(:file_path) { "" }
 
-    it 'raises an error' do
-      expect { run_rake_task }.to raise_error(RuntimeError, 'You must provide a file path')
+    it "raises an error" do
+      expect { run_rake_task }.to raise_error(RuntimeError, "You must provide a file path")
     end
   end
 end
