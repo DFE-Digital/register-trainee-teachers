778f286d19e6fc886f873c38781133af09a213b3
De Fong <470137+defong@users.noreply.github.com>
2024-05-24 12:46:24 +0100

    [7185] Added version guard serialiser

--- spec/serializers/serializer_spec.rb
+++ spec/serializers/serializer_spec.rb
@@ -3,8 +3,9 @@
 require "rails_helper"
 
 RSpec.describe Serializer do
+  serializers = %i[degree hesa_trainee_detail placement trainee]
   context "v0.1" do
-    %i[degree hesa_trainee_detail placement trainee].each do |serializer|
+    serializers.each do |serializer|
       it "#{serializer} has been implemented" do
         expect(described_class.for(model: serializer, version: "v0.1")).to be(Object.const_get("#{serializer}_Serializer::V01".camelize))
       end
@@ -12,9 +13,21 @@ RSpec.describe Serializer do
   end
 
   context "v1.0" do
-    %i[degree hesa_trainee_detail placement trainee].each do |serializer|
-      it "#{serializer} has been implemented" do
+    serializers.each do |serializer|
+      it "#{serializer} has not been implemented" do
         expect { described_class.for(model: serializer, version: "v1.0") }.to raise_error(NotImplementedError, "#{serializer}_Serializer::V10".camelize)
+      end
+    end
+  end
+
+  context "v0.1 not on the allowed versions" do
+    before do
+      allow(Settings.api).to receive(:allowed_versions).and_return([])
+    end
+
+    serializers.each do |serializer|
+      it "#{serializer} has not been implemented" do
+        expect { described_class.for(model: serializer, version: "v0.1") }.to raise_error(NotImplementedError, "#{serializer}_Serializer::V01".camelize)
       end
     end
   end
