ffb55fde56cbe7905c6d7f0cab4be53195d2b47a
Steve Hook <steve.hook@gmail.com>
2024-05-29 14:58:44 +0100

    WIP Autocomplete

---
+++ app/javascript/scripts/components/form_components/lead_partners_autocomplete.js
@@ -0,0 +1,119 @@
+import accessibleAutocomplete from 'accessible-autocomplete'
+import tracker from './tracker.js'
+
+const $allAutocompleteElements = document.querySelectorAll('[data-module="app-lead-partners-autocomplete"]')
+const idElement = document.getElementById('lead-partners-id')
+
+let statusMessage = ' '
+
+const guard = (data) => {
+  if (data === undefined) {
+    return new Error('An error occured')
+  }
+
+  return data
+}
+
+const mapToLeadPartners = (data) => data.lead_partners
+
+const tryUpdateStatusMessage = (lead_partners) => {
+  if (lead_partners.length === 0) {
+    statusMessage = 'No results found'
+  }
+
+  return lead_partners
+}
+
+const findLeadPartners = ({ query, populateResults }) => {
+  idElement.value = ''
+
+  const encodedQuery = encodeURIComponent(query)
+
+  statusMessage = 'Loading...' // Shared state
+
+  const.log('fetching lead partners')
+  window.fetch(`/autocomplete/lead_partners?query=${encodedQuery} ? 'true' : 'false'}`)
+    .then(response => response.json())
+    .then(guard)
+    .then(mapToLeadPartners)
+    .then(tryUpdateStatusMessage)
+    .then(populateResults)
+    .catch(console.log)
+}
+
+const inputTemplate = (value) => {
+  return value && value.name
+}
+
+const suggestionTemplate = (result) => {
+  if (result) {
+    if (typeof result === 'string') {
+      return result
+    } else if (typeof result === 'object') {
+      const hints = [`URN ${result.urn}`, result.town, result.postcode].filter(Boolean)
+
+      return `${result.name} <span class="autocomplete__option--hint">${hints.join(', ')}</span>`
+    }
+  } else return ''
+}
+
+const renderTemplate = {
+  inputValue: inputTemplate,
+  suggestion: suggestionTemplate
+}
+
+const setLeadPartnerHiddenField = (value) => {
+  if (value === undefined) {
+    return
+  }
+
+  idElement.value = value.id
+}
+
+const setupAutoComplete = (form) => {
+  const element = form.querySelector('#lead-partners-autocomplete-element')
+  const inputs = form.querySelectorAll('[data-field="lead-partners-autocomplete"]')
+  const defaultValueOption = element.getAttribute('data-default-value') || ''
+  const fieldName = element.getAttribute('data-field-name') || ''
+
+  try {
+    inputs.forEach(input => {
+      accessibleAutocomplete({
+        element,
+        id: input.id,
+        minLength: 2,
+        defaultValue: defaultValueOption,
+        name: fieldName,
+        source: (query, populateResults) => {
+          tracker.trackSearch(query)
+          return findLeadPartners({
+            query,
+            populateResults,
+          })
+        },
+        templates: renderTemplate,
+        onConfirm: (value) => {
+          if (value?.id && element.dataset.systemAdminRedirectLeadPartners) {
+            window.location.assign(`/system-admin/lead-lead-partners/${value.id}`)
+          } else {
+            tracker.sendTrackingEvent(value, fieldName)
+            setLeadPartnerHiddenField(value)
+          }
+        },
+        tNoResults: () => statusMessage
+      })
+
+      // Move autocomplete to the form group containing the input to be replaced
+      const inputFormGroup = element.previousElementSibling
+      if (inputFormGroup.contains(input)) {
+        inputFormGroup.appendChild(element)
+      }
+
+      input.remove()
+    })
+  } catch (err) {
+    console.error('Could not enhance:', err)
+  }
+}
+
+$allAutocompleteElements.forEach(setupAutoComplete)
