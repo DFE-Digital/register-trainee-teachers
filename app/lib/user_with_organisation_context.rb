23fee3079f7c7b5e2fa278ec781561bf5674c4c0
Serafeim Maroulis <serafeim@hey.com>
2024-05-31 16:53:00 +0100

    Refactor UserWithOrganisationContext
    Add support for LeadPartner

--- app/lib/user_with_organisation_context.rb
+++ app/lib/user_with_organisation_context.rb
@@ -34,13 +34,14 @@ class UserWithOrganisationContext < SimpleDelegator
 
     return if session[:current_organisation].blank?
 
-    school_or_provider
+    lead_partner_or_school_or_provider
   end
 
   def multiple_organisations?
     return false unless FeatureService.enabled?(:user_can_have_multiple_organisations)
 
-    (user.lead_schools + user.providers).count > 1
+    (user.lead_schools + user.providers).count > 1 ||
+    (user.lead_partners + user.providers).count > 1
   end
 
   def provider?
@@ -51,39 +52,51 @@ class UserWithOrganisationContext < SimpleDelegator
     organisation.is_a?(School)
   end
 
+  def lead_partner?
+    organisation.is_a?(LeadPartner)
+  end
+
   def no_organisation?
     return false unless FeatureService.enabled?(:user_can_have_multiple_organisations)
 
-    user.providers.none? && user.lead_schools.none? && !user.system_admin?
+    user.providers.none? && user.lead_schools.none? && user.lead_partners.none? && !user.system_admin?
   end
 
 private
 
   attr_reader :session
 
-  def organisation_class
-    session[:current_organisation][:type] == "School" ? School : Provider
+  def organisation_type
+    session[:current_organisation][:type]
   end
 
   def organisation_id
     session[:current_organisation][:id]
   end
 
-  def school_or_provider
-    if organisation_class == Provider
-      user.providers.find_by(id: organisation_id)
-    else
-      user.lead_schools.find_by(id: organisation_id)
-    end
+  def lead_partner_or_school_or_provider
+    @_lead_partner_or_school_or_provider ||= {
+      "LeadPartner" => user.lead_partners.find_by(id: organisation_id),
+      "School" => user.lead_schools.find_by(id: organisation_id),
+      "Provider" => user.providers.find_by(id: organisation_id),
+    }[organisation_type]
   end
 
   def single_organisation
-    raise(Pundit::NotAuthorizedError) if user_only_has_lead_school? && !FeatureService.enabled?(:user_can_have_multiple_organisations)
+    if (user_only_has_lead_school? || user_only_has_lead_partner?) &&
+        !FeatureService.enabled?(:user_can_have_multiple_organisations)
+
+      raise(Pundit::NotAuthorizedError)
+    end
 
     user.providers.first || user.lead_schools.first
   end
 
   def user_only_has_lead_school?
     !user.system_admin && user.providers.empty? && user.lead_schools.present?
+  end
+
+  def user_only_has_lead_partner?
+    !user.system_admin && user.providers.empty? && user.lead_partners.present?
   end
 end
