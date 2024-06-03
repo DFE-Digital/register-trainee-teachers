6fb5657ca6607e2735cd80203a2622b708c38d98
Adam Colvin <adamc@colvinapps.com>
2024-06-03 13:39:35 +0100

    Update boot.sh

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,3 +1,9 @@
 echo "Creating database..."
+bin/rails db:setup
+
+echo "Starting site..."
+bundle exec rails s -p 5001 &
+yarn build --watch &
+yarn build:css --watch &
+
 echo "Done!"
-# bin/rails db:setup
