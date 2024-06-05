5bac5ae80523b3dc8c72ca1b52a7959aded9f127
Serafeim Maroulis <serafeim@hey.com>
2024-06-05 15:40:04 +0100

    Refactor SendWelcomeEmailService for LeadPartner

--- spec/services/send_welcome_email_service_spec.rb
+++ spec/services/send_welcome_email_service_spec.rb
@@ -48,13 +48,47 @@ describe SendWelcomeEmailService do
       end
     end
 
-    context "not a lead school user" do
+    context "lead partner user" do
       let(:user) do
         spy(
           first_name: "Meowington",
           email: "meowington@cat.net",
           welcome_email_sent_at: nil,
           lead_schools: [],
+          lead_partners: [build(:lead_partner, :lead_school)],
+        )
+      end
+
+      before do
+        allow(WelcomeEmailMailer).to receive_message_chain(:generate, :deliver_later)
+        described_class.call(user:)
+      end
+
+      it "sets their welcome email date to now" do
+        expect(user).to have_received(:update!).with(hash_including(welcome_email_sent_at: Time.zone.now))
+      end
+
+      it "sends the welcome email" do
+        expect(WelcomeEmailMailer).to have_received(:generate)
+      end
+
+      it "sends the email to the user" do
+        expect(WelcomeEmailMailer).to have_received(:generate).with(hash_including(email: "meowington@cat.net"))
+      end
+
+      it "sends the users first name in the email" do
+        expect(WelcomeEmailMailer).to have_received(:generate).with(hash_including(first_name: "Meowington"))
+      end
+    end
+
+    context "not a lead school or lead partner user" do
+      let(:user) do
+        spy(
+          first_name: "Meowington",
+          email: "meowington@cat.net",
+          welcome_email_sent_at: nil,
+          lead_schools: [],
+          lead_partners: [],
         )
       end
 
@@ -74,6 +108,32 @@ describe SendWelcomeEmailService do
         first_name: "Meowington",
         welcome_email_sent_at: Time.zone.local(2021, 1, 1),
         lead_schools: [build(:school, :lead)],
+      )
+    end
+
+    before do
+      allow(WelcomeEmailMailer).to receive_message_chain(:generate, :deliver_later)
+      described_class.call(user:)
+    end
+
+    context "and has received a welcome email" do
+      it "does not update their welcome email date" do
+        expect(user).not_to have_received(:update!).with(hash_including(welcome_email_sent_at: Time.zone.now))
+      end
+
+      it "does not send the welcome email" do
+        expect(WelcomeEmailMailer).not_to have_received(:generate)
+      end
+    end
+  end
+
+  context "when a partner user has logged in before" do
+    let(:user) do
+      spy(
+        first_name: "Meowington",
+        welcome_email_sent_at: Time.zone.local(2021, 1, 1),
+        lead_schools: [],
+        lead_partners: [build(:lead_partner, :lead_school)],
       )
     end
 
