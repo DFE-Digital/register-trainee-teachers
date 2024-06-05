b3b23ad90165cec0a848e1915adcf83fe2495a67
Serafeim Maroulis <serafeim@hey.com>
2024-06-05 16:12:41 +0100

    Refactor TraineePersonalDetails::View for LeadPartner users

--- spec/components/trainee_personal_details/view_spec.rb
+++ spec/components/trainee_personal_details/view_spec.rb
@@ -3,43 +3,86 @@
 require "rails_helper"
 
 describe TraineePersonalDetails::View do
-  let(:lead_school_user?) { false }
   let(:trainee) { create(:trainee) }
-  let(:current_user) { create(:school) }
   let(:policy) { instance_double(TraineePolicy, update?: true) }
 
   subject(:component) { described_class.new(trainee:, current_user:) }
 
   before do
     allow(TraineePolicy).to receive(:new).and_return(policy)
-    allow(current_user).to receive(:lead_school?).and_return(lead_school_user?)
-    render_inline(component)
   end
 
-  context "with a trainee with no degree" do
-    it "renders the incomplete section component" do
-      expect(rendered_content).to have_text("Add degree details")
+  context "with a lead school user" do
+    let(:current_user) do
+      UserWithOrganisationContext.new(
+        user: create(:user, :with_lead_school_organisation), session: {}
+      )
+    end
+
+    before do
+      render_inline(component)
+    end
+
+    context "with a trainee with no degree" do
+      it "renders the incomplete section component" do
+        expect(rendered_content).to have_text("Add degree details")
+      end
+    end
+
+    context "with a trainee with a degree" do
+      let(:trainee) { create(:trainee, :in_progress) }
+
+      it "renders the confirmation component" do
+        expect(rendered_content).to have_text("Add another degree")
+      end
+    end
+
+    context "minimal mode" do
+      it "doesn't display sex or nationality" do
+        expect(rendered_content).not_to have_text("Sex")
+        expect(rendered_content).not_to have_text("Nationality")
+      end
+
+      it "doesn't display diversity information" do
+        expect(rendered_content).not_to have_text("Diversity")
+      end
     end
   end
 
-  context "with a trainee with a degree" do
-    let(:trainee) { create(:trainee, :in_progress) }
-
-    it "renders the confirmation component" do
-      expect(rendered_content).to have_text("Add another degree")
-    end
-  end
-
-  context "minimal mode" do
-    let(:lead_school_user?) { true }
-
-    it "doesn't display sex or nationality" do
-      expect(rendered_content).not_to have_text("Sex")
-      expect(rendered_content).not_to have_text("Nationality")
+  context "with a lead partner user" do
+    let(:current_user) do
+      UserWithOrganisationContext.new(
+        user: create(:user, :with_lead_partner_organisation), session: {}
+      )
     end
 
-    it "doesn't display diversity information" do
-      expect(rendered_content).not_to have_text("Diversity")
+    before do
+      render_inline(component)
+    end
+
+    context "with a trainee with no degree" do
+      it "renders the incomplete section component" do
+        expect(rendered_content).to have_text("Add degree details")
+      end
+    end
+
+    context "with a trainee with a degree" do
+      let(:trainee) { create(:trainee, :in_progress) }
+
+      it "renders the confirmation component" do
+        expect(rendered_content).to have_text("Add another degree")
+      end
+    end
+
+    context "minimal mode" do
+      it "doesn't display sex or nationality" do
+        expect(rendered_content).not_to have_text("Sex")
+        expect(rendered_content).not_to have_text("Nationality")
+      end
+
+      it "doesn't display diversity information" do
+        expect(rendered_content).not_to have_text("Diversity")
+      end
     end
   end
 end
