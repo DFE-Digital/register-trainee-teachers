7414eba8e8437e3a0496336d4340ff36624cd965
Adam Colvin <adamc@colvinapps.com>
2024-06-06 15:16:46 +0100

    Rubocop

--- app/controllers/concerns/codespaceable.rb
+++ app/controllers/concerns/codespaceable.rb
@@ -3,15 +3,15 @@
 module Codespaceable
   extend ActiveSupport::Concern
 
-  GITHUB_APP_DOMAIN = "app.github.dev".freeze
+  GITHUB_APP_DOMAIN = "app.github.dev"
 
   included do
     skip_before_action :verify_authenticity_token, if: :running_in_codespace?
   end
 
-  private
+private
 
-    def running_in_codespace?
-      Rails.env.development? && request.host.ends_with?(GITHUB_APP_DOMAIN)
-    end
+  def running_in_codespace?
+    Rails.env.development? && request.host.ends_with?(GITHUB_APP_DOMAIN)
+  end
 end
