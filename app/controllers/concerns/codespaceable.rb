613909ccfebd7227df58b8a40c0c3759364f30ec
Adam Colvin <adamc@colvinapps.com>
2024-06-06 11:37:01 +0100

    Add codespaceable concern

---
+++ app/controllers/concerns/codespaceable.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Codespaceable
+  extend ActiveSupport::Concern
+
+  GITHUB_APP_DOMAIN = "app.github.dev".freeze
+
+  included do
+    skip_before_action :verify_authenticity_token if: :running_in_codespace?
+  end
+
+  private
+
+    def running_in_codespace?
+      Rails.env.development? && request.host.contains?(GITHUB_APP_DOMAIN)
+    end
+end
