8a33455c8b55ea5e5c251cb4a8a96cdf85986413
Adam Colvin <adamc@colvinapps.com>
2024-06-14 10:23:30 +0100

    More rubocop fixes

--- db/migrate/20240613142252_drop_application_choice_id_from_apply_applications.rb
+++ db/migrate/20240613142252_drop_application_choice_id_from_apply_applications.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class DropApplicationChoiceIdFromApplyApplications < ActiveRecord::Migration[7.1]
   def change
-    safety_assured { remove_column :apply_applications, :application_choice_id }
+    safety_assured { remove_column :apply_applications, :application_choice_id, :string }
   end
 end
