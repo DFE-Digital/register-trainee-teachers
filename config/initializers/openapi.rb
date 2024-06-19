13f4e5abcf91c3f3d038da21093518888e3d5b65
Chris Gannon <cjgannon@gmail.com>
2024-06-19 12:26:08 +0100

    update docs and config to look for specs in correct location

--- config/initializers/openapi.rb
+++ config/initializers/openapi.rb
@@ -5,7 +5,9 @@ require "rspec/openapi"
 RSpec::OpenAPI.title = "Register API"
 
 RSpec::OpenAPI.path = lambda { |example|
-  if (match = example.file_path.match(%r{spec/requests/api/(v\d+\.\d+)/}))
-    "public/openapi/#{match[1]}.yaml"
+  if (match = example.file_path.match(%r{spec/requests/api/(v\d+\_\d+)/}))
+    version = match[1].gsub("_", ".")
+
+    "public/openapi/#{version}.yaml"
   end
 }
