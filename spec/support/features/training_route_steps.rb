66d33bc5e0a72aa45ee6168aabb6f1b5d11833f7
Steve Hook <steve.hook@gmail.com>
2024-06-27 00:54:34 +0100

    Add feature spec for provider_led_postgrad_salaried
    High-level test. Follows the same pattern as other routes.

--- spec/support/features/training_route_steps.rb
+++ spec/support/features/training_route_steps.rb
@@ -6,6 +6,10 @@ module Features
       choose_training_route_for(TRAINING_ROUTE_ENUMS[:provider_led_postgrad])
     end
 
+    def given_i_have_created_a_provider_led_salaried_trainee
+      choose_training_route_for(TRAINING_ROUTE_ENUMS[:provider_led_postgrad_salaried])
+    end
+
     def given_i_have_created_a_provider_led_undergrad_trainee
       choose_training_route_for(TRAINING_ROUTE_ENUMS[:provider_led_undergrad])
     end
