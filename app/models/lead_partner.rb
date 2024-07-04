9a4a87ffc9832bff10664e95ebb5fb4818170794
Adam Colvin <adamcolv@icloud.com>
2024-07-04 17:02:11 +0100

    Add SCITT Providers to Lead Partners (#4439)
    * Add scitt record type
    
    * Add specs for scitt record source

--- app/models/lead_partner.rb
+++ app/models/lead_partner.rb
@@ -31,6 +31,7 @@ class LeadPartner < ApplicationRecord
   RECORD_TYPES = [
     LEAD_SCHOOL = "lead_school",
     HEI = "hei",
+    SCITT = "scitt",
   ].freeze
   enum record_type: RECORD_TYPES.to_h { |record_type| [record_type, record_type] }
 
