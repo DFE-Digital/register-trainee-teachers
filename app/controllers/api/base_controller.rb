8cf5589739def4e5e88de932453f13f977a4944c
Adam Colvin <adamcolv@icloud.com>
2024-07-11 15:44:13 +0100

    Track unsuccessful requests

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -82,10 +82,36 @@ module Api
 
     def track_request_metrics
       start = Time.zone.now
-      Yabeda.register_api.requests_total.increment({})
-      yield
+      track_total_requests
+      begin
+        yield
+      rescue => ex
+        track_unsuccessful_requests
+        raise ex
+      ensure
+        track_request_duration(start:)
+      end
+    end
+
+    def track_total_requests
+      Yabeda.register_api.requests_total.increment(tracking_labels)
+    end
+
+    def track_unsuccessful_requests
+      Yabeda.register_api.unsuccessful_requests_total.increment(tracking_labels)
+    end
+
+    def track_request_duration(start:)
       duration = Time.zone.now - start
-      Yabeda.register_api.request_duration.measure({}, duration)
+      Yabeda.register_api.request_duration.measure(tracking_labels, duration)
+    end
+
+    def tracking_labels
+      @tracking_labels ||= {
+        method: request.method,
+        controller: controller_name,
+        action: action_name
+      }
     end
   end
 end
--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -2,10 +2,11 @@
 
 Yabeda.configure do
   group :register_api do
-    counter   :requests_total, comment: "Total number of Request API requests"
+    counter   :requests_total, comment: "Total number of Register API requests"
     histogram :request_duration,
               buckets: [0.1, 0.5, 1, 5, 10],
               unit: :seconds,
               comment: "Request API request duration"
+    counter :unsuccessful_requests_total, comment: "Total number of unsuccessful Register API requests"
   end
 end
