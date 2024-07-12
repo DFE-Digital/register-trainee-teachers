c8971b73d63ad3b755af1f398a651d9ad21e462e
Adam Colvin <adamcolv@icloud.com>
2024-07-12 10:28:51 +0100

    Rubocop

--- spec/requests/api/monitoring_spec.rb
+++ spec/requests/api/monitoring_spec.rb
@@ -1,6 +1,8 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-describe 'API Monitoring', type: :request do
+require "rails_helper"
+
+describe "API Monitoring" do
   let(:token) { "trainee_token" }
   let!(:auth_token) { create(:authentication_token, hashed_token: AuthenticationToken.hash_token(token)) }
   let!(:trainee) { create(:trainee, :with_hesa_trainee_detail, slug: "12345", provider: auth_token.provider) }
@@ -12,18 +14,18 @@ describe 'API Monitoring', type: :request do
     )
   end
 
-  it 'increments request_total and measures request_duration for successful request' do
+  it "increments request_total and measures request_duration for successful request" do
     expect(Yabeda.register_api.requests_total).to receive(:increment)
     expect(Yabeda.register_api.request_duration).to receive(:measure)
     response
   end
 
-  it 'measures response size for every request' do
+  it "measures response size for every request" do
     expect(Yabeda.register_api.response_size).to receive(:measure)
     response
   end
 
-  it 'increments unsuccessful_requests_total for failed request' do
+  it "increments unsuccessful_requests_total for failed request" do
     allow_any_instance_of(Api::TraineesController).to receive(:show).and_raise(StandardError)
     expect(Yabeda.register_api.unsuccessful_requests_total).to receive(:increment)
     expect { response }.to raise_error(StandardError)
