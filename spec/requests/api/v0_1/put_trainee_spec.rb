c7788d08bc91640245f525c2950c0621af14531c
Serafeim Maroulis <serafeim@hey.com>
2024-07-26 16:20:20 +0100

    Fix request specs

--- spec/requests/api/v0_1/put_trainee_spec.rb
+++ spec/requests/api/v0_1/put_trainee_spec.rb
@@ -534,7 +534,16 @@ describe "`PUT /api/v0.1/trainees/:id` endpoint" do
       let(:ethnic_background) { Dttp::CodeSets::Ethnicities::MAPPING.keys.sample }
       let(:ethnic_group) { Diversities::BACKGROUNDS.select { |_key, values| values.include?(ethnic_background) }&.keys&.first }
       let(:trainee) do
-        create(:trainee, :in_progress, :with_training_route, :with_hesa_trainee_detail, :with_diversity_information, ethnic_group:, ethnic_background:)
+        create(
+          :trainee,
+          :in_progress,
+          :with_lead_partner,
+          :with_training_route,
+          :with_hesa_trainee_detail,
+          :with_diversity_information,
+          ethnic_group:,
+          ethnic_background:,
+        )
       end
 
       before do
