98ab55a4046ac38e368ea4c82b11f37b31a2c59d
Serafeim Maroulis <serafeim@hey.com>
2024-07-26 15:07:34 +0100

    Remove alias

--- app/models/trainee.rb
+++ app/models/trainee.rb
@@ -208,8 +208,6 @@ class Trainee < ApplicationRecord
 
   delegate :update_training_route!, to: :route_data_manager
 
-  alias_attribute :lead_partner_not_applicable, :lead_school_not_applicable
-
   validates :training_route, presence: {
     message: I18n.t("activerecord.errors.models.trainee.attributes.training_route"),
   }
