592062e2fb8c4921867c1fee88f967558ea3f9e9
Adam Colvin <adamcolv@icloud.com>
2024-08-06 11:13:24 +0100

    Remove lead_school_user helper methods
    These aren’t relevant anymore, we only need the lead_partner_user? helper method where applicable.

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -5,38 +5,12 @@ require "rails_helper"
 describe UsersHelper do
   include UsersHelper
 
-  describe "#lead_school_user?" do
-    describe "lead school user" do
-      let(:current_user) { double(UserWithOrganisationContext, lead_school?: true) }
-
-      it "returns true" do
-        expect(lead_school_user?).to be(true)
-      end
-    end
-
-    describe "non-lead school user" do
-      let(:current_user) { double(UserWithOrganisationContext, lead_school?: false) }
-
-      it "returns false" do
-        expect(lead_school_user?).to be(false)
-      end
-    end
-  end
-
   describe "#lead_partner_user?" do
     describe "lead partner user" do
       let(:current_user) { double(UserWithOrganisationContext, lead_partner?: true) }
 
       it "returns true" do
         expect(lead_partner_user?).to be(true)
-      end
-    end
-
-    describe "non-lead school user" do
-      let(:current_user) { double(UserWithOrganisationContext, lead_school?: false) }
-
-      it "returns false" do
-        expect(lead_school_user?).to be(false)
       end
     end
   end
