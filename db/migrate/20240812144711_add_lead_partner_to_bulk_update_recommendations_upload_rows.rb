faa40a52a55c264abeea261f98a63742d7ede867
Adam Colvin <adamcolv@icloud.com>
2024-08-12 16:59:24 +0100

    Rubocop

--- db/migrate/20240812144711_add_lead_partner_to_bulk_update_recommendations_upload_rows.rb
+++ db/migrate/20240812144711_add_lead_partner_to_bulk_update_recommendations_upload_rows.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddLeadPartnerToBulkUpdateRecommendationsUploadRows < ActiveRecord::Migration[7.1]
   def change
     add_column :bulk_update_recommendations_upload_rows, :lead_partner, :string
