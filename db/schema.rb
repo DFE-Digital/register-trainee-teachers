9b95d5f2609e06d27293495517cf769a331d1579
Adam Colvin <adamcolv@icloud.com>
2024-08-12 16:28:42 +0100

    Correct schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_19_095753) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_12_144711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "citext"
