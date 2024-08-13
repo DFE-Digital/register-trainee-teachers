6d9400c6db7f2d607a7ad11be28cf9f20d806ca0
Adam Colvin <adamcolv@icloud.com>
2024-08-13 13:57:23 +0100

    Rubocop

--- db/migrate/20240813122304_add_lead_parnter_urn_to_funding_trainee_summary_rows.rb
+++ db/migrate/20240813122304_add_lead_parnter_urn_to_funding_trainee_summary_rows.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddLeadParnterUrnToFundingTraineeSummaryRows < ActiveRecord::Migration[7.1]
   def change
     add_column :funding_trainee_summary_rows, :lead_partner_urn, :string
