893cc47e697d8948bc60cb3749e6423d9c539367
Serafeim Maroulis <serafeim@hey.com>
2024-08-19 09:18:07 +0100

    Add Funding::UpdateTraineeSummaryRowRouteService
    * Will be used to fill the TraineeSummaryRow#route_key

---
+++ app/services/funding/update_trainee_summary_row_route_service.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Funding
+  class UpdateTraineeSummaryRowRouteService
+    include ServicePattern
+
+    ROUTE_MAPPING = {
+      "Early years graduate employment based" => :early_years_salaried,
+      "Provider-led" => :provider_led,
+      "School Direct tuition fee" => :school_direct_tuition_fee,
+      "School Direct salaried" => :school_direct_salaried,
+      "Assessment only" => :assessment_only,
+      "Early years assessment only" => :early_years_assessment_only,
+      "Early years" => :early_years,
+      "International qualified teacher status (iQTS)" => :iqts,
+      "Primary and secondary" => :provider_led_undergrad,
+      "Teaching apprenticeship" => :pg_teaching_apprenticeship,
+      "School direct" => :school_direct_salaried,
+      "HPITT" => :hpitt_postgrad,
+      "Opt-in" => :opt_in_undergrad,
+    }.freeze
+
+    def initialize(academic_year: "2024/2025")
+      @academic_year = academic_year
+    end
+
+    def call
+      rows.find_each(batch_size: 100) do |row|
+        row.update!(route_key: ROUTE_MAPPING[row.route])
+      end
+    end
+
+  private
+
+    attr_reader :academic_year
+
+    def rows
+      TraineeSummaryRow
+        .joins(:trainee_summary)
+        .where(
+          trainee_summary: { academic_year: },
+          route_key: nil,
+        )
+    end
+  end
+end
---
+++ spec/services/funding/update_trainee_summary_row_route_service_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Funding::UpdateTraineeSummaryRowRouteService do
+  subject { described_class }
+
+  describe "#call" do
+    let(:previous_academic_year) { "2023/2024" }
+    let(:current_academic_year) { "2024/2025" }
+
+    let!(:previous_academic_year_funding_trainee_summary) do
+      create(
+        :trainee_summary,
+        :for_school,
+        :with_bursary_and_scholarship_and_multiple_amounts,
+        academic_year: previous_academic_year,
+      )
+    end
+
+    let!(:current_academic_year_funding_trainee_summary) do
+      create(
+        :trainee_summary,
+        :for_school,
+        :with_bursary_and_scholarship_and_multiple_amounts,
+        academic_year: current_academic_year,
+      )
+    end
+
+    let(:mapping) do
+      {
+        "Early years graduate employment based" => :early_years_salaried,
+        "Provider-led" => :provider_led,
+        "School Direct tuition fee" => :school_direct_tuition_fee,
+        "School Direct salaried" => :school_direct_salaried,
+        "Assessment only" => :assessment_only,
+        "Early years assessment only" => :early_years_assessment_only,
+        "Early years" => :early_years,
+        "International qualified teacher status (iQTS)" => :iqts,
+        "Primary and secondary" => :provider_led_undergrad,
+        "Teaching apprenticeship" => :pg_teaching_apprenticeship,
+        "School direct" => :school_direct_salaried,
+        "HPITT" => :hpitt_postgrad,
+        "Opt-in" => :opt_in_undergrad,
+      }
+    end
+
+    it "has the correct mapping" do
+      expect(subject::ROUTE_MAPPING).to eq(mapping)
+    end
+
+    it "sets the route_key only for the current_academic_year" do
+      subject.call
+
+      previous_academic_year_funding_trainee_summary.rows.reload.each do |row|
+        expect(row.route_key).to be_nil
+      end
+
+      current_academic_year_funding_trainee_summary.rows.reload.each do |row|
+        expect(row.route_key).to eq(mapping[row.route].to_s)
+      end
+    end
+  end
+end
