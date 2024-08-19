d18295c6a7b5635ce8f9e81a55fe97a0300a9de6
Serafeim Maroulis <serafeim@hey.com>
2024-08-19 09:42:48 +0100

    Lint

--- db/migrate/20240816113944_add_route_key_to_funding_trainee_summary_rows.rb
+++ db/migrate/20240816113944_add_route_key_to_funding_trainee_summary_rows.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddRouteKeyToFundingTraineeSummaryRows < ActiveRecord::Migration[7.1]
   def change
     add_column :funding_trainee_summary_rows, :route_key, :string
