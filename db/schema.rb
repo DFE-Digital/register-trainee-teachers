201df3b888c8a1d17d3c70481eee9f51997fd988
Chris Gannon <cjgannon@gmail.com>
2024-08-30 09:17:23 +0100

    remove merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.2].define(version: 2024_08_22_150430) do
-=======
-ActiveRecord::Schema[7.2].define(version: 2024_08_15_130820) do
->>>>>>> 463531690 (adjust specs)
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "citext"
