b14f4b90d3f7b627aae4fc25787f7f2033deb913
Adam Colvin <adamcolv@icloud.com>
2024-09-03 11:32:56 +0100

    Update navigation view object
    Removed a route that doesn’t exist anymore

--- app/view_objects/funding/navigation_view.rb
+++ app/view_objects/funding/navigation_view.rb
@@ -16,11 +16,7 @@ module Funding
     def path_for_funding_payment_schedule(year)
       return funding_payment_schedule_path(year) unless system_admin
 
-      if organisation.is_a?(Provider)
-        provider_funding_payment_schedule_path(provider_id: organisation.id, academic_year: year)
-      else
-        lead_school_funding_payment_schedule_path(lead_school_id: organisation.id, academic_year: year)
-      end
+      provider_funding_payment_schedule_path(provider_id: organisation.id, academic_year: year)
     end
 
     def path_for_funding_trainee_summary(year)
