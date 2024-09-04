386f160a53ec46239820bfac8af4393b920c58fb
Adam Colvin <adamcolv@icloud.com>
2024-09-04 15:20:02 +0100

    Rubocop

--- db/data/20240904135736_migrate_lead_school_record_type_to_school.rb
+++ db/data/20240904135736_migrate_lead_school_record_type_to_school.rb
@@ -2,10 +2,10 @@
 
 class MigrateLeadSchoolRecordTypeToSchool < ActiveRecord::Migration[7.2]
   def up
-    LeadPartner.where(record_type: 'lead_school').update_all(record_type: 'school')
+    LeadPartner.where(record_type: "lead_school").update_all(record_type: "school")
   end
 
   def down
-    LeadPartner.where(record_type: 'school').update_all(record_type: 'lead_school')
+    LeadPartner.where(record_type: "school").update_all(record_type: "lead_school")
   end
 end
