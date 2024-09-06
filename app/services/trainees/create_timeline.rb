22c524d539f22bb28f52b22ca2eee22fdd7c27bb
Serafeim Maroulis <serafeim@hey.com>
2024-09-06 14:53:10 +0100

    Ensure Degrees are been displayed in the Timeline
    * Refactor query for audit events

--- app/services/trainees/create_timeline.rb
+++ app/services/trainees/create_timeline.rb
@@ -17,6 +17,8 @@ module Trainees
 
     attr_reader :trainee, :current_user
 
+    delegate :own_and_associated_audits, to: :trainee
+
     def events
       filtered_audits.map { |audit| CreateTimelineEvents.call(audit:, current_user:) }
     end
@@ -60,9 +62,9 @@ module Trainees
     end
 
     def audits
-      trainee
-        .own_and_associated_audits
-        .where.not("username = ? AND auditable_type = ?", "HESA", "Degree")
+      own_and_associated_audits
+        .where.not(username: "HESA", auditable_type: "Degree")
+        .or(own_and_associated_audits.where(username: nil))
     end
   end
 end
