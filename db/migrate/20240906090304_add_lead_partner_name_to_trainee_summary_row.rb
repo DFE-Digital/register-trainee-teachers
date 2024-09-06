6b61f520a12129238f23af03b6f447be548de81d
Adam Colvin <adamcolv@icloud.com>
2024-09-06 11:32:01 +0100

    Rubocop

--- db/migrate/20240906090304_add_lead_partner_name_to_trainee_summary_row.rb
+++ db/migrate/20240906090304_add_lead_partner_name_to_trainee_summary_row.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddLeadPartnerNameToTraineeSummaryRow < ActiveRecord::Migration[7.2]
   def change
     add_column :funding_trainee_summary_rows, :lead_partner_name, :string
