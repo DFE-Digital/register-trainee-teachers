7318da6d0d1fddbe0571faf65da3ce1d032ff077
Adam Colvin <adamcolv@icloud.com>
2024-09-09 14:10:07 +0100

    Update personas view_spec

--- spec/components/personas/view_spec.rb
+++ spec/components/personas/view_spec.rb
@@ -42,8 +42,8 @@ module Personas
       let(:persona) { create(:user, id: persona_id, lead_partners: create_list(:lead_partner, 2, :school)) }
 
       it "renders the all provider names" do
-        persona.lead_partners.each do |lead_school|
-          expect(component).to have_text(lead_school.name)
+        persona.lead_partners.each do |lead_partner|
+          expect(component).to have_text(lead_partner.name)
         end
       end
     end
