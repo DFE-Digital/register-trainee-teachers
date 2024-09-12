c2238a1dd40d38dfaf32b87e35aab85ffb6e9190
Adam Colvin <adamcolv@icloud.com>
2024-09-12 14:53:10 +0100

    Drop lead school urn from hesa student

---
+++ db/migrate/20240912135058_drop_lead_school_urn_from_hesa_student.rb
@@ -0,0 +1,5 @@
+class DropLeadSchoolUrnFromHesaStudent < ActiveRecord::Migration[7.2]
+  def change
+    safety_assured { remove_column :hesa_students, :lead_school_urn, :string }
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_06_090304) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_12_135058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "citext"
@@ -601,7 +601,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_06_090304) do
     t.string "course_subject_three"
     t.string "itt_end_date"
     t.string "employing_school_urn"
-    t.string "lead_school_urn"
     t.string "mode"
     t.string "course_age_range"
     t.string "training_initiative"
@@ -649,10 +648,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_06_090304) do
     t.string "surname16"
     t.string "ttcid"
     t.string "hesa_committed_at"
+    t.string "previous_hesa_id"
     t.string "application_choice_id"
     t.string "itt_start_date"
     t.string "trainee_start_date"
-    t.string "previous_hesa_id"
     t.string "provider_trainee_id"
     t.string "lead_partner_urn"
     t.index ["hesa_id", "rec_id"], name: "index_hesa_students_on_hesa_id_and_rec_id", unique: true
