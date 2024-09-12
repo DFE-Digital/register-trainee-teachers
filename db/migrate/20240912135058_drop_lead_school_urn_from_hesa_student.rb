2bc5ccefe00e30d4dfae632545bf1fb73bd27bce
Adam Colvin <adamcolv@icloud.com>
2024-09-12 15:03:57 +0100

    Rubocop

--- db/migrate/20240912135058_drop_lead_school_urn_from_hesa_student.rb
+++ db/migrate/20240912135058_drop_lead_school_urn_from_hesa_student.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DropLeadSchoolUrnFromHesaStudent < ActiveRecord::Migration[7.2]
   def change
     safety_assured { remove_column :hesa_students, :lead_school_urn, :string }
