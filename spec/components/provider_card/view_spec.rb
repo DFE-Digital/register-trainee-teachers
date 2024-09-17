8c2ac37a4726f812232ec594b2b63c85e14a978d
Steve Hook <steve.hook@gmail.com>
2024-09-17 15:22:40 +0100

    Update ProviderCard component to show provider code

--- spec/components/provider_card/view_spec.rb
+++ spec/components/provider_card/view_spec.rb
@@ -7,7 +7,7 @@ module ProviderCard
     include SummaryHelper
 
     context "with a provider" do
-      let(:provider) { create(:provider, name: "University of Wantage") }
+      let(:provider) { create(:provider, name: "University of Wantage", code: "WG1") }
       let(:bob) { create(:user) }
       let(:alice) { create(:user) }
       let(:norman) { create(:user, discarded_at: 2.weeks.ago) }
@@ -19,16 +19,16 @@ module ProviderCard
       end
 
       it "render the correct provider details" do
-        expect(rendered_content).to have_text("University of Wantage")
+        expect(rendered_content).to have_text("University of Wantage (WG1)")
         expect(rendered_content).to have_text("2 users")
         expect(rendered_content).not_to have_text("Previously accredited")
       end
 
       context "when the provider has lost accreditation" do
-        let(:provider) { create(:provider, name: "University of Wantage", accredited: false) }
+        let(:provider) { create(:provider, name: "University of Wantage", accredited: false, code: "WG1") }
 
         it "render the correct provider details and tag to indicate that the provider was previously accredited" do
-          expect(rendered_content).to have_text("University of Wantage")
+          expect(rendered_content).to have_text("University of Wantage (WG1)")
           expect(rendered_content).to have_text("2 users")
           expect(rendered_content).to have_text("Previously accredited")
         end
