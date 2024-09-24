8c4d16b3e14c79113a76381830c1e92adc814eda
Steve Hook <steve.hook@gmail.com>
2024-09-24 01:00:11 +0100

    Add feature flag for `bulk_add_trainees` feature

--- app/controllers/bulk_update/trainees/add_new_trainees_controller.rb
+++ app/controllers/bulk_update/trainees/add_new_trainees_controller.rb
@@ -4,6 +4,7 @@ module BulkUpdate
   module Trainees
     class AddNewTraineesController < ApplicationController
       before_action { check_for_provider }
+      before_action { require_feature_flag(:bulk_add_trainees) }
 
       def show
         @navigation_view = ::Funding::NavigationView.new(organisation:)
--- spec/features/bulk_upload/bulk_add_trainees_spec.rb
+++ spec/features/bulk_upload/bulk_add_trainees_spec.rb
@@ -7,7 +7,13 @@ feature "bulk add trainees" do
     given_i_am_authenticated
   end
 
-  scenario "the bulk add trainees page is visible" do
+  scenario "the bulk add trainees page is not-visible when feature flag is off", feature_bulk_add_trainees: false do
+    when_i_visit_the_bulk_update_index_page
+    then_i_cannot_see_the_bulk_add_trainees_link
+    and_i_cannot_navigate_directly_to_the_bulk_add_trainees_page
+  end
+
+  scenario "the bulk add trainees page is visible when feature flag is on", feature_bulk_add_trainees: true do
     when_i_visit_the_bulk_update_index_page
     and_i_click_the_bulk_add_trainees_page
     then_i_see_how_instructions_on_how_to_bulk_add_trainees
@@ -18,6 +24,15 @@ private
 
   def when_i_visit_the_bulk_update_index_page
     visit bulk_update_path
+  end
+
+  def then_i_cannot_see_the_bulk_add_trainees_link
+    expect(page).not_to have_link("Bulk add new trainees")
+  end
+
+  def and_i_cannot_navigate_directly_to_the_bulk_add_trainees_page
+    visit bulk_update_trainees_add_new_trainees_path
+    expect(page).to have_current_path(not_found_path)
   end
 
   def and_i_click_the_bulk_add_trainees_page
