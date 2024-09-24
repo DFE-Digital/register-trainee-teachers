c2c39bc5b03a91244593a98fd623bd09469681bf
Chris Gannon <cjgannon@gmail.com>
2024-09-24 14:10:27 +0100

    adjust specs for email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :email, uniqueness: { case_sensitive: false }, if: :active_user?
+  validates :email, uniqueness: true, if: :active_user?
 
   validate do |record|
     EmailFormatValidator.new(record).validate
