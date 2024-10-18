af0e1376093f3a2eb52d7540434bed9dc8013cb6
Steve Hook <steve.hook@gmail.com>
2024-10-18 09:37:47 +0100

    Tests for `ImportRows` service
    Outline of error handling and rollback

--- spec/services/bulk_update/add_trainees/import_rows_spec.rb
+++ spec/services/bulk_update/add_trainees/import_rows_spec.rb
@@ -16,29 +16,44 @@ module BulkUpdate
         end
 
         context "when the feature flag is on", "feature_bulk_add_trainees": true do
+          before { @original_trainee_count = Trainee.count }
+
           context "when all rows are valid and can be imported" do
             let(:trainee_upload) { create(:bulk_update_trainee_upload) }
+            before do
+              allow(ImportRow).to receive(:call).and_return(true)
+            end
 
             it "creates 5 trainee records" do
               expect(ImportRow).to receive(:call).exactly(5).times
-              described_class.call(id: trainee_upload.id)
+              expect(described_class.call(id: trainee_upload.id)).to be(true)
+
+              pending "need to assert that trainee records are created"
+              expect(Trainee.count - @original_trainee_count).to eq(5)
             end
 
             it "sets the status to `succeeded`" do
-
+              described_class.call(id: trainee_upload.id)
+              expect(trainee_upload.reload).to be_succeeded
             end
           end
 
           context "when some rows are valid and can be imported whilst others are not" do
-            let(:trainee_upload) { create(:bulk_update_trainee_upload, :with_errors) }
-            subject(:service) { described_class.call(id: trainee_upload.id) }
+            let(:trainee_upload) { create(:bulk_update_trainee_upload) }
+            before do
+              allow(ImportRow).to receive(:call).and_return(true, true, true, true, false)
+            end
 
             it "does not create any trainee records" do
+              expect(ImportRow).to receive(:call).exactly(5).times
+              expect(described_class.call(id: trainee_upload.id)).to be(false)
 
+              expect(Trainee.count).to eq(@original_trainee_count)
             end
 
             it "sets the status to `failed`" do
-
+              described_class.call(id: trainee_upload.id)
+              expect(trainee_upload.reload).to be_failed
             end
           end
         end
