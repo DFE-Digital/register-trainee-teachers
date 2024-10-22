636b7392ee6cb5eed99ff88ecd41e75dbec18993
Adam Colvin <adamcolv@icloud.com>
2024-10-22 11:56:56 +0100

    Rubocop

--- spec/models/degree_spec.rb
+++ spec/models/degree_spec.rb
@@ -48,9 +48,9 @@ describe Degree do
         it "validates" do
           expect { degree.save! }.to raise_error(ActiveRecord::RecordInvalid)
           expect(degree.errors.messages[:graduation_year]).to include(I18n.t(
-            "activerecord.errors.models.degree.attributes.graduation_year.invalid",
-            value: degree.graduation_year
-          ))
+                                                                        "activerecord.errors.models.degree.attributes.graduation_year.invalid",
+                                                                        value: degree.graduation_year,
+                                                                      ))
         end
       end
 
