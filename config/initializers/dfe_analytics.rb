1fab9695da7a25e2a795e1c010af1d5bae16bd39
asatwal <asatwal01@googlemail.com>
2024-11-01 11:30:11 +0000

    Change BigQuery project and dataset to send events to git

--- config/initializers/dfe_analytics.rb
+++ config/initializers/dfe_analytics.rb
@@ -19,15 +19,33 @@ DfE::Analytics.configure do |config|
 
   # The name of the BigQuery table we’re writing to.
   #
-  config.bigquery_table_name = Settings.google.big_query.table_name
+  config.bigquery_table_name = 'events'
 
   # The name of the BigQuery project we’re writing to.
   #
-  config.bigquery_project_id = Settings.google.big_query.project_id
+  config.bigquery_project_id = 'get-into-teaching'
 
   # The name of the BigQuery dataset we're writing to.
   #
-  config.bigquery_dataset = Settings.google.big_query.dataset
+  config.bigquery_dataset = 'bat_cross_project'
+
+  # Ensure JSON API Key does not get set
+  config.bigquery_api_json_key = ''
+
+  config.google_cloud_credentials = {
+    universe_domain: "googleapis.com",
+    type: "external_account",
+    audience: "//iam.googleapis.com/projects/574582782335/locations/global/workloadIdentityPools/azure-cip-identity-pool/providers/azure-cip-oidc-provider",
+    subject_token_type: "urn:ietf:params:oauth:token-type:jwt",
+    token_url: "https://sts.googleapis.com/v1/token",
+    credential_source: {
+      url: "https://login.microsoftonline.com/9c7d9dd3-840c-4b3f-818e-552865082e16/oauth2/v2.0/token"
+    },
+    service_account_impersonation_url: "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/bat-cross-project@get-into-teaching.iam.gserviceaccount.com:generateAccessToken",
+    service_account_impersonation: {
+      token_lifetime_seconds: 3600
+    }
+  }
 
   # Passed directly to the retries: option on the BigQuery client
   #
