1006c9d6cd764ff47146a50d81d3a480346e7704
Serafeim Maroulis <serafeim@hey.com>
2024-11-05 14:48:11 +0000

    Add cancel / back to trainee uploads links

--- spec/features/bulk_upload/bulk_add_trainees_spec.rb
+++ spec/features/bulk_upload/bulk_add_trainees_spec.rb
@@ -3,121 +3,179 @@
 require "rails_helper"
 
 feature "bulk add trainees" do
-  context "when authenticated as a provider user" do
-    let(:hei_provider) { create(:provider, :hei) }
-    let(:user) { create(:user, providers: [hei_provider]) }
+  include ActiveJob::TestHelper
 
+  before do
+    and_there_is_a_nationality
+  end
+
+  context "when the feature flag is off", feature_bulk_add_trainees: false do
     before do
-      allow(BulkUpdate::AddTrainees::ImportRowsJob).to receive(:perform_later)
-      given_i_am_authenticated(user:)
-      and_there_is_a_nationality
+      given_i_am_authenticated
     end
 
-    scenario "the bulk add trainees page is not-visible when feature flag is off", feature_bulk_add_trainees: false do
+    scenario "the bulk add trainees page is not-visible when feature flag is off" do
       when_i_visit_the_bulk_update_index_page
       then_i_cannot_see_the_bulk_add_trainees_link
       and_i_cannot_navigate_directly_to_the_bulk_add_trainees_page
     end
+  end
 
-    scenario "the bulk add trainees page is visible when feature flag is on", feature_bulk_add_trainees: true do
-      when_i_visit_the_bulk_update_index_page
-      and_i_click_the_bulk_add_trainees_page
-      then_i_see_how_instructions_on_how_to_bulk_add_trainees
-      and_i_see_the_empty_csv_link
+  context "when the feature flag is on", feature_bulk_add_trainees: true do
+    context "when the User is not a Provider" do
+      let(:user) { create(:user, :with_lead_partner_organisation) }
 
-      when_i_attach_an_empty_file
-      and_i_click_the_upload_button
-      then_i_see_the_upload_page_with_errors
+      before do
+        given_i_am_authenticated(user:)
+      end
 
-      when_i_visit_the_bulk_update_index_page
-      and_i_click_the_bulk_add_trainees_page
-      and_i_attach_a_valid_file
-      and_i_click_the_upload_button
-      then_i_see_the_review_page_with_no_errors
-
-      when_the_upload_validation_background_job_is_run
-      and_i_refresh_the_page
-      then_i_see_the_review_page_with_no_errors
-
-      when_i_click_the_submit_button
-      then_a_job_is_queued_to_process_the_upload
-      and_i_see_the_summary_page
-
-      when_the_submit_background_job_is_run
-      and_i_visit_the_trainees_page
-      then_i_can_see_the_new_trainees
+      scenario "attempts to visit the new upload trainees page" do
+        when_i_visit_the_new_bulk_update_trainees_upload_path
+        then_i_see_the_unauthorized_message
+      end
     end
 
-    scenario "when I try to look at the status of a different providers upload", feature_bulk_add_trainees: true do
-      when_there_is_a_bulk_update_trainee_upload
-      when_i_visit_the_bulk_update_status_page_for_another_provider
+    context "when the User is a Provider" do
+      before do
+        given_i_am_authenticated
+      end
+
+      scenario "the bulk add trainees page is visible" do
+        when_i_visit_the_bulk_update_index_page
+        and_i_click_the_bulk_add_trainees_page
+        then_i_see_how_instructions_on_how_to_bulk_add_trainees
+        and_i_see_the_empty_csv_link
+
+        when_i_attach_an_empty_file
+        and_i_click_the_upload_button
+        then_i_see_the_upload_page_with_errors(empty: true)
+
+        when_i_click_the_upload_button
+        then_i_see_the_upload_page_with_errors(empty: false)
+
+        when_i_visit_the_bulk_update_index_page
+        and_i_click_the_bulk_add_trainees_page
+        and_i_attach_a_valid_file
+        and_i_click_the_upload_button
+        then_i_see_that_the_upload_is_processing
+        and_i_dont_see_the_cancel_bulk_updates_link
+
+        when_i_click_the_back_to_bulk_updates_page_link
+        and_i_click_the_bulk_add_trainees_page
+        and_i_attach_a_valid_file
+
+        and_i_click_the_upload_button
+        then_i_see_that_the_upload_is_processing
+
+        when_the_background_job_is_run
+        and_i_refresh_the_page
+        then_i_see_the_review_page
+        and_i_dont_see_the_review_errors_link
+        and_i_dont_see_the_back_to_bulk_updates_link
+
+        when_i_click_the_cancel_bulk_updates_link
+        and_i_click_the_bulk_add_trainees_page
+        and_i_attach_a_valid_file
+        and_i_click_the_upload_button
+        then_a_job_is_queued_to_process_the_upload
+        then_i_see_that_the_upload_is_processing
+
+        when_the_background_job_is_run
+        and_i_refresh_the_page
+        then_i_see_the_review_page
+
+        when_i_click_the_submit_button
+        then_a_job_is_queued_to_process_the_upload
+        and_i_see_the_summary_page
+        and_i_dont_see_the_review_errors_message
+
+        and_i_visit_the_trainees_page
+        then_i_can_see_the_new_trainees
+
+        when_i_try_resubmit_the_same_upload
+        and_i_click_the_submit_button
+        then_i_see_the_unauthorized_message
+      end
+
+      scenario "when I try to look at the status of a different providers upload" do
+        when_there_is_a_bulk_update_trainee_upload
+
+        expect {
+          when_i_visit_the_bulk_update_status_page_for_another_provider
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+      scenario "view the upload summary page with errors" do
+        when_the_upload_has_failed_with_validation_errors
+        and_i_dont_see_that_the_upload_is_processing
+        and_i_visit_the_summary_page(upload: @failed_upload)
+        then_i_see_the_review_page
+        and_i_see_the_number_of_trainees_that_can_be_added(number: 3)
+        and_i_see_the_validation_errors(number: 2)
+        and_i_dont_see_any_duplicate_errors
+        and_i_see_the_review_errors_message
+        and_i_see_the_review_errors_link
+        and_i_dont_see_the_submit_button
+      end
+
+      scenario "view the upload summary page with duplicate errors" do
+        when_the_upload_has_failed_with_duplicate_errors
+        and_i_dont_see_that_the_upload_is_processing
+        and_i_visit_the_summary_page(upload: @failed_upload)
+        then_i_see_the_review_page
+        and_i_see_the_number_of_trainees_that_can_be_added(number: 3)
+        and_i_dont_see_any_validation_errors
+        and_i_see_the_duplicate_errors(number: 2)
+        and_i_see_the_review_errors_message
+        and_i_see_the_review_errors_link
+        and_i_dont_see_the_submit_button
+      end
+
+      scenario "view the upload summary page with validation and duplicate errors" do
+        when_the_upload_has_failed_with_validation_and_duplicate_errors
+        and_i_dont_see_that_the_upload_is_processing
+        and_i_visit_the_summary_page(upload: @failed_upload)
+        then_i_see_the_review_page
+        and_i_dont_the_number_of_trainees_that_can_be_added
+        and_i_see_the_validation_errors(number: 2)
+        and_i_see_the_duplicate_errors(number: 3)
+        and_i_see_the_review_errors_message
+        and_i_see_the_review_errors_link
+        and_i_dont_see_the_submit_button
+      end
     end
-  end
-
-  context "when authenticated but not as a provider user" do
-    before do
-      given_i_am_authenticated_as_system_admin
-    end
-
-    scenario "the bulk add trainees page is visible when feature flag is on", feature_bulk_add_trainees: true do
-      when_i_visit_the_bulk_update_add_trainees_page
-      then_i_am_redirected_to_the_root_path
-    end
-  end
-
-  def when_i_visit_the_bulk_update_add_trainees_page
-    visit new_bulk_update_trainees_upload_path
-  end
-
-  def then_i_am_redirected_to_the_root_path
-    expect(page).to have_current_path(root_path)
-  end
-
-  scenario "view the upload summary page with errors", feature_bulk_add_trainees: true do
-    when_the_upload_has_failed_with_validation_errors
-    and_i_dont_see_that_the_upload_is_processing
-    and_i_visit_the_summary_page(upload: @failed_upload)
-    then_i_see_the_review_page
-    and_i_see_the_number_of_trainees_that_can_be_added(number: 3)
-    and_i_see_the_validation_errors(number: 2)
-    and_i_dont_see_any_duplicate_errors
-    and_i_see_the_review_errors_message
-    and_i_see_the_review_errors_link
-    and_i_dont_see_the_submit_button
-  end
-
-  scenario "view the upload summary page with duplicate errors", feature_bulk_add_trainees: true do
-    when_the_upload_has_failed_with_duplicate_errors
-    and_i_dont_see_that_the_upload_is_processing
-    and_i_visit_the_summary_page(upload: @failed_upload)
-    then_i_see_the_review_page
-    and_i_see_the_number_of_trainees_that_can_be_added(number: 3)
-    and_i_dont_see_any_validation_errors
-    and_i_see_the_duplicate_errors(number: 2)
-    and_i_see_the_review_errors_message
-    and_i_see_the_review_errors_link
-    and_i_dont_see_the_submit_button
-  end
-
-  scenario "view the uplaod summary page with validation and duplicate errors", feature_bulk_add_trainees: true do
-    when_the_upload_has_failed_with_validation_and_duplicate_errors
-    and_i_dont_see_that_the_upload_is_processing
-    and_i_visit_the_summary_page(upload: @failed_upload)
-    then_i_see_the_review_page
-    and_i_dont_the_number_of_trainees_that_can_be_added
-    and_i_see_the_validation_errors(number: 2)
-    and_i_see_the_duplicate_errors(number: 3)
-    and_i_see_the_review_errors_message
-    and_i_see_the_review_errors_link
-    and_i_dont_see_the_submit_button
   end
 
 private
 
+  def when_i_try_resubmit_the_same_upload
+    visit bulk_update_trainees_upload_path(BulkUpdate::TraineeUpload.last)
+  end
+
+  def then_i_see_the_unauthorized_message
+    expect(page).to have_content("You do not have permission to perform this action")
+  end
+
+  def and_i_dont_see_the_back_to_bulk_updates_link
+    expect(page).not_to have_link("Back to bulk updates page")
+  end
+
+  def and_i_dont_see_the_cancel_bulk_updates_link
+    expect(page).not_to have_link("Cancel bulk updates to records")
+  end
+
+  def when_i_click_the_cancel_bulk_updates_link
+    click_on "Cancel bulk updates to records"
+  end
+
+  def when_i_click_the_back_to_bulk_updates_page_link
+    click_on "Back to bulk updates page"
+  end
+
   def then_i_see_that_the_upload_is_processing
     expect(page).to have_content("File uploaded")
     expect(page).to have_content("Your file is being processed")
-    expect(page).to have_content("We're currently processing #{BulkUpdate::TraineeUpload.last.file_name}.")
+    expect(page).to have_content("We're currently processing #{BulkUpdate::TraineeUpload.last.filename}.")
     expect(page).to have_content("This is taking longer than usual")
     expect(page).to have_content("You'll receive and email to tell you when this is complete.")
     expect(page).to have_content("You can also check the status of new trainee files.")
@@ -127,7 +185,7 @@ private
   def and_i_dont_see_that_the_upload_is_processing
     expect(page).not_to have_content("File uploaded")
     expect(page).not_to have_content("Your file is being processed")
-    expect(page).not_to have_content("We're currently processing #{BulkUpdate::TraineeUpload.last.file_name}.")
+    expect(page).not_to have_content("We're currently processing #{BulkUpdate::TraineeUpload.last.filename}.")
     expect(page).not_to have_content("This is taking longer than usual")
     expect(page).not_to have_content("You'll receive and email to tell you when this is complete.")
     expect(page).not_to have_content("You can also check the status of new trainee files.")
@@ -147,8 +205,12 @@ private
   end
 
   def and_i_cannot_navigate_directly_to_the_bulk_add_trainees_page
-    visit new_bulk_update_trainees_upload_path
+    when_i_visit_the_new_bulk_update_trainees_upload_path
     expect(page).to have_current_path(not_found_path)
+  end
+
+  def when_i_visit_the_new_bulk_update_trainees_upload_path
+    visit new_bulk_update_trainees_upload_path
   end
 
   def and_i_click_the_bulk_add_trainees_page
@@ -168,7 +230,7 @@ private
     and_i_attach_a_file("")
   end
 
-  def and_i_attach_a_valid_file
+  def when_i_attach_a_valid_file
     csv = Rails.root.join("spec/fixtures/files/bulk_update/trainee_uploads/five_trainees.csv").read
     and_i_attach_a_file(csv)
   end
@@ -186,17 +248,10 @@ private
     click_on "Upload records"
   end
 
-  def then_i_see_the_upload_page_with_errors
+  def then_i_see_the_upload_page_with_errors(empty:)
     expect(page).to have_current_path(bulk_update_trainees_uploads_path)
     expect(page).to have_content("There is a problem")
-    expect(page).to have_content("The selected file is empty")
-  end
-
-  def then_i_see_the_review_page_with_no_errors
-    expect(page).to have_current_path(
-      bulk_update_trainees_upload_path(id: BulkUpdate::TraineeUpload.last.id),
-    )
-    expect(page).to have_content("You uploaded a CSV file with details of 5 trainees.")
+    expect(page).to have_content(empty ? "The selected file is empty" : "Select a CSV file")
   end
 
   def then_i_see_the_review_page
@@ -253,12 +308,14 @@ private
   end
 
   def then_a_job_is_queued_to_process_the_upload
-    expect(BulkUpdate::AddTrainees::ImportRowsJob).to have_received(:perform_later).with(BulkUpdate::TraineeUpload.last).at_least(:once)
+    expect(BulkUpdate::AddTrainees::ImportRowsJob).to have_been_enqueued.with(
+      id: BulkUpdate::TraineeUpload.last.id,
+    )
   end
 
   def and_i_see_the_summary_page
     expect(page).to have_current_path(
-      bulk_update_trainees_submission_path(id: BulkUpdate::TraineeUpload.last.id),
+      bulk_update_trainees_submission_path(BulkUpdate::TraineeUpload.last),
     )
     within(".govuk-panel") do
       expect(page).to have_content("Trainees submitted")
@@ -271,27 +328,15 @@ private
   end
 
   def when_i_visit_the_bulk_update_status_page_for_another_provider
-    visit bulk_update_trainees_upload_path(id: @upload_for_different_provider.id)
-  end
-
-  def then_i_see_a_not_found_page
-    expect(page).to have_current_path(not_found_path)
-  end
-
-  def when_the_upload_validation_background_job_is_run
-    Sidekiq::Testing.inline! do
-      BulkUpdate::AddTrainees::ImportRowsJob.perform_now(BulkUpdate::TraineeUpload.last)
-    end
+    visit bulk_update_trainees_upload_path(@upload_for_different_provider)
   end
 
   def and_i_refresh_the_page
     visit bulk_update_trainees_upload_path(id: BulkUpdate::TraineeUpload.last.id)
   end
 
-  def when_the_submit_background_job_is_run
-    Sidekiq::Testing.inline! do
-      BulkUpdate::AddTrainees::ImportRowsJob.perform_now(BulkUpdate::TraineeUpload.last)
-    end
+  def when_the_background_job_is_run
+    perform_enqueued_jobs
   end
 
   def and_i_visit_the_trainees_page
@@ -329,4 +374,8 @@ private
   def and_i_visit_the_summary_page(upload:)
     visit bulk_update_trainees_upload_path(upload)
   end
+
+  alias_method :and_i_attach_a_valid_file, :when_i_attach_a_valid_file
+  alias_method :and_i_click_the_submit_button, :when_i_click_the_submit_button
+  alias_method :when_i_click_the_upload_button, :and_i_click_the_upload_button
 end
