a93e792ecba7e8d8977b09e3e7d4b95be0d40d97
Chris Gannon <cjgannon@gmail.com>
2024-11-12 14:52:02 +0000

    fix linting issues

--- spec/services/send_csv_submitted_for_processing_email_service.rb
+++ spec/services/send_csv_submitted_for_processing_email_service.rb
@@ -5,15 +5,12 @@ require "rails_helper"
 describe SendCsvSubmittedForProcessingEmailService do
   before do
     enable_features(:send_emails)
+    allow(CsvSubmittedForProcessingEmailMailer).to receive_message_chain(:generate, :deliver_later)
+    described_class.call(user:, upload:)
   end
 
   let(:user) { create(:user) }
   let(:upload) { create(:bulk_update_trainee_upload) }
-
-  before do
-    allow(CsvSubmittedForProcessingEmailMailer).to receive_message_chain(:generate, :deliver_later)
-    described_class.call(user:, upload:)
-  end
 
   it "sends the csv submitted for processing email" do
     expect(CsvSubmittedForProcessingEmailMailer).to have_received(:generate)
