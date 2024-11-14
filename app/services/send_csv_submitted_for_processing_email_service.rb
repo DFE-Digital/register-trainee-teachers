9291c2e494ce565de5c572d4418af1efd9dd5cb7
Chris Gannon <cjgannon@gmail.com>
2024-11-14 10:06:37 +0000

    Update app/services/send_csv_submitted_for_processing_email_service.rb
    Co-authored-by: Serafeim Maroulis <serafeim@hey.com>

--- app/services/send_csv_submitted_for_processing_email_service.rb
+++ app/services/send_csv_submitted_for_processing_email_service.rb
@@ -16,7 +16,7 @@ class SendCsvSubmittedForProcessingEmailService
       first_name: user.first_name,
       email: user.email,
       file_name: upload.file_name,
-      file_link: bulk_update_trainees_upload_url(upload.id),
+      file_link: bulk_update_trainees_upload_url(upload),
       submitted_at: upload.created_at.strftime("%d.%M.%Y, %l%p"),
     ).deliver_later
   end
