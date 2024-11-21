f75df049a63947835f4ff353b02a2b8f29abfe67
defong <470137+defong@users.noreply.github.com>
2024-11-21 12:36:02 +0000

    [7851] Added performance banner

---
+++ app/components/performance_profile_banner/view.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module PerformanceProfileBanner
+  class View < ViewComponent::Base
+    DEFAULT_ACADEMIC_CYCLE_START_DATE = "1 August"
+
+    def initialize(previous_academic_cycle: AcademicCycle.previous, sign_off_period: DetermineSignOffPeriod.call, provider:)
+      @previous_academic_cycle = previous_academic_cycle
+      @provider = provider
+      @sign_off_period = sign_off_period
+    end
+
+    def render?
+      performance_period? && provider_awaiting_sign_off?
+    end
+
+    def banner_heading_text
+      "The #{previous_academic_cycle_label} performance profile sign off deadline is #{deadline}."
+    end
+
+    delegate :label, :end_year, to: :previous_academic_cycle, prefix: true
+
+  private
+
+    attr_reader :previous_academic_cycle, :provider, :sign_off_period
+
+    def deadline
+      "31 January #{previous_academic_cycle_end_year}"
+    end
+
+    def provider_awaiting_sign_off?
+      !provider.performance_signed_off
+    end
+
+    def performance_period?
+      sign_off_period == :performance_period
+    end
+  end
+end
