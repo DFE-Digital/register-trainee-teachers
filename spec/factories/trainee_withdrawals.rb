191dde6f72dafe5212bde5951a868f067aefa26d
Dave G <david.georgiou@digital.education.gov.uk>
2024-11-25 10:47:56 +0000

    [7827] - Encapsulate Withdrawal (#4826)

---
+++ spec/factories/trainee_withdrawals.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :trainee_withdrawal, class: "TraineeWithdrawal" do
+    trainee
+    date { "2024-11-18" }
+    trigger { :provider }
+    another_reason { "Not enough coffee" }
+    future_interest { :yes }
+  end
+end
