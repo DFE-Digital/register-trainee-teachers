adf376ff4b119cfb94657dee4bf212b05d9dd1a4
Serafeim Maroulis <serafeim@hey.com>
2024-11-26 14:56:41 +0000

    Refactor colours for statuses

--- app/components/bulk_update/trainee_uploads/row/view.rb
+++ app/components/bulk_update/trainee_uploads/row/view.rb
@@ -4,30 +4,31 @@ module BulkUpdate
   module TraineeUploads
     module Row
       class View < ViewComponent::Base
+        COLOURS = {
+          "pending" => "govuk-tag--light-blue",
+          "validated" => "govuk-tag--turquoise",
+          "in_progress" => "govuk-tag--blue",
+          "succeeded" => "govuk-tag--green",
+          "failed" => "govuk-tag--red",
+          "cancelled" => "govuk-tag--yellow",
+        }.freeze
+
         attr_reader :upload
 
-        delegate :created_at, :filename, :status, to: :upload
+        delegate :filename, to: :upload
 
         def initialize(upload:)
           @upload = upload
         end
 
         def status
-          content_tag(:span, class: "govuk-tag #{colour}") do
+          content_tag(:span, class: "govuk-tag #{COLOURS[upload.status]}") do
             upload.status.humanize
           end
         end
 
-      private
-
-        def colour
-          {
-            "pending" => "govuk-tag--grey",
-            "validated" => "govuk-tag--grey",
-            "in_progress" => "govuk-tag--grey",
-            "succeeded" => "govuk-tag--green",
-            "failed" => "govuk-tag--red",
-          }[upload.status]
+        def submitted_at
+          upload.submitted_at&.to_fs(:govuk_date_and_time)
         end
       end
     end
