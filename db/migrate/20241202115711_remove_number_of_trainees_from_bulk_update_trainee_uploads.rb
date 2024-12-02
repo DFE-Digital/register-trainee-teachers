549d9237ec057a358b329a9e853d04c349dd2abc
Serafeim Maroulis <serafeim@hey.com>
2024-12-02 15:56:09 +0000

    Add frozen_string_literal to migration

--- db/migrate/20241202115711_remove_number_of_trainees_from_bulk_update_trainee_uploads.rb
+++ db/migrate/20241202115711_remove_number_of_trainees_from_bulk_update_trainee_uploads.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveNumberOfTraineesFromBulkUpdateTraineeUploads < ActiveRecord::Migration[7.2]
   def change
     safety_assured { remove_column :bulk_update_trainee_uploads, :number_of_trainees, :integer }
