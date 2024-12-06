90135591075b0a145b824aa7a09979ccb0fa265f
David Georgiou <david.georgiou@gmail.com>
2024-12-06 16:55:21 +0000

    [7891] - adapt stacktrace and exception formatting

--- config/initializers/semantic_logger.rb
+++ config/initializers/semantic_logger.rb
@@ -5,8 +5,8 @@ class CustomLogFormatter < SemanticLogger::Formatters::Raw
     super
     format_job_data
     format_exception
+    format_stacktrace
     format_json_message_context
-    format_backtrace
     remove_post_params
     filter_authorization_header
     hash.to_json
@@ -14,16 +14,28 @@ class CustomLogFormatter < SemanticLogger::Formatters::Raw
 
 private
 
+  # Place a more readable version of the exception into the message field.
+  def format_exception
+    exception_message = hash.dig(:exception, :message)
+    hash[:message] = "Exception occurred: #{exception_message}" if exception_message.present?
+  end
+
+  def format_stacktrace
+    # If the payload usually has a stack trace, that can make
+    # the whole object too big in which case Logstash
+    # will fail and the log will not be passed
+    # We need to trim the stack trace.
+    stack_trace = hash.dig(:exception, :stack_trace)
+
+    if stack_trace.present?
+      hash[:stacktrace] = stack_trace.first(3)
+      hash[:exception].delete(:stack_trace)
+    end
+  end
+
   def format_job_data
     hash[:job_id] = RequestStore.store[:job_id] if RequestStore.store[:job_id].present?
     hash[:job_queue] = RequestStore.store[:job_queue] if RequestStore.store[:job_queue].present?
-  end
-
-  def format_exception
-    exception_message = hash.dig(:exception, :message)
-    if exception_message.present?
-      hash[:message] = "Exception occured: #{exception_message}"
-    end
   end
 
   def format_json_message_context
@@ -34,16 +46,6 @@ private
     end
   rescue JSON::ParserError
     nil
-  end
-
-  def format_backtrace
-    if hash[:message]&.start_with?("/")
-      message_lines = hash[:message].split("\n")
-      if message_lines.all? { |line| line.start_with?("/") }
-        hash[:backtrace] = hash[:message]
-        hash[:message] = "Exception occured: #{message_lines.first}"
-      end
-    end
   end
 
   def remove_post_params
