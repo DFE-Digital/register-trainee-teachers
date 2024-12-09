b0552bb7314b839da8868a049a32067f5a352e30
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2024-12-09 18:11:44 +0000

    Output azure storage account configuration
    Remove the need to store details in key vault

--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -48,12 +48,12 @@ variable "alert_window_size" {
 variable "config_short" {}
 variable "service_short" {}
 
-variable app_config_file { default = "workspace-variables/app_config.yml" }
-variable env_config {}
+variable "app_config_file" { default = "workspace-variables/app_config.yml" }
+variable "env_config" {}
 
 variable "service_name" {}
 variable "worker_apps" {
-  type    = map(
+  type = map(
     object({
       startup_command = optional(list(string), [])
       probe_command   = optional(list(string), [])
@@ -64,7 +64,7 @@ variable "worker_apps" {
   default = {}
 }
 variable "main_app" {
-  type    = map(
+  type = map(
     object({
       startup_command = optional(list(string), [])
       probe_path      = optional(string, null)
@@ -90,7 +90,7 @@ variable "enable_prometheus_monitoring" {
 }
 
 locals {
-  app_name_suffix  = var.app_name == null ? var.app_environment : var.app_name
+  app_name_suffix = var.app_name == null ? var.app_environment : var.app_name
 
   azure_credentials = try(jsondecode(var.azure_credentials), null)
   kv_app_secrets    = yamldecode(data.azurerm_key_vault_secret.app_secrets.value)
@@ -101,7 +101,10 @@ locals {
   app_env_values = merge(
     local.base_url_env_var,
     local.app_config,
-    { DB_SSLMODE = var.db_sslmode }
+    {
+      DB_SSLMODE                                  = var.db_sslmode
+      SETTINGS__AZURE__STORAGE__TEMP_DATA_ACCOUNT = local.azure_tempdata_storage_account_name
+    }
   )
 
   app_resource_group_name = "${var.azure_resource_prefix}-${var.service_short}-${var.config_short}-rg"
@@ -111,10 +114,11 @@ locals {
   app_secrets = merge(
     local.kv_app_secrets,
     {
-      DATABASE_URL                    = module.postgres.url
-      SETTINGS__BLAZER_DATABASE_URL   = module.postgres.url
-      REDIS_QUEUE_URL                 = module.redis-queue.url
-      REDIS_CACHE_URL                 = module.redis-cache.url
+      DATABASE_URL                                   = module.postgres.url
+      SETTINGS__BLAZER_DATABASE_URL                  = module.postgres.url
+      REDIS_QUEUE_URL                                = module.redis-queue.url
+      REDIS_CACHE_URL                                = module.redis-cache.url
+      SETTINGS__AZURE__STORAGE__TEMP_DATA_ACCESS_KEY = module.azure.storage_account_key
     },
     var.snapshot_databases_to_deploy == 1 ? { ANALYSIS_DATABASE_URL = module.postgres_snapshot[0].url } : {}
   )
@@ -122,12 +126,12 @@ locals {
   azure_tempdata_storage_account_name         = var.azure_tempdata_storage_account_name != null ? var.azure_tempdata_storage_account_name : local.default_azure_tempdata_storage_account_name
 }
 
-variable azure_resource_group_name { default = null }
+variable "azure_resource_group_name" { default = null }
 
-variable azure_tempdata_storage_account_name { default = null }
+variable "azure_tempdata_storage_account_name" { default = null }
 
-variable azure_storage_account_replication_type { default = "LRS" }
+variable "azure_storage_account_replication_type" { default = "LRS" }
 
-variable azure_region_name { default = "uk south" }
+variable "azure_region_name" { default = "uk south" }
 
 variable "deploy_temp_data_storage_account" { default = true }
