f5dd076fd979521cdbe3ee4b228c309a1fb484a5
Steve Hook <steve.hook@gmail.com>
2024-12-09 11:43:21 +0000

    Test whether AzureStorage works on review apps

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -4,5 +4,5 @@ require Rails.root.join("config/environments/production")
 
 Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :microsoft
 end
