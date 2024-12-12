474fae58f21d47e9377c326a6f2535d8eae22185
Steve Hook <steve.hook@gmail.com>
2024-12-12 14:22:04 +0000

    [7901] Test whether AzureStorage works on review apps (#4865)
    * Test whether AzureStorage works on review apps
    
    * Make review app storage account name dynamic
    
    * Output azure storage account configuration
    
    Remove the need to store details in key vault
    
    * Remove unused terraform variables
    
    * Remove terraform azure credentials
    
    They are not required as the provider reads the azure environment
    variables
    
    * Remove unnecessary storage terraform variables
    
    * Remove deploy_temp_data_storage_account when default is used
    
    * Make the use of Azure storage in review apps conditional
    
    We should only use Azure storage if a data access key is defined.
    
    ---------
    
    Co-authored-by: Colin Saliceti <colin.saliceti@ukdevops.co.uk>

--- terraform/modules/azure/variables.tf
+++ terraform/modules/azure/variables.tf
@@ -1,11 +1,3 @@
-variable "environment_name" {
-  type = string
-}
-
-variable "backend_resource_group_name" {
-  type = string
-}
-
 variable "resource_group_name" {
   type = string
 }
@@ -16,15 +8,6 @@ variable "tempdata_storage_account_name" {
 
 variable "storage_account_replication_type" {
   type = string
-}
-
-variable "region_name" {
-  default = "west europe"
-  type    = string
-}
-
-locals {
-  hosting_environment = var.environment_name
 }
 
 variable "deploy_temp_data_storage_account" { default = true }
