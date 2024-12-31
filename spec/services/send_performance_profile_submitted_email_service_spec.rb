63a5e3a614a7150151e153ba6f971fae5371cf4f
Chris Gannon <cjgannon@gmail.com>
2024-12-31 12:05:25 +0000

    remove timecop from spec

--- spec/services/send_performance_profile_submitted_email_service_spec.rb
+++ spec/services/send_performance_profile_submitted_email_service_spec.rb
@@ -9,25 +9,21 @@ describe SendPerformanceProfileSubmittedEmailService do
 
   context "when the 'send_emails' feature is enabled", feature_send_emails: true do
     it "sends the performance profile submitted email" do
-      Timecop.freeze do
-        expect {
-          described_class.call(provider:, submitted_at:)
-        }.to have_enqueued_job.with(
-          "PerformanceProfileSubmittedEmailMailer", "generate", "deliver_now", args: [user:, submitted_at:]
-        )
-      end
+      expect {
+        described_class.call(provider:, submitted_at:)
+      }.to have_enqueued_job.with(
+        "PerformanceProfileSubmittedEmailMailer", "generate", "deliver_now", args: [user:, submitted_at:]
+      )
     end
   end
 
   context "when the 'send_emails' feature is not enabled" do
     it "does not send the performance profile submitted email" do
-      Timecop.freeze do
-        expect {
-          described_class.call(provider:, submitted_at:)
-        }.not_to have_enqueued_job.with(
-          "PerformanceProfileSubmittedEmailMailer", "generate", "deliver_now", args: [user:, submitted_at:]
-        )
-      end
+      expect {
+        described_class.call(provider:, submitted_at:)
+      }.not_to have_enqueued_job.with(
+        "PerformanceProfileSubmittedEmailMailer", "generate", "deliver_now", args: [user:, submitted_at:]
+      )
     end
   end
 end
