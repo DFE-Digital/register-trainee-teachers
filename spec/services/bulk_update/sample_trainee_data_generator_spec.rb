75340cb5a8ab42adde7347c17c127f4e56364aba
Steve Hook <steve.hook@gmail.com>
2025-01-03 13:21:55 +0000

    Appease Rubocop

--- spec/services/bulk_update/sample_trainee_data_generator_spec.rb
+++ spec/services/bulk_update/sample_trainee_data_generator_spec.rb
@@ -14,7 +14,7 @@ module BulkUpdate
     let(:count) { 1 }
 
     after do
-      File.delete(file_name) if File.exist?(file_name)
+      FileUtils.rm_f(file_name)
     end
 
     describe "#call" do
