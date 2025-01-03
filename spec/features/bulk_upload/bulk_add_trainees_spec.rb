0eb0b345b425c43a763932d70340ab6389c46552
Steve Hook <steve.hook@gmail.com>
2025-01-03 09:50:50 +0000

    Add test and fix sample data for degree data
    It should now be possible to upload details for one degree per trainee.

--- spec/features/bulk_upload/bulk_add_trainees_spec.rb
+++ spec/features/bulk_upload/bulk_add_trainees_spec.rb
@@ -248,6 +248,37 @@ feature "bulk add trainees" do
         then_i_can_see_the_new_trainees_with_placements
       end
 
+      scenario "the bulk add trainees page is visible and I upload a file with a degree" do
+        when_i_visit_the_bulk_update_index_page
+        and_i_click_the_bulk_add_trainees_page
+        and_i_attach_a_valid_file_with_a_degree
+        and_i_click_the_upload_button
+        then_a_job_is_queued_to_process_the_upload
+        then_i_see_that_the_upload_is_processing
+
+        when_the_background_job_is_run
+        and_i_refresh_the_page
+        then_i_see_the_review_page_without_validation_errors
+
+        Timecop.travel 1.hour.from_now do
+          and_i_click_the_submit_button
+        end
+
+        then_a_job_is_queued_to_process_the_upload
+
+        when_the_background_job_is_run
+        and_i_refresh_the_summary_page
+
+        and_i_see_the_summary_page
+        and_i_dont_see_the_review_errors_message
+
+        when_i_click_on_home_link
+        then_i_see_the_root_page
+
+        and_i_visit_the_trainees_page
+        then_i_can_see_the_new_trainees_with_a_degree
+      end
+
       scenario "when I try to look at the status of a different providers upload" do
         when_there_is_a_bulk_update_trainee_upload
 
@@ -681,6 +712,14 @@ private
     and_i_attach_a_file(content, filename)
   end
 
+  def and_i_attach_a_valid_file_with_a_degree
+    file     = Rails.root.join("spec/fixtures/files/bulk_update/trainee_uploads/five_trainees_with_degree.csv")
+    filename = File.basename(file)
+    content  = file.read
+
+    and_i_attach_a_file(content, filename)
+  end
+
   def when_i_attach_a_file_with_invalid_rows
     csv = Rails.root.join("spec/fixtures/files/bulk_update/trainee_uploads/five_trainees_with_two_errors.csv").read
     and_i_attach_a_file(csv)
@@ -921,6 +960,22 @@ private
     expect(page).to have_content("URN 587111")
     expect(page).not_to have_content("First placement is missing")
     expect(page).to have_content("Second placement is missing")
+  end
+
+  def then_i_can_see_the_new_trainees_with_a_degree
+
+    expect(page).to have_content("Mitsuko Larkin")
+    expect(page).to have_content("Ronnie White")
+    expect(page).to have_content("Joannie Kuhlman")
+    expect(page).to have_content("Angelica Anderson")
+    expect(page).to have_content("Yelena Schamberger")
+
+    click_on "Mitsuko Larkin"
+
+    expect(page).to have_content("Bachelor of Metallurgy")
+    expect(page).to have_content("Business law")
+    expect(page).to have_content("Anglia Ruskin University")
+    expect(page).to have_content("Third-class honours")
   end
 
   def when_the_upload_has_failed_with_validation_errors
