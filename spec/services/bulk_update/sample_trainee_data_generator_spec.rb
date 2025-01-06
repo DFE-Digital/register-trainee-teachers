04addc95a6a16e58edc69463f98a962de905d179
Steve Hook <steve.hook@gmail.com>
2025-01-06 09:39:44 +0000

    Fix gaps in code coverage

--- spec/services/bulk_update/sample_trainee_data_generator_spec.rb
+++ spec/services/bulk_update/sample_trainee_data_generator_spec.rb
@@ -4,11 +4,13 @@ require "rails_helper"
 
 module BulkUpdate
   describe SampleTraineeDataGenerator do
-    subject { described_class.call(file_name:, count:, with_invalid_records:, with_incomplete_records:) }
+    subject { described_class.call(file_name:, count:, with_invalid_records:, with_incomplete_records:, with_placement:, with_degree:) }
 
     let(:file_name) { "tmp/test_trainees.csv" }
     let(:with_invalid_records) { false }
     let(:with_incomplete_records) { false }
+    let(:with_placement) { false }
+    let(:with_degree) { false }
     let(:count) { 1 }
 
     after do
@@ -17,6 +19,16 @@ module BulkUpdate
 
     describe "#call" do
       context "with valid data" do
+        it "generates a CSV file" do
+          subject
+          expect(File.exist?(file_name)).to be true
+        end
+      end
+
+      context "with valid degrees and placements" do
+        let(:with_placement) { true }
+        let(:with_degree) { true }
+
         it "generates a CSV file" do
           subject
           expect(File.exist?(file_name)).to be true
