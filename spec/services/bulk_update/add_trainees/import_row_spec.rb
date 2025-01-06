3fc19d8ae9ccc84365d0a4af61128fee1a149408
Steve Hook <steve.hook@gmail.com>
2025-01-06 12:33:13 +0000

    Extend tests to cover placements

--- spec/services/bulk_update/add_trainees/import_row_spec.rb
+++ spec/services/bulk_update/add_trainees/import_row_spec.rb
@@ -51,17 +51,35 @@ module BulkUpdate
           end
         end
 
+        context "when the row is valid and includes placement data" do
+          let(:csv) { Rails.root.join("spec/fixtures/files/bulk_update/trainee_uploads/five_trainees_with_placement.csv").read }
+          let(:row) { parsed_csv.first }
+
+          before do
+            create(:school, urn: row["First Placement URN"])
+          end
+
+          it "creates a trainee and a placement record" do
+            original_count = Trainee.count
+            result = described_class.call(row:, current_provider:)
+            expect(result.success).to be(true)
+            expect(Trainee.count - original_count).to eq(1)
+            expect(Trainee.last.placements.count).to eq(1)
+            expect(Trainee.last.degrees.count).to eq(0)
+          end
+        end
+
         context "when the row is valid and includes degree data" do
           let(:csv) { Rails.root.join("spec/fixtures/files/bulk_update/trainee_uploads/five_trainees_with_degree.csv").read }
           let(:row) { parsed_csv.first }
 
           it "creates a trainee and a placement record" do
             original_count = Trainee.count
-            original_degree_count = Degree.count
             result = described_class.call(row:, current_provider:)
             expect(result.success).to be(true)
             expect(Trainee.count - original_count).to eq(1)
-            expect(Degree.count - original_degree_count).to eq(1)
+            expect(Trainee.last.placements.count).to eq(0)
+            expect(Trainee.last.degrees.count).to eq(1)
           end
         end
       end
