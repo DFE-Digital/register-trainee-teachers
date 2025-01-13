f614b7fe4344489a92f08d6af0eb3ef41bcbccb9
Serafeim Maroulis <serafeim@hey.com>
2025-01-13 13:36:37 +0000

    Refactor BulkdUpdate::TraineeUploads::Row::View
    * Add paths for the remaining statuses

--- app/components/bulk_update/trainee_uploads/row/view.rb
+++ app/components/bulk_update/trainee_uploads/row/view.rb
@@ -31,6 +31,8 @@ module BulkUpdate
 
         def upload_path
           {
+            "pending" => bulk_update_add_trainees_upload_path(upload),
+            "validated" => bulk_update_add_trainees_upload_path(upload),
             "succeeded" => bulk_update_add_trainees_details_path(upload),
             "in_progress" => bulk_update_add_trainees_submission_path(upload),
             "failed" => bulk_update_add_trainees_review_error_path(upload),
