b9f80de98e5a023c894918c86602a6068b7e7289
David Georgiou <david.georgiou@icloud.com>
2025-01-17 12:44:19 +0000

    [7891] - rubocop

--- app/controllers/system_admin/logging_test_controller.rb
+++ app/controllers/system_admin/logging_test_controller.rb
@@ -6,28 +6,28 @@ module SystemAdmin
 
     def info
       Rails.logger.info("LoggingTestController: Info test at #{Time.now.utc.iso8601}")
-      render json: { level: "info", time: Time.now.utc.iso8601 }
+      render(json: { level: "info", time: Time.now.utc.iso8601 })
     end
 
     def warn
       Rails.logger.warn("LoggingTestController: Warning test at #{Time.now.utc.iso8601}")
-      render json: { level: "warn", time: Time.now.utc.iso8601 }
+      render(json: { level: "warn", time: Time.now.utc.iso8601 })
     end
 
     def debug
-      Rails.logger.debug("LoggingTestController: Debug test at #{Time.now.utc.iso8601}")
-      render json: { level: "debug", time: Time.now.utc.iso8601 }
+      Rails.logger.debug { "LoggingTestController: Debug test at #{Time.now.utc.iso8601}" }
+      render(json: { level: "debug", time: Time.now.utc.iso8601 })
     end
 
     def error
       Rails.logger.error("LoggingTestController: Error test at #{Time.now.utc.iso8601}")
-      render json: { level: "error", time: Time.now.utc.iso8601 }
+      render(json: { level: "error", time: Time.now.utc.iso8601 })
     end
 
     def background_job
       TestLoggingJob.perform_later
       Rails.logger.info("LoggingTestController: Enqueued TestLoggingJob at #{Time.now.utc.iso8601}")
-      render json: { status: "job_enqueued", time: Time.now.utc.iso8601 }
+      render(json: { status: "job_enqueued", time: Time.now.utc.iso8601 })
     end
   end
 end
