4ecb9c14f8ac12d9cc151d5ec9cfe07ef3b88f82
David Georgiou <david.georgiou@icloud.com>
2025-01-17 12:46:39 +0000

    [7891] - corrects sidekiq init

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,6 +8,12 @@ Sidekiq.configure_server do |config|
   }
 end
 
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: QUEUE_URL,
+  }
+end
+
 # Sidekiq Cron
 if Settings.sidekiq.schedule_file && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash!(YAML.load_file(Settings.sidekiq.schedule_file))
