b9182bc196631cd7326c0d03f2c0ce19fb3498af
Serafeim Maroulis <serafeim@hey.com>
2025-01-20 14:47:50 +0000

    Refactor spec to test for CSV content

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,14 +6,33 @@ Capybara.register_driver :cuprite do |app|
   Capybara::Cuprite::Driver.new(
     app,
     window_size: [1200, 800],
-    browser_options: { "ignore-certificate-errors" => nil, "no-sandbox" => nil },
+    browser_options: {
+      "ignore-certificate-errors": nil,
+      "no-sandbox": nil,
+      "disable-gpu": nil,
+      "disable-popup-blocking": nil
+    },
     process_timeout: 10,
     inspector: true,
     headless: true,
+    browser_args: [
+      "--disable-popup-blocking",
+      "--disable-extensions",
+      "--disable-infobars",
+      "--disable-dev-shm-usage",
+      "--disable-translate",
+      "--disable-notifications",
+    ],
+    context_attributes: {
+      prefs: {
+        'download.default_directory' => DownloadHelper::PATH.to_s,
+        'download.prompt_for_download' => false,
+      }
+    }
   )
 end
 
 Capybara.server = :puma, { Silent: true }
 Capybara.default_driver = :rack_test
 Capybara.javascript_driver = :cuprite
-Capybara.default_max_wait_time = 10
+Capybara.default_max_wait_time = 20
---
+++ spec/support/download_helper.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module DownloadHelper
+  TIMEOUT = 10
+  PATH    = Capybara.save_path
+
+  def downloads
+    Dir[PATH.join('*')]
+  end
+
+  def download
+    downloads.first
+  end
+
+  def download_content
+    wait_for_download
+    File.read(download)
+  end
+
+  def wait_for_download
+    Timeout.timeout(TIMEOUT) do
+      sleep 0.1 until downloaded?
+    end
+  end
+
+  def downloaded?
+    !downloading? && downloads.any?
+  end
+
+  def downloading?
+    downloads.grep(/\.crdownload$/).any?
+  end
+
+  def clear_downloads
+    FileUtils.rm_f(downloads)
+  end
+end
