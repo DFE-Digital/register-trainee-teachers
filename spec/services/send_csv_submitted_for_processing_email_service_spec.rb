b5ffd356fbcee9ef761542fb409bccd8f8b965f9
Chris Gannon <cjgannon@gmail.com>
2025-01-22 17:37:37 +0000

    remove feature flag from mailers

--- spec/services/send_csv_submitted_for_processing_email_service_spec.rb
+++ spec/services/send_csv_submitted_for_processing_email_service_spec.rb
@@ -6,27 +6,13 @@ describe SendCsvSubmittedForProcessingEmailService do
   let(:wait_time) { 30.seconds }
   let(:upload) { create(:bulk_update_trainee_upload, :in_progress) }
 
-  context "when the 'send_emails' feature is enabled", feature_send_emails: true do
-    it "sends the csv submitted for processing email" do
-      Timecop.freeze do
-        expect {
-          described_class.call(upload:)
-        }.to have_enqueued_job.with(
-          "CsvSubmittedForProcessingEmailMailer", "generate", "deliver_now", args: [upload:]
-        ).at(wait_time.from_now)
-      end
-    end
-  end
-
-  context "when the 'send_emails' feature is not enabled" do
-    it "sends the csv submitted for processing email" do
-      Timecop.freeze do
-        expect {
-          described_class.call(upload:)
-        }.not_to have_enqueued_job.with(
-          "CsvSubmittedForProcessingEmailMailer", "generate", "deliver_now", args: [upload:]
-        ).at(wait_time.from_now)
-      end
+  it "sends the csv submitted for processing email" do
+    Timecop.freeze do
+      expect {
+        described_class.call(upload:)
+      }.to have_enqueued_job.with(
+        "CsvSubmittedForProcessingEmailMailer", "generate", "deliver_now", args: [upload:]
+      ).at(wait_time.from_now)
     end
   end
 end
--- spec/services/send_performance_profile_submitted_email_service_spec.rb
+++ spec/services/send_performance_profile_submitted_email_service_spec.rb
@@ -7,23 +7,11 @@ describe SendPerformanceProfileSubmittedEmailService do
   let(:provider) { user.providers.first }
   let(:submitted_at) { Time.zone.now }
 
-  context "when the 'send_emails' feature is enabled", feature_send_emails: true do
-    it "sends the performance profile submitted email" do
-      expect {
-        described_class.call(provider:, submitted_at:)
-      }.to have_enqueued_job.with(
-        "PerformanceProfileSubmittedEmailMailer", "generate", "deliver_now", args: [user:, submitted_at:]
-      )
-    end
-  end
-
-  context "when the 'send_emails' feature is not enabled" do
-    it "does not send the performance profile submitted email" do
-      expect {
-        described_class.call(provider:, submitted_at:)
-      }.not_to have_enqueued_job.with(
-        "PerformanceProfileSubmittedEmailMailer", "generate", "deliver_now", args: [user:, submitted_at:]
-      )
-    end
+  it "sends the performance profile submitted email" do
+    expect {
+      described_class.call(provider:, submitted_at:)
+    }.to have_enqueued_job.with(
+      "PerformanceProfileSubmittedEmailMailer", "generate", "deliver_now", args: [user:, submitted_at:]
+    )
   end
 end
