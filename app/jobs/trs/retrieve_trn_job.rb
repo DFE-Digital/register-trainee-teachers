382c7536ae9ca3fe05e2f96ccba2d5ef7fc7209d
David Georgiou <david.georgiou@icloud.com>
2025-01-30 10:29:12 +0000

    [8113] - retrieve trn

---
+++ app/jobs/trs/retrieve_trn_job.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+module Trs
+  class RetrieveTrnJob < Dqt::BaseJob
+    queue_as :dqt
+    retry_on Client::HttpError
+    include NotifyOnTimeout
+
+    class TraineeAttributeError < StandardError; end
+
+    def perform(trn_request, timeout_after = nil)
+      return unless FeatureService.enabled?(:integrate_with_trs)
+
+      @trn_request = trn_request
+      @timeout_after = timeout_after
+      @trainee = trn_request.trainee
+
+      if @timeout_after.nil?
+        schedule_next_attempt
+        return
+      end
+
+      process_trn_request
+    end
+
+  private
+
+    attr_reader :trn_request, :trainee, :timeout_after
+
+    def schedule_next_attempt
+      next_attempt_time = trainee.submitted_for_trn_at + timeout_duration
+      self.class.perform_later(trn_request, next_attempt_time)
+    end
+
+    def process_trn_request
+      trn = RetrieveTrn.call(trn_request:)
+
+      if trn
+        trainee.trn_received!(trn)
+        trn_request.received!
+      elsif continue_polling?
+        requeue
+      else
+        notify_failure
+      end
+    end
+
+    def continue_polling?
+      validate_trainee_attributes
+      Time.zone.now.utc < timeout_after
+    end
+
+    def validate_trainee_attributes
+      if trainee.submitted_for_trn_at.nil?
+        raise(TraineeAttributeError, "Trainee#submitted_for_trn_at is nil - it should be timestamped (id: #{trainee.id})")
+      end
+    end
+
+    def requeue
+      self.class.set(wait: Settings.jobs.poll_delay_hours.hours).perform_later(trn_request, timeout_after)
+    end
+
+    def notify_failure
+      send_message_to_slack(trainee, self.class.name)
+    end
+
+    def timeout_duration
+      return (Settings.jobs.max_poll_duration_days + 6).days if Time.zone.now.month == 10 # Increase timeout for jobs during October to deal with increased registrations
+
+      Settings.jobs.max_poll_duration_days.days
+    end
+  end
+end
---
+++ spec/jobs/trs/retrieve_trn_job_spec.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Trs
+  describe RetrieveTrnJob do
+    include ActiveJob::TestHelper
+
+    let(:trn) { nil }
+    let(:trainee) { create(:trainee, :submitted_for_trn) }
+    let(:configured_delay) { 6 }
+    let(:configured_poll_timeout_days) { 4 }
+    let(:timeout_date) { configured_poll_timeout_days.days.from_now }
+    let(:trn_request) { create(:dqt_trn_request, trainee:) }
+
+    before do
+      enable_features(:integrate_with_trs)
+      allow(RetrieveTrn).to receive(:call).with(trn_request:).and_return(trn)
+      allow(Settings.jobs).to receive_messages(poll_delay_hours: configured_delay, max_poll_duration_days: configured_poll_timeout_days)
+      allow(SlackNotifierService).to receive(:call)
+    end
+
+    context "when timeout_after is nil" do
+      context "during October" do
+        let(:timeout_date) { trainee.submitted_for_trn_at + (configured_poll_timeout_days + 6).days }
+
+        it "re-enqueues RetrieveTrnJob with the trainee and default timeout_after plus 6 days" do
+          Timecop.travel(Time.zone.now.year, 10, 1) do
+            expect {
+              described_class.perform_now(trn_request, nil)
+            }.to enqueue_job(RetrieveTrnJob).with(trn_request, timeout_date)
+          end
+        end
+      end
+
+      context "during any time of the year other than October" do
+        let(:timeout_date) { trainee.submitted_for_trn_at + configured_poll_timeout_days.days }
+
+        it "re-enqueues RetrieveTrnJob with the trainee and default timeout_after" do
+          (1..9).to_a + [11, 12].each do |month|
+            Timecop.travel(Time.zone.now.year, month, 1) do
+              expect {
+                described_class.perform_now(trn_request, nil)
+              }.to enqueue_job(RetrieveTrnJob).with(trn_request, timeout_date)
+            end
+          end
+        end
+      end
+    end
+
+    context "TRN is available" do
+      let(:trn) { "123" }
+
+      it "updates the trainee TRN attribute" do
+        expect {
+          described_class.perform_now(trn_request, timeout_date)
+        }.to change(trainee, :trn).to(trn)
+      end
+
+      it "doesn't queue another job" do
+        described_class.perform_now(trn_request, timeout_date)
+        expect(RetrieveTrnJob).not_to have_been_enqueued
+      end
+    end
+
+    context "TRN is not available" do
+      it "queues another job to fetch the TRN after the configured delay" do
+        Timecop.freeze(Time.zone.now) do
+          described_class.perform_now(trn_request, timeout_date)
+          expect(RetrieveTrnJob).to have_been_enqueued.at(configured_delay.hours.from_now)
+            .with(trn_request, timeout_date)
+        end
+      end
+    end
+
+    context "timeout_after has passed" do
+      it "doesn't queue another job" do
+        expect(SlackNotifierService).to receive(:call)
+        described_class.perform_now(trn_request, 1.minute.ago)
+        expect(RetrieveTrnJob).not_to have_been_enqueued
+      end
+    end
+
+    context "the trainee attribute submitted_for_trn_at is nil" do
+      let(:trainee) { create(:trainee, :submitted_for_trn, submitted_for_trn_at: nil) }
+      let(:error_msg) { "Trainee#submitted_for_trn_at is nil - it should be timestamped (id: #{trainee.id})" }
+
+      it "raises a TraineeAttributeError" do
+        expect {
+          described_class.perform_now(trn_request, timeout_date)
+        }.to raise_error(RetrieveTrnJob::TraineeAttributeError, error_msg)
+      end
+    end
+  end
+end
