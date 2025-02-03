cca7076103f57847b89d4e78573feb631a7279cc
Serafeim Maroulis <serafeim@hey.com>
2025-02-03 09:48:34 +0000

    [8096] Add 'uploaded' screen and refactor the flow to submission (#4994)
    * Refactor policy spec to test for uploaded
    
    * Allow uploaded to transition to cancelled
    
    * Refactor CSV bulk trainee flow
    
    * Split the validation of the CSV file and trainee records into
      different actions
    
    * Use BulkUpdate::TraineeUploads::Show::Uploaded
    
    * Replace new action for BulkUpdate::AddTrainees::ImportsController#new with a
    component in BulkUpdate::AddTrainees::UploadsController#show
    
    * Fix spec
    
    * Refactor spec
    
    * Make #csv private
    
    * Address PR feedback
    
    * Fix back link

--- config/routes.rb
+++ config/routes.rb
@@ -91,7 +91,7 @@ Rails.application.routes.draw do
     namespace :add_trainees, path: "add-trainees" do
       resources :uploads, only: %i[index show new create destroy] do
         member do
-          resource :imports, only: :create
+          resource :imports, only: %i[create]
           resource :submission, only: %i[show create]
         end
       end
