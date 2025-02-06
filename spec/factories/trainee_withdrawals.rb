862901e2173c9fca11818159e8cc1704a4cb28bd
Chris Gannon <cjgannon@gmail.com>
2025-02-06 13:09:47 +0000

    fix rubocop linting

--- spec/factories/trainee_withdrawals.rb
+++ spec/factories/trainee_withdrawals.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :trainee_withdrawal, class: "TraineeWithdrawal" do
     trainee
-    date { Time.new(2024, 11, 18) }
+    date { Time.zone.local(2024, 11, 18) }
     trigger { :provider }
     another_reason { "Not enough coffee" }
     future_interest { :yes }
