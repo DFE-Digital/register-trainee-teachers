d3f9fd8c2372eff3151efda1468b84187e62a125
Steve Hook <steve.hook@gmail.com>
2025-02-10 09:54:19 +0000

    Refactor to inject form into view component
    `BulkUpdate::BulkAddTraineesUploadForm` can be instantiated in the
    controller and injected into the various
    `BulkUpdate::TraineeUploads::Show::*` components.

--- app/components/bulk_update/trainee_uploads/show/failed/view.rb
+++ app/components/bulk_update/trainee_uploads/show/failed/view.rb
@@ -5,13 +5,6 @@ module BulkUpdate
     module Show
       module Failed
         class View < BulkUpdate::TraineeUploads::Show::Base::View
-          def initialize(upload:, current_user: nil)
-            super
-
-            @bulk_add_trainee_upload_form = BulkUpdate::BulkAddTraineesUploadForm.new(
-              provider: current_user.organisation,
-            )
-          end
         end
       end
     end
