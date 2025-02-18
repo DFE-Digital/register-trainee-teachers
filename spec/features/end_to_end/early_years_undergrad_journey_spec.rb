8df28ecd0270ea6fcfaea4ce693ee5016eba1328
Serafeim Maroulis <serafeim@hey.com>
2025-02-18 10:06:39 +0000

    [8162] Enable lead partners for early_years_undergrad (#5040)
    * Enable lead partners for early_years_undergrad
    
    * Lint
    
    * Update config/initializers/training_routes.rb
    
    Co-authored-by: Tom Trentham <1910056+tomtrentham@users.noreply.github.com>
    
    * Address PR feedback
    
    ---------
    
    Co-authored-by: Tom Trentham <1910056+tomtrentham@users.noreply.github.com>

---
+++ spec/features/end_to_end/early_years_undergrad_journey_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+feature "Early years undergrad end-to-end journey" do
+  background { given_i_am_authenticated }
+
+  include_context "perform enqueued jobs"
+
+  scenario "submit for TRN", "feature_routes.early_years_undergrad": true do
+    given_i_have_created_an_early_years_undergrad_trainee
+    and_the_personal_details_is_complete
+    and_the_contact_details_is_complete
+    and_the_diversity_information_is_complete
+    and_the_course_details_is_complete(early_years_undergrad: true)
+    and_the_placements_details_is_complete
+    and_the_trainee_id_is_complete
+    and_the_funding_details_is_complete
+    and_the_lead_partner_section_is_complete
+    and_the_draft_record_has_been_reviewed
+    and_all_sections_are_complete
+    when_i_submit_for_trn
+    then_i_am_redirected_to_the_trn_success_page
+  end
+end
