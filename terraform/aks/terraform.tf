75aedada1298d464dc3f3386a18bfe8944a4a9f8
RMcVelia <rick.mcvelia@gmail.com>
2025-02-21 13:17:48 +0000

    Create separate storage account for sanitised bkp

--- terraform/aks/terraform.tf
+++ terraform/aks/terraform.tf
@@ -7,4 +7,6 @@ module "azure" {
   tempdata_storage_account_name    = local.azure_tempdata_storage_account_name
   storage_account_replication_type = var.azure_storage_account_replication_type
   deploy_temp_data_storage_account = var.deploy_temp_data_storage_account
+  sanitised_storage_account_name   = local.azure_sanitised_storage_account_name
+  enable_sanitised_storage         = var.enable_sanitised_storage
 }
--- terraform/modules/azure/storage.tf
+++ terraform/modules/azure/storage.tf
@@ -30,3 +30,56 @@ resource "azurerm_storage_container" "tempdata" {
   storage_account_name  = resource.azurerm_storage_account.tempdata[0].name
   container_access_type = "private"
 }
+
+resource "azurerm_storage_account" "sanitised_uploads" {
+  count = var.enable_sanitised_storage ? 1 : 0
+
+  name                              = var.sanitised_storage_account_name
+  resource_group_name               = var.resource_group_name
+  location                          = "UK South"
+  account_replication_type          = "LRS"
+  account_tier                      = "Standard"
+  account_kind                      = "StorageV2"
+  min_tls_version                   = "TLS1_2"
+  infrastructure_encryption_enabled = true
+  allow_nested_items_to_be_public   = false
+  cross_tenant_replication_enabled  = false
+
+  blob_properties {
+
+    container_delete_retention_policy {
+      days = 7
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_storage_container" "sanitised_uploads" {
+  count                 = var.enable_sanitised_storage ? 1 : 0
+
+  name                  = "database-backup"
+  storage_account_name  = azurerm_storage_account.sanitised_uploads[0].name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_management_policy" "sanitised_backup" {
+  count = var.enable_sanitised_storage ? 1 : 0
+
+  storage_account_id = azurerm_storage_account.sanitised_uploads[0].id
+
+  rule {
+    name    = "DeleteAfter7Days"
+    enabled = true
+    filters {
+      blob_types = ["blockBlob"]
+    }
+    actions {
+      base_blob {
+        delete_after_days_since_modification_greater_than = 7
+      }
+    }
+  }
+}
