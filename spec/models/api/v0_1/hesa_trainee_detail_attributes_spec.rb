fa013b5e38b9f016854ae45886858b7ae3ebc309
Serafeim Maroulis <serafeim@hey.com>
2025-02-21 13:53:57 +0000

    Add trainee attributes inclusion validations
    * Refactor API::V01::TraineeAttributes to validate for inclusion on POST
      / PUT

--- spec/models/api/v0_1/hesa_trainee_detail_attributes_spec.rb
+++ spec/models/api/v0_1/hesa_trainee_detail_attributes_spec.rb
@@ -13,9 +13,8 @@ RSpec.describe Api::V01::HesaTraineeDetailAttributes do
 
     describe "course_age_range" do
       it { is_expected.to validate_presence_of(:course_age_range) }
-
       it {
-        expect(subject).to validate_inclusion_of(:course_age_range)
+        is_expected.to validate_inclusion_of(:course_age_range)
           .in_array(Hesa::CodeSets::AgeRanges::MAPPING.keys)
       }
     end
