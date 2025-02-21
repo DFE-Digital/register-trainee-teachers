196fb4a6aae8a37a54528bcafa0ef1236c6f1620
Steve Hook <steve.hook@gmail.com>
2025-02-21 16:53:52 +0000

    Add organisation_switcher

--- app/components/organisation_switcher/view.rb
+++ app/components/organisation_switcher/view.rb
@@ -1,6 +1,23 @@
 # frozen_string_literal: true
 
 class OrganisationSwitcher::View < ViewComponent::Base
-  def initialize()
+  include OrganisationHelper
+
+  attr_reader :current_user
+
+  def initialize(current_user:)
+    @current_user = current_user
+  end
+
+  def render?
+    current_user&.organisation.present?
+  end
+
+  def organisation_name
+    current_user&.organisation&.name
+  end
+
+  def link_to_switch_organisation
+    organisations_path
   end
 end
