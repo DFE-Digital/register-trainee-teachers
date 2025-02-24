56bfef3256f5b60e6ed5345cc05729ceb82cae1f
RMcVelia <rick.mcvelia@gmail.com>
2025-02-24 11:49:32 +0000

    Use oidc for github workflows

--- terraform/aks/provider.tf
+++ terraform/aks/provider.tf
@@ -30,16 +30,11 @@ provider "statuscake" {
 
 provider "kubernetes" {
   host                   = module.cluster_data.kubernetes_host
-  client_certificate     = module.cluster_data.kubernetes_client_certificate
-  client_key             = module.cluster_data.kubernetes_client_key
   cluster_ca_certificate = module.cluster_data.kubernetes_cluster_ca_certificate
 
-  dynamic "exec" {
-    for_each = module.cluster_data.azure_RBAC_enabled ? [1] : []
-    content {
+  exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "kubelogin"
       args        = module.cluster_data.kubelogin_args
-    }
   }
 }
