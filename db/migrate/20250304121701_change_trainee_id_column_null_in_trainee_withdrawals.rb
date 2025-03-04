f98f9eab18b0985f07810a1b4a6ae086e907deb5
Serafeim Maroulis <serafeim@hey.com>
2025-03-04 12:19:52 +0000

    Change TraineeWithdrawal#trainee_id to be null false

---
+++ db/migrate/20250304121701_change_trainee_id_column_null_in_trainee_withdrawals.rb
@@ -0,0 +1,7 @@
+class ChangeTraineeIdColumnNullInTraineeWithdrawals < ActiveRecord::Migration[7.2]
+  def change
+    safety_assured do
+      change_column_null :trainee_withdrawals, :trainee_id, false
+    end
+  end
+end
