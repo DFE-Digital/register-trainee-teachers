138376a968df90d2687b62287fa68e58d4513423
Chris Gannon <cjgannon@gmail.com>
2025-03-18 12:02:29 +0000

    update not null constraint on name

--- db/migrate/20250314141354_add_attributes_to_authetication_tokens.rb
+++ db/migrate/20250314141354_add_attributes_to_authetication_tokens.rb
@@ -5,7 +5,7 @@ class AddAttributesToAutheticationTokens < ActiveRecord::Migration[7.2]
 
   def change
     safety_assured do
-      add_column :authentication_tokens, :name, :string, null: false, default: "Test token"
+      add_column :authentication_tokens, :name, :string
       add_column :authentication_tokens, :revoked_at, :datetime
       add_column :authentication_tokens, :last_used_at, :datetime
       add_reference :authentication_tokens, :created_by, foreign_key: { to_table: :users }
@@ -16,7 +16,7 @@ class AddAttributesToAutheticationTokens < ActiveRecord::Migration[7.2]
       AuthenticationToken.find_each do |authentication_token|
         provider = authentication_token.provider
 
-        authentication_token.update!(created_by: provider.users.kept.first)
+        authentication_token.update!(created_by: provider.users.kept.first, name: "Test token")
       end
 
       change_column_null :authentication_tokens, :name, false
