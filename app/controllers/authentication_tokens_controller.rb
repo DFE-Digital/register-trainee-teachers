f0a287484548ca0fc9ef52606ddee61c9fff91db
Steve Hook <steve.hook@gmail.com>
2025-03-18 10:27:07 +0000

    WIP - build create action

--- app/controllers/authentication_tokens_controller.rb
+++ app/controllers/authentication_tokens_controller.rb
@@ -12,11 +12,29 @@ class AuthenticationTokensController < ApplicationController
     authorize(@token)
   end
 
+  def create
+    authorize(AuthenticationToken, :create?)
+    @token, auth_token = AuthenticationToken.create_with_random_token(token_params)
+
+    # TODO: Set the `created_by` attribute to the current user
+
+    if @token.persisted?
+      redirect_to(authentication_tokens_path)
+    else
+      render(:new)
+    end
+  end
+
 private
 
   def tokens
     @tokens ||= policy_scope(AuthenticationToken)
       .includes(:provider, :created_by, :revoked_by)
       .by_status_and_last_used_at
+  end
+
+  def token_params
+    #TODO: Add the `name` to the strong params
+    params.require(:authentication_token).permit(:expires_at)
   end
 end
