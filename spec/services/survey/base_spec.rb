5da9dd70ef365725439447084df9dedd9445140a
David Georgiou <david.georgiou@icloud.com>
2025-03-20 15:45:56 +0000

    [8221] - tidy up base class spec

--- spec/services/survey/base_spec.rb
+++ spec/services/survey/base_spec.rb
@@ -4,36 +4,7 @@ require "rails_helper"
 
 module Survey
   describe Base do
-    let(:first_names) { "Trentham" }
-    let(:last_name) { "Fong" }
-    let(:email) { "trentham.fong@example.com" }
-    let(:training_route) { "provider_led_postgrad" }
-
-    let(:trainee) do
-      build(:trainee,
-            first_names:,
-            last_name:,
-            email:,
-            training_route:)
-    end
-
-    let(:settings_stub) do
-      allow(Settings).to receive_messages(qualtrics: OpenStruct.new(
-        api_token: "please_change_me",
-        directory_id: "FAKE_DIRECTORY_ID",
-        base_url: "https://fra1.qualtrics.com/API/v3/",
-        library_id: "FAKE_LIBRARY_ID",
-      ), data_email: "data@example.com")
-    end
-
-    before do
-      settings_stub
-    end
-
-    # A simple test to verify the ServicePattern is being used
-    it "includes ServicePattern" do
-      expect(described_class.included_modules).to include(ServicePattern)
-    end
+    let(:trainee) { create(:trainee) }
 
     describe "abstract methods" do
       # Use allocate to create an instance without calling initialize
