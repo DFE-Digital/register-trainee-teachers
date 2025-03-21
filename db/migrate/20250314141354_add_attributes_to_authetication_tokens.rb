46dd0ed137081b5b6e1de89f549603b05b23f350
Serafeim Maroulis <serafeim@hey.com>
2025-03-21 16:30:37 +0000

    Make migration reversible

--- db/migrate/20250314141354_add_attributes_to_authetication_tokens.rb
+++ db/migrate/20250314141354_add_attributes_to_authetication_tokens.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class AddAttributesToAutheticationTokens < ActiveRecord::Migration[7.2]
-  class AuthenticationToken < ApplicationRecord; end
+  class AuthenticationToken < ApplicationRecord
+    belongs_to :provider
+    belongs_to :created_by, class_name: "User", optional: true
+    belongs_to :revoked_by, class_name: "User", optional: true
+  end
 
   def change
     safety_assured do
@@ -11,15 +15,19 @@ class AddAttributesToAutheticationTokens < ActiveRecord::Migration[7.2]
       add_reference :authentication_tokens, :created_by, foreign_key: { to_table: :users }
       add_reference :authentication_tokens, :revoked_by, foreign_key: { to_table: :users }
 
-      # Ensure existing tokens have a created_by user and name going forward
+      reversible do |dir|
+        dir.up do
+          # Ensure existing tokens have a created_by user and name going forward
 
-      AuthenticationToken.find_each do |authentication_token|
-        provider = authentication_token.provider
+          AuthenticationToken.find_each do |authentication_token|
+            provider = authentication_token.provider
 
-        authentication_token.update!(created_by: provider.users.kept.first, name: "Test token")
+            authentication_token.update!(created_by: provider.users.kept.first, name: "Test token")
+          end
+
+          change_column_null :authentication_tokens, :name, false
+        end
       end
-
-      change_column_null :authentication_tokens, :name, false
     end
   end
 end
