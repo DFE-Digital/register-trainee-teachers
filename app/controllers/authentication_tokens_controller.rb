0800ca1d01af439ca5038271fd4af8f2f2f94428
Serafeim Maroulis <serafeim@hey.com>
2025-03-21 14:40:14 +0000

    Order tokens by status and last_used_at

--- app/controllers/authentication_tokens_controller.rb
+++ app/controllers/authentication_tokens_controller.rb
@@ -4,13 +4,13 @@ class AuthenticationTokensController < ApplicationController
   before_action { require_feature_flag(:token_management) }
 
   def index
-    authorize(tokens)
+    @tokens = authorize(tokens).by_status_and_last_used_at
   end
 
 private
 
   def tokens
-    @tokens ||= policy_scope(AuthenticationToken)
+    policy_scope(AuthenticationToken)
       .includes(:provider, :created_by, :revoked_by)
   end
 end
