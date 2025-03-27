f99d827b975366bdf531a624e891a41938c69181
Chris Gannon <cjgannon@gmail.com>
2025-03-27 09:29:16 +0000

    update migration to make sure tokens are disabled on rollback

--- db/migrate/20250325114517_remove_enabled_from_authentication_tokens.rb
+++ db/migrate/20250325114517_remove_enabled_from_authentication_tokens.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class RemoveEnabledFromAuthenticationTokens < ActiveRecord::Migration[7.2]
-  def change
-    safety_assured { remove_column :authentication_tokens, :enabled, type: :boolean, default: true, null: false }
+  def up
+    safety_assured do
+      AuthenticationToken.where(status: "active").where(enabled: false).update_all(status: "revoked")
+
+      remove_column :authentication_tokens, :enabled
+    end
+  end
+
+  def down
+    safety_assured do
+      add_column :authentication_tokens, :enabled, :boolean, default: true, null: false
+
+      AuthenticationToken.where(status: "revoked").update_all(status: "active", enabled: false)
+    end
   end
 end
