1871e890d9d3a10113e8a57676247c8e5c0b36bb
Steve Hook <steve.hook@gmail.com>
2025-03-28 10:11:41 +0000

    WIP

--- app/models/api/v1_0_pre/hesa_trainee_detail_attributes.rb
+++ app/models/api/v1_0_pre/hesa_trainee_detail_attributes.rb
@@ -4,6 +4,8 @@ module Api
   module V10Pre
     class HesaTraineeDetailAttributes < Api::V01::HesaTraineeDetailAttributes
       validates_with RulesValidator
+
+      include Api::ErrorAttributeAdapter
     end
   end
 end
