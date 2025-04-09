b17cb7abfd6b8563ee5d197b1c20c76e2f5b6a77
Steve Hook <steve.hook@gmail.com>
2025-04-09 20:20:38 +0100

    Basic specs for `HeQualificationsReport`

--- spec/models/reports/he_qualifications_report_spec.rb
+++ spec/models/reports/he_qualifications_report_spec.rb
@@ -3,17 +3,23 @@
 require "rails_helper"
 
 describe Reports::HeQualificationsReport do
+  let(:csv) { StringIO.new }
+
   context "given an empty list of qualifications" do
     let(:qualifications) { [] }
 
+    before do
+      report = described_class.new(CSV.new(csv), scope: qualifications)
+      report.generate_report
+    end
+
     it "generates a CSV with the correct headers and no rows" do
-      pending "TODO"
+      expect(csv.string).to include("trn,date_of_birth,nino,subject_code,description\n")
     end
   end
 
   context "given a list of qualifications" do
     let(:qualifications) { Degree.all }
-    let(:csv) { StringIO.new }
 
     before do
       @trainees = create_list(:trainee, 3, :trn_received, :with_hesa_trainee_detail)
@@ -26,11 +32,16 @@ describe Reports::HeQualificationsReport do
     end
 
     it "generates a CSV with the correct headers" do
-      pending "TODO"
+      expect(csv.string).to include("trn,date_of_birth,nino,subject_code,description\n")
     end
 
-    it "outputs the one row for each qualification" do
-      expect(csv.string).to include("Foo")
+    it "outputs one row for each qualification" do
+      qualifications.each do |degree|
+        degree_subject = DfEReference::DegreesQuery::SUBJECTS.one(degree.subject_uuid)
+        expect(csv.string).to include(
+          "#{degree.trainee.trn},#{degree.trainee.date_of_birth.iso8601},#{degree.trainee.hesa_trainee_detail.ni_number},#{degree_subject.hecos_code},#{CsvValueSanitiser.new(degree_subject.name).sanitise}\n",
+        )
+      end
     end
   end
 end
