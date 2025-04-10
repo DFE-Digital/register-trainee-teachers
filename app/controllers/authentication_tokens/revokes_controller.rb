e4c9461681eace040e7b7b098c06c9df9995f9de
Chris Gannon <cjgannon@gmail.com>
2025-04-10 15:55:03 +0100

    Update app/controllers/authentication_tokens/revokes_controller.rb
    Co-authored-by: Serafeim Maroulis <serafeim@hey.com>

--- app/controllers/authentication_tokens/revokes_controller.rb
+++ app/controllers/authentication_tokens/revokes_controller.rb
@@ -17,7 +17,7 @@ module AuthenticationTokens
   private
 
     def token
-      @token ||= policy_scope(AuthenticationToken).where(id: token_params).first
+      @token ||= policy_scope(AuthenticationToken).find(params[:authentication_token_id)
     end
 
     def token_params
