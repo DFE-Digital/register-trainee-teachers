72a5c16a7f89db138f095d83df415c1a2e076f5b
Serafeim Maroulis <serafeim@hey.com>
2025-04-22 16:37:50 +0100

    Refactor NotFutureDateValidator
    * Use value
    * Use iso8601 instead of parse

--- app/validators/not_future_date_validator.rb
+++ app/validators/not_future_date_validator.rb
@@ -4,13 +4,13 @@ class NotFutureDateValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    record.errors.add(attribute, :future) if future_date?(record.public_send(attribute))
+    record.errors.add(attribute, :future) if future_date?(value)
   end
 
 private
 
   def future_date?(date)
-    Date.parse(date).future?
+    Date.iso8601(date).future?
   rescue Date::Error
     false
   end
