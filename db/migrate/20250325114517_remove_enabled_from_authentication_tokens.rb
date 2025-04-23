4a0a41587af5f08f904a65647eca6f463aa8ac08
Serafeim Maroulis <serafeim@hey.com>
2025-04-23 15:42:04 +0100

    Fix migration

--- db/migrate/20250325114517_remove_enabled_from_authentication_tokens.rb
+++ db/migrate/20250325114517_remove_enabled_from_authentication_tokens.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class RemoveEnabledFromAuthenticationTokens < ActiveRecord::Migration[7.2]
+  class AuthenticationToken < ApplicationRecord; end
+
   def up
     safety_assured do
-      AuthenticationToken.active.where(enabled: false).update_all(status: "revoked")
+      AuthenticationToken.where(status: "active", enabled: false).update_all(status: "revoked")
 
       remove_column :authentication_tokens, :enabled
     end
@@ -13,7 +15,7 @@ class RemoveEnabledFromAuthenticationTokens < ActiveRecord::Migration[7.2]
     safety_assured do
       add_column :authentication_tokens, :enabled, :boolean, default: true, null: false
 
-      AuthenticationToken.revoked.update_all(status: "active", enabled: false)
+      AuthenticationToken.where(status: "revoked").update_all(status: "active", enabled: false)
     end
   end
 end
