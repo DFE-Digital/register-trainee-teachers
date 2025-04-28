37281b832a531ff4a1946126c958fb55429a88dd
Serafeim Maroulis <serafeim@hey.com>
2025-04-28 11:03:05 +0100

    [8433] API token with a backdated expiry date (#5234)
    * Use create! when creating tokens
    
    * Refactor NotFutureDateValidator
    
    * Use value
    * Use iso8601 instead of parse
    
    * Validate AuthenticationToken expires_at future date
    
    * Refactor specs
    
    * Ensure hashed_token is unique
    
    * Use DateRelativeToTimeValidator
    
    Replaces:
    
    * FutureDateValidator
    * NotFutureDateValidator

--- app/validators/date_validator.rb
+++ app/validators/date_validator.rb
@@ -4,7 +4,7 @@ class DateValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.blank?
 
-    Date.iso8601(value)
+    Date.iso8601(value.to_s)
   rescue Date::Error
     record.errors.add(attribute, :invalid)
   end
