16ef65128a9a52cb9fe3e1b996329725a2e732dc
Serafeim Maroulis <serafeim@hey.com>
2025-04-29 11:33:21 +0100

    [8460] Trainee disabilities are not validated or created through CSV bulk upload (#5244)
    * Fix migration
    
    * Bug fix for CSV bulk upload disabilities creation
    
    * Lint
    
    * Fix technical fields

--- spec/factories/disabilities.rb
+++ spec/factories/disabilities.rb
@@ -13,6 +13,30 @@ FactoryBot.define do
       name { "Deaf" }
     end
 
+    trait :development_condition do
+      name { "Development condition" }
+    end
+
+    trait :learning_difficulty do
+      name { "Learning difficulty" }
+    end
+
+    trait :long_standing_illness do
+      name { "Long-standing illness" }
+    end
+
+    trait :mental_health_condition do
+      name { "Mental health condition" }
+    end
+
+    trait :physical_disability_or_mobility_issue do
+      name { "Physical disability or mobility issue" }
+    end
+
+    trait :social_or_communication_impairment do
+      name { "Social or communication impairment" }
+    end
+
     trait :other do
       name { "Other" }
     end
