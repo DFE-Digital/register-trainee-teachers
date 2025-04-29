e85e26550604e87518cb77ff9842f4fbbd441292
Chris Gannon <cjgannon@gmail.com>
2025-04-29 11:21:16 +0100

    update spec

--- spec/requests/api/v1_0_pre/get_trainee_spec.rb
+++ spec/requests/api/v1_0_pre/get_trainee_spec.rb
@@ -18,7 +18,7 @@ describe "`GET /api/v1.0-pre/trainees/:id` endpoint" do
     end
 
     it "returns the trainee" do
-      parsed_trainee = JSON.parse(Api::GetVersionedItem.for_serializer(model: :trainee, version: "v1.0-pre").new(trainee).as_hash.to_json)
+      parsed_trainee = JSON.parse(Api::GetVersionedItem.for_serializer(model: :trainee, version: "v1.0-rc").new(trainee).as_hash.to_json)
       expect(response.parsed_body).to eq(parsed_trainee)
     end
 
