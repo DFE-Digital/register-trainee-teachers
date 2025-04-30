a4e7fb9a67996c609eb3166ce7efa951f6c125a3
Steve Hook <steve.hook@gmail.com>
2025-04-30 08:59:27 +0100

    [8311] Update error messages so that they reference the API field name and CSV column name (#5174)
    * WIP
    
    * WIP
    
    * Add unit test spec for customised fields
    
    * Fix spec regression
    
    * Fix spec regression
    
    * Address review comments
    
    * Map attribute names for API error messages
    
    The attributes names that appear in API error messages should match the
    JSON properties that they refer to.
    
    * WIP
    
    * Fix spec regressions
    
    * Extend attribute name mapping to errors in nested API object
    
    * Remove the nested attribute names in CSV error full_messages
    
    Otherwise you get messages like:
    
    _hesa_trainee_detail_attributes ITT Aim has some invalid values_
    
    This change just maps the parent attribute names to an empty string (for
    CSV only).
    
    * Further refinements to nested attribute errors
    
    * Correct handling of nested degree/placement errors
    
    * Appease Rubocop
    
    * Fix spec regressions following rebase
    
    * Fix spec regression
    
    * Fix specs
    
    * Map trainee_disabilities_attributes to disabilities
    
    ---------
    
    Co-authored-by: Serafeim Maroulis <serafeim@hey.com>

---
+++ app/models/api/error_attribute_adapter.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Api
+  module ErrorAttributeAdapter
+    def self.included(base)
+      base.extend(ClassMethods)
+      base.class_attribute(:csv_field_mappings)
+    end
+
+    NESTED_CSV_ATTRIBUTE_NAMES = {
+      hesa_trainee_detail_attributes: "",
+      placements_attributes: "",
+      degrees_attributes: "",
+    }.freeze
+
+    NESTED_API_ATTRIBUTE_NAMES = {
+      hesa_trainee_detail_attributes: "",
+      trainee_disabilities_attributes: "disabilities",
+    }.freeze
+
+    module ClassMethods
+      def attribute_mappings
+        self.csv_field_mappings ||= begin
+          fields = YAML.load_file(CsvFields::View::FIELD_DEFINITION_PATH)
+          fields.to_h do |field|
+            [field["technical"].to_sym, field["field_name"]]
+          end
+        end
+      end
+
+      def human_attribute_name(attr, options = {})
+        if options[:base]&.record_source == Trainee::CSV_SOURCE
+          attribute_mappings[attr.to_sym] || NESTED_CSV_ATTRIBUTE_NAMES[attr.to_sym]
+        else
+          NESTED_API_ATTRIBUTE_NAMES[attr.to_sym] || attr.to_s
+        end || super
+      end
+    end
+  end
+end
--- app/models/api/v1_0_pre/trainee_attributes.rb
+++ app/models/api/v1_0_pre/trainee_attributes.rb
@@ -3,6 +3,29 @@
 module Api
   module V10Pre
     class TraineeAttributes < Api::V01::TraineeAttributes
+      include Api::ErrorAttributeAdapter
+
+      def build_nested_models(new_attributes)
+        new_attributes[:placements_attributes]&.each do |placement_params|
+          placements_attributes << PlacementAttributes.new(placement_params, record_source:)
+        end
+
+        new_attributes[:degrees_attributes]&.each do |degree_params|
+          degrees_attributes << DegreeAttributes.new(degree_params, record_source:)
+        end
+
+        new_attributes[:nationalisations_attributes]&.each do |nationalisation_params|
+          nationalisations_attributes << NationalityAttributes.new(nationalisation_params)
+        end
+
+        new_hesa_trainee_detail_attributes = new_attributes.slice(*HesaTraineeDetailAttributes::ATTRIBUTES)
+        if new_hesa_trainee_detail_attributes.present?
+          self.hesa_trainee_detail_attributes = V10Pre::HesaTraineeDetailAttributes.new(
+            new_hesa_trainee_detail_attributes.merge(trainee_attributes: self),
+            record_source:,
+          )
+        end
+      end
     end
   end
 end
