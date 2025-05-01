467cd49271f934ca9d118902c7bdbec3a35f9506
Steve Hook <steve.hook@gmail.com>
2025-05-01 09:39:06 +0100

    Outline solution for funding_method validation
    Includes failing test for validation against funding rules.

---
+++ app/validators/api/v1_0_pre/hesa_trainee_detail_attributes/rules/funding_method.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module Api
+  module V10Pre
+    class HesaTraineeDetailAttributes
+      module Rules
+        class FundingMethod < Api::Rules::Base
+          attr_reader :hesa_trainee_detail_attributes
+
+          delegate :fund_code,
+                   :funding_method,
+                   :trainee_attributes, to: :hesa_trainee_detail_attributes
+          delegate :training_route,
+                   :course_subject_one, to: :trainee_attributes
+
+          def initialize(hesa_trainee_detail_attributes)
+            @hesa_trainee_detail_attributes = hesa_trainee_detail_attributes
+          end
+
+          def call
+            return true if fund_code != Hesa::CodeSets::FundCodes::ELIGIBLE || training_route.nil?
+
+            funding_method = ::FundingMethod.joins(allocation_subjects: :subject_specialisms).where(
+              academic_cycle_id: AcademicCycle.current&.id,
+              funding_type: funding_type,
+              training_route: training_route,
+              subject_specialisms: { name: course_subject_one },
+            ).first
+
+            funding_method.present?
+          end
+
+        private
+
+          def funding_type
+            @funding_type ||= {
+              Hesa::CodeSets::BursaryLevels::SCHOLARSHIP => FUNDING_TYPES[:scholarship],
+              Hesa::CodeSets::BursaryLevels::NONE => nil,
+              Hesa::CodeSets::BursaryLevels::TIER_ONE => FUNDING_TYPES[:bursary],
+              Hesa::CodeSets::BursaryLevels::TIER_TWO => FUNDING_TYPES[:bursary],
+              Hesa::CodeSets::BursaryLevels::TIER_THREE => FUNDING_TYPES[:bursary],
+              Hesa::CodeSets::BursaryLevels::UNDERGRADUATE_BURSARY => FUNDING_TYPES[:bursary],
+              Hesa::CodeSets::BursaryLevels::VETERAN_TEACHING_UNDERGRADUATE_BURSARY => FUNDING_TYPES[:bursary],
+              Hesa::CodeSets::BursaryLevels::POSTGRADUATE_BURSARY => FUNDING_TYPES[:bursary],
+              Hesa::CodeSets::BursaryLevels::GRANT => FUNDING_TYPES[:grant],
+            }[funding_method]
+          end
+        end
+      end
+    end
+  end
+end
