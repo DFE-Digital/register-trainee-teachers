4ae8349b4bd35875ad7877e1bf8054b2b3573e9f
Steve Hook <steve.hook@gmail.com>
2025-05-01 09:39:06 +0100

    Outline solution for funding_method validation
    Includes failing test for validation against funding rules.

--- app/lib/hesa/code_sets/fund_codes.rb
+++ app/lib/hesa/code_sets/fund_codes.rb
@@ -3,11 +3,14 @@
 module Hesa
   module CodeSets
     module FundCodes
+      NOT_ELIGIBLE = "2"
+      ELIGIBLE = "7"
+
       # https://www.hesa.ac.uk/collection/c22053/xml/c22053/c22053codelists.xsd
       # https://www.hesa.ac.uk/collection/c22053/e/fundcode
       MAPPING = {
-        "2" => "Not fundable by funding council/body",
-        "7" => "Eligible for funding from the DfE",
+        NOT_ELIGIBLE => "Not fundable by funding council/body",
+        ELIGIBLE => "Eligible for funding from the DfE",
       }.freeze
     end
   end
--- spec/models/api/v1_0_pre/hesa_trainee_detail_attributes_spec.rb
+++ spec/models/api/v1_0_pre/hesa_trainee_detail_attributes_spec.rb
@@ -5,6 +5,8 @@ require "rails_helper"
 RSpec.describe Api::V10Pre::HesaTraineeDetailAttributes do
   subject { described_class }
 
+  let!(:academic_cycle) { create(:academic_cycle, :current) }
+
   it { is_expected.to be < Api::V01::HesaTraineeDetailAttributes }
 
   describe "validations" do
@@ -12,6 +14,34 @@ RSpec.describe Api::V10Pre::HesaTraineeDetailAttributes do
       expect(described_class.validators.map(&:class)).to include(
         Api::V10Pre::HesaTraineeDetailAttributes::RulesValidator,
       )
+    end
+
+    describe "funding_method" do
+      describe "Postgraduate bursaries require a certain degree and course type (QR.C24053.Student.BURSLEV.20)" do
+        let(:trainee_attributes) do
+          Api::V10Pre::TraineeAttributes.new(
+            training_route: "provider_led_postgraduate",
+          )
+        end
+
+        subject { described_class.new({ trainee_attributes: trainee_attributes, funding_method: "D", fund_code: "7" }, record_source: "api") }
+
+        context "when funding_method is 'D' (postgraduate bursary)" do
+          context "when course subject is declared by a `FundingMethod` record" do
+            it "funding_method should be valid" do
+              subject.validate
+              expect(subject.errors[:funding_method]).to be_blank
+            end
+          end
+
+          context "when course subject is NOT declared by a `FundingMethod` record" do
+            it "funding_method should NOT be valid" do
+              subject.validate
+              expect(subject.errors[:funding_method]).to be_present
+            end
+          end
+        end
+      end
     end
   end
 end
