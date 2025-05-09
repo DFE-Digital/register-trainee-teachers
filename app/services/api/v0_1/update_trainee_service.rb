4531f3b7c587a584284968eccbd2f7217af1c573
Serafeim Maroulis <serafeim@hey.com>
2025-05-09 09:26:23 +0100

    Refactor Api/V01/UpdateTraineeService disabilities
    * Remove any existing disabilities when the trainee
      disability_disclosure changes and the trainee is not disabled

--- app/services/api/v0_1/update_trainee_service.rb
+++ app/services/api/v0_1/update_trainee_service.rb
@@ -15,12 +15,13 @@ module Api
 
         attributes_to_save = attributes.deep_attributes.with_indifferent_access
         trainee.nationalities.destroy_all if attributes_to_save[:nationalisations_attributes].present?
-        trainee.trainee_disabilities.destroy_all if attributes_to_save[:trainee_disabilities_attributes].present?
+        trainee.clear_disabilities if attributes_to_save[:trainee_disabilities_attributes].present?
 
         if trainee_attributes_validation.invalid?
           [false, trainee_attributes_validation]
         else
           trainee.assign_attributes(attributes_to_save)
+          trainee.disabilities = [] if !trainee.disabled? && trainee.disability_disclosure_changed?
 
           if validation.all_errors.any?
             [false, validation]
