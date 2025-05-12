98ec1916e0e6b30734d1853750dfa5d780048424
Steve Hook <steve.hook@gmail.com>
2025-05-12 11:57:33 +0100

    Update app/validators/api/v1_0_pre/hesa_trainee_detail_attributes/rules_validator.rb
    Co-authored-by: Serafeim Maroulis <serafeim@hey.com>

--- app/validators/api/v1_0_pre/hesa_trainee_detail_attributes/rules_validator.rb
+++ app/validators/api/v1_0_pre/hesa_trainee_detail_attributes/rules_validator.rb
@@ -5,7 +5,6 @@ module Api
     class HesaTraineeDetailAttributes
       class RulesValidator < ActiveModel::Validator
         def validate(record)
-          # return if record.errors.present?
 
           record.errors.add(:fund_code, :ineligible) unless Rules::FundCode.valid?(record)
           record.errors.add(:funding_method, :ineligible) unless Rules::FundingMethod.valid?(record)
