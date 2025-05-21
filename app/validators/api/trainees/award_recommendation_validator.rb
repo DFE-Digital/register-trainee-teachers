9c853835623b3ad96ca692ad65cb788775ca47d2
Serafeim Maroulis <serafeim@hey.com>
2025-05-21 09:45:27 +0100

    Add custom error messages for award recommendation

--- app/validators/api/trainees/award_recommendation_validator.rb
+++ app/validators/api/trainees/award_recommendation_validator.rb
@@ -6,8 +6,8 @@ module Api
       def validate(record)
         return unless record.errors.empty?
 
-        record.errors.add(:trainee, :degrees) if record.trainee_requires_degree? && record.trainee_degree_missing?
-        record.errors.add(:trainee, :state) unless record.trainee_can_recommend_for_award?
+        record.errors.add(:degree_id) if record.trainee_requires_degree? && record.trainee_degree_missing?
+        record.errors.add(:state) unless record.trainee_can_recommend_for_award?
       end
     end
   end
