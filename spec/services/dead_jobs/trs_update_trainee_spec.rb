ab36f715ede1e9550193d6f863acf230adb0dd00
David Georgiou <david.georgiou@icloud.com>
2025-05-21 14:32:16 +0100

    [8529] - removes trs update trainee reporting from admin

--- spec/services/dead_jobs/trs_update_trainee_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe DeadJobs::TrsUpdateTrainee do
-  it_behaves_like "Dead jobs", Trs::UpdateTraineeJob, "TRS Update Trainee"
-end
