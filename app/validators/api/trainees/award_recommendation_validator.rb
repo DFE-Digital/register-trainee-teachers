31603ec938cb6ac3f561548fcaba64d9de60ddd5
Serafeim Maroulis <serafeim@hey.com>
2025-05-23 09:51:41 +0100

    Remove check for empty errors

--- app/validators/api/trainees/award_recommendation_validator.rb
+++ app/validators/api/trainees/award_recommendation_validator.rb
@@ -4,8 +4,6 @@ module Api
   module Trainees
     class AwardRecommendationValidator < ActiveModel::Validator
       def validate(record)
-        return unless record.errors.empty?
-
         record.errors.add(:degree_id) if record.trainee_requires_degree? && record.trainee_degree_missing?
         record.errors.add(:state) unless record.trainee_can_recommend_for_award?
       end
