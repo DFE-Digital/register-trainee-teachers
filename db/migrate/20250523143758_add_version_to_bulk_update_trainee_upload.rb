d0589e97bb3d727231479ba197588213ce3a084f
Serafeim Maroulis <serafeim@hey.com>
2025-05-27 16:36:08 +0100

    Lint

--- db/migrate/20250523143758_add_version_to_bulk_update_trainee_upload.rb
+++ db/migrate/20250523143758_add_version_to_bulk_update_trainee_upload.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddVersionToBulkUpdateTraineeUpload < ActiveRecord::Migration[7.2]
   def change
     add_column :bulk_update_trainee_uploads, :version, :string
