89b0470ace43c332e793694045eef2fba78dd9a9
Steve Hook <steve.hook@gmail.com>
2025-05-27 09:47:32 +0100

    Fix and extend specs

--- spec/support/page_objects/trainees/confirm_deferral_details.rb
+++ spec/support/page_objects/trainees/confirm_deferral_details.rb
@@ -6,6 +6,7 @@ module PageObjects
       set_url "/trainees/{id}/defer/confirm"
 
       element :start_date_change_link, "#trainee-start-date .govuk-link"
+      element :defer_reason_change_link, "#reason-for-deferral .govuk-link"
       element :defer, "button[type='submit']"
       element :cancel, ".govuk-link.qa-cancel-link"
     end
