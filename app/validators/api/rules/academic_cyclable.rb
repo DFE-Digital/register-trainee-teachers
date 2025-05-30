4b4a858acbfa8cba08b91d2be0791751913221ff
Steve Hook <steve.hook@gmail.com>
2025-05-30 13:35:44 +0100

    Reverting last suggestion because it alters logic
    The suggested logic doesn't handle values that are already a `Date`

--- app/validators/api/rules/academic_cyclable.rb
+++ app/validators/api/rules/academic_cyclable.rb
@@ -11,7 +11,10 @@ module Api
 
       def start_date
         value = trainee_attributes.trainee_start_date || trainee_attributes.itt_start_date
-        Date.iso8601(value) if valid_date_string?(value)
+        if value.is_a?(String)
+          value = valid_date_string?(value) ? Date.iso8601(value) : nil
+        end
+        value
       end
     end
   end
