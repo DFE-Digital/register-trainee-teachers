1ecf46f310d049380c12a8eb339f21b1bf00ba60
David Georgiou <david.georgiou@icloud.com>
2025-06-01 23:03:41 +0100

    [8293] - new initiatives for 2025/26 academic year

---
+++ spec/helpers/funding_helper_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe FundingHelper do
+  include FundingHelper
+
+  describe "#training_initiative_options" do
+    context "when recruitment_cycle_year is 2024" do
+      before do
+        allow(Settings).to receive(:current_recruitment_cycle_year).and_return(2024)
+      end
+
+      it "returns initiatives from TRAINING_INITIATIVES_2024_TO_2025" do
+        expected_initiatives = %w[
+          international_relocation_payment
+          now_teach
+          veterans_teaching_undergraduate_bursary
+        ].sort
+
+        expect(training_initiative_options).to eq(expected_initiatives)
+      end
+    end
+
+    context "when recruitment_cycle_year is 2025" do
+      before do
+        allow(Settings).to receive(:current_recruitment_cycle_year).and_return(2025)
+      end
+
+      it "returns initiatives from TRAINING_INITIATIVES_2025_TO_2026" do
+        expected_initiatives = %w[
+          international_relocation_payment
+          now_teach
+          veterans_teaching_undergraduate_bursary
+          primary_mathematics_specialist
+        ].sort
+
+        expect(training_initiative_options).to eq(expected_initiatives)
+      end
+    end
+  end
+
+  describe "#available_training_initiatives_for_cycle" do
+    context "when recruitment_cycle_year is 2024" do
+      before do
+        allow(Settings).to receive(:current_recruitment_cycle_year).and_return(2024)
+      end
+
+      it "returns TRAINING_INITIATIVES_2024_TO_2025" do
+        expect(available_training_initiatives_for_cycle).to eq(TRAINING_INITIATIVES_2024_TO_2025)
+      end
+    end
+
+    context "when recruitment_cycle_year is 2025" do
+      before do
+        allow(Settings).to receive(:current_recruitment_cycle_year).and_return(2025)
+      end
+
+      it "returns TRAINING_INITIATIVES_2025_TO_2026" do
+        expect(available_training_initiatives_for_cycle).to eq(TRAINING_INITIATIVES_2025_TO_2026)
+      end
+    end
+
+    context "when a constant doesn't exist for the current recruitment cycle year" do
+      before do
+        allow(Settings).to receive(:current_recruitment_cycle_year).and_return(2026)
+
+        # Allow const_defined? to be called with any arguments but return false specifically for 2026-2027
+        allow(Object).to receive(:const_defined?).and_call_original
+        allow(Object).to receive(:const_defined?).with("TRAINING_INITIATIVES_2026_TO_2027").and_return(false)
+      end
+
+      it "uses the initiatives from the most recent available year" do
+        # The method should return the 2025-2026 initiatives since 2026-2027 doesn't exist
+        result = available_training_initiatives_for_cycle
+
+        expect(result).to eq(TRAINING_INITIATIVES_2025_TO_2026)
+      end
+    end
+  end
+end
