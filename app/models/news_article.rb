52688e8d2650b9a3961a9ecb19423d97a39a9b31
Steve Hook <steve.hook@gmail.com>
2025-06-03 09:42:29 +0100

    Add migration to populate `news_articles` table

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,8 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: news_articles
+#
+#  id           :bigint           not null, primary key
+#  content      :text             not null
+#  published    :boolean          default(FALSE), not null
+#  published_at :datetime
+#  slug         :string           not null
+#  summary      :string           not null
+#  title        :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_news_articles_on_slug  (slug) UNIQUE
+#
 class NewsArticle < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :summary, presence: true
-  validates :date, presence: true
 end
