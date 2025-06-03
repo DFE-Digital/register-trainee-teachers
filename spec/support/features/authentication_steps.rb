9c354442efdbf227f409d302e45e28f2a0634e56
Tom Trentham <tom.trentham@education.gov.uk>
2025-06-03 17:11:03 +0100

    Review feedback

--- spec/support/features/authentication_steps.rb
+++ spec/support/features/authentication_steps.rb
@@ -11,8 +11,8 @@ module Features
       visit_sign_in_page
     end
 
-    def given_i_am_authenticated_as_an_hei_provider_user(user: create(:user, :hei))
-      given_i_am_authenticated(user:)
+    def given_i_am_authenticated_as_an_hei_provider_user
+      given_i_am_authenticated(user: create(:user, :hei))
     end
 
     def given_i_am_authenticated_as_a_lead_partner_user(user: create(:user, :with_lead_partner_organisation))
