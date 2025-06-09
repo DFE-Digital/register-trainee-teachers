27c88c6affb49b588eb7cf9d6b035a6cbae9ff25
Steve Hook <steve.hook@gmail.com>
2025-06-09 17:24:56 +0100

    Refactor validators to support more detailed errors
    We want to expose the error details to the code that forms the error
    message.

--- app/validators/api/v2025_0_rc/hesa_trainee_detail_attributes/rules_validator.rb
+++ app/validators/api/v2025_0_rc/hesa_trainee_detail_attributes/rules_validator.rb
@@ -6,7 +6,9 @@ module Api
       class RulesValidator < ActiveModel::Validator
         def validate(record)
           record.errors.add(:fund_code, :ineligible) unless Rules::FundCode.valid?(record)
-          record.errors.add(:funding_method, :ineligible) unless Rules::FundingMethod.valid?(record)
+
+          validation_result = Rules::FundingMethod.valid?(record)
+          record.errors.add(:funding_method, :ineligible, validation_result.second) unless validation_result.first
         end
       end
     end
