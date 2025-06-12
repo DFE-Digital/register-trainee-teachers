7d5b28b7f73c116193293e6e470a2d858b1b393c
Chris Gannon <cjgannon@gmail.com>
2025-06-12 14:33:21 +0100

    create hesa trainee detail record on update

---
+++ app/services/api/v0_1/create_hesa_trainee_detail_service.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Api
+  module V01
+    class CreateHesaTraineeDetailService
+      include ServicePattern
+
+      def initialize(trainee:)
+        @trainee = trainee
+      end
+
+      def call
+        hesa_trainee_detail = Hesa::TraineeDetail.build(trainee:)
+
+        attributes = {}
+        attributes.merge!(extract_attributes_from_student_record)
+        attributes.merge!(extract_attributes_from_metadatum_record)
+        hesa_trainee_detail.assign_attributes(attributes)
+
+        hesa_trainee_detail.save
+      end
+
+    private
+
+      attr_reader :trainee
+
+      def extract_attributes_from_metadatum_record
+        return {} if trainee.hesa_metadatum.nil?
+
+        required_attributes = Hesa::Metadatum.new.attributes.keys.intersection(Hesa::TraineeDetail.new.attributes.keys) - %w[id trainee_id created_at updated_at]
+
+        trainee.hesa_metadatum.slice(required_attributes).compact!
+      end
+
+      def extract_attributes_from_student_record
+        return {} if trainee.hesa_students.empty?
+
+        required_attributes = Hesa::Student.new.attributes.keys.intersection(Hesa::TraineeDetail.new.attributes.keys) - %w[id trainee_id created_at updated_at]
+
+        trainee.hesa_students.last.slice(required_attributes).compact!
+      end
+    end
+  end
+end
