c7b519427f9191f62157a7df2277646e7a8dcd5b
Serafeim Maroulis <serafeim@hey.com>
2025-06-20 11:13:17 +0100

    Add csv-docs index.html

--- tech_docs/config.rb
+++ tech_docs/config.rb
@@ -1,6 +1,9 @@
 require "govuk_tech_docs"
-require_relative "lib/govuk_tech_docs/path_helpers"
-require_relative "lib/govuk_tech_docs/contribution_banner"
+require "config/environment"
+
+require "lib/govuk_tech_docs/path_helpers"
+require "lib/govuk_tech_docs/contribution_banner"
+require "services/bulk_update/add_trainees/config"
 
 GovukTechDocs.configure(self, livereload: { host: "0.0.0.0" })
 
