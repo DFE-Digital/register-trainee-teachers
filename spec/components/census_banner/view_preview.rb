f55a6785fe47d2b67d8e09f18146e6fb3ff8d595
Chris Gannon <cjgannon@gmail.com>
2025-06-27 11:57:55 +0100

    fix banner previews

--- spec/components/census_banner/view_preview.rb
+++ spec/components/census_banner/view_preview.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
 class CensusBanner::ViewPreview < ViewComponent::Preview
-  def census_period
-    render(CensusBanner::View.new(current_academic_cycle: AcademicCycle.previous, sign_off_period: :census_period, provider: provider_awaiting_sign_off))
+  def performance_profile_period
+    render(CensusBanner::View.new(current_academic_cycle: AcademicCycle.current, sign_off_period: :perfomance_profile_period, provider: provider_awaiting_sign_off))
   end
 
   def outside_period
-    render(CensusBanner::View.new(current_academic_cycle: AcademicCycle.previous, sign_off_period: :outside_period, provider: provider_awaiting_sign_off))
+    render(CensusBanner::View.new(current_academic_cycle: AcademicCycle.current, sign_off_period: :outside_period, provider: provider_awaiting_sign_off))
   end
 
   def census_signed_off
-    render(CensusBanner::View.new(current_academic_cycle: AcademicCycle.previous, sign_off_period: :census_period, provider: provider_census_signed_off))
+    render(CensusBanner::View.new(current_academic_cycle: AcademicCycle.current, sign_off_period: :census_period, provider: provider_census_signed_off))
   end
 
   def census_awaiting_sign_off
-    render(CensusBanner::View.new(current_academic_cycle: AcademicCycle.previous, sign_off_period: :census_period, provider: provider_awaiting_sign_off))
+    render(CensusBanner::View.new(current_academic_cycle: AcademicCycle.current, sign_off_period: :census_period, provider: provider_awaiting_sign_off))
   end
 
 private
 
   def provider_awaiting_sign_off
-    OpenStruct.new(census_signed_off?: false)
+    OpenStruct.new(census_awaiting_sign_off?: true)
   end
 
   def provider_census_signed_off
-    OpenStruct.new(census_signed_off?: true)
+    OpenStruct.new(census_awaiting_sign_off?: false)
   end
 end
