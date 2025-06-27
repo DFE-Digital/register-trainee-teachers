a04fae5f95a4cd0dd0ab94eb4c3b923b099515f4
David Georgiou <david.georgiou@icloud.com>
2025-06-27 14:54:03 +0100

    [8627] - remove serializer inheritance

--- app/serializers/api/v2025_0_rc/placement_serializer.rb
+++ app/serializers/api/v2025_0_rc/placement_serializer.rb
@@ -2,7 +2,34 @@
 
 module Api
   module V20250Rc
-    class PlacementSerializer < Api::V01::PlacementSerializer
+    class PlacementSerializer
+      EXCLUDED_ATTRIBUTES = %w[
+        id
+        slug
+        trainee_id
+        school_id
+      ].freeze
+
+      attr_reader :placement
+
+      def initialize(placement)
+        @placement = placement
+      end
+
+      def as_hash
+        placement
+          .as_json(except: EXCLUDED_ATTRIBUTES)
+          .merge(school_attributes)
+      end
+
+    private
+
+      def school_attributes
+        {
+          "placement_id" => placement.slug,
+          "address" => placement.full_address,
+        }
+      end
     end
   end
 end
