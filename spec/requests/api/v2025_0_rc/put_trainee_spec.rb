97905a95c89502e8a49d529a9847fbdf1bc7ab0e
David Georgiou <david.georgiou@icloud.com>
2025-07-03 09:14:13 +0100

    Flatten hesa_mapper/attributes.rb inheritance + fix cross-references (service 3.2/5)

--- spec/requests/api/v2025_0_rc/put_trainee_spec.rb
+++ spec/requests/api/v2025_0_rc/put_trainee_spec.rb
@@ -1376,7 +1376,7 @@ describe "`PUT /api/v2025.0-rc/trainees/:id` endpoint" do
       end
 
       before do
-        allow(Api::V01::HesaMapper::Attributes).to receive(:call).and_call_original
+                  allow(Api::V20250Rc::HesaMapper::Attributes).to receive(:call).and_call_original
         allow(Trainees::MapFundingFromDttpEntityId).to receive(:call).and_call_original
 
         post "/api/v2025.0-rc/trainees", params: params_for_create.to_json, headers: headers
