0b2e7d948f8f83a19758b87d38028e40432f01be
Serafeim Maroulis <serafeim@hey.com>
2025-07-11 15:29:02 +0100

    Refactor Dockerfile
    Build reference-data documentation

--- tech_docs/config/environment.rb
+++ tech_docs/config/environment.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-lib = File.expand_path("../lib")
+lib = File.expand_path("../lib", __dir__)
 app = File.expand_path("../../app", __dir__)
 
 $LOAD_PATH.unshift(lib, app)
