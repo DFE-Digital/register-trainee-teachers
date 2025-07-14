2797175a191424fa95da8931da78a2ca0eb34781
Steve Hook <steve.hook@gmail.com>
2025-07-14 10:57:46 +0100

    Modify `Header::View` component to reflect new brand
    This no longer has any `items` or a `title` because they no longer go in
    the header. It's just the GOV.UK logo.

--- app/components/header/view.rb
+++ app/components/header/view.rb
@@ -1,18 +1,7 @@
 # frozen_string_literal: true
 
 class Header::View < ViewComponent::Base
-  attr_reader :service_name, :items
-
   include ActiveModel
 
-  def initialize(service_name:, items: nil)
-    @service_name = service_name
-    @items = items
-  end
-
-  def header_class
-    klass = "govuk-header__content app-header__content"
-    klass += " app-header__content--single-item" if items.length == 1
-    klass
-  end
+  def initialize; end
 end
