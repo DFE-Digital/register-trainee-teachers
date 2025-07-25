69ee5416b39fd4a812f8ee2d4c90a32313719840
Steve Hook <steve.hook@gmail.com>
2025-07-25 09:53:38 +0100

    Add spec for unhappy path

--- spec/jobs/teacher_training_api/publish_provider_checker_job_spec.rb
+++ spec/jobs/teacher_training_api/publish_provider_checker_job_spec.rb
@@ -67,8 +67,40 @@ module TeacherTrainingApi
                 "Matching lead partners: 2",
                 "Matching providers: 1",
                 "Missing providers: 0",
+                "Total: 6",
               ),
               icon_emoji: ":inky-the-octopus:",
+            ),
+          )
+        end
+      end
+
+      context "when there are no missing providers" do
+        before do
+          allow(result).to receive_messages(
+            school_matches: [1, 2, 3],
+            lead_partner_matches: [4, 5],
+            provider_matches: [6],
+            missing: [provider_record],
+            total_count: 7,
+          )
+        end
+
+        it "sends a success message to Slack" do
+          described_class.perform_now
+          expect(SlackNotifierService).to have_received(:call).with(
+            hash_including(
+              message: include(
+                "[test] Publish Provider Checker Results #{current_time.to_fs(:govuk_date_and_time)} for #{Settings.current_recruitment_cycle_year}",
+                "Matching lead schools: 3",
+                "Matching lead schools: 3",
+                "Matching lead partners: 2",
+                "Matching providers: 1",
+                "Missing providers: 1",
+                "  - University of BAT (A01), UKPRN 12345678",
+                "Total: 7",
+              ),
+              icon_emoji: ":alert:",
             ),
           )
         end
