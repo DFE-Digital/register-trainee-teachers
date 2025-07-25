863ba5d1be10a1cad826c8a202bd8a284c8871e9
Tom Trentham <tom.trentham@education.gov.uk>
2025-07-25 09:39:25 +0100

    Remove unused controller

--- app/controllers/csv_docs/base_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module CsvDocs
-  class BaseController < ::ApplicationController
-    skip_before_action :authenticate
-    before_action { require_feature_flag(:bulk_add_trainees) }
-  end
-end
