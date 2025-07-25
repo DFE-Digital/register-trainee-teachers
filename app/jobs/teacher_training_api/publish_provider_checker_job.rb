7b92f1b3db8134d17662c3f2cf21613c37303008
Steve Hook <steve.hook@gmail.com>
2025-07-25 02:14:00 +0100

    Assert content of Slack message

--- app/jobs/teacher_training_api/publish_provider_checker_job.rb
+++ app/jobs/teacher_training_api/publish_provider_checker_job.rb
@@ -5,6 +5,8 @@ module TeacherTrainingApi
     queue_as :default
     retry_on TeacherTrainingApi::Client::HttpError
 
+    MAX_MISSING_PROVIDERS_TO_DISPLAY = 50
+
     def perform
       return unless FeatureService.enabled?("publish_provider_checker")
 
@@ -13,12 +15,15 @@ module TeacherTrainingApi
       )
 
       message = "Publish Provider Checker Results #{Time.zone.now.to_fs(:govuk_date_and_time)} for #{checker.recruitment_cycle_year}:\n"
-      message << "Matching lead schools:  #{checker.school_matches.count}\n"
+      message << "Matching lead schools: #{checker.school_matches.count}\n"
       message << "Matching lead partners: #{checker.lead_partner_matches.count}\n"
       message << "Matching providers: #{checker.provider_matches.count}\n"
       message << "Missing providers: #{checker.missing.count}\n"
-      checker.missing.first(10).each do |provider|
-      message << "  -  \n"
+      checker.missing.first(MAX_MISSING_PROVIDERS_TO_DISPLAY).each do |provider|
+        message << "  - #{format_provider_details(provider)}\n"
+      end
+      if checker.missing.count > MAX_MISSING_PROVIDERS_TO_DISPLAY
+        message << "  - ...\n"
       end
       message << "Total: #{checker.total_count}\n"
 
@@ -27,6 +32,12 @@ module TeacherTrainingApi
         icon_emoji: checker.missing.count.zero? ? ":inky-the-octopus:" : ":alert:",
         username: "Register Trainee Teachers: Job Failure",
       )
+    end
+
+  private
+
+    def format_provider_details(provider)
+      "#{provider['name']} (#{provider['code']}), UKPRN #{provider['ukprn']}\n"
     end
   end
 end
