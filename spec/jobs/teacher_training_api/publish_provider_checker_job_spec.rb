36b91d3bcb96a8792571839f6f791725cf0cdb51
Steve Hook <steve.hook@gmail.com>
2025-07-25 02:14:00 +0100

    Assert content of Slack message

--- spec/jobs/teacher_training_api/publish_provider_checker_job_spec.rb
+++ spec/jobs/teacher_training_api/publish_provider_checker_job_spec.rb
@@ -8,25 +8,70 @@ module TeacherTrainingApi
 
     let(:recruitment_cycle_year) { Settings.current_recruitment_cycle_year }
     let(:result) { TeacherTrainingApi::PublishProviderChecker.send(:new, recruitment_cycle_year:) }
+    let(:current_time) { Time.zone.now }
+
+    let(:provider_record) do
+      {
+        "ukprn" => "12345678",
+        "urn" => nil,
+        "postcode" => "N1 1NO",
+        "provider_type" => "hei",
+        "region_code" => "london",
+        "code" => "A01",
+        "county" => "London",
+        "name" => "University of BAT",
+      }
+    end
+
+    around do |example|
+      Timecop.freeze(current_time) { example.run }
+    end
 
     before do
-      allow(TeacherTrainingApi::PublishProviderChecker).to receive(:call).with(recruitment_cycle_year:).and_return(result)
+      allow(TeacherTrainingApi::PublishProviderChecker).to receive(:call).and_return(result)
       allow(SlackNotifierService).to receive(:call).and_return(true)
-
-      described_class.perform_now
     end
 
     context "when the feature flag is turned off", feature_publish_provider_checker: false do
       it "does not call TTAPI or Slack" do
-        expect(TeacherTrainingApi::PublishProviderChecker).not_to have_received(:call)
+        described_class.perform_now
         expect(SlackNotifierService).not_to have_received(:call)
       end
     end
 
     context "when the feature flag is turned on", feature_publish_provider_checker: true do
       it "generates the correct message and sends it to Slack" do
-        expect(TeacherTrainingApi::PublishProviderChecker).to have_received(:call)
+        described_class.perform_now
         expect(SlackNotifierService).to have_received(:call)
+      end
+
+      context "when there are no missing providers" do
+        before do
+          allow(result).to receive_messages(
+            school_matches: [1, 2, 3],
+            lead_partner_matches: [4, 5],
+            provider_matches: [6],
+            missing: [],
+            total_count: 6,
+          )
+        end
+
+        it "sends a success message to Slack" do
+          described_class.perform_now
+          expect(SlackNotifierService).to have_received(:call).with(
+            hash_including(
+              message: include(
+                "Publish Provider Checker Results #{current_time.to_fs(:govuk_date_and_time)} for #{Settings.current_recruitment_cycle_year}",
+                "Matching lead schools: 3",
+                "Matching lead schools: 3",
+                "Matching lead partners: 2",
+                "Matching providers: 1",
+                "Missing providers: 0",
+              ),
+              icon_emoji: ":inky-the-octopus:",
+            ),
+          )
+        end
       end
     end
   end
