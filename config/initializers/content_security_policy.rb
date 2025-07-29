2fe16cdda9de19a28b3fc5ae06b1820cb1156262
Steve Hook <steve.hook@gmail.com>
2025-07-29 14:42:19 +0100

    [8679] Harden config of `content-security-policy` header (#5440)
    * Harden config of `content-security-policy` header
    
    As recommended by ITHC review
    
    * Add nonce to inline JS
    
    To allow us to tighten up content security policy.
    
    * Relax HTTPS requirements when running locally
    
    * Remove unecessary nonce generator declaration
    
    This is covered by the `csp_meta_tag` declaration.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -34,10 +34,10 @@ Rails.application.config.content_security_policy do |policy|
                  "https://www.google-analytics.com")
   policy.object_src(:none)
   policy.script_src(:self,
-                    "'unsafe-inline'",
                     "https://www.googletagmanager.com",
                     "https://www.google-analytics.com",
                     "https://az416426.vo.msecnd.net") # needed for App Insights
   policy.style_src(:self, "'sha256-WAyOw4V+FqDc35lQPyRADLBWbuNK8ahvYEaQIYF1+Ps='") # Turbo progress bar stylesheet https://github.com/hotwired/turbo/issues/809
   policy.font_src(:self, :data)
+  policy.upgrade_insecure_requests(!Rails.env.local?)
 end
