7e3f19066d2417e67545a70414cac26696b8fcd3
Steve Hook <steve.hook@gmail.com>
2025-07-30 14:24:42 +0100

    Fix call to `FilterToggleButton`
    The constructor now needs a parent element as first param.

--- app/javascript/scripts/filter_toggle.js
+++ app/javascript/scripts/filter_toggle.js
@@ -4,25 +4,29 @@ import { FilterToggleButton } from '@ministryofjustice/frontend/moj/all.mjs'
 export default class FilterToggle {
   static init () {
     const filterContainer = $('.moj-filter-layout__filter')
+    const buttonContainer = $('.moj-action-bar__filter')
 
-    if (filterContainer.length) {
-      return new FilterToggleButton({
-        bigModeMediaQuery: '(min-width: 48.063em)',
-        startHidden: false,
-        toggleButton: {
-          container: $('.moj-action-bar__filter'),
-          showText: 'Show filters',
-          hideText: 'Hide filters',
-          classes: 'govuk-button--secondary'
-        },
-        closeButton: {
-          container: $('.moj-filter__header-action'),
-          text: 'Close'
-        },
-        filter: {
-          container: filterContainer
+    if (filterContainer.length && buttonContainer.length) {
+      return new FilterToggleButton(
+        buttonContainer[0],
+        {
+          bigModeMediaQuery: '(min-width: 48.063em)',
+          startHidden: false,
+          toggleButton: {
+            container: buttonContainer,
+            showText: 'Show filters',
+            hideText: 'Hide filters',
+            classes: 'govuk-button--secondary'
+          },
+          closeButton: {
+            container: $('.moj-filter__header-action'),
+            text: 'Close'
+          },
+          filter: {
+            container: filterContainer
+          }
         }
-      })
+      )
     }
   }
 }
