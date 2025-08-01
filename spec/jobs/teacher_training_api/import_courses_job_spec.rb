0671f18938583f98a0c031cee4f3511fd168c199
Chris Gannon <cjgannon@gmail.com>
2025-08-01 12:43:27 +0100

    add coverage for followup job

--- spec/jobs/teacher_training_api/import_courses_job_spec.rb
+++ spec/jobs/teacher_training_api/import_courses_job_spec.rb
@@ -8,19 +8,38 @@ module TeacherTrainingApi
 
     let(:course_data) { double }
     let(:provider_data) { double }
-    let(:course_payload) { { data: [course_data], included: provider_data, links: { next: nil } } }
+    let(:course_payload) { { data: [course_data], included: provider_data, links: { next: follow_up_uri } } }
+    let(:follow_up_uri) { nil }
 
-    before do
-      allow(RetrieveCourses).to receive(:call).and_return(course_payload)
-      allow(ImportCourse).to receive(:call).with(course_data:, provider_data:)
+    describe ".call" do
+      before do
+        allow(RetrieveCourses).to receive(:call).and_return(course_payload)
+        allow(ImportCourse).to receive(:call).with(course_data:, provider_data:)
+        allow(described_class).to receive(:perform_later)
+        allow(CheckCoursesForMissingProvidersJob).to receive(:perform_later)
 
-      described_class.perform_now
-    end
+        described_class.perform_now
+      end
 
-    context "when the feature flag is turned on", feature_import_courses_from_ttapi: true do
-      it "queues up a job to import courses" do
-        expect(RetrieveCourses).to have_received(:call).with(request_uri: nil)
-        expect(ImportCourse).to have_received(:call).with(course_data:, provider_data:)
+      context "when the feature flag is turned on", feature_import_courses_from_ttapi: true do
+        it "queues up a job to import courses" do
+          expect(RetrieveCourses).to have_received(:call).with(request_uri: nil)
+          expect(ImportCourse).to have_received(:call).with(course_data:, provider_data:)
+        end
+
+        context "when there is a follow up uri" do
+          let(:follow_up_uri) { "https://www.link.to/thing" }
+
+          it "schedules an ImportCoursesJob" do
+            expect(described_class).to have_received(:perform_later)
+          end
+        end
+
+        context "when there is not a follow up uri" do
+          it "schedules a CheckCoursesForMissingProvidersJob" do
+            expect(CheckCoursesForMissingProvidersJob).to have_received(:perform_later)
+          end
+        end
       end
     end
   end
