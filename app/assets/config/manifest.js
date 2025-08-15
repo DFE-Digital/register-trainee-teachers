d14b5e50e3b5de266badccd0af103df26c4c5315
Steve Hook <steve.hook@gmail.com>
2025-08-15 13:27:06 +0100

    Add explict `link`s to fonts too

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,7 +3,7 @@
 //= link_tree ../images
 
 // govuk-frontend assets
-//= link govuk-opengraph-image.png
+//= link favicon.ico
 //= link favicon.svg
 //= link govuk-crest.svg
 //= link govuk-icon-180.png
@@ -11,4 +11,8 @@
 //= link govuk-icon-512.png
 //= link govuk-icon-mask.svg
 //= link govuk-opengraph-image.png
-//= link_directory ../../../node_modules/govuk-frontend/dist/govuk/assets/fonts
+//
+//= link bold-affa96571d-v2.woff
+//= link bold-b542beb274-v2.woff2
+//= link light-94a07e06a1-v2.woff2
+//= link light-f591b13f7d-v2.woff
