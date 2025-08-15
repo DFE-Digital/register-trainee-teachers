9b61140e69595a962f826f3ea9841f8823c7a508
Steve Hook <steve.hook@gmail.com>
2025-08-15 15:28:28 +0100

    [8735] Add explicit link statements for missing precompiled images (#5520)
    * Add explicit link statement for missing precompiled images
    
    * Add explict `link`s to fonts too

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,5 +3,16 @@
 //= link_tree ../images
 
 // govuk-frontend assets
-//= link_directory ../../../node_modules/govuk-frontend/dist/govuk/assets/rebrand/images
-//= link_directory ../../../node_modules/govuk-frontend/dist/govuk/assets/fonts
+//= link favicon.ico
+//= link favicon.svg
+//= link govuk-crest.svg
+//= link govuk-icon-180.png
+//= link govuk-icon-192.png
+//= link govuk-icon-512.png
+//= link govuk-icon-mask.svg
+//= link govuk-opengraph-image.png
+//
+//= link bold-affa96571d-v2.woff
+//= link bold-b542beb274-v2.woff2
+//= link light-94a07e06a1-v2.woff2
+//= link light-f591b13f7d-v2.woff
