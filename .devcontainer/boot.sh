d83df2713b2ecfff6a087e25b13423c388272c24
Carlos Martinez <carlosjmtz@gmail.com>
2025-08-19 13:27:39 +0100

    CodeSpaces config

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,8 +1,14 @@
-echo "Creating database..."
+echo "Creating database"
 bin/rails db:setup
 bin/rails db:seed example_data:generate
 
-echo "Creating Procfile.dev"
+echo "Precompiling assets"
+bin/rake assets:precompile
+
+echo "Creating Procfile"
 cp Procfile.dev.sample Procfile.dev
+
+echo "Running Foreman to start the application"
+foreman start -f Procfile.dev web
 
 echo "Done!"
