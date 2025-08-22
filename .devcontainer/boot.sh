b3626ef7c22a482ad682a44c7af812c0bbeb6cbb
Carlos Martinez <carlosmartinez@fastmail.com>
2025-08-22 21:53:27 +0100

    Set up Codespace with less seed data (#5532)
    * Set up Codespace with less seed data
    
    * Fix Git integration

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,6 +1,6 @@
 echo "Creating database"
 bin/rails db:setup
-bin/rails db:seed example_data:generate
+bin/rails db:seed example_data:generate_light
 
 echo "Precompiling assets"
 bin/rake assets:precompile
@@ -9,6 +9,6 @@ echo "Creating Procfile"
 cp Procfile.dev.sample Procfile.dev
 
 echo "Running Foreman to start the application"
-foreman start -f Procfile.dev web
-
+foreman start -f Procfile.dev web &
 echo "Done!"
+wait %1
