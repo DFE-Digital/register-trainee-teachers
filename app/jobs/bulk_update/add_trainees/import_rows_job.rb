7715039fde4c863d3a74543a3ca1e0c5587da340
Serafeim Maroulis <serafeim@hey.com>
2025-09-08 12:39:16 +0100

    Use trainee_upload queue in ImportRowsJob

--- app/jobs/bulk_update/add_trainees/import_rows_job.rb
+++ app/jobs/bulk_update/add_trainees/import_rows_job.rb
@@ -3,7 +3,7 @@
 module BulkUpdate
   module AddTrainees
     class ImportRowsJob < ApplicationJob
-      queue_as :bulk_update
+      queue_as :trainee_upload
 
       def perform(trainee_upload)
         return unless FeatureService.enabled?(:bulk_add_trainees)
