15228ecd3f6750eb8aa79be21c3ea49f00e838e0
Chris Gannon <cjgannon@gmail.com>
2025-09-17 14:01:11 +0100

    [8928] - Prevent emails being generated for non-valid statuses (#5622)
    * prevent emails being generated for non-valid statuses
    
    * rubocop fixes
    
    * add extra statuses

--- spec/mailers/csv_submitted_for_processing_first_stage_email_mailer_spec.rb
+++ spec/mailers/csv_submitted_for_processing_first_stage_email_mailer_spec.rb
@@ -31,6 +31,18 @@ describe CsvSubmittedForProcessingFirstStageEmailMailer do
       end
     end
 
+    %i[uploaded in_progress succeeded cancelled].each do |status|
+      context "when the upload status is #{status}" do
+        let(:upload) { create(:bulk_update_trainee_upload, status) }
+
+        it "does not send an email" do
+          %i[govuk_notify_template first_name email file_name created_at].each do |attribute|
+            expect(mail.send(attribute)).to be_nil
+          end
+        end
+      end
+    end
+
     it "sends an email to the correct email address" do
       expect(mail.to).to eq([user.email])
     end
