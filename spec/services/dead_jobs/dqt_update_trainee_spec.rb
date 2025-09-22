633a4e3220dcd8ef63a75f24233a374545a34579
Steve Hook <steve.hook@gmail.com>
2025-09-22 13:59:18 +0100

    WIP

--- spec/services/dead_jobs/dqt_update_trainee_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe DeadJobs::DqtUpdateTrainee do
-  it_behaves_like "Dead jobs", Dqt::UpdateTraineeJob, "DQT Update Trainee"
-end
