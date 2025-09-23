b49ca3633c0b796bf94934737aeb97c9de962c12
Tom Trentham <tom.trentham@education.gov.uk>
2025-09-23 16:55:27 +0100

    Order alphabetically

--- db/scripts/sanitise.sql
+++ db/scripts/sanitise.sql
@@ -1,35 +1,53 @@
-DELETE FROM "sessions";
+DELETE FROM "active_storage_attachments";
+DELETE FROM "active_storage_blobs";
+DELETE FROM "active_storage_variant_records";
+DELETE FROM "apply_application_sync_requests";
 DELETE FROM "audits";
 DELETE FROM "blazer_audits";
-DELETE FROM "validation_errors";
-DELETE FROM "apply_application_sync_requests";
-DELETE FROM "hesa_students";
+DELETE FROM "bulk_update_recommendations_uploads";
+DELETE FROM "bulk_update_recommendations_upload_rows";
+DELETE FROM "bulk_update_row_errors";
 DELETE FROM "funding_payment_schedules";
 DELETE FROM "funding_payment_schedule_rows";
 DELETE FROM "funding_payment_schedule_row_amounts";
 DELETE FROM "funding_trainee_summaries";
 DELETE FROM "funding_trainee_summary_rows";
 DELETE FROM "funding_trainee_summary_row_amounts";
+DELETE FROM "hesa_students";
+DELETE FROM "sessions";
 DELETE FROM "uploads";
-DELETE FROM "active_storage_attachments";
-DELETE FROM "active_storage_blobs";
-DELETE FROM "active_storage_variant_records";
-DELETE FROM "bulk_update_recommendations_upload_rows";
-DELETE FROM "bulk_update_recommendations_uploads";
-DELETE FROM "bulk_update_row_errors";
+DELETE FROM "validation_errors";
 
--- Users
-UPDATE "users"
+-- Apply sync
+UPDATE "apply_applications"
 SET
-  first_name = 'User',
-  last_name = concat('RegisterUser', id),
-  email = concat('register_test_user_', id, '@example.com')
+  application = NULL;
+
+-- Dttp Users
+UPDATE "dttp_users"
+SET
+  email = concat('test_dttp_user', id, '@example.org'),
+  first_name = 'Dttp',
+  last_name = concat('DttpUser', id)
 WHERE email NOT LIKE '%@digital.education.gov.uk'
       AND email NOT LIKE '%@education.gov.uk';
 
-UPDATE "users"
+-- DTTP sync
+UPDATE "dttp_dormant_periods"
 SET
-  otp_secret = NULL;
+  response = NULL;
+
+UPDATE "dttp_degree_qualifications"
+SET
+  response = NULL;
+
+UPDATE "dttp_placement_assignments"
+SET
+  response = NULL;
+
+UPDATE "dttp_trainees"
+SET
+  response = NULL;
 
 -- Trainees
 UPDATE "trainees"
@@ -43,33 +61,15 @@ SET
   trn = CASE WHEN trn IS NULL THEN NULL ELSE rpad(id::text, 7, '0') END,
   additional_dttp_data = NULL;
 
--- Dttp Users
-UPDATE "dttp_users"
+-- Users
+UPDATE "users"
 SET
-  email = concat('test_dttp_user', id, '@example.org'),
-  first_name = 'Dttp',
-  last_name = concat('DttpUser', id)
+  first_name = 'User',
+  last_name = concat('RegisterUser', id),
+  email = concat('register_test_user_', id, '@example.com')
 WHERE email NOT LIKE '%@digital.education.gov.uk'
       AND email NOT LIKE '%@education.gov.uk';
 
--- Apply sync
-UPDATE "apply_applications"
-SET 
-  application = NULL;
-
--- DTTP sync
-UPDATE "dttp_trainees"
+UPDATE "users"
 SET
-  response = NULL;
-
-UPDATE "dttp_placement_assignments"
-SET
-  response = NULL;
-
-UPDATE "dttp_degree_qualifications"
-SET
-  response = NULL;
-
-UPDATE "dttp_dormant_periods"
-SET
-  response = NULL;
+  otp_secret = NULL;
