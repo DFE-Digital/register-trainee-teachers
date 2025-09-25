222c2b42209d870cb1750e4c31dc377f70f2b568
Chris Gannon <cjgannon@gmail.com>
2025-09-25 09:11:05 +0100

    linting fix

--- db/data/20250919101152_amend_pgta_grant_amounts_for202526.rb
+++ db/data/20250919101152_amend_pgta_grant_amounts_for202526.rb
@@ -11,12 +11,10 @@ class AmendPgtaGrantAmountsFor202526 < ActiveRecord::Migration[7.2]
       .where(academic_cycle: academic_cycle,
              funding_type: funding_type,
              "allocation_subjects.name": AllocationSubjects::ENGLISH,
-             training_route: TRAINING_ROUTE_ENUMS[:pg_teaching_apprenticeship]
-      ).destroy_all
+             training_route: TRAINING_ROUTE_ENUMS[:pg_teaching_apprenticeship]).destroy_all
 
     # This migration will update the amounts for the funding methods to the correct amounts
     # without risking the data for trainees who have already been registered for the year
-
 
     GRANTS_2025_TO_2026.each do |grant_type|
       FundingMethod.joins(:allocation_subjects)
