354ae5e49e23dff2d1f2e1ec482a3b69a8465ddb
Chris Gannon <cjgannon@gmail.com>
2025-09-26 16:13:47 +0100

    [8838] Remove unneeded example text (#5591)
    * remove unneeded example text
    
    * remove references to unsupported codes
    
    * remove unneeded training initiatives from yaml
    
    * remove unneeded hesa code sets

--- app/lib/hesa/code_sets/training_initiatives.rb
+++ app/lib/hesa/code_sets/training_initiatives.rb
@@ -5,10 +5,7 @@ module Hesa
     module TrainingInitiatives
       # https://www.hesa.ac.uk/collection/c22053/e/initiatives
       MAPPING = {
-        "001"	=> ROUTE_INITIATIVES_ENUMS[:abridged_itt_course],
         "009"	=> ROUTE_INITIATIVES_ENUMS[:maths_physics_chairs_programme_researchers_in_schools],
-        "011"	=> ROUTE_INITIATIVES_ENUMS[:primary_mathematics_specialist],
-        "019"	=> ROUTE_INITIATIVES_ENUMS[:additional_itt_place_for_pe_with_a_priority_subject],
         "025"	=> ROUTE_INITIATIVES_ENUMS[:transition_to_teach],
         "026"	=> ROUTE_INITIATIVES_ENUMS[:now_teach],
         "036" => ROUTE_INITIATIVES_ENUMS[:international_relocation_payment],
