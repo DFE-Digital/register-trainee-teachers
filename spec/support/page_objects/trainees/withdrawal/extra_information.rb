7ae63138a3b61ad5a48adebce2b60435275f7ada
Serafeim Maroulis <serafeim@hey.com>
2025-09-29 15:35:23 +0100

    Remove more references to removed columns

--- spec/support/page_objects/trainees/withdrawal/extra_information.rb
+++ spec/support/page_objects/trainees/withdrawal/extra_information.rb
@@ -8,9 +8,6 @@ module PageObjects
 
         set_url "/trainees/{id}/withdrawal/extra-information/edit"
 
-        element :withdraw_reasons_details, "#withdrawal-extra-information-form-withdraw-reasons-details-field"
-        element :withdraw_reasons_dfe_details, "#withdrawal-extra-information-form-withdraw-reasons-dfe-details-field"
-
         element :continue, "button[type='submit']"
         element :cancel, "a", text: "Cancel and return to record"
       end
