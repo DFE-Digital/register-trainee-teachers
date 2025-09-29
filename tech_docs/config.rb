89e9fddf45301f55f9bc39bc94a0804b810978df
Steve Hook <steve.hook@gmail.com>
2025-09-29 13:24:37 +0100

    Use redcarpet and fenced code blocks
    This renders triple backticks as expected (as a
    `<pre><code>{contents}</code></pre>`). Triple backticks are not part of
    standard markdown, rather they are 'Github flavoured markdown'. This is
    supported by redcarpet but no the default kramdown markdown parser.

--- tech_docs/config.rb
+++ tech_docs/config.rb
@@ -11,7 +11,8 @@ require "services/bulk_update/add_trainees/config"
 GovukTechDocs.configure(self, livereload: { host: "0.0.0.0" })
 
 set :relative_links, true
-set :markdown_engine, :kramdown
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true
 
 activate :relative_assets
 
