45b18013104a5ac5687b3df116c0936d956ffde5
Serafeim Maroulis <serafeim@hey.com>
2025-09-30 09:25:28 +0100

    Make migration reversible

--- db/migrate/20250926143021_remove_withdrawal_fields_from_trainees.rb
+++ db/migrate/20250926143021_remove_withdrawal_fields_from_trainees.rb
@@ -4,9 +4,10 @@ class RemoveWithdrawalFieldsFromTrainees < ActiveRecord::Migration[7.2]
   def change
     safety_assured do
       remove_columns :trainees,
-                     :withdraw_date,
                      :withdraw_reasons_details,
-                     :withdraw_reasons_dfe_details
+                     :withdraw_reasons_dfe_details, type: :string
+
+      remove_column :trainees, :withdraw_date, :datetime
     end
   end
 end
