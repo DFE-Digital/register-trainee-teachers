bcdce3b023cc64966c5a42cbd038d5349b3c08af
Steve Hook <steve.hook@gmail.com>
2025-10-07 10:26:09 +0100

    Add migration to add safeguarding withdrawal reasons
    This is an upsert because it's possible that the record might already
    exist if earlier migrations are run in order.

---
+++ db/migrate/20251007092157_add_safeguarding_withdrawal_reason.rb
@@ -0,0 +1,12 @@
+class AddSafeguardingWithdrawalReason < ActiveRecord::Migration[7.2]
+  def change
+    WithdrawalReason.upsert(
+      { name: WithdrawalReasons::SAFEGUARDING_CONCERNS },
+      unique_by: :name,
+    )
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
