098f638065df92f695ce54135db099f765ba31b6
Steve Hook <steve.hook@gmail.com>
2025-10-07 17:21:13 +0100

    Migration to add `TraineeWithdrawal#safety_concern_reasons`

---
+++ db/migrate/20251007154220_add_trainee_withdrawals_safeguarding_concern_reasons.rb
@@ -0,0 +1,5 @@
+class AddTraineeWithdrawalsSafeguardingConcernReasons < ActiveRecord::Migration[7.2]
+  def change
+    add_column :trainee_withdrawals, :safeguarding_concern_reasons, :string, null: true
+  end
+end
