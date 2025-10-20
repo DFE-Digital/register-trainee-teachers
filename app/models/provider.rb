4cb5e522186e27daf411f4cd5a4916be8cec4310
Chris Gannon <cjgannon@gmail.com>
2025-10-20 13:30:27 +0100

    add niot to active hei scope

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -79,7 +79,7 @@ class Provider < ApplicationRecord
                     },
                   }
 
-  scope :active_hei, -> { kept.where(accredited: true).where("accreditation_id ~ ?", "^1[0-9]{3}$") }
+  scope :active_hei, -> { kept.where(accredited: true).where("accreditation_id ~ ?", "^(1[0-9]{3}|#{NIOT_ACCREDITATION_ID})$") }
 
   TEACH_FIRST_PROVIDER_CODE = "1TF"
   AMBITION_PROVIDER_CODE = "2A2"
