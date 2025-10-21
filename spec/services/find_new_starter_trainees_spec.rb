ffa7421db8d9b166db5169d54055b024598fc04e
Steve Hook <steve.hook@gmail.com>
2025-10-21 11:42:02 +0100

    Remove the hardcoded census date
    We don't need to freeze time to 2022.

--- spec/services/find_new_starter_trainees_spec.rb
+++ spec/services/find_new_starter_trainees_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 require "csv"
 
 describe FindNewStarterTrainees do
-  let(:census_date) { DateTime.new(2022, 10, 12) }
+  let(:census_date) { AcademicCycle.current.second_wednesday_of_october }
   let(:valid_trainee) { create(:trainee, state: 1, itt_start_date: 2.months.ago, start_academic_cycle: AcademicCycle.current, training_route: :provider_led_postgrad) }
   let(:valid_trainee_with_no_trainee_start_date) { create(:trainee, state: 1, trainee_start_date: nil, start_academic_cycle: AcademicCycle.current, training_route: :provider_led_postgrad) }
   let(:valid_draft_trainee) { create(:trainee, state: 0, itt_start_date: 2.months.ago, start_academic_cycle: AcademicCycle.current, training_route: :provider_led_postgrad) }
@@ -16,12 +16,6 @@ describe FindNewStarterTrainees do
     create(:academic_cycle)
     create(:academic_cycle, previous_cycle: true, id: 10)
     allow(Trainees::SetAcademicCycles).to receive(:call) # deactivate so it doesn't override factories
-  end
-
-  around do |example|
-    Timecop.freeze(Time.zone.local(2022, 9, 3)) do
-      example.run
-    end
   end
 
   it "to contain non draft current academic cycle trainees starting before the census date" do
@@ -92,7 +86,7 @@ describe FindNewStarterTrainees do
       create(
         :trainee,
         :withdrawn,
-        withdrawal_date: AcademicCycle.current.second_wednesday_of_october - 1.day,
+        withdrawal_date: census_date - 1.day,
         itt_start_date: 2.months.ago,
         start_academic_cycle: AcademicCycle.current,
         training_route: :provider_led_postgrad,
