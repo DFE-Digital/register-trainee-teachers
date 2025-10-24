af5ca1bad176ad0496f3e97e655f3b26be79b9a5
Serafeim Maroulis <serafeim@hey.com>
2025-10-24 12:11:36 +0100

    Refactor specs

--- spec/requests/api/v2025_0/post_hesa_trainees_spec.rb
+++ spec/requests/api/v2025_0/post_hesa_trainees_spec.rb
@@ -1839,7 +1839,7 @@ describe "`POST /api/v2025.0/trainees` endpoint" do
         )
         expect(response.parsed_body["errors"]).to eq(
           "funding_method" => [
-            "training route 'teacher_degree_apprenticeship' and subject code 'biology' are not eligible for 'bursary' in academic cycle '#{academic_cycle.label}'",
+            "'bursary' is not allowed when fund_code is '2' and course_subject_one is 'biology'",
           ],
         )
       end
