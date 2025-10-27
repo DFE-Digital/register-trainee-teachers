90790983da1f478bb4968bfa0ff5b6ade826ec06
De Fong <470137+defong@users.noreply.github.com>
2025-10-27 11:44:42 +0000

    [9187] Fixed degree validation whereby it is not needed if not required (#5731)
    * [9187] Fixed degree validation  whereby it is not needed if not required
    * [9187] Fixed missing tests
    * Apply suggestion from @tomtrentham
    
    Co-authored-by: Tom Trentham <1910056+tomtrentham@users.noreply.github.com>

--- spec/forms/apply_applications/apply_trainee_data_form_spec.rb
+++ spec/forms/apply_applications/apply_trainee_data_form_spec.rb
@@ -18,14 +18,48 @@ module ApplyApplications
           expect(subject.progress).to be false
           expect(subject.errors).not_to be_empty
         end
+
+        it "does not validate the degrees form" do
+          expect(subject.progress_status(:degrees)).to eq(:not_provided)
+        end
+
+        context "when degree is not required" do
+          let(:trainee) { create(:trainee, :teacher_degree_apprenticeship, :with_apply_application, degrees: []) }
+
+          it "does not validate the degrees form" do
+            expect(subject.progress_status(:degrees)).to eq(:not_provided)
+          end
+
+          it "returns the entire form as invalid" do
+            expect(subject.progress).to be false
+            expect(subject.errors).not_to be_empty
+          end
+        end
       end
 
       context "when all of the forms are valid" do
         let(:trainee) { create(:trainee, :with_apply_application, :completed) }
 
-        it "returns the entire form as invalid" do
+        it "returns the entire form as valid" do
           expect(subject.progress).to be true
           expect(subject.errors).to be_empty
+        end
+
+        it "validates the degrees form" do
+          expect(subject.progress_status(:degrees)).to eq(:completed)
+        end
+
+        context "when degree is not required" do
+          let(:trainee) { create(:trainee, :teacher_degree_apprenticeship, :with_apply_application, :completed, degrees: []) }
+
+          it "validates the degrees form" do
+            expect(subject.progress_status(:degrees)).to eq(:not_provided)
+          end
+
+          it "returns the entire form as valid" do
+            expect(subject.progress).to be true
+            expect(subject.errors).to be_empty
+          end
         end
       end
     end
