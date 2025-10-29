0d2f17154bc6c77c6ccb4468df577d368594883e
Tom Trentham <tom.trentham@education.gov.uk>
2025-10-29 09:50:06 +0000

    Remove unused helper methods

--- app/helpers/training_routes_helper.rb
+++ app/helpers/training_routes_helper.rb
@@ -3,27 +3,11 @@
 module TrainingRoutesHelper
   include ApplicationHelper
 
-  def training_routes_options
-    enabled_routes.map do |training_route|
-      {
-        name: TRAINING_ROUTE_ENUMS[training_route],
-        hint: hint(training_route),
-        label: t("activerecord.attributes.trainee.training_routes.#{training_route}"),
-      }
-    end
-  end
-
   def enabled_route?(route)
     FeatureService.enabled?("routes.#{route}")
   end
 
 private
-
-  def enabled_routes
-    TRAINING_ROUTE_FEATURE_FLAGS.select do |route|
-      FeatureService.enabled?("routes.#{route}")
-    end
-  end
 
   def hint(training_route)
     {
