670e3984f7cd9abf37f99402a43c6426cf18f0e9
Steve Hook <steve.hook@gmail.com>
2025-10-31 11:23:54 +0000

    Use policy in view component

--- app/components/authentication_tokens/manage/view.rb
+++ app/components/authentication_tokens/manage/view.rb
@@ -5,17 +5,14 @@ module AuthenticationTokens
     class View < ApplicationComponent
       include Pundit::Authorization
 
-      MAX_LIVE_TOKENS = 2
-
       attr_reader :current_user
 
-      def initialize(tokens:, current_user:)
-        @tokens = tokens
+      def initialize(current_user:)
         @current_user = current_user
       end
 
       def can_generate_new_token?
-        @tokens.active.count < MAX_LIVE_TOKENS
+        policy(AuthenticationToken.new).create?
       end
     end
   end
