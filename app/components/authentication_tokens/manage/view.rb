d273739bb03cb35b04b4c078e935a2d9e4e0f66c
Steve Hook <steve.hook@gmail.com>
2025-10-31 09:10:50 +0000

    Conditional rendering of 'Generate token' button
    Only display the button when there are less than 2 active tokens.

--- app/components/authentication_tokens/manage/view.rb
+++ app/components/authentication_tokens/manage/view.rb
@@ -5,10 +5,17 @@ module AuthenticationTokens
     class View < ApplicationComponent
       include Pundit::Authorization
 
+      MAX_LIVE_TOKENS = 2
+
       attr_reader :current_user
 
-      def initialize(current_user = Current.user)
+      def initialize(tokens:, current_user:)
+        @tokens = tokens
         @current_user = current_user
+      end
+
+      def can_generate_new_token?
+        @tokens.active.count < MAX_LIVE_TOKENS
       end
     end
   end
