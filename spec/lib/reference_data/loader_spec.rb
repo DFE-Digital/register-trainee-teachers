44fa36e421969a662367b4c75017de6a17892e32
Steve Hook <steve.hook@gmail.com>
2025-11-19 12:57:21 +0000

    Find reference data types by name

--- spec/lib/reference_data/loader_spec.rb
+++ spec/lib/reference_data/loader_spec.rb
@@ -3,14 +3,14 @@
 require "rails_helper"
 
 RSpec.describe ReferenceData::Loader do
-  describe ".load_all" do
+  describe "#types" do
     it "loads an array of `ReferenceData::Type`s" do
       types = described_class.instance.types
       expect(types).to be_an(Array)
       expect(types.first).to be_a(ReferenceData::Type)
     end
 
-    it "includes the course study mode reference data type" do
+    it "includes the course study mode reference data type and it's values" do
       types = described_class.instance.types
       study_mode_type = types.find { |type| type.name == "trainee_study_mode" }
       expect(study_mode_type).not_to be_nil
@@ -22,9 +22,15 @@ RSpec.describe ReferenceData::Loader do
   end
 
   describe "#find" do
-    it "can lookup reference data type by name"
-    it "can lookup up values by name within a type"
-    it "can lookup up values by id within a type"
-    it "can lookup up values by hesa_code within a type"
+    it "can lookup reference data type by name" do
+      study_mode_type = described_class.instance.find("trainee_study_mode")
+      expect(study_mode_type).to be_a(ReferenceData::Type)
+      expect(study_mode_type.name).to eq("trainee_study_mode")
+    end
+
+    it "returns nil for unknown reference data type names" do
+      wheel_size_type = described_class.instance.find("wheel_sizes")
+      expect(wheel_size_type).to be_nil
+    end
   end
 end
