00c795939bcdfc02e07fc265412c5243979f92db
Serafeim Maroulis <serafeim@hey.com>
2025-11-19 15:42:36 +0000

    Namespace BulkUpdate::AddTrainees::V20250

---
+++ app/services/bulk_update/add_trainees.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module BulkUpdate
+  module AddTrainees
+    VERSION = "BulkUpdate::AddTrainees::#{Api::GetVersionedItem.module_name(Config::VERSION)}".constantize
+  end
+end
