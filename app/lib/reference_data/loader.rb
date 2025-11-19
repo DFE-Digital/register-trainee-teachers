f47eb15b442f40ff4557817fa07b253f29b94a8a
Steve Hook <steve.hook@gmail.com>
2025-11-19 12:57:21 +0000

    Find reference data types by name

--- app/lib/reference_data/loader.rb
+++ app/lib/reference_data/loader.rb
@@ -4,14 +4,34 @@ require "singleton"
 
 module ReferenceData
   class Loader
-    def self.load_all
-      @types ||= Dir[Rails.root.join("config/reference_data/*.yml")].map do |file_path|
+    include Singleton
+
+    attr_reader :types
+
+    def find(type_name)
+      @types_by_name[type_name]
+    end
+
+  private
+
+    def initialize
+      load_all
+    end
+
+    def load_all
+      @types = Dir[Rails.root.join("config/reference_data/*.yml")].map do |file_path|
         type_data = YAML.load_file(file_path)
         ReferenceData::Type.from_yaml(
           metadata: type_data["metadata"].with_indifferent_access,
           data: type_data["data"],
         )
       end
+
+      index_types_by_name
+    end
+
+    def index_types_by_name
+      @types_by_name = @types.index_by(&:name).with_indifferent_access
     end
   end
 end
