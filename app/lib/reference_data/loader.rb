fa3fa9cc46022ff865a26819edc14b663c4d8686
Steve Hook <steve.hook@gmail.com>
2025-11-19 12:30:07 +0000

    Refactor to make `ReferenceData::Loader` a singleton class

--- app/lib/reference_data/loader.rb
+++ app/lib/reference_data/loader.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
+require 'singleton'
 
 module ReferenceData
   class Loader
-    def self.load_all
-      @types ||= Dir[Rails.root.join("config", "reference_data", "*.yml")].map do |file_path|
+    include Singleton
+
+    attr_reader :types
+
+  private
+
+    def initialize
+      load_all
+    end
+
+    def load_all
+      @types = Dir[Rails.root.join("config", "reference_data", "*.yml")].map do |file_path|
         type_data = YAML.load_file(file_path)
         ReferenceData::Type.from_yaml(
           metadata: type_data["metadata"].with_indifferent_access,
