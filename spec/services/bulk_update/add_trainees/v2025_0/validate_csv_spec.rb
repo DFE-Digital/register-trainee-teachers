a9ea9dfda69aeca753b762048ce79d128c2eddd1
Serafeim Maroulis <serafeim@hey.com>
2025-11-20 13:24:40 +0000

    Version validate_csv_spec.rb

--- spec/services/bulk_update/add_trainees/v2025_0/validate_csv_spec.rb
+++ spec/services/bulk_update/add_trainees/v2025_0/validate_csv_spec.rb
@@ -34,7 +34,7 @@ module BulkUpdate
 
       context "given a CSV with the correct columns in the 'wrong' order" do
         let(:file_content) do
-          "#{BulkUpdate::AddTrainees::VERSION::ImportRows::ALL_HEADERS.keys.reverse.join(',')}\nfoo,bar,baz"
+          "#{BulkUpdate::AddTrainees::V20250::ImportRows::ALL_HEADERS.keys.reverse.join(',')}\nfoo,bar,baz"
         end
         let(:csv) { CSVSafe.new(file_content, headers: true).read }
 
@@ -43,7 +43,7 @@ module BulkUpdate
 
       context "given a CSV with the correct columns except one that is an empty string" do
         let(:file_content) do
-          "#{BulkUpdate::AddTrainees::VERSION::ImportRows::ALL_HEADERS.keys.join(',').gsub('First Names', '')}\nfoo,bar,baz"
+          "#{BulkUpdate::AddTrainees::V20250::ImportRows::ALL_HEADERS.keys.join(',').gsub('First Names', '')}\nfoo,bar,baz"
         end
         let(:csv) { CSVSafe.new(file_content, headers: true).read }
 
@@ -52,7 +52,7 @@ module BulkUpdate
 
       context "given a CSV with the correct columns and one extra column" do
         let(:file_content) do
-          "#{(BulkUpdate::AddTrainees::VERSION::ImportRows::ALL_HEADERS.keys + ['Shoe Size']).join(',')}\nfoo,bar,baz"
+          "#{(BulkUpdate::AddTrainees::V20250::ImportRows::ALL_HEADERS.keys + ['Shoe Size']).join(',')}\nfoo,bar,baz"
         end
         let(:csv) { CSVSafe.new(file_content, headers: true).read }
 
@@ -61,7 +61,7 @@ module BulkUpdate
 
       context "given a CSV with the correct columns with validation results and errors" do
         let(:file_content) do
-          "#{BulkUpdate::AddTrainees::VERSION::ImportRows::ALL_HEADERS.keys.reverse.join(',')},Validation results,Errors\nfoo,bar,baz"
+          "#{BulkUpdate::AddTrainees::V20250::ImportRows::ALL_HEADERS.keys.reverse.join(',')},Validation results,Errors\nfoo,bar,baz"
         end
         let(:csv) { CSVSafe.new(file_content, headers: true).read }
 
@@ -71,7 +71,7 @@ module BulkUpdate
       context "given a CSV file with no data (just a header)" do
         let(:csv) do
           CSVSafe.new(
-            BulkUpdate::AddTrainees::VERSION::ImportRows::ALL_HEADERS.keys.join(","),
+            BulkUpdate::AddTrainees::V20250::ImportRows::ALL_HEADERS.keys.join(","),
             headers: true,
           ).read
         end
@@ -82,7 +82,7 @@ module BulkUpdate
       context "given a CSV file with no data (just a header and some empty lines)" do
         let(:csv) do
           CSVSafe.new(
-            "#{BulkUpdate::AddTrainees::VERSION::ImportRows::ALL_HEADERS.keys.join(',')}\n\n\n",
+            "#{BulkUpdate::AddTrainees::V20250::ImportRows::ALL_HEADERS.keys.join(',')}\n\n\n",
             headers: true,
           ).read
         end
