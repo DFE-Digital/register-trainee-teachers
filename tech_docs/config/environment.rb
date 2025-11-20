9d580620dcb2e82c694417f6eab37fb354257f4e
Serafeim Maroulis <serafeim@hey.com>
2025-11-20 14:47:29 +0000

    Load settings in tech_docs

--- tech_docs/config/environment.rb
+++ tech_docs/config/environment.rb
@@ -4,3 +4,5 @@ lib = File.expand_path("../lib", __dir__)
 app = File.expand_path("../../app", __dir__)
 
 $LOAD_PATH.unshift(lib, app)
+
+Config.load_and_set_settings(Config.setting_files("../config", ENV["RAILS_ENV"]))
