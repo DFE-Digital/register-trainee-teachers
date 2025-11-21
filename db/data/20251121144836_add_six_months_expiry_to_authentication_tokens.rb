8abb1ba1c728e149c35476c3f8fb3b696f707285
Serafeim Maroulis <serafeim@hey.com>
2025-11-21 14:35:17 +0000

    Move migration to db/data

--- db/data/20251121144836_add_six_months_expiry_to_authentication_tokens.rb
+++ db/data/20251121144836_add_six_months_expiry_to_authentication_tokens.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddSixMonthsExpiryToAuthenticationTokens < ActiveRecord::Migration[7.2]
   def up
     AuthenticationToken.where(expires_at: nil).update_all(expires_at: Time.current + 6.months)
