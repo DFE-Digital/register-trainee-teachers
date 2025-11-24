bb94cb303abe53a5285239dd6be774404dc6216c
Steve Hook <steve.hook@gmail.com>
2025-11-24 10:28:44 +0000

    Add more specs for reference data loader etc.
    Create constants under `ReferenceData` module

--- app/lib/reference_data/type.rb
+++ app/lib/reference_data/type.rb
@@ -8,14 +8,43 @@ module ReferenceData
       @name = name
       @display_name = display_name
       @values = values
+      @values_by_id = @values.index_by(&:id).transform_keys(&:to_s).with_indifferent_access
+      @values_by_name = @values.index_by(&:name).transform_keys(&:to_s).with_indifferent_access
+      @values_by_hesa_code = {}.tap do |hash|
+        @values.each do |value|
+          value.hesa_codes.each do |hesa_code|
+            hash[hesa_code.to_s] = value
+          end
+        end
+      end
     end
 
     def self.from_yaml(metadata:, data:)
       new(
         name: metadata[:name],
         display_name: metadata[:display_name],
-        values: data.map { |value_attrs| ReferenceData::Value.from_yaml(value_attrs.with_indifferent_access) }
+        values: data.map { |value_attrs| ReferenceData::Value.from_yaml(value_attrs.with_indifferent_access) },
       )
+    end
+
+    def find(identifier)
+      @values_by_id[identifier.to_s] || @values_by_name[identifier.to_s]
+    end
+
+    def ids
+      @values_by_id.keys
+    end
+
+    def names
+      @values.map(&:name)
+    end
+
+    def hesa_codes
+      @values_by_hesa_code.keys
+    end
+
+    def find_by_hesa_code(hesa_code)
+      @values_by_hesa_code[hesa_code.to_s]
     end
   end
 end
