3afb0af0d91b697450ddb39f41fee46db7e50175
Steve Hook <steve.hook@gmail.com>
2025-11-26 17:45:36 +0000

    Add convenience constant for training routes

--- app/lib/reference_data.rb
+++ app/lib/reference_data.rb
@@ -2,4 +2,5 @@
 
 module ReferenceData
   TRAINEE_STUDY_MODES = ReferenceData::Loader.instance.find(:trainee_study_mode)
+  TRAINING_ROUTES = ReferenceData::Loader.instance.find(:training_route)
 end
