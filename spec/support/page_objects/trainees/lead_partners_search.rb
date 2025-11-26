41126f450cfd78974abf2619bd90e03bb06489bf
Serafeim Maroulis <serafeim@hey.com>
2025-11-26 16:05:50 +0000

    Modify trainees/lead_partners_seacrh.rb

---
+++ spec/support/page_objects/trainees/lead_partners_search.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module PageObjects
+  module Trainees
+    class LeadPartnersSearch < PageObjects::Base
+      set_url "/trainees/{trainee_id}/training-partners?query={query}"
+
+      element :search_again_option, "input#partners-training-partner-form-lead-partner-id-results-search-again-field"
+      element :results_search_again_input, "input#partners-training-partner-form-results-search-again-query-field"
+      element :zero_results_search_again_input, "input#partners-training-partner-form-no-results-search-again-query-field"
+      element :continue, "button[type='submit']"
+
+      def choose_lead_partner(id:)
+        find("#partners-training-partner-form-lead-partner-id-#{id}-field").choose
+      end
+    end
+  end
+end
