dd63c7c5741dbf96ad40e081881d3f7e01e26674
Serafeim Maroulis <serafeim@hey.com>
2025-11-26 15:51:50 +0000

    Modify trainees/edit_lead_partner.rb

--- spec/support/page_objects/trainees/edit_lead_partner.rb
+++ spec/support/page_objects/trainees/edit_lead_partner.rb
@@ -5,10 +5,10 @@ module PageObjects
     class EditLeadPartner < PageObjects::Base
       set_url "/trainees/{trainee_id}/training-partners/edit"
 
-      element :lead_partner, "#partners-lead-partner-form-query-field"
-      element :lead_partner_no_js, "#partners-lead-partner-form-query-field"
-      element :autocomplete_list_item, "#partners-lead-partner-form-query-field__listbox li:first-child"
-      element :not_applicable_checkbox, "#partners-lead-partner-form-lead-partner-not-applicable-1-field", visible: false
+      element :lead_partner, "#partners-training-partner-form-query-field"
+      element :lead_partner_no_js, "#partners-training-partner-form-query-field"
+      element :autocomplete_list_item, "#partners-training-partner-form-query-field__listbox li:first-child"
+      element :not_applicable_checkbox, "#partners-training-partner-form-lead-partner-not-applicable-1-field", visible: false
       element :submit, 'button.govuk-button[type="submit"]'
     end
   end
