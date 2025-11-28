0be918c902bbb7aa2a22a0badd7d78beb86614a1
Serafeim Maroulis <serafeim@hey.com>
2025-11-28 15:51:17 +0000

    Rename to training_partner in system admin

--- app/controllers/system_admin/training_partners_controller.rb
+++ app/controllers/system_admin/training_partners_controller.rb
@@ -3,14 +3,14 @@
 module SystemAdmin
   class TrainingPartnersController < ApplicationController
     def index
-      @lead_partners = policy_scope(
+      @training_partners = policy_scope(
         LeadPartner,
         policy_scope_class: LeadPartnerPolicy::Scope,
       ).kept.order(:name).page(params[:page] || 1)
     end
 
     def show
-      @lead_partner = authorize(LeadPartner.find(params[:id]))
+      @training_partner = authorize(LeadPartner.find(params[:id]))
     end
   end
 end
