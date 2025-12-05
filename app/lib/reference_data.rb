7efccd8a976b2a4ea119d0c3d71253afe07a4ddc
Steve Hook <steve.hook@gmail.com>
2025-12-05 13:04:00 +0000

    Add `course_age_ranges` reference data

--- app/lib/reference_data.rb
+++ app/lib/reference_data.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ReferenceData
+  COURSE_AGE_RANGES = ReferenceData::Loader.instance.find(:course_age_range)
   FUND_CODES = ReferenceData::Loader.instance.find(:fund_code)
   STUDY_MODES = ReferenceData::Loader.instance.find(:study_mode)
   TRAINING_ROUTES = ReferenceData::Loader.instance.find(:training_route)
