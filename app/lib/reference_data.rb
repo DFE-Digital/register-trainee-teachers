10ada8fa0bb6ff98e1e9c04474cb88e023f92841
Steve Hook <steve.hook@gmail.com>
2025-12-05 16:40:35 +0000

    Add `sex` reference data type

--- app/lib/reference_data.rb
+++ app/lib/reference_data.rb
@@ -3,6 +3,7 @@
 module ReferenceData
   COURSE_AGE_RANGES = ReferenceData::Loader.instance.find(:course_age_range)
   FUND_CODES = ReferenceData::Loader.instance.find(:fund_code)
+  SEXES = ReferenceData::Loader.instance.find(:sex)
   STUDY_MODES = ReferenceData::Loader.instance.find(:study_mode)
   TRAINING_ROUTES = ReferenceData::Loader.instance.find(:training_route)
 end
