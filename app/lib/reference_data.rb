7bcd87c92bf5202f345948118cb2398a7a0b314a
Steve Hook <steve.hook@gmail.com>
2025-12-05 12:11:50 +0000

    Add `fund_code` reference data type

--- app/lib/reference_data.rb
+++ app/lib/reference_data.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ReferenceData
+  FUND_CODES = ReferenceData::Loader.instance.find(:fund_code)
   STUDY_MODES = ReferenceData::Loader.instance.find(:study_mode)
   TRAINING_ROUTES = ReferenceData::Loader.instance.find(:training_route)
 end
