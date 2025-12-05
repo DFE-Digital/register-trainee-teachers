99771ee75ba35f819f7e727501afeff33e55dd54
Steve Hook <steve.hook@gmail.com>
2025-12-05 12:09:49 +0000

    Add `training_initiative` reference data type

--- app/lib/reference_data.rb
+++ app/lib/reference_data.rb
@@ -3,4 +3,5 @@
 module ReferenceData
   STUDY_MODES = ReferenceData::Loader.instance.find(:study_mode)
   TRAINING_ROUTES = ReferenceData::Loader.instance.find(:training_route)
+  TRAINING_INITIATIVES = ReferenceData::Loader.instance.find(:training_initiative)
 end
