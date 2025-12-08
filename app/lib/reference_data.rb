54ff5ba16aa507a790bd5c51a1c4b80e08a368ee
Steve Hook <steve.hook@gmail.com>
2025-12-08 01:07:32 +0000

    Add ethnicity reference data type

--- app/lib/reference_data.rb
+++ app/lib/reference_data.rb
@@ -2,6 +2,7 @@
 
 module ReferenceData
   COURSE_AGE_RANGES = ReferenceData::Loader.instance.find(:course_age_range)
+  ETHNICITIES = ReferenceData::Loader.instance.find(:ethnicity)
   FUND_CODES = ReferenceData::Loader.instance.find(:fund_code)
   SEXES = ReferenceData::Loader.instance.find(:sex)
   STUDY_MODES = ReferenceData::Loader.instance.find(:study_mode)
