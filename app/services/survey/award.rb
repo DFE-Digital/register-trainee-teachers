b3c23ad3eca7a6dcb23438961fd58bcc5d8b58b7
walkers-cd <laura.walkerdine@education.gov.uk>
2025-12-09 12:28:32 +0000

    QTS survey update with new QTS terminology

--- app/services/survey/award.rb
+++ app/services/survey/award.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Survey
-  # Service for sending QTS award surveys to trainees
+  # Service for sending QTS surveys to trainees
   class Award < Base
     delegate :awarded_at, to: :trainee
 
@@ -24,12 +24,12 @@ module Survey
     end
 
     def subject
-      "QTS Award Survey"
+      "QTS Survey"
     end
 
     def embedded_data_for_distribution
       {
-        award_date: awarded_at,
+        award_date: date_QTS_gained,
         training_route: training_route,
       }
     end
