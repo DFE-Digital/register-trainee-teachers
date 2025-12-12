2775eb2ab72ac23429ca464b1ab82bc588684fe0
Serafeim Maroulis <serafeim@hey.com>
2025-12-12 16:39:22 +0000

    Rename to training_partner in view_spec, helpers

--- spec/views/trainees/record_completion.html.erb_spec.rb
+++ spec/views/trainees/record_completion.html.erb_spec.rb
@@ -9,7 +9,7 @@ describe "trainees/_record_completion_filter", "feature_routes.provider_led_post
         search_path: trainees_path,
         filters: nil,
         lead_school_user?: lead_school_user?,
-        lead_partner_user?: lead_partner_user?,
+        training_partner_user?: training_partner_user?,
       )
 
       render
@@ -17,7 +17,7 @@ describe "trainees/_record_completion_filter", "feature_routes.provider_led_post
   end
 
   context "placements enabled", feature_placements: true do
-    context "with a lead partner user" do
+    context "with a training partner user" do
       let(:lead_school_user?) { false }
       let(:training_partner_user?) { true }
 
@@ -26,7 +26,7 @@ describe "trainees/_record_completion_filter", "feature_routes.provider_led_post
       end
     end
 
-    context "with a non lead school and non lead partner user" do
+    context "with a non lead school and non training partner user" do
       let(:lead_school_user?) { false }
       let(:training_partner_user?) { false }
 
