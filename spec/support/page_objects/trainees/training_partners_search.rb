ed7f31723ca9d2080f69860894b5f6d74cf149e4
Serafeim Maroulis <serafeim@hey.com>
2025-12-12 09:37:56 +0000

    Reference training partner in page object

--- spec/support/page_objects/trainees/training_partners_search.rb
+++ spec/support/page_objects/trainees/training_partners_search.rb
@@ -5,13 +5,13 @@ module PageObjects
     class TrainingPartnersSearch < PageObjects::Base
       set_url "/trainees/{trainee_id}/training-partners?query={query}"
 
-      element :search_again_option, "input#partners-training-partner-form-lead-partner-id-results-search-again-field"
+      element :search_again_option, "input#partners-training-partner-form-training-partner-id-results-search-again-field"
       element :results_search_again_input, "input#partners-training-partner-form-results-search-again-query-field"
       element :zero_results_search_again_input, "input#partners-training-partner-form-no-results-search-again-query-field"
       element :continue, "button[type='submit']"
 
       def choose_training_partner(id:)
-        find("#partners-training-partner-form-lead-partner-id-#{id}-field").choose
+        find("#partners-training-partner-form-training-partner-id-#{id}-field").choose
       end
     end
   end
