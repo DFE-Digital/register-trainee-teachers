af4bd3a1d312a30e531e96d95625dfb65bfc8c5a
Steve Hook <steve.hook@gmail.com>
2025-12-18 10:21:42 +0000

    Remove some hard-coded training route references

--- app/lib/hesa/code_sets/training_routes.rb
+++ app/lib/hesa/code_sets/training_routes.rb
@@ -5,13 +5,13 @@ module Hesa
     module TrainingRoutes
       # https://www.hesa.ac.uk/collection/c22053/xml/c22053/c22053codelists.xsd
       MAPPING = {
-        "02" => "school_direct_tuition_fee",
-        "03" => "school_direct_salaried",
-        "09" => "opt_in_undergrad",
-        "10" => "pg_teaching_apprenticeship",
-        "11" => "provider_led_undergrad",
-        "12" => "provider_led_postgrad",
-        "14" => "teacher_degree_apprenticeship",
+        "02" => ReferenceData::TRAINING_ROUTES.school_direct_tuition_fee.name,
+        "03" => ReferenceData::TRAINING_ROUTES.school_direct_salaried,
+        "09" => ReferenceData::TRAINING_ROUTES.opt_in_undergrad,
+        "10" => ReferenceData::TRAINING_ROUTES.pg_teaching_apprenticeship,
+        "11" => ReferenceData::TRAINING_ROUTES.provider_led_undergrad,
+        "12" => ReferenceData::TRAINING_ROUTES.provider_led_postgrad,
+
       }.freeze
     end
   end
