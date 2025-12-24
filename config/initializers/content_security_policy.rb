e96d88b90690261782a3b3cfd4b57a78381dfe57
Steve Hook <steve.hook@gmail.com>
2025-12-24 12:19:38 +0000

    Run `rails app:update`
    This has likely caused some breaking changes so next job is fixing
    those.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Be sure to restart your server when you modify this file.
 
 # Define an application-wide content security policy.
@@ -18,30 +16,10 @@
 #     # policy.report_uri "/csp-violation-report-endpoint"
 #   end
 #
-#   # Generate session nonces for permitted importmap and inline scripts
+#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
 #   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src)
+#   config.content_security_policy_nonce_directives = %w(script-src style-src)
 #
 #   # Report violations without enforcing the policy.
 #   # config.content_security_policy_report_only = true
 # end
-
-Rails.application.configure do
-  config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
-
-  config.content_security_policy do |policy|
-    policy.default_src(:self)
-    policy.connect_src(:self,
-                       "https://www.google-analytics.com")
-    policy.img_src(:self,
-                   "https://www.google-analytics.com")
-    policy.object_src(:none)
-    policy.script_src(:self,
-                      "https://www.googletagmanager.com",
-                      "https://www.google-analytics.com",
-                      "https://az416426.vo.msecnd.net") # needed for App Insights
-    policy.style_src(:self, "'sha256-WAyOw4V+FqDc35lQPyRADLBWbuNK8ahvYEaQIYF1+Ps='") # Turbo progress bar stylesheet https://github.com/hotwired/turbo/issues/809
-    policy.font_src(:self, :data)
-    policy.upgrade_insecure_requests(!Rails.env.local?)
-  end
-end
--- config/puma.rb
+++ config/puma.rb
@@ -1,72 +1,41 @@
-# frozen_string_literal: true
-
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
+# This configuration file will be evaluated by Puma. The top-level methods that
+# are invoked here are part of Puma's configuration DSL. For more information
+# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
 #
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+# Puma starts a configurable number of processes (workers) and each process
+# serves each request in a thread from an internal thread pool.
+#
+# You can control the number of workers using ENV["WEB_CONCURRENCY"]. You
+# should only set this value when you want to run 2 or more workers. The
+# default is already 1.
+#
+# The ideal number of threads per worker depends both on how much time the
+# application spends waiting for IO operations and on how much you wish to
+# prioritize throughput over latency.
+#
+# As a rule of thumb, increasing the number of threads will increase how much
+# traffic a given process can handle (throughput), but due to CRuby's
+# Global VM Lock (GVL) it has diminishing returns and will degrade the
+# response time (latency) of the application.
+#
+# The default is set to 3 threads as it's deemed a decent compromise between
+# throughput and latency for the average Rails application.
+#
+# Any libraries that use a connection pool or another resource pool should
+# be configured to provide at least as many connections as the number of
+# threads. This includes Active Record's `pool` parameter in `database.yml`.
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
+port ENV.fetch("PORT", 3000)
 
-listen_port = ENV.fetch("PORT") { Settings.port }
-
-# Specifies the `environment` that Puma will run in.
-#
-env = ENV.fetch("RAILS_ENV", "development")
-
-environment env
-# Specifies the `pidfile` that Puma will use.
-# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
-
-# Allow puma to be restarted by `rails restart` command.
+# Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-if env == "development" && FeatureService.enabled?("use_ssl")
-  cert = "#{Dir.pwd}/#{File.join('config', 'localhost', 'https', 'localhost.crt')}"
-  key = "#{Dir.pwd}/#{File.join('config', 'localhost', 'https', 'localhost.key')}"
+# Run the Solid Queue supervisor inside of Puma for single-server deployments
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
-  unless File.exist?(cert) && File.exist?(key)
-    def generate_root_cert(root_key)
-      root_ca = OpenSSL::X509::Certificate.new
-      root_ca.version = 2 # cf. RFC 5280 - to make it a "v3" certificate
-      root_ca.serial = rand(100_000) # randomized for local development to prevent SEC_ERROR_REUSED_ISSUER_AND_SERIAL errors in firefox after a git-clean
-      root_ca.subject = OpenSSL::X509::Name.parse("/C=GB/L=London/O=DfE/CN=localhost")
-      root_ca.issuer = root_ca.subject # root CA's are "self-signed"
-      root_ca.public_key = root_key.public_key
-      root_ca.not_before = Time.zone.now
-      root_ca.not_after = root_ca.not_before + (2 * 365 * 24 * 60 * 60) # 2 years validity
-      root_ca.sign(root_key, OpenSSL::Digest.new("SHA256"))
-      root_ca
-    end
-
-    root_key = OpenSSL::PKey::RSA.new(2048)
-    File.write(key, root_key, mode: "wb")
-
-    root_cert = generate_root_cert(root_key)
-    File.write(cert, root_cert, mode: "wb")
-  end
-
-  ssl_bind "0.0.0.0", listen_port, cert: cert, key: key, verify_mode: "none"
-else
-  port listen_port
-end
+# Specify the PID file. Defaults to tmp/pids/server.pid in development.
+# In other environments, only set the PID file if requested.
+pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
