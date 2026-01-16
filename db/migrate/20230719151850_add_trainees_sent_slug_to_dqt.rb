ec56d92122971c932563b029e53063b987a3ebb9
Serafeim Maroulis <serafeim@hey.com>
2026-01-16 09:35:19 +0000

    Restore migration scope

--- db/migrate/20230719151850_add_trainees_sent_slug_to_dqt.rb
+++ db/migrate/20230719151850_add_trainees_sent_slug_to_dqt.rb
@@ -2,6 +2,6 @@
 
 class AddTraineesSentSlugToDqt < ActiveRecord::Migration[7.0]
   def change
-    add_column :trainees, :slug_sent_to_trs_at, :datetime, null: true
+    add_column :trainees, :slug_sent_to_dqt_at, :datetime, null: true
   end
 end
