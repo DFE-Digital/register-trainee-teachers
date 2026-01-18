2f472a8f85b18f3641f81b8e5aa3e9bdf1bcd8d3
asatwal <asatwal01@googlemail.com>
2026-01-18 19:59:55 +0000

    Add model and update analytics / airbyte stream

---
+++ app/models/new_partner_user.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: new_partner_users
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+class NewPartnerUser < ApplicationRecord
+end
