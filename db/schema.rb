0faa252b99c8c5c40dae64bbf817416becb9b1a8
asatwal <asatwal01@googlemail.com>
2026-01-18 14:40:06 +0000

    Add new table and perform clean db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_18_132249) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_18_143540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "citext"
@@ -737,6 +737,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_18_132249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_nationalities_on_name", unique: true
+  end
+
+  create_table "new_partner_users", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "placements", force: :cascade do |t|
