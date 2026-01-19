601beb6d2aa9afc618f8236be564e97e3b5c8a9f
Tom Trentham <tom.trentham@education.gov.uk>
2026-01-19 00:26:50 +0000

    Update grant amounts and subjects

--- config/initializers/training_routes/funding/2026_2027/grants.rb
+++ config/initializers/training_routes/funding/2026_2027/grants.rb
@@ -2,45 +2,29 @@
 
 GRANTS_2026_TO_2027 = [
   OpenStruct.new(
-    training_route: TRAINING_ROUTE_ENUMS[:early_years_postgrad],
-    amount: 7_000,
-    allocation_subjects: [
-      AllocationSubjects::EARLY_YEARS_ITT,
-    ],
-  ),
-  OpenStruct.new(
     training_route: TRAINING_ROUTE_ENUMS[:early_years_salaried],
-    amount: 14_000,
+    amount: 16_535,
     allocation_subjects: [
       AllocationSubjects::EARLY_YEARS_ITT,
     ],
   ),
   OpenStruct.new(
     training_route: TRAINING_ROUTE_ENUMS[:pg_teaching_apprenticeship],
-    amount: 1_000,
+    amount: 5_000,
     allocation_subjects: [
-      AllocationSubjects::ART_AND_DESIGN,
-      AllocationSubjects::MUSIC,
-      AllocationSubjects::RELIGIOUS_EDUCATION,
+      AllocationSubjects::BIOLOGY,
+      AllocationSubjects::GEOGRAPHY,
     ],
   ),
   OpenStruct.new(
     training_route: TRAINING_ROUTE_ENUMS[:pg_teaching_apprenticeship],
-    amount: 17_000,
+    amount: 20_000,
     allocation_subjects: [
       AllocationSubjects::ANCIENT_LANGUAGES,
-      AllocationSubjects::BIOLOGY,
       AllocationSubjects::DESIGN_AND_TECHNOLOGY,
-      AllocationSubjects::GEOGRAPHY,
-      AllocationSubjects::MODERN_LANGUAGES,
-    ],
-  ),
-  OpenStruct.new(
-    training_route: TRAINING_ROUTE_ENUMS[:pg_teaching_apprenticeship],
-    amount: 26_000,
-    allocation_subjects: [
       AllocationSubjects::FRENCH_LANGUAGE,
       AllocationSubjects::GERMAN_LANGUAGE,
+      AllocationSubjects::MODERN_LANGUAGES,
       AllocationSubjects::SPANISH_LANGUAGE,
     ],
   ),
@@ -52,46 +36,36 @@ GRANTS_2026_TO_2027 = [
       AllocationSubjects::COMPUTING,
       AllocationSubjects::MATHEMATICS,
       AllocationSubjects::PHYSICS,
-    ],
-  ),
-  OpenStruct.new(
-    training_route: TRAINING_ROUTE_ENUMS[:school_direct_salaried],
-    amount: 29_000,
-    allocation_subjects: [
-      AllocationSubjects::CHEMISTRY,
-      AllocationSubjects::COMPUTING,
-      AllocationSubjects::MATHEMATICS,
-      AllocationSubjects::PHYSICS,
-    ],
-  ),
-  OpenStruct.new(
-    training_route: TRAINING_ROUTE_ENUMS[:school_direct_salaried],
-    amount: 26_000,
-    allocation_subjects: [
-      AllocationSubjects::ANCIENT_LANGUAGES,
-      AllocationSubjects::BIOLOGY,
-      AllocationSubjects::DESIGN_AND_TECHNOLOGY,
-      AllocationSubjects::GEOGRAPHY,
-      AllocationSubjects::MODERN_LANGUAGES,
-      AllocationSubjects::FRENCH_LANGUAGE,
-      AllocationSubjects::GERMAN_LANGUAGE,
-      AllocationSubjects::SPANISH_LANGUAGE,
-    ],
-  ),
-  OpenStruct.new(
-    training_route: TRAINING_ROUTE_ENUMS[:school_direct_salaried],
-    amount: 10_000,
-    allocation_subjects: [
-      AllocationSubjects::ART_AND_DESIGN,
-      AllocationSubjects::MUSIC,
-      AllocationSubjects::RELIGIOUS_EDUCATION,
     ],
   ),
   OpenStruct.new(
     training_route: TRAINING_ROUTE_ENUMS[:school_direct_salaried],
     amount: 5_000,
     allocation_subjects: [
-      AllocationSubjects::ENGLISH,
+      AllocationSubjects::BIOLOGY,
+      AllocationSubjects::GEOGRAPHY,
+    ],
+  ),
+  OpenStruct.new(
+    training_route: TRAINING_ROUTE_ENUMS[:school_direct_salaried],
+    amount: 20_000,
+    allocation_subjects: [
+      AllocationSubjects::ANCIENT_LANGUAGES,
+      AllocationSubjects::DESIGN_AND_TECHNOLOGY,
+      AllocationSubjects::FRENCH_LANGUAGE,
+      AllocationSubjects::GERMAN_LANGUAGE,
+      AllocationSubjects::MODERN_LANGUAGES,
+      AllocationSubjects::SPANISH_LANGUAGE,
+    ],
+  ),
+  OpenStruct.new(
+    training_route: TRAINING_ROUTE_ENUMS[:school_direct_salaried],
+    amount: 29_000,
+    allocation_subjects: [
+      AllocationSubjects::CHEMISTRY,
+      AllocationSubjects::COMPUTING,
+      AllocationSubjects::MATHEMATICS,
+      AllocationSubjects::PHYSICS,
     ],
   ),
 ].freeze
