c2d2aa1a1172a7c4a525fd9e9faf3f8ea1b1de8b
Tom Trentham <tom.trentham@education.gov.uk>
2026-01-19 00:28:13 +0000

    Reorder Scholarships

--- config/initializers/training_routes/funding/2026_2027/scholarships.rb
+++ config/initializers/training_routes/funding/2026_2027/scholarships.rb
@@ -3,21 +3,21 @@
 SCHOLARSHIPS_2026_TO_2027 = [
   OpenStruct.new(
     training_route: TRAINING_ROUTE_ENUMS[:provider_led_postgrad],
+    amount: 28_000,
+    allocation_subjects: [
+      AllocationSubjects::FRENCH_LANGUAGE,
+      AllocationSubjects::GERMAN_LANGUAGE,
+      AllocationSubjects::SPANISH_LANGUAGE,
+    ],
+  ),
+  OpenStruct.new(
+    training_route: TRAINING_ROUTE_ENUMS[:provider_led_postgrad],
     amount: 31_000,
     allocation_subjects: [
       AllocationSubjects::CHEMISTRY,
       AllocationSubjects::COMPUTING,
       AllocationSubjects::MATHEMATICS,
       AllocationSubjects::PHYSICS,
-    ],
-  ),
-  OpenStruct.new(
-    training_route: TRAINING_ROUTE_ENUMS[:provider_led_postgrad],
-    amount: 28_000,
-    allocation_subjects: [
-      AllocationSubjects::FRENCH_LANGUAGE,
-      AllocationSubjects::GERMAN_LANGUAGE,
-      AllocationSubjects::SPANISH_LANGUAGE,
     ],
   ),
 ].freeze
