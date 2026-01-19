b41b4bbfd4db24b1ff25cd445ac7eab669bbfdea
asatwal <asatwal01@googlemail.com>
2026-01-19 20:16:46 +0000

    Add table new_groups with migrations and data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_19_181215) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_19_194204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "citext"
@@ -737,6 +737,13 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_19_181215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_nationalities_on_name", unique: true
+  end
+
+  create_table "new_groups", force: :cascade do |t|
+    t.string "description"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "new_partner_users", force: :cascade do |t|
