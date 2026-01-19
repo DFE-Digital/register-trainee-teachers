b3394c94f42b88e1ad4fa3a3cce46d68d9624d8d
asatwal <asatwal01@googlemail.com>
2026-01-19 20:28:05 +0000

    Fix rubocop offenses

--- db/migrate/20260119194204_create_new_groups.rb
+++ db/migrate/20260119194204_create_new_groups.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
-#
+
 class CreateNewGroups < ActiveRecord::Migration[7.2]
   class NewGroup < ActiveRecord::Base
-    self.table_name = 'new_groups'
+    self.table_name = "new_groups"
   end
 
   def change
@@ -15,9 +15,9 @@ class CreateNewGroups < ActiveRecord::Migration[7.2]
 
     safety_assured do
       NewGroup.reset_column_information
-      NewGroup.create!(description: 'First group', email: 'first@group.com')
-      NewGroup.create!(description: 'Second group', email: 'second@group.com')
-      NewGroup.create!(description: 'Third group', email: 'third@group.com')
+      NewGroup.create!(description: "First group", email: "first@group.com")
+      NewGroup.create!(description: "Second group", email: "second@group.com")
+      NewGroup.create!(description: "Third group", email: "third@group.com")
     end
   end
 end
