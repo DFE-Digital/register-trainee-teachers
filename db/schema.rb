2fe19c09d055c88a736fd38a8b07265d5c43ffb7
asatwal <asatwal01@googlemail.com>
2026-01-20 09:29:00 +0000

    Add DB Migratun for new_trainees

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_19_194204) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_20_092259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "citext"
@@ -754,6 +754,12 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_19_194204) do
   end
 
   create_table "new_schools", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "new_trainees", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
