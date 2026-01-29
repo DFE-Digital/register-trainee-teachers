626c134b6c1d51b27a57c2b9f67cb6c8b99ee812
Steve Hook <steve.hook@gmail.com>
2026-01-29 09:41:04 +0000

    [9233] Implement reference data management system tech docs (#5836)
    * Initial attempt to codify study-mode reference type
    
    * Initial experiment
    
    * Refactor to make `ReferenceData::Loader` a singleton class
    
    * Find reference data types by name
    
    * More tests
    
    * Add more specs for reference data loader etc.
    
    Create constants under `ReferenceData` module
    
    * Some missing unit tests
    
    * Add `training_route` reference data type YAML
    
    * Add convenience constant for training routes
    
    * Tidy up reference data files
    
    - Make display_name and descriptions consistent with HESA labels used elsewhere
    - Remove comments
    
    * Add `training_route` reference data type
    
    * Add `disability` reference data type
    
    * Add `itt_aim` reference data type
    
    * Add `qualification_aim` reference data type
    
    * Add `nationalities` reference data type
    
    * Add `degree_grades` reference data type
    
    * Add `degree_type` reference data type
    
    * Add `country` reference data type
    
    * Add `institution` reference data type
    
    * Add `degree_subject` reference data type
    
    * Add `course_subject` reference data type
    
    * Added `training_initiative` reference data type
    
    Missed this first time round
    
    * Fix spec regressions
    
    * WIP
    
    * Refactor `generate_from_yaml` rake task
    
    - Stop using a temporary directory to generate the docs
    - Extract a helper to reduce bloat in the task itself
    
    * Fix directory name for 2026 reference data
    
    * Refactor Rakefile again to generate configured versions
    
    * Hide 2026 reference docs for now
    
    * Fix some links broken by 2026 reference data
    
    * WIP add download link to 2026 docs
    
    * WIP
    
    * WIP
    
    * Appease Rubocop
    
    * Fix specs for `Hesa::ReferenceData::V20260`
    
    * Fix integration spec
    
    * Appease Rubocop
    
    * Fix missing reference data values in CSV download
    
    These went missing when we converted to a Hash due to each non-HESA
    values having the same dummy key.
    
    * Rebase fix
    
    ---------
    
    Co-authored-by: Tom Trentham <tom.trentham@education.gov.uk>

--- app/lib/hesa/reference_data/v2026_0.rb
+++ app/lib/hesa/reference_data/v2026_0.rb
@@ -3,6 +3,46 @@
 module Hesa
   module ReferenceData
     class V20260 < V20250
+      def self.all
+        {
+          country: ::ReferenceData::COUNTRIES,
+          course_age_range: ::ReferenceData::COURSE_AGE_RANGES,
+          course_subject: ::ReferenceData::COURSE_SUBJECTS,
+          degree_subject: ::ReferenceData::DEGREE_SUBJECTS,
+          degree_grade: ::ReferenceData::DEGREE_GRADES,
+          degree_type: ::ReferenceData::DEGREE_TYPES,
+          disability: ::ReferenceData::DISABILITIES,
+          ethnicity: ::ReferenceData::ETHNICITIES,
+          fund_code: ::ReferenceData::FUND_CODES,
+          institution: ::ReferenceData::INSTITUTIONS,
+          itt_aim: ::ReferenceData::ITT_AIMS,
+          nationality: ::ReferenceData::NATIONALITIES,
+          itt_qualification_aim: ::ReferenceData::ITT_QUALIFICATION_AIMS,
+          sex: ::ReferenceData::SEXES,
+          study_mode: ::ReferenceData::STUDY_MODES,
+          training_route: ::ReferenceData::TRAINING_ROUTES,
+          training_initiative: ::ReferenceData::TRAINING_INITIATIVES,
+        }.transform_values do |type|
+          Hesa::ReferenceData::V20260.values_for(type)
+        end.freeze
+      end
+
+      # rubocop:disable Style/HashEachMethods
+      def self.values_for(type)
+        transformed_values = []
+        type.values.each do |value|
+          hesa_codes = value.hesa_codes || []
+          if hesa_codes.present?
+            hesa_codes.each do |code|
+              transformed_values << [code, value.display_name]
+            end
+          else
+            transformed_values << ["-", value.display_name]
+          end
+        end.flatten
+        transformed_values
+      end
+      # rubocop:enable Style/HashEachMethods
     end
   end
 end
