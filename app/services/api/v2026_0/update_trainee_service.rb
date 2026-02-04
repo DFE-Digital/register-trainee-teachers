b1669a6f082a1ff4ff6678f6f6685361804db2d7
Serafeim Maroulis <serafeim@hey.com>
2026-02-04 15:02:31 +0000

    Skip ApiTrnValidator for v2026.0

--- app/services/api/v2026_0/update_trainee_service.rb
+++ app/services/api/v2026_0/update_trainee_service.rb
@@ -3,6 +3,21 @@
 module Api
   module V20260
     class UpdateTraineeService < Api::V20250::UpdateTraineeService
+      def call
+        trainee_attributes_validation = TraineeAttributesValidation.new(trainee_attributes:)
+        attributes_to_save = attributes.deep_attributes.with_indifferent_access
+        trainee.nationalities.destroy_all if attributes_to_save[:nationalisations_attributes].present?
+        trainee.clear_disabilities if attributes_to_save[:trainee_disabilities_attributes].present?
+
+        if trainee_attributes_validation.invalid?
+          [false, trainee_attributes_validation]
+        else
+          trainee.assign_attributes(attributes_to_save)
+          trainee.disabilities = [] if !trainee.disabled? && trainee.disability_disclosure_changed?
+
+          [trainee.save, trainee]
+        end
+      end
     end
   end
 end
