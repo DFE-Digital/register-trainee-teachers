d93250411b6cdbe1598a7b4e75f58f34df848ac3
David Georgiou <david.georgiou@icloud.com>
2026-02-13 16:56:34 +0000

    [9404] - refactor parameter filtering

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
-  config.before_send = lambda do |event, _hint|
-    filter.filter(event.to_hash)
-  end
-
   # NOTE: turn off Sentry Cron monitoring it seems to automatically create new monitor without notice
   # https://dfe-teacher-services.sentry.io/crons/
   # config.enabled_patches += [:sidekiq_cron]
+
+  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+  config.before_send = lambda do |event, _hint|
+    event.extra = filter.filter(event.extra) if event.extra
+    event.user = filter.filter(event.user) if event.user
+    event.contexts = filter.filter(event.contexts) if event.contexts
+    event
+  end
 
   config.traces_sample_rate = 0.05
   config.profiles_sample_rate = 0.05
