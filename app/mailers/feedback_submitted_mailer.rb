484494cd2d3b7314591d3f73e67e53cb6dd25831
Serafeim Maroulis <serafeim@hey.com>
2026-02-18 16:22:46 +0000

    Send feedback email

---
+++ app/mailers/feedback_submitted_mailer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class FeedbackSubmittedMailer < GovukNotifyRails::Mailer
+  def generate(satisfaction_level:, improvement_suggestion:, name:, email:)
+    set_template(Settings.govuk_notify.feedback_submitted_email_template_id)
+
+    set_personalisation(
+      satisfaction_level:,
+      improvement_suggestion:,
+      name: name.presence || "Not entered",
+      email: email.presence || "Not entered",
+    )
+
+    mail(to: Settings.feedback_email)
+  end
+end
