ced146e66146cd3607584b229608d6712ce55ff7
Serafeim Maroulis <serafeim@hey.com>
2026-02-25 14:31:55 +0000

    Add v2026.1 API version

---
+++ app/serializers/api/v2026_1/degree_serializer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Api
+  module V20261
+    class DegreeSerializer < Api::V20260::DegreeSerializer
+    end
+  end
+end
---
+++ spec/serializers/api/v2026_1/trainee_serializer_spec.rb
@@ -0,0 +1,140 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Api::V20261::TraineeSerializer do
+  let(:trainee) { create(:trainee, :with_training_partner_scitt, :with_hesa_trainee_detail, :with_diversity_information, :in_progress, :with_placements, :with_french_nationality) }
+  let(:json) { described_class.new(trainee).as_hash }
+
+  describe "serialization" do
+    let(:fields) do
+      %w[
+        trainee_id
+        provider_trainee_id
+        hesa_id
+        first_names
+        last_name
+        date_of_birth
+        created_at
+        updated_at
+        email
+        middle_names
+        training_route
+        sex
+        diversity_disclosure
+        ethnic_group
+        ethnicity
+        disability_disclosure
+        itt_start_date
+        outcome_date
+        itt_end_date
+        trn
+        submitted_for_trn_at
+        withdrawal_date
+        withdrawal_reasons
+        withdrawal_trigger
+        withdrawal_future_interest
+        withdrawal_another_reason
+        withdrawal_safeguarding_concern_reasons
+        defer_date
+        defer_reason
+        recommended_for_award_at
+        trainee_start_date
+        reinstate_date
+        course_min_age
+        course_max_age
+        course_subject_1
+        course_subject_2
+        course_subject_3
+        awarded_at
+        training_initiative
+        study_mode
+        course_education_phase
+        record_source
+        ukprn
+        course_qualification
+        course_title
+        course_level
+        course_itt_start_date
+        course_age_range
+        employing_school_urn
+        training_partner_urn
+        training_partner_ukprn
+        fund_code
+        course_year
+        itt_aim
+        ni_number
+        pg_apprenticeship_start_date
+        previous_last_name
+        additional_training_initiative
+        nationality
+        placements
+        degrees
+        disability1
+        application_id
+        ethnic_background
+        additional_ethnic_background
+        funding_method
+        itt_qualification_aim
+        state
+      ]
+    end
+
+    it "matches the fields" do
+      expect(json.keys).to match_array(fields)
+    end
+
+    it "includes the correct disability values" do
+      expect(json["disability1"]).not_to be_nil
+      expect(json["disability1"]).to eq(trainee.hesa_trainee_detail.hesa_disabilities["disability1"])
+    end
+
+    describe "placements" do
+      let(:placements) do
+        trainee.placements.map { |placement| Api::V20261::PlacementSerializer.new(placement).as_hash.with_indifferent_access }
+      end
+
+      it "serializes with Api::V20261::PlacementSerializer" do
+        expect(json[:placements]).to eq(placements)
+      end
+    end
+
+    describe "degrees" do
+      let(:degrees) do
+        trainee.degrees.map { |degree| Api::V20261::DegreeSerializer.new(degree).as_hash.with_indifferent_access }
+      end
+
+      it "serializes with Api::V20261::DegreeSerializer" do
+        expect(json[:degrees]).to eq(degrees)
+      end
+    end
+
+    describe "HESA trainee details" do
+      let(:hesa_trainee_detail) do
+        Api::V20261::HesaTraineeDetailSerializer.new(trainee.hesa_trainee_detail).as_hash.with_indifferent_access
+      end
+
+      it "serializes with Api::V20261::HesaTraineeDetailSerializer" do
+        expect(trainee.hesa_trainee_detail.attributes.except(*Api::V20261::HesaTraineeDetailSerializer::EXCLUDED_ATTRIBUTES)).to eq(hesa_trainee_detail)
+      end
+    end
+
+    describe "nationality" do
+      it "serializes nationality to HESA code" do
+        expect(json[:nationality]).to eq("FR")
+      end
+    end
+
+    describe "training partner attributes" do
+      it "serializes the urn correctly" do
+        expect(json[:training_partner_urn]).not_to be_nil
+        expect(json[:training_partner_urn]).to eq(trainee.training_partner.urn)
+      end
+
+      it "serializes the ukprn correctly" do
+        expect(json[:training_partner_ukprn]).not_to be_nil
+        expect(json[:training_partner_ukprn]).to eq(trainee.training_partner.ukprn)
+      end
+    end
+  end
+end
