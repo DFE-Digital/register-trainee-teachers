4835f5285d945c99067b55302e202b2f0e682bbb
Serafeim Maroulis <serafeim@hey.com>
2026-02-25 14:31:55 +0000

    Add v2026.1 API version

---
+++ app/serializers/api/v2026_1/hesa_trainee_detail_serializer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Api
+  module V20261
+    class HesaTraineeDetailSerializer < Api::V20260::HesaTraineeDetailSerializer
+    end
+  end
+end
---
+++ spec/requests/api/v2026_1/trainees/post_deferral_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "POST /trainees/{trainee_id}/defer" do
+  let!(:token) { create(:authentication_token, provider: trainee.provider).token }
+
+  let(:defer_date) { Time.zone.today.iso8601 }
+
+  describe "success" do
+    let(:trainee) do
+      create(:trainee, :trn_received)
+    end
+
+    it "defers a trainee" do
+      post "/api/v2026.1/trainees/#{trainee.slug}/defer",
+           headers: { authorization: "Bearer #{token}" },
+           params: { data: { defer_date: } }, as: :json
+
+      expect(response).to have_http_status(:ok)
+      expect(response.parsed_body).not_to have_key(:errors)
+      expect(response.parsed_body[:data][:defer_date]).to eq(defer_date)
+    end
+  end
+
+  describe "failure" do
+    let(:trainee) { create(:trainee, :trn_received) }
+
+    context "when the trainee cannot be found" do
+      let(:another_trainee) { create(:trainee) }
+
+      it "returns status code 404" do
+        post "/api/v2026.1/trainees/#{another_trainee.slug}/defer",
+             headers: { authorization: "Bearer #{token}" },
+             params: { data: { defer_date: } }, as: :json
+
+        expect(response).to have_http_status(:not_found)
+        expect(response.parsed_body[:errors]).to contain_exactly(
+          "error" => "NotFound",
+          "message" => "Trainee(s) not found",
+        )
+      end
+    end
+
+    it "does not defer the trainee" do
+      post "/api/v2026.1/trainees/#{trainee.slug}/defer",
+           headers: { authorization: "Bearer #{token}" },
+           params: { data: { defer_date: nil } }, as: :json
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response.parsed_body[:errors]).not_to be_empty
+      expect(response.parsed_body[:errors]).to contain_exactly(
+        {
+          "error" => "UnprocessableEntity",
+          "message" => "defer_date can't be blank",
+        },
+      )
+
+      expect(trainee.deferred?).to be(false)
+    end
+  end
+end
