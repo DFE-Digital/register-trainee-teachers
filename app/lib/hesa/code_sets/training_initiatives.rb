557bc749ff31d4efdbfe13bed8bbc9c8aef48608
Tom Trentham <tom.trentham@education.gov.uk>
2026-03-04 15:34:28 +0000

    Add primary_mathematics_specialist to Hesa CodeSet

--- app/lib/hesa/code_sets/training_initiatives.rb
+++ app/lib/hesa/code_sets/training_initiatives.rb
@@ -3,9 +3,9 @@
 module Hesa
   module CodeSets
     module TrainingInitiatives
-      # https://www.hesa.ac.uk/collection/c22053/e/initiatives
       MAPPING = {
         "009"	=> ROUTE_INITIATIVES_ENUMS[:maths_physics_chairs_programme_researchers_in_schools],
+        "011"	=> ROUTE_INITIATIVES_ENUMS[:primary_mathematics_specialist],
         "025"	=> ROUTE_INITIATIVES_ENUMS[:transition_to_teach],
         "026"	=> ROUTE_INITIATIVES_ENUMS[:now_teach],
         "036" => ROUTE_INITIATIVES_ENUMS[:international_relocation_payment],
