dc64eea0d97551f51ca2716dac0eea36d839d4c5
Serafeim Maroulis <serafeim@hey.com>
2026-03-06 12:05:30 +0000

    Clear iqts_country if route changed from iqts

--- app/services/api/v2026_1/update_trainee_service.rb
+++ app/services/api/v2026_1/update_trainee_service.rb
@@ -2,7 +2,61 @@
 
 module Api
   module V20261
-    class UpdateTraineeService < Api::V20260::UpdateTraineeService
+    class UpdateTraineeService
+      include ServicePattern
+
+      def initialize(trainee:, attributes:)
+        @trainee = trainee
+        @attributes = attributes
+      end
+
+      def call
+        trainee_attributes_validation = TraineeAttributesValidation.new(trainee_attributes:)
+        clear_iqts_country if route_changed_from_iqts?
+        attributes_to_save = attributes.deep_attributes.with_indifferent_access
+        trainee.nationalities.destroy_all if attributes_to_save[:nationalisations_attributes].present?
+        trainee.clear_disabilities if attributes_to_save[:trainee_disabilities_attributes].present?
+
+        if trainee_attributes_validation.invalid?
+          [false, trainee_attributes_validation]
+        else
+          trainee.assign_attributes(attributes_to_save)
+          trainee.disabilities = [] if !trainee.disabled? && trainee.disability_disclosure_changed?
+
+          [trainee.save, trainee]
+        end
+      end
+
+      TraineeAttributesValidation = Struct.new(:trainee_attributes) do
+        def errors_count = validation_errors.count
+        def all_errors = validation_errors
+        def invalid? = !errors_count.zero?
+
+        def validation_errors
+          trainee_errors.compact.flatten.map(&:strip)
+        end
+
+        def trainee_errors
+          @trainee_errors ||= begin
+            trainee_attributes.validate
+            trainee_attributes.errors&.full_messages
+          end
+        end
+      end
+
+    private
+
+      attr_reader :trainee, :attributes
+
+      alias_method :trainee_attributes, :attributes
+
+      def route_changed_from_iqts?
+        trainee.iqts? && attributes.training_route != TRAINING_ROUTE_ENUMS[:iqts]
+      end
+
+      def clear_iqts_country
+        attributes.iqts_country = nil
+      end
     end
   end
 end
