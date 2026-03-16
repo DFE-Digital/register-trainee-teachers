06c08191806cc1fcbb53e9beb59491a066174e0c
Serafeim Maroulis <serafeim@hey.com>
2026-03-16 09:13:23 +0000

    Add placements validation to API

--- app/validators/api/trainees/award_recommendation_validator.rb
+++ app/validators/api/trainees/award_recommendation_validator.rb
@@ -5,6 +5,7 @@ module Api
     class AwardRecommendationValidator < ActiveModel::Validator
       def validate(record)
         record.errors.add(:degree_id) if record.trainee_requires_degree? && record.trainee_degree_missing?
+        record.errors.add(:placements) if record.trainee_requires_placements? && record.trainee_placements_missing?
         record.errors.add(:state) unless record.trainee_can_recommend_for_award?
       end
     end
