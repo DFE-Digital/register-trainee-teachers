04ac6b996e3510c368ea54f889589af0379084cb
Serafeim Maroulis <serafeim@hey.com>
2026-03-18 16:48:45 +0000

    Redesign check answers page

--- app/components/outcome_details/view.rb
+++ app/components/outcome_details/view.rb
@@ -14,5 +14,11 @@ module OutcomeDetails
     def outcome_date
       date_for_summary_view(data_model.date)
     end
+
+    def outcome_date_with_label
+      return outcome_date if outcome_date.nil? || data_model.date_string == "other"
+
+      "#{data_model.date_string.capitalize} - #{outcome_date}"
+    end
   end
 end
