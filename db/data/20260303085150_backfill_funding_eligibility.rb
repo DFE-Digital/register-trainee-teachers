5ba061b143688dd56a5b23d4cab5218e3b1b38c4
David Georgiou <david.georgiou@icloud.com>
2026-03-23 15:54:07 +0000

    [9483] - rubocop

--- db/data/20260303085150_backfill_funding_eligibility.rb
+++ db/data/20260303085150_backfill_funding_eligibility.rb
@@ -5,32 +5,32 @@ class BackfillFundingEligibility < ActiveRecord::Migration[8.0]
     safety_assured do
       # Set from hesa_trainee_details.fund_code
       execute <<~SQL
-      UPDATE trainees
-      SET funding_eligibility = CASE htd.fund_code
-                                  WHEN '7' THEN 'eligible'
-                                  WHEN '2' THEN 'not_eligible'
-                                END
-      FROM hesa_trainee_details htd
-      WHERE htd.trainee_id = trainees.id
-        AND htd.fund_code IN ('7', '2')
-        AND trainees.funding_eligibility IS NULL
+        UPDATE trainees
+        SET funding_eligibility = CASE htd.fund_code
+                                    WHEN '7' THEN 'eligible'
+                                    WHEN '2' THEN 'not_eligible'
+                                  END
+        FROM hesa_trainee_details htd
+        WHERE htd.trainee_id = trainees.id
+          AND htd.fund_code IN ('7', '2')
+          AND trainees.funding_eligibility IS NULL
       SQL
 
       # Fall back to most recent hesa_students.fund_code for trainees still nil
       execute <<~SQL
-      UPDATE trainees
-      SET funding_eligibility = CASE hs.fund_code
-                                  WHEN '7' THEN 'eligible'
-                                  WHEN '2' THEN 'not_eligible'
-                                END
-      FROM (
-        SELECT DISTINCT ON (hesa_id) hesa_id, fund_code
-        FROM hesa_students
-        WHERE fund_code IN ('7', '2')
-        ORDER BY hesa_id, created_at DESC
-      ) hs
-      WHERE hs.hesa_id = trainees.hesa_id
-        AND trainees.funding_eligibility IS NULL
+        UPDATE trainees
+        SET funding_eligibility = CASE hs.fund_code
+                                    WHEN '7' THEN 'eligible'
+                                    WHEN '2' THEN 'not_eligible'
+                                  END
+        FROM (
+          SELECT DISTINCT ON (hesa_id) hesa_id, fund_code
+          FROM hesa_students
+          WHERE fund_code IN ('7', '2')
+          ORDER BY hesa_id, created_at DESC
+        ) hs
+        WHERE hs.hesa_id = trainees.hesa_id
+          AND trainees.funding_eligibility IS NULL
       SQL
     end
   end
