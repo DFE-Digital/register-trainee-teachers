c1303acdf1597fe24c8672e7c08ca3e276eafdbe
Tom Trentham <tom.trentham@education.gov.uk>
2026-03-26 10:45:51 +0000

    Remove links for now - they don't display correctly

--- app/lib/notify_on_timeout.rb
+++ app/lib/notify_on_timeout.rb
@@ -4,9 +4,7 @@ module NotifyOnTimeout
   def notify_on_timeout(trainee, class_name)
     TeamsNotifierService.call(
       title: "#{class_name} timed out",
-      message: "#{class_name} for Trainee with slug: #{trainee.slug} has timed out after #{Settings.jobs.max_poll_duration_days} days.\n\n
-        [Check the trainee record](#{Settings.base_url}/trainees/#{trainee.slug})\n\n
-        [Check the pending TRNs](#{Settings.base_url}/system-admin/pending_trns)",
+      message: "#{class_name} for Trainee with slug: #{trainee.slug} has timed out after #{Settings.jobs.max_poll_duration_days} days.",
       icon_emoji: "⏳",
     )
   end
