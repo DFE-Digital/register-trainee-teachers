2728aeff6f2d274d5d58fd1d2eb9a5d0d812832a
Serafeim Maroulis <serafeim@hey.com>
2026-05-15 09:50:42 +0100

    Remove iqts_country from v2026.1 ref data

--- app/lib/hesa/reference_data/v2026_1.rb
+++ app/lib/hesa/reference_data/v2026_1.rb
@@ -3,7 +3,6 @@
 module Hesa
   module ReferenceData
     class V20261 < V20260
-      TYPES = V20260::TYPES.merge(iqts_country: ::ReferenceData::COUNTRIES).freeze
     end
   end
 end
--- spec/lib/hesa/reference_data/v20261_spec.rb
+++ spec/lib/hesa/reference_data/v20261_spec.rb
@@ -3,20 +3,7 @@
 require "rails_helper"
 
 RSpec.describe Hesa::ReferenceData::V20261 do
-  describe "TYPES" do
-    it "extends V20260::TYPES with iqts_country" do
-      expect(described_class::TYPES).to eq(
-        Hesa::ReferenceData::V20260::TYPES.merge(iqts_country: ReferenceData::COUNTRIES),
-      )
-    end
-  end
+  subject { described_class }
 
-  describe "iqts_country" do
-    let(:rows) { CSV.parse(described_class.find(:iqts_country).as_csv, headers: true) }
-
-    it "produces a CSV mirroring the country reference data" do
-      expect(rows).not_to be_empty
-      expect(rows.headers).to eq(%w[Code Label])
-    end
-  end
+  it { is_expected.to be < Hesa::ReferenceData::V20260 }
 end
