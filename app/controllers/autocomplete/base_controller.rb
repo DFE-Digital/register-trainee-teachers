7dfb3728d2da76d7f7ebeb0449c89ba2cb0a8f52
Tom Trentham <tom.trentham@education.gov.uk>
2026-06-30 17:37:44 +0100

    Keep autocomplete requests out of page tracking

--- app/controllers/autocomplete/base_controller.rb
+++ app/controllers/autocomplete/base_controller.rb
@@ -2,6 +2,9 @@
 
 module Autocomplete
   class BaseController < ::ApplicationController
+    skip_before_action :track_page
+    skip_after_action :save_origin_path
+
   private
 
     def render_json_error(code: nil, message:, status: :internal_server_error)
