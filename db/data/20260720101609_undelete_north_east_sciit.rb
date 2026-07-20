fe82d797a5c3d4c8a6a48710e29173e846e70642
Serafeim Maroulis <serafeim@hey.com>
2026-07-20 15:34:04 +0100

    Use find_by

--- db/data/20260720101609_undelete_north_east_sciit.rb
+++ db/data/20260720101609_undelete_north_east_sciit.rb
@@ -2,12 +2,12 @@
 
 class UndeleteNorthEastSciit < ActiveRecord::Migration[8.1]
   def up
-    Provider.find_by!(name: "North East SCITT", code: "L06")
-      .undiscard!
+    Provider.find_by(name: "North East SCITT", code: "L06")
+      &.undiscard!
   end
 
   def down
-    Provider.find_by!(name: "North East SCITT", code: "L06")
-      .discard!
+    Provider.find_by(name: "North East SCITT", code: "L06")
+      &.discard!
   end
 end
