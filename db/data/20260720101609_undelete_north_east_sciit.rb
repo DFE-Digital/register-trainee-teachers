08fcd1894880badd469ff3ee8a3fd189e4291104
Serafeim Maroulis <serafeim@hey.com>
2026-07-21 09:03:56 +0100

    [9710] Un-delete North East SCITT (#6326)
    * Undiscard North East Sciit
    
    * Use find_by

---
+++ db/data/20260720101609_undelete_north_east_sciit.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class UndeleteNorthEastSciit < ActiveRecord::Migration[8.1]
+  def up
+    Provider.find_by(name: "North East SCITT", code: "L06")
+      &.undiscard!
+  end
+
+  def down
+    Provider.find_by(name: "North East SCITT", code: "L06")
+      &.discard!
+  end
+end
