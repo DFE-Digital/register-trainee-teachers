52d61675fff87b4135006df5569bbb5082ed9b03
Serafeim Maroulis <serafeim@hey.com>
2026-09-21 11:33:17 +0100

    Remove payment schedule from persona

--- config/initializers/personas.rb
+++ config/initializers/personas.rb
@@ -6,7 +6,7 @@ PROVIDER_C = "University of BAT"
 
 PERSONAS = [
   { first_name: "Agatha", last_name: "Baker", email: "agatha_baker@example.org", system_admin: true },
-  { first_name: "Annie", last_name: "Bell", email: "annie_bell@example.org", provider: PROVIDER_A, system_admin: false },
+  { first_name: "Annie", last_name: "Bell", email: "annie_bell@example.org", provider: PROVIDER_A, system_admin: false, without_payment_schedule: true },
   { first_name: "Damian", last_name: "Campbell", email: "damian_campbell@example.org", provider: PROVIDER_B, system_admin: false },
   { first_name: "Denise", last_name: "Theominis", email: "denise_theominis@example.org", provider: PROVIDER_B, system_admin: false, training_partner: true },
   { first_name: "Emma", last_name: "Smith", email: "emma_smith@example.org", provider: PROVIDER_C, system_admin: false, training_partner: true },
